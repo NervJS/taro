58e608e0c43366fb5f6ab57729017c04268d0086
yuche <i@yuche.me>
2018-04-12 20:43:55 +0800

    feat(taro): 导出编译期使用的内部方法 详细说明： taro/src/internal/README.md

--- packages/taro/src/index.js
+++ packages/taro/src/index.js
@@ -1,18 +1,22 @@
+/* eslint-disable camelcase */
 import Component from './component'
 import createApp from './create-app'
 import createPage from './create-page'
 import initNativeApi from './native-api'
+import { get as internal_safe_get } from './internal/safe-get'
 
 export {
   Component,
   createApp,
   createPage,
-  initNativeApi
+  initNativeApi,
+  internal_safe_get
 }
 
 export default {
   Component,
   createApp,
   createPage,
-  initNativeApi
+  initNativeApi,
+  internal_safe_get
 }
