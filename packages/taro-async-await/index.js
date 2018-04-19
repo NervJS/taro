faff50fecff6e98b2bc1929197307f3b8c73e251
luckyadam <weitaozsh@gmail.com>
2018-04-20 00:52:27 +0800

    fix(async-await): 代码错误

--- packages/taro-async-await/index.js
+++ packages/taro-async-await/index.js
@@ -1,5 +1,5 @@
-const global = typeof window !== 'undefined' && window.Math === Math 
-? window : typeof self !== 'undefined' && self.Math === Math ? self : this
+const global = typeof window !== 'undefined' &&
+window.Math === Math ? window : typeof self !== 'undefined' && self.Math === Math ? self : this
 
 global.Promise = require('promise-polyfill')
 global.regeneratorRuntime = require('regenerator-runtime/runtime')
