02e7ad8e7b5c92ae081bc9c5d674217943ecf4a9
luckyadam <weitaozsh@gmail.com>
2018-04-20 15:35:29 +0800

    feat(taro): 暴露环境获取API

--- packages/taro/src/index.js
+++ packages/taro/src/index.js
@@ -4,12 +4,15 @@ import createApp from './create-app'
 import createPage from './create-page'
 import initNativeApi from './native-api'
 import { get as internal_safe_get } from './internal/safe-get'
+import { getEnv, ENV_TYPE } from './util'
 
 export {
   Component,
   createApp,
   createPage,
   initNativeApi,
+  getEnv,
+  ENV_TYPE,
   internal_safe_get
 }
 
@@ -18,5 +21,7 @@ export default {
   createApp,
   createPage,
   initNativeApi,
+  getEnv,
+  ENV_TYPE,
   internal_safe_get
 }
