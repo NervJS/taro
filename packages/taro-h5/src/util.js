f21ef16e37a50c9474991467eca35e44a307cc60
luckyadam <weitaozsh@gmail.com>
2018-05-03 23:22:11 +0800

    fix(taro-h5): 丢失文件

---
+++ packages/taro-h5/src/util.js
@@ -0,0 +1,55 @@
+export function isEmptyObject (obj) {
+  if (!obj) {
+    return false
+  }
+  for (const n in obj) {
+    if (obj.hasOwnProperty(n) && obj[n]) {
+      return false
+    }
+  }
+  return true
+}
+
+/**
+ * JSON 克隆
+ * @param {Object | Json} jsonObj json对象
+ * @return {Object | Json} 新的json对象
+ */
+export function objClone (jsonObj) {
+  var buf
+  if (jsonObj instanceof Array) {
+    buf = []
+    var i = jsonObj.length
+    while (i--) {
+      buf[i] = objClone(jsonObj[i])
+    }
+    return buf
+  } else if (jsonObj instanceof Object) {
+    buf = {}
+    for (var k in jsonObj) {
+      buf[k] = objClone(jsonObj[k])
+    }
+    return buf
+  } else {
+    return jsonObj
+  }
+}
+
+export function getPrototype (obj) {
+  /* eslint-disable */
+  if (Object.getPrototypeOf) {
+    return Object.getPrototypeOf(obj)
+  } else if (obj.__proto__) {
+    return obj.__proto__
+  }
+  /* eslint-enable */
+  return obj.constructor.prototype
+}
+
+export function getPrototypeChain (obj) {
+  const protoChain = []
+  while ((obj = getPrototype(obj))) {
+    protoChain.push(obj)
+  }
+  return protoChain
+}
