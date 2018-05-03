38f695d2f0d4607c85541a217046d98c26b393ef
luckyadam <weitaozsh@gmail.com>
2018-05-03 23:43:53 +0800

    fix(taro-h5): 文件引用错误

--- packages/taro-h5/src/api/storage/index.js
+++ packages/taro-h5/src/api/storage/index.js
@@ -1,6 +1,3 @@
-import { getEnv, ENV_TYPE } from '../../util'
-const env = getEnv()
-
 function setStorage (options = {}) {
   const { key, data, success, fail, complete } = options
   const res = { errMsg: 'getStorage:ok' }
@@ -32,11 +29,7 @@ function setStorageSync (key, data = '') {
   } else {
     obj = { data }
   }
-  if (env === ENV_TYPE.WEAPP) {
-    wx.setStorageSync(key, obj)
-  } else {
-    localStorage.setItem(key, JSON.stringify(obj))
-  }
+  localStorage.setItem(key, JSON.stringify(obj))
 }
 
 function getStorage (options = {}) {
@@ -62,11 +55,7 @@ function getStorage (options = {}) {
 function getStorageSync (key) {
   if (typeof key !== 'string') throw new Error(`getStorageSync:fail parameter error: parameter should be String instead of ${typeof key}`)
   let res
-  if (env === ENV_TYPE.WEAPP) {
-    res = wx.getStorageSync(key)
-  } else {
-    res = JSON.parse(localStorage.getItem(key))
-  }
+  res = wx.getStorageSync(key)
 
   // 只返回使用 Taro.setStorage API 存储的数据
   if (res && res.data) return res.data
@@ -89,9 +78,6 @@ function getStorageInfo (options = {}) {
 }
 
 function getStorageInfoSync () {
-  if (env === ENV_TYPE.WEAPP) {
-    return wx.getStorageInfoSync()
-  }
   const res = {}
   res.keys = Object.keys(localStorage)
   res.limitSize = null
@@ -121,19 +107,11 @@ function removeStorage (options = {}) {
 
 function removeStorageSync (key) {
   if (typeof key !== 'string') throw new Error(`removeStorageSync:fail parameter error: parameter should be String instead of ${typeof key}`)
-  if (env === ENV_TYPE.WEAPP) {
-    wx.removeStorageSync(key)
-  } else {
-    localStorage.removeItem(key)
-  }
+  localStorage.removeItem(key)
 }
 
 function clearStorage () {
-  if (env === ENV_TYPE.WEAPP) {
-    wx.clearStorage()
-  } else {
-    localStorage.clear()
-  }
+  localStorage.clear()
 }
 
 export {
--- packages/taro-h5/src/index.js
+++ packages/taro-h5/src/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
+import { getEnv, ENV_TYPE, internal_safe_get, internal_dynamic_recursive } from '@tarojs/taro/index.esm.js'
+
 import initNativeApi from './native-api'
-import { getEnv, ENV_TYPE, get as internal_safe_get, dynamicRecursive as internal_dynamic_recursive } from '@tarojs/taro'
 
 export {
   initNativeApi,
