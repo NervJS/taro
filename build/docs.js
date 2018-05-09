624082237c6e79d670452dacc83165f991c6aeb3
luckyadam <weitaozsh@gmail.com>
2018-05-09 23:40:09 +0800

    chore: ci提交文档到gh-pages分支

---
+++ build/docs.js
@@ -0,0 +1,18 @@
+const cp = require('child_process')
+const ghPages = require('gh-pages')
+const ora = require('ora')
+const spinner = ora('Publishing gitbooks...').start()
+
+cp.exec('npm run docs', err => {
+  if (!err) {
+    ghPages.publish('./docs/_book', err => {
+      if (!err) {
+        spinner.succeed('Publish successfully.')
+      } else {
+        spinner.fail(err)
+      }
+    })
+  } else {
+    spinner.fail(err)
+  }
+})
