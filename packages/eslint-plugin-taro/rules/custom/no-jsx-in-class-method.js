baf85b726ab287b9cb6592d573e92f535b023158
yuche <i@yuche.me>
2018-05-24 21:33:15 +0800

    feat(eslint): 新规则: no-jsx-in-class-method

---
+++ packages/eslint-plugin-taro/rules/custom/no-jsx-in-class-method.js
@@ -0,0 +1,24 @@
+const { buildDocsMeta } = require('../../utils/utils')
+
+const ERROR_MESSAGE = '暂不支持在 render() 之外的方法定义 JSX'
+
+module.exports = {
+  meta: {
+    docs: buildDocsMeta(ERROR_MESSAGE, 'no-jsx-in-class-method')
+  },
+
+  create (context) {
+    return {
+      JSXElement (node) {
+        const parents = context.getAncestors(node)
+        const classMethod = parents.find(p => p.type === 'ClassMethod')
+        if (classMethod && classMethod.key.name !== 'render') {
+          context.report({
+            message: ERROR_MESSAGE,
+            node: classMethod
+          })
+        }
+      }
+    }
+  }
+}
