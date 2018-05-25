afed5f37924bb0790a0ba9382aa703567501991d
yuche <i@yuche.me>
2018-05-25 16:50:11 +0800

    feat(taro): 加入 Component 类型

--- packages/taro/types/index.d.ts
+++ packages/taro/types/index.d.ts
@@ -2,5 +2,37 @@ export = Taro;
 export as namespace Taro;
 
 declare namespace Taro {
+  interface ComponentLifecycle<P, S> {
+    componentWillMount?(): void;
+    componentDidMount?(): void;
+    componentWillReceiveProps?(nextProps: Readonly<P>, nextContext: any): void;
+    shouldComponentUpdate?(nextProps: Readonly<P>, nextState: Readonly<S>, nextContext: any): boolean;
+    componentWillUpdate?(nextProps: Readonly<P>, nextState: Readonly<S>, nextContext: any): void;
+    componentDidUpdate?(prevProps: Readonly<P>, prevState: Readonly<S>, prevContext: any): void;
+    componentWillUnmount?(): void;
+    componentDidShow?(): void;
+    componentDidHide?(): void;
+  }
 
+  interface Component<P = {}, S = {}> extends ComponentLifecycle<P, S> { }
+
+  class Component<P, S> {
+    constructor(props?: P, context?: any);
+
+    setState<K extends keyof S>(
+        state: ((prevState: Readonly<S>, props: P) => (Pick<S, K> | S)) | (Pick<S, K> | S),
+        callback?: () => any
+    ): void;
+
+    forceUpdate(callBack?: () => any): void;
+
+    render(): any;
+
+    props: Readonly<P>;
+    state: Readonly<S>;
+    context: any;
+    refs: {
+        [key: string]: any
+    };
+  }
 }
