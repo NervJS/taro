049b2f11fe82ec2f83f051b58f75aea236016822
yuche <i@yuche.me>
2018-05-26 17:13:13 +0800

    test(eslint): manipulate-jsx-as-array

--- packages/eslint-plugin-taro/rules/custom/manipulate-jsx-as-array.js
+++ packages/eslint-plugin-taro/rules/custom/manipulate-jsx-as-array.js
@@ -4,7 +4,8 @@ const { DEFAULT_Components_SET } = require('../../components')
 
 const ERROR_MESSAGE = '不能使用 Array#map 之外的方法操作 JSX 数组'
 
-const ARRAY_METHODS = new Set([
+// 可以传值的数组方法
+const ARRAY_METHODS_EXCEPT_MAP = new Set([
   'concat',
   'copyWithin',
   'every',
@@ -14,7 +15,6 @@ const ARRAY_METHODS = new Set([
   'findIndex',
   'flatMap',
   'forEach',
-  'map',
   'pop',
   'push',
   'reduce',
@@ -34,7 +34,7 @@ module.exports = {
       JSXElement (node) {
         const parents = context.getAncestors(node)
         const callExpression = parents.find(p => p.type === 'CallExpression' && p.callee.type === 'MemberExpression')
-        if (callExpression && ARRAY_METHODS.has(callExpression.callee.property.name)) {
+        if (callExpression && ARRAY_METHODS_EXCEPT_MAP.has(callExpression.callee.property.name)) {
           context.report({
             message: ERROR_MESSAGE,
             node: callExpression
