acc78d5661b47ea7b9e103dd76772fc1d5638f97
yuche <i@yuche.me>
2018-05-28 11:26:13 +0800

    fix(eslint): 规则格式设置不正确

--- packages/eslint-plugin-taro/index.js
+++ packages/eslint-plugin-taro/index.js
@@ -3,15 +3,17 @@ const { allRules, activeRules } = require('./rules/custom')
 module.exports = {
   rules: allRules,
   configs: {
-    plugins: [
-      'taro'
-    ],
-    parserOptions: {
-      ecmaVersion: 2018,
-      ecmaFeatures: {
-        jsx: true
-      }
-    },
-    rules: activeRules
+    all: {
+      plugins: [
+        'taro'
+      ],
+      parserOptions: {
+        ecmaVersion: 2018,
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      rules: activeRules
+    }
   }
 }
--- packages/eslint-plugin-taro/rules/custom/custom-component-children.js
+++ packages/eslint-plugin-taro/rules/custom/custom-component-children.js
@@ -1,6 +1,41 @@
 const { buildDocsMeta } = require('../../utils/utils')
 // eslint-disable-next-line
-const { DEFAULT_Components_SET } = require('../../components')
+const DEFAULT_Components_SET = new Set([
+  'View',
+  'ScrollView',
+  'Swiper',
+  'MovableView',
+  'CoverView',
+  'Icon',
+  'Text',
+  'RichText',
+  'Progress',
+  'Button',
+  'Checkbox',
+  'Form',
+  'Input',
+  'Label',
+  'Picker',
+  'PickerView',
+  'Radio',
+  'RadioGroup',
+  'CheckboxGroup',
+  'Slider',
+  'Switch',
+  'Textarea',
+  'Navigator',
+  'Audio',
+  'Image',
+  'Video',
+  'Camera',
+  'LivePlayer',
+  'LivePusher',
+  'Map',
+  'Canvas',
+  'OpenData',
+  'WebView',
+  'SwiperItem'
+])
 
 const ERROR_MESSAGE = '不能在自定义组件中写 children'
 
