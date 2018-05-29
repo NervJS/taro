4ad36ac99082f10bed8fe80aaf727a2ef2d720cf
luckyadam <weitaozsh@gmail.com>
2018-05-29 23:02:51 +0800

    feat(taro-rn): 增加rn框架

---
+++ packages/taro-rn/src/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+import {
+  getEnv,
+  ENV_TYPE,
+  Events,
+  eventCenter,
+  render,
+  internal_safe_get,
+  internal_safe_set,
+  internal_dynamic_recursive
+} from '@tarojs/taro'
+
+export {
+  getEnv,
+  ENV_TYPE,
+  Events,
+  eventCenter,
+  render,
+  internal_safe_get,
+  internal_safe_set,
+  internal_dynamic_recursive
+}
+
+export default {
+  getEnv,
+  ENV_TYPE,
+  Events,
+  eventCenter,
+  render,
+  internal_safe_get,
+  internal_safe_set,
+  internal_dynamic_recursive
+}
