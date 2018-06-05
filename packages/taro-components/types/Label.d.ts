3c484ef8b427cd8534d34d7e7f19948405ae117f
yuche <i@yuche.me>
2018-06-05 20:13:10 +0800

    feat(components): typings for Label, Picker, Radio

---
+++ packages/taro-components/types/Label.d.ts
@@ -0,0 +1,14 @@
+import { ComponentType } from 'react'
+import { StandardProps } from './common'
+
+interface LabelProps extends StandardProps {
+  
+  /**
+   * 绑定控件的 id
+   */
+  for: string
+}
+
+declare const Label: ComponentType<LabelProps>
+
+export { Label }
