a47cf71aa949900cd916f864fc25f455ff8731d1
liangwei73 <liangwei10@jd.com>
2018-06-07 10:57:46 +0800

    feat(taro-rn): 更新storageAPI

--- packages/taro-rn/src/api/storage/index.js
+++ packages/taro-rn/src/api/storage/index.js
@@ -1,21 +1,8 @@
 import { AsyncStorage } from 'react-native'
 
-// function throwErr (api, key) {
-//   const e = new Error(`${api}:fail parameter error: parameter should be String instead of ${typeof key}`)
-//   return e
-// }
-
 function setStorage (opts = {}) {
   const { key, data, success, fail, complete } = opts
   const res = { errMsg: 'setStorage:ok' }
-
-  // if (typeof key !== 'string') {
-  //   const e = throwErr('setStorage', key)
-  //   res.errMsg = e.message
-  //   fail && fail(res)
-  //   complete && complete(res)
-  //   return Promise.reject(e)
-  // }
 
   return AsyncStorage.setItem(key, JSON.stringify(data))
     .then((e) => {
@@ -65,9 +52,79 @@ async function getStorageSync (key) {
   return JSON.parse(res)
 }
 
+function getStorageInfo (opts = {}) {
+  const { success, fail, complete } = opts
+  const res = { errMsg: 'getStorageInfo:ok' }
+
+  return AsyncStorage.getAllKeys()
+    .then((data) => {
+      res.keys = data
+      res.currentSize = null
+      res.limitSize = null
+      success && success(res)
+      complete && complete(res)
+
+      return Promise.resolve(res)
+    }).catch((err) => {
+      res.errMsg = err.message
+      fail && fail(res)
+      complete && complete(res)
+
+      return Promise.reject(err)
+    })
+}
+
+async function getStorageInfoSync () {
+  const res = {}
+  res.keys = await AsyncStorage.getAllKeys().catch((err) => {
+    return Promise.reject(err)
+  })
+  res.currentSize = null
+  res.limitSize = null
+  return res
+}
+
+function removeStorage (opts = {}) {
+  const { key, success, fail, complete } = opts
+  const res = { errMsg: 'removeStorage:ok' }
+
+  return AsyncStorage.removeItem(key)
+    .then(() => {
+      success && success(res)
+      complete && complete(res)
+
+      return Promise.resolve(res)
+    }).catch((err) => {
+      res.errMsg = err.message
+      fail && fail(res)
+      complete && complete(res)
+
+      return Promise.reject(err)
+    })
+}
+
+async function removeStorageSync (key) {
+  const res = await AsyncStorage.removeItem(key).catch((err) => {
+    return Promise.reject(err)
+  })
+  return JSON.parse(res)
+}
+
+async function clearStorage () {
+  const res = await getStorageInfoSync()
+  const keys = res.keys
+  return AsyncStorage.multiRemove(keys)
+}
+
 export {
   setStorage,
   setStorageSync,
   getStorage,
-  getStorageSync
+  getStorageSync,
+  getStorageInfo,
+  getStorageInfoSync,
+  removeStorage,
+  removeStorageSync,
+  clearStorage,
+  clearStorage as clearStorageSync
 }
