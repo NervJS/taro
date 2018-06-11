56e46eff6791e04eee980d81a8f335b0f0d01379
Pines-Cheng <spider.cs.nuc@gmail.com>
2018-06-11 09:50:47 +0800

    fix(postcss-pxtransform): 单位转换问题

--- packages/postcss-pxtransform/index.js
+++ packages/postcss-pxtransform/index.js
@@ -41,24 +41,23 @@ module.exports = postcss.plugin('postcss-pxtransform', function (opts) {
 
 function dealWithWeapp ({root, opts, result}) {
   opts = Object.assign({}, DEFAULT_WEAPP_OPTIONS, opts)
-  return postcss(pxtorem(opts)).process(result)
-    .then(newResult => {
-      root = newResult.root
-      root.walkDecls(function (decl) {
-        let value = decl.value
-        value = value.replace(/([0-9.]+)rem/ig, function (match, size) {
-          return Math.ceil(size / DEVICE_RATIO[opts.designWidth] *
-            10000) / 10000 + 'rpx'
-        })
-        decl.value = value
-      })
+  var css = postcss(pxtorem(opts)).process(root.toResult()).css
+  var cssRoot = postcss.parse(css)
+  root.nodes = cssRoot.nodes
+
+  root.walkDecls(function (decl) {
+    let value = decl.value
+    value = value.replace(/([0-9.]+)rem/ig, function (match, size) {
+      return Math.ceil(size / DEVICE_RATIO[opts.designWidth] *
+        10000) / 10000 + 'rpx'
     })
+    decl.value = value
+  })
 }
 
 function dealWithH5 ({root, result, opts}) {
   opts = Object.assign({}, DEFAULT_H5_OPTIONS, opts)
-  return postcss(pxtorem(opts)).process(result)
-    .then(newResult => {
-      result = newResult
-    })
+  var css = postcss(pxtorem(opts)).process(root.toResult()).css
+  var cssRoot = postcss.parse(css)
+  root.nodes = cssRoot.nodes
 }
