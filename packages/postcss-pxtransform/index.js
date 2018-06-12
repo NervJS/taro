37f7bca08ff411c1637eb7844b60eb493e472fbf
Pines-Cheng <spider.cs.nuc@gmail.com>
2018-06-12 15:23:17 +0800

    chore(postcss-pxtransform): 去除无用的console

--- packages/postcss-pxtransform/index.js
+++ packages/postcss-pxtransform/index.js
@@ -64,7 +64,6 @@ module.exports = postcss.plugin('postcss-pxtransform', function (options) {
   var satisfyPropList = createPropListMatcher(opts.propList)
 
   return function (css) {
-    console.log(css, '\r\n\r\n')
     if (css.nodes[0] &&
       css.nodes[0].type === 'comment' &&
       css.nodes[0].text === 'postcss-pxtransform disable') {
