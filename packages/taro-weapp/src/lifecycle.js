ad9c05e931a49d2a251720bee9d8fad47501110b
luckyadam <weitaozsh@gmail.com>
2018-06-23 00:29:15 +0800

    fix(taro-weapp): 页面执行完 componentWillReceiveProps 后需要更新下 state

--- packages/taro-weapp/src/lifecycle.js
+++ packages/taro-weapp/src/lifecycle.js
@@ -16,19 +16,19 @@ export function updateComponent (component, update) {
     const displayName = type.name || type.toString().match(/^function\s*([^\s(]+)/)[1]
     PropTypes.checkPropTypes(propTypes, props, 'prop', displayName)
   }
-  let state = component.getState()
-  if (component._createData) {
-    state = component._createData(state)
-  }
-  state.__data && (state.__data.$path = component.$path)
   const prevProps = component.prevProps || props
-  const prevState = component.prevState || state
   component.props = prevProps
   if (component._unsafeCallUpdate === true && component.componentWillReceiveProps) {
     component._disable = true
     component.componentWillReceiveProps(props)
     component._disable = false
   }
+  let state = component.getState()
+  if (component._createData) {
+    state = component._createData(state)
+  }
+  state.__data && (state.__data.$path = component.$path)
+  const prevState = component.prevState || state
   let skip = false
   if (typeof component.shouldComponentUpdate === 'function' &&
     component.shouldComponentUpdate(props, state) === false) {
