65ffe844888079f958fd5b1cdab7ca665dd77d22
Manjiz <25199230@qq.com>
2018-06-26 15:08:26 +0800

    chore(tcr): jest test configuration of es2015 modules import

--- packages/taro-components-rn/jest.config.js
+++ packages/taro-components-rn/jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   verbose: true,
   preset: 'jest-react-native',
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@nart\/react-native-swiper|react-native-vertical-view-pager)/)'
+  ],
   setupTestFrameworkScriptFile: './setupTests.js'
 }
