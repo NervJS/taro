f9a35e599916c481ab67c017f329f553a6c3a46f
liangwei73 <liangwei10@jd.com>
2018-07-25 19:14:12 +0800

    fix(taro-rn): 修复Clipboard的测试用例

---
+++ packages/taro-rn/src/__tests__/vibrate.test.js
@@ -0,0 +1,29 @@
+import Vibration from './__mock__/mockVibrate'
+import Taro from '../index.js'
+
+jest.setMock('Vibration', Vibration)
+Taro.initNativeApi(Taro)
+
+describe('vibrate', () => {
+  describe('vibrateShort', () => {
+    test('能正常震动', () => {
+      const expectMsg = 'vibrateShort:ok'
+
+      expect.assertions(1)
+      return Taro.vibrateShort().then((res) => {
+        expect(res.errMsg).toMatch(expectMsg)
+      })
+    })
+  })
+
+  describe('vibrateLong', () => {
+    test('能正常震动', () => {
+      const expectMsg = 'vibrateLong:ok'
+
+      expect.assertions(1)
+      return Taro.vibrateLong().then((res) => {
+        expect(res.errMsg).toMatch(expectMsg)
+      })
+    })
+  })
+})
