c94058a4976d1e8105cf805d7f54e01630dd1696
yuche <i@yuche.me>
2018-09-07 14:46:16 +0800

    feat(taroize): render 函数和 parseWxml 结合

--- packages/wx-to-taro/src/index.ts
+++ packages/wx-to-taro/src/index.ts
@@ -1,9 +1,14 @@
+import * as t from 'babel-types'
+import { parseWXML } from './wxml'
+import { parseScript } from './script'
+
 interface Option {
-  json: string,
-  js: string,
+  json?: string,
+  script: string,
   wxml: string
 }
 
-export default function parse (option: Option): string {
-  return ''
+export default function parse (option: Option): t.File {
+  const wxml = parseWXML(option.wxml)
+  return parseScript(option.script, wxml as t.Expression)
 }
