3928eb3ff706282b1a03496e0a5568e2a0a7613d
littly <544028951@qq.com>
2018-09-18 23:03:34 +0800

    feat(webpack-runner): 整理webpack-runner的log函数

--- packages/taro-webpack-runner/src/index.ts
+++ packages/taro-webpack-runner/src/index.ts
@@ -1,37 +1,29 @@
 import chalk from 'chalk'
 import { merge } from 'lodash'
 import * as opn from 'opn'
-import * as ora from 'ora'
 import * as path from 'path'
 import { format as formatUrl } from 'url'
 import { deprecate } from 'util'
 import * as webpack from 'webpack'
 import * as WebpackDevServer from 'webpack-dev-server'
 import * as webpackMerge from 'webpack-merge'
+import { pipe, partial } from 'lodash/fp'
 
 import buildConf from './config/build.conf'
 import devConf from './config/dev.conf'
 import baseDevServerOption from './config/devServer.conf'
 import prodConf from './config/prod.conf'
-import formatWebpackMessage from './util/format_webpack_message'
+import { bindDevLogger, bindProdLogger } from './util/logHelper'
 import { BuildConfig } from './util/types'
 
 const appPath = process.cwd()
-let isFirst = true
-
-const getServeSpinner = (() => {
-  let spinner
-  return () => {
-    if (!spinner) spinner = ora(`Starting development server, please wait~`)
-    return spinner
-  }
-})()
 
 const customizeChain = (chain, config) => {
   if (config.webpackChain instanceof Function) {
     config.webpackChain(chain)
   }
 }
+
 const deprecatedCustomizeConfig = deprecate((baseConfig, customConfig) => {
   if (customConfig instanceof Function) {
     return customConfig(baseConfig, webpack)
@@ -39,62 +31,6 @@ const deprecatedCustomizeConfig = deprecate((baseConfig, customConfig) => {
     return webpackMerge({}, baseConfig, customConfig)
   }
 }, chalk.yellow(`h5.webpack配置项即将停止支持，请尽快迁移到新配置项。新配置项文档：https://nervjs.github.io/taro/docs/config-detail.html#h5`))
-
-const printBuildError = (err: Error): void => {
-  const message = err != null && err.message
-  const stack = err != null && err.stack
-  if (stack && typeof message === 'string' && message.indexOf('from UglifyJs') !== -1) {
-    try {
-      const matched = /(.+)\[(.+):(.+),(.+)\]\[.+\]/.exec(stack)
-      if (!matched) {
-        throw new Error('Using errors for control flow is bad.')
-      }
-      const problemPath = matched[2]
-      const line = matched[3]
-      const column = matched[4]
-      console.log('Failed to minify the code from this file: \n\n', chalk.yellow(`\t${problemPath}:${line}${column !== '0' ? ':' + column : ''}`), '\n')
-    } catch (ignored) {
-      console.log('Failed to minify the bundle.', err)
-    }
-  } else {
-    console.log((message || err) + '\n')
-  }
-  console.log()
-}
-
-const createCompiler = (webpackConf): webpack.Compiler => {
-  const compiler = webpack(webpackConf)
-  compiler.hooks.invalid.tap('taroInvalid', filepath => {
-    // console.log(chalk.grey(`[${formatTime()}]Modified: ${filepath}`))
-    getServeSpinner().text = 'Compiling...'
-    getServeSpinner().render()
-  })
-  compiler.hooks.done.tap('taroDone', stats => {
-    const { errors, warnings } = formatWebpackMessage(stats.toJson(true))
-    if (!errors.length) {
-      getServeSpinner().stopAndPersist({
-        symbol: '✅ ',
-        text: chalk.green('Compile successfully!\n')
-      })
-    } else {
-      errors.splice(1)
-      getServeSpinner().stopAndPersist({
-        symbol: '🙅  ',
-        text: chalk.red('Compile failed!\n')
-      })
-      console.log(errors.join('\n\n') + '\n')
-    }
-    if (warnings.length) {
-      warnings.splice(1)
-      getServeSpinner().stopAndPersist({
-        symbol: '⚠️  ',
-        text: chalk.yellow('Compile completes with warnings.\n')
-      })
-      console.log(warnings.join('\n\n') + '\n')
-    }
-  })
-  return compiler
-}
 
 const buildProd = (config: BuildConfig): void => {
   const webpackChain = prodConf(config)
@@ -107,49 +43,13 @@ const buildProd = (config: BuildConfig): void => {
   } else {
     webpackConfig = webpackChain.toConfig()
   }
-  const compiler = webpack(webpackConfig)
 
-  getServeSpinner().text = 'Compiling...'
-  getServeSpinner().start()
+  const compiler = pipe(
+    webpack as (config: any) => any,
+    partial(bindProdLogger, [{}])
+  )(webpackConfig)
 
-  compiler.run((err, stats) => {
-    if (err) {
-      return printBuildError(err)
-    }
-
-    const { errors, warnings } = formatWebpackMessage(stats.toJson({}))
-    if (!errors.length) {
-      getServeSpinner().stopAndPersist({
-        symbol: '✅ ',
-        text: chalk.green('Compile successfully!\n')
-      })
-      return process.stdout.write(
-        stats.toString({
-          colors: true,
-          modules: false,
-          children: false,
-          chunks: false,
-          chunkModules: false
-        }) + '\n'
-      )
-    } else {
-      errors.splice(1)
-      getServeSpinner().stopAndPersist({
-        symbol: '🙅  ',
-        text: chalk.red('Compile failed!\n')
-      })
-      return printBuildError(new Error(errors.join('\n\n')))
-    }
-    if (warnings.length) {
-      getServeSpinner().stopAndPersist({
-        symbol: '⚠️  ',
-        text: chalk.yellow('Compile completes with warnings.\n')
-      })
-      console.log(warnings.join('\n\n'))
-      console.log('\nSearch for the ' + chalk.underline(chalk.yellow('keywords')) + ' to learn more about each warning.')
-      console.log('To ignore, add ' + chalk.cyan('// eslint-disable-next-line') + ' to the line before.\n')
-    }
-  })
+  compiler.run()
 }
 
 const buildDev = (config: BuildConfig): void => {
@@ -182,21 +82,11 @@ const buildDev = (config: BuildConfig): void => {
     pathname: publicPath
   })
   WebpackDevServer.addDevServerEntrypoints(webpackConfig, devServerOptions)
-  const compiler = createCompiler(webpackConfig)
-  compiler.hooks.done.tap('taroDoneFirst', stats => {
-    if (isFirst) {
-      isFirst = false
-      getServeSpinner().clear()
-      console.log()
-      console.log(chalk.cyan(`ℹ️  Listening at ${devUrl}`))
-      console.log(chalk.gray('\n监听文件修改中...\n'))
-    }
-  })
+  const compiler = pipe(
+    webpack as (config: any) => any,
+    partial(bindDevLogger, [{ devUrl }])
+  )(webpackConfig)
   const server = new WebpackDevServer(compiler, devServerOptions)
-  console.log()
-  getServeSpinner().text = 'Compiling...'
-  getServeSpinner().start()
-
   server.listen(devServerOptions.port as number, devServerOptions.host as string, err => {
     if (err) return console.log(err)
     opn(devUrl)
---
+++ packages/taro-webpack-runner/src/util/logHelper.ts
@@ -0,0 +1,221 @@
+import chalk from 'chalk';
+import * as ora from 'ora';
+import { partial, pipe } from 'lodash/fp';
+
+const syntaxErrorLabel = 'Syntax error:';
+
+const getServeSpinner = (() => {
+  let spinner
+  return () => {
+    if (!spinner) spinner = ora(`Starting development server, please wait~`)
+    return spinner
+  }
+})()
+
+const isLikelyASyntaxError = (message: string): boolean => {
+  return message.indexOf(syntaxErrorLabel) >= 0;
+}
+
+const formatMessage = (message: string): string => {
+  let lines = message.split('\n');
+  if (lines.length > 2 && lines[1] === '') {
+    lines.splice(1, 1);
+  }
+  if (lines[0].lastIndexOf('!') >= 0) {
+    lines[0] = lines[0].substr(lines[0].lastIndexOf('!') + 1);
+  }
+  lines = lines.filter(line => line.indexOf(' @ ') !== 0);
+  if (!lines[0] || !lines[1]) {
+    return lines.join('\n');
+  }
+  if (lines[1].indexOf('Module not found: ') === 0) {
+    lines = [
+      lines[0],
+      lines[1]
+        .replace("Cannot resolve 'file' or 'directory' ", '')
+        .replace('Cannot resolve module ', '')
+        .replace('Error: ', '')
+        .replace('[CaseSensitivePathsPlugin] ', '')
+    ];
+  } else if (lines[1].indexOf('Module build failed: ') === 0) {
+    lines[1] = lines[1].replace(
+      'Module build failed: SyntaxError:',
+      syntaxErrorLabel
+    );
+  }
+
+  const exportError = /\s*(.+?)\s*(")?export '(.+?)' was not found in '(.+?)'/;
+  if (lines[1].match(exportError)) {
+    lines[1] = lines[1].replace(
+      exportError,
+      "$1 '$4' does not contain an export named '$3'."
+    );
+  }
+  lines[0] = chalk.inverse(lines[0]);
+  message = lines.join('\n');
+
+  message = message.replace(
+    /^\s*at\s((?!webpack:).)*:\d+:\d+[\s)]*(\n|$)/gm,
+    ''
+  );
+  return message.trim();
+}
+
+const formatWebpackMessage = (message): { errors, warnings } => {
+  const errors = message.errors.map(item => formatMessage(item));
+  const warnings = message.warnings.map(item => formatMessage(item));
+
+  const result = {
+    errors,
+    warnings
+  };
+  if (result.errors.some(isLikelyASyntaxError)) {
+    result.errors = result.errors.filter(isLikelyASyntaxError);
+  }
+  return result;
+}
+
+const printCompiling = () => {
+  getServeSpinner().text = 'Compiling...'
+  getServeSpinner().start()
+}
+
+const printBuildError = (err: Error): void => {
+  const message = err.message
+  const stack = err.stack
+  if (stack && typeof message === 'string' && message.indexOf('from UglifyJs') !== -1) {
+    try {
+      const matched = /(.+)\[(.+):(.+),(.+)\]\[.+\]/.exec(stack)
+      if (!matched) {
+        throw new Error('Using errors for control flow is bad.')
+      }
+      const problemPath = matched[2]
+      const line = matched[3]
+      const column = matched[4]
+      console.log('Failed to minify the code from this file: \n\n', chalk.yellow(`\t${problemPath}:${line}${column !== '0' ? ':' + column : ''}`), '\n')
+    } catch (ignored) {
+      console.log('Failed to minify the bundle.', err)
+    }
+  } else {
+    console.log((message || err) + '\n')
+  }
+  console.log()
+}
+
+const printSuccess = () => {
+  getServeSpinner().stopAndPersist({
+    symbol: '✅ ',
+    text: chalk.green('Compile successful!\n')
+  })
+}
+
+const printWarning = () => {
+  getServeSpinner().stopAndPersist({
+    symbol: '⚠️  ',
+    text: chalk.yellow('Compile completes with warnings.\n')
+  })
+}
+
+const printFailed = () => {
+  getServeSpinner().stopAndPersist({
+    symbol: '🙅  ',
+    text: chalk.red('Compile failed!\n')
+  })
+}
+
+const printWhenBeforeCompile = compiler => {
+  compiler.hooks.beforeCompile.tap('taroBeforeCompile', filepath => {
+    printCompiling()
+  })
+  return compiler
+}
+
+const printWhenInvalid = compiler => {
+  compiler.hooks.invalid.tap('taroInvalid', filepath => {
+    printCompiling()
+  })
+  return compiler
+}
+
+const printWhenFailed = compiler => {
+  compiler.hooks.failed.tap('taroFailed', error => {
+    printBuildError(error)
+  })
+  return compiler
+}
+
+let isFirst = false
+const printWhenFirstDone = (devUrl, compiler) => {
+  compiler.hooks.done.tap('taroDone', stats => {
+    if (isFirst) {
+      isFirst = false
+      getServeSpinner().clear()
+      console.log()
+      console.log(chalk.cyan(`ℹ️  Listening at ${devUrl}`))
+      console.log(chalk.gray('\n监听文件修改中...\n'))
+    }
+  })
+  return compiler
+}
+
+const _printWhenDone = ({
+  verbose = false
+}, compiler) => {
+  compiler.hooks.done.tap('taroDone', stats => {
+    const { errors, warnings } = formatWebpackMessage(stats.toJson(true))
+
+    if (errors.length) {
+      printFailed()
+      printBuildError(new Error(errors.join('\n\n')))
+    }
+
+    if (warnings.length) {
+      printWarning()
+      console.log(warnings.join('\n\n'))
+    } else {
+      printSuccess()
+      verbose && console.log(
+        stats.toString({
+          colors: true,
+          modules: false,
+          children: false,
+          chunks: false,
+          chunkModules: false
+        }) + '\n'
+      )
+    }
+  })
+  return compiler
+}
+
+const printWhenDone = partial(_printWhenDone, [{ verbose: false }])
+
+const printWhenDoneVerbosely = partial(_printWhenDone, [{ verbose: true }])
+
+const bindDevLogger = ({ devUrl }, compiler) => {
+  pipe(
+    printWhenBeforeCompile,
+    partial(printWhenFirstDone, [devUrl]),
+    printWhenDone,
+    printWhenFailed,
+    printWhenInvalid
+  )(compiler)
+  return compiler
+}
+
+const bindProdLogger = ({}, compiler) => {
+  pipe(
+    printWhenBeforeCompile,
+    printWhenDoneVerbosely,
+    printWhenFailed
+  )(compiler)
+  return compiler
+}
+
+export {
+  printBuildError,
+  printCompiling,
+  getServeSpinner,
+  bindDevLogger,
+  bindProdLogger
+}
