81b9390d038837f2c30cab7ed0c608b3022026e1
yuche <i@yuche.me>
2018-09-26 15:30:43 +0800

    chore(taroize): 把包加入 lerna 管理

--- packages/taro-with-weapp/src/index.ts
+++ packages/taro-with-weapp/src/index.ts
@@ -1,73 +1,36 @@
-import { Component, ComponentLifecycle, internal_safe_set as safeSet } from '@tarojs/taro'
+import { Component, ComponentLifecycle } from '@tarojs/taro'
 
 type WeappLifeCycle = () => void
 
 interface WeappComponent<P, S> extends Component<P, S> {
   created?: WeappLifeCycle
-  ready?: WeappLifeCycle
-  detached?: WeappLifeCycle
-  attached?: WeappLifeCycle
-  moved?: WeappLifeCycle
-  setData: Function
 }
 
 interface ComponentClass<P = {}, S = {}> extends ComponentLifecycle<P, S> {
   new (props: P): WeappComponent<P, S>
 }
 
-function defineGetter (component: Component, key: string, getter: string) {
-  Object.defineProperty(component, key, {
-    enumerable: true,
-    configurable: true,
-    get: () => component[getter]
-  })
+function safeExecute (func?: Function, ...arg) {
+  if (func) func(arg)
 }
 
 export function withWeapp (componentType: string) {
-  const isComponent = componentType === 'Component'
+  const isComponent = componentType === 'component'
 
-  return (ConnectComponent: ComponentClass) => class BaseComponent<_ = {}, S = {}> extends ConnectComponent {
+  function executeComponentFunc (func?: Function, ...args) {
+    if (isComponent) {
+      safeExecute(func, args)
+    }
+  }
+
+  return (ConnectComponent: ComponentClass) => class extends ConnectComponent {
     constructor (props) {
       super(props)
-      defineGetter(this, 'data', 'state')
-      if (isComponent) {
-        defineGetter(this, 'properties', 'props')
-      }
-    }
-
-    private safeExecute = (func?: Function, ...arg) => {
-      if (func) func.apply(this, arg)
-    }
-
-    private executeComponentFunc = (func?: Function, ...args) => {
-      if (isComponent) {
-        this.safeExecute(func, args)
-      }
-    }
-
-    setData = (obj: S) => {
-      const state = Object.assign({}, this.state)
-      Object.keys(obj).forEach(key => {
-        safeSet(state, key, obj[key])
-      })
-      this.setState(state)
     }
 
     componentWillMount () {
-      this.executeComponentFunc(this.created)
-      this.safeExecute(super.componentWillMount)
+      executeComponentFunc(this.created)
+      safeExecute(super.componentWillMount)
     }
-
-    componentDidMount () {
-      this.executeComponentFunc(this.attached)
-      this.safeExecute(super.componentDidMount)
-      this.executeComponentFunc(this.ready)
-    }
-
-    componentWillUnmount () {
-      this.safeExecute(super.componentWillUnmount)
-      this.executeComponentFunc(this.detached)
-    }
-
   }
 }
