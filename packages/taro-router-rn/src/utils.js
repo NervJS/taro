fe9430321d542544876a70d86c1997568ee3d94e
Pines-Cheng <spider.cs.nuc@gmail.com>
2018-09-26 15:01:43 +0800

    feat(RN): 添加 config.window.navigationStyle 配置  close #675

--- packages/taro-router-rn/src/utils.js
+++ packages/taro-router-rn/src/utils.js
@@ -2,7 +2,8 @@ export const HEADER_CONFIG_MAP = {
   navigationBarTitleText: 'title', // 导航栏标题文字内容
   navigationBarTextStyle: 'headerTintColor', // 导航栏标题颜色，仅支持 black/white
   navigationBarBackgroundColor: 'backgroundColor', // 导航栏背景颜色
-  enablePullDownRefresh: 'enablePullDownRefresh' // 是否全局开启下拉刷新，暂时放这里吧
+  enablePullDownRefresh: 'enablePullDownRefresh', // 是否全局开启下拉刷新，暂时放这里吧
+  navigationStyle: 'navigationStyle' // 导航栏样式，仅支持以下值：default 默认样式 custom 自定义导航栏，只保留右上角胶囊按钮
 }
 
 export function getNavigationOptions (config) {
