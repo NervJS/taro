bbe20d84a4216097d27424faa1d34b3585632bc4
吴俊骁 <mushan0x0@gmail.com>
2018-10-09 11:35:57 +0800

    update: 完善Map组件markers属性

--- packages/taro-components/types/Map.ts
+++ packages/taro-components/types/Map.ts
@@ -26,7 +26,205 @@ interface MapProps extends StandardProps {
   /**
    * 标记点
    */
-  markers?: [],
+  markers?: Array<{
+
+    /**
+     * 标记点id
+     *
+     * 备注：marker点击事件回调会返回此id。建议为每个marker设置上Number类型id，保证更新marker时有更好的性能。
+     */
+    id?: number;
+
+    /**
+     * 纬度
+     *
+     * 备注：浮点数，范围 -90 ~ 90
+     */
+    latitude: number;
+
+    /**
+     * 经度
+     *
+     * 备注：浮点数，范围 -180 ~ 180
+     */
+    longitude: number;
+
+    /**
+     * 标注点名
+     */
+    title?: string;
+
+    /**
+     * 显示层级
+     */
+    zIndex?: number;
+
+    /**
+     * 显示的图标
+     *
+     * 备注：项目目录下的图片路径，支持相对路径写法，以'/'开头则表示相对小程序根目录；也支持临时路径和网络图片
+     */
+    iconPath: string;
+
+    /**
+     * 旋转角度
+     *
+     * 备注：顺时针旋转的角度，范围 0 ~ 360，默认为 0
+     */
+    rotate?: number;
+
+    /**
+     * 标注的透明度
+     *
+     * 备注：默认1，无透明，范围 0 ~ 1
+     */
+    alpha?: number;
+
+    /**
+     * 标注图标宽度
+     *
+     * 备注：默认为图片实际宽度
+     */
+    width?: number;
+
+    /**
+     * 标注图标高度
+     *
+     * 备注：默认为图片实际高度
+     */
+    height?: number;
+
+    /**
+     * 自定义标记点上方的气泡窗口
+     *
+     * 备注：支持的属性见下表，可识别换行符。
+     */
+    callout?: {
+
+      /**
+       * 文本
+       */
+      content: string;
+
+      /**
+       * 文本颜色
+       */
+      color: string;
+
+      /**
+       * 文字大小
+       */
+      fontSize: number;
+
+      /**
+       * 边框圆角
+       */
+      borderRadius: number;
+
+      /**
+       * 边框宽度
+       */
+      borderWidth: number;
+
+      /**
+       * 边框颜色
+       */
+      borderColor: string;
+
+      /**
+       * 背景色
+       */
+      bgColor: string;
+
+      /**
+       * 文本边缘留白
+       */
+      padding: number;
+
+      /**
+       * 'BYCLICK':点击显示; 'ALWAYS':常显
+       */
+      display: 'BYCLICK' | 'ALWAYS';
+
+      /**
+       * 文本对齐方式。有效值: left, right, center
+       */
+      textAlign: 'left' | 'right' | 'center';
+    };
+
+    /**
+     * 为标记点旁边增加标签
+     *
+     * 备注：支持的属性见下表，可识别换行符。
+     */
+    label?: {
+
+      /**
+       * 文本
+       */
+      content: string;
+
+      /**
+       * 文本颜色
+       */
+      color: string;
+
+      /**
+       * 文字大小
+       */
+      fontSize: number;
+
+      /**
+       * label的坐标，原点是 marker 对应的经纬度
+       */
+      anchorX: number;
+
+      /**
+       * label的坐标，原点是 marker 对应的经纬度
+       */
+      anchorY: number;
+
+      /**
+       * 边框宽度
+       */
+      borderWidth: number;
+
+      /**
+       * 边框颜色
+       */
+      borderColor: string;
+
+      /**
+       * 边框圆角
+       */
+      borderRadius: number;
+
+      /**
+       * 背景色
+       */
+      bgColor: string;
+
+      /**
+       * 文本边缘留白
+       */
+      padding: number;
+
+      /**
+       * 文本对齐方式。有效值: left, right, center
+       */
+      textAlign: 'left' | 'right' | 'center';
+    };
+
+    /**
+     * 经纬度在标注图标的锚点，默认底边中点
+     *
+     * 备注：{x, y}，x表示横向(0-1)，y表示竖向(0-1)。{x: .5, y: 1} 表示底边中点
+     */
+    anchor?: {
+      x: number;
+      y: number;
+    };
+  }>,
 
   /**
    * @deprecated
