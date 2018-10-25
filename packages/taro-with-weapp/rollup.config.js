61f6fbe32c4e874e8677f6d303e6cdb762998a9c
luckyadam <weitaozsh@gmail.com>
2018-10-25 22:28:20 +0800

    feat(with-weapp): 更新 rollup 配置

--- packages/taro-with-weapp/rollup.config.js
+++ packages/taro-with-weapp/rollup.config.js
@@ -1,21 +1,74 @@
-const typescript = require('rollup-plugin-typescript2')
 const { join } = require('path')
+const resolve = require('rollup-plugin-node-resolve')
+const babel = require('rollup-plugin-babel')
+const common = require('rollup-plugin-commonjs')
+const alias = require('rollup-plugin-alias')
+const typescript = require('rollup-plugin-typescript2')
+const cwd = __dirname
 
-module.exports = {
-  input: 'src/index.ts',
-  plugins: [
-    typescript()
-  ],
+const baseConfig = {
+  input: join(cwd, 'src/index.ts'),
   output: [
     {
+      file: join(cwd, 'dist/index.js'),
       format: 'cjs',
       sourcemap: true,
-      file: join(__dirname, 'dist/index.js')
+      exports: 'named'
     },
     {
-      format: 'es',
+      file: join(cwd, 'dist/with-weapp.js'),
+      format: 'umd',
+      name: 'TaroWithWeapp',
       sourcemap: true,
-      file: join(__dirname, 'dist/index.esm.js')
+      exports: 'named'
     }
+  ],
+  plugins: [
+    typescript(),
+    alias({
+      '@tarojs/taro': join(cwd, '../taro/src/index')
+    }),
+    resolve({
+      preferBuiltins: false
+    }),
+
+    common({
+      include: 'node_modules/**'
+    }),
+    babel({
+      babelrc: false,
+      presets: [
+        ['@babel/preset-env', {
+          modules: false
+        }]
+      ],
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-object-rest-spread',
+        ['@babel/plugin-transform-react-jsx', {
+          'pragma': 'Nerv.createElement'
+        }]
+      ]
+    })
   ]
 }
+const esmConfig = Object.assign({}, baseConfig, {
+  output: Object.assign({}, baseConfig.output, {
+    sourcemap: true,
+    format: 'es',
+    file: join(cwd, 'dist/index.esm.js')
+  })
+})
+
+function rollup () {
+  const target = process.env.TARGET
+
+  if (target === 'umd') {
+    return baseConfig
+  } else if (target === 'esm') {
+    return esmConfig
+  } else {
+    return [baseConfig, esmConfig]
+  }
+}
+module.exports = rollup()
