159c4bf25af8da914b7381740c9ec3ff035e57db
luckyadam <weitaozsh@gmail.com>
2018-11-02 11:21:18 +0800

    fix(cli): 模板创建项目出错

--- packages/taro-cli/templates/default/index.js
+++ packages/taro-cli/templates/default/index.js
@@ -44,7 +44,9 @@ module.exports = function (creater, params, helper, cb) {
   })
   creater.template(template, 'gitignore', path.join(projectPath, '.gitignore'))
   creater.template(template, 'editorconfig', path.join(projectPath, '.editorconfig'))
-  creater.template(template, 'eslintrc', path.join(projectPath, '.eslintrc'))
+  creater.template(template, 'eslintrc', path.join(projectPath, '.eslintrc'), {
+    typescript
+  })
   creater.template(template, 'indexhtml', path.join(sourceDir, 'index.html'))
   if (typescript) {
     creater.template(template, 'appjs', path.join(sourceDir, 'app.tsx'), {
