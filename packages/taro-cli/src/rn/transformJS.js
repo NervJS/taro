6162b636d3baa83e63888f5761d91a4f2230dfc2
luckyadam <weitaozsh@gmail.com>
2018-11-06 17:54:26 +0800

    fix(cli): 使用官方插件 babel-plugin-danger-remove-unused-import ，解决 Computed Property 支持问题，close #1015

--- packages/taro-cli/src/rn/transformJS.js
+++ packages/taro-cli/src/rn/transformJS.js
@@ -449,7 +449,7 @@ function parseJSCode ({code, filePath, isEntryFile, projectConfig}) {
       plugins: [
         [require('babel-plugin-transform-jsx-to-stylesheet'), {filePath}],
         require('babel-plugin-transform-decorators-legacy').default,
-        [require('babel-plugin-danger-remove-unused-import-taro'), {ignore: ['@tarojs/taro', 'react', 'react-native', 'nervjs']}],
+        [require('babel-plugin-danger-remove-unused-import'), {ignore: ['@tarojs/taro', 'react', 'react-native', 'nervjs']}],
         [require('babel-plugin-transform-define').default, constantsReplaceList]
       ]
     }).ast
