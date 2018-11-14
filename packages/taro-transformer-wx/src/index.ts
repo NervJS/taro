02fc89a2369f8574f0dc275bfa0cc156bc038864
luckyadam <weitaozsh@gmail.com>
2018-11-14 16:28:10 +0800

    fix(cli): 回退使用 babel-plugin-remove-dead-code 插件

--- packages/taro-transformer-wx/src/index.ts
+++ packages/taro-transformer-wx/src/index.ts
@@ -179,7 +179,7 @@ export default function transform (options: Options): TransformResult {
     plugins: [
       require('babel-plugin-transform-flow-strip-types'),
       [require('babel-plugin-transform-define').default, options.env]
-    ].concat((process.env.NODE_ENV === 'test') ? [] : require('babel-plugin-minify-dead-code'))
+    ].concat((process.env.NODE_ENV === 'test') ? [] : require('babel-plugin-remove-dead-code').default)
   }).ast as t.File
   if (options.isNormal) {
     return { ast } as any
