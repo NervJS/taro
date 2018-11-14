cf389a14114aeb3a0c990f0cefdabe19df0bf190
Manjiz <25199230@qq.com>
2018-11-14 18:47:32 +0800

    refactor(taro-components-rn): 开放传入Text组件的RN特有属性 close #1086

--- packages/taro-components-rn/App.js
+++ packages/taro-components-rn/App.js
@@ -93,7 +93,7 @@ export default class App extends Component {
 
         <EXVideo />
 
-        <Text>Welcome to React Native!</Text>
+        <Text numberOfLines={1}>Welcome to React Native!Welcome to React Native!Welcome to React Native!Welcome to React Native!</Text>
 
         <EXAudio />
 
