4f0a38c70982708303c0bcc1c1e01a0f4559ecef
liangwei73 <liangwei10@jd.com>
2018-11-21 10:48:01 +0800

    chore(taro-rn): 修改storage api中Promise的调用方式

--- packages/taro-rn/src/api/storage/index.js
+++ packages/taro-rn/src/api/storage/index.js
@@ -5,80 +5,88 @@ export function setStorage (opts = {}) {
   const { key, data, success, fail, complete } = opts
   const res = { errMsg: 'setStorage:ok' }
 
-  return AsyncStorage.setItem(key, JSON.stringify(data))
-    .then((e) => {
-      success && success(res)
-      complete && complete(res)
+  return new Promise((resolve, reject) => {
+    AsyncStorage.setItem(key, JSON.stringify(data))
+      .then((e) => {
+        success && success(res)
+        complete && complete(res)
 
-      return Promise.resolve(res)
-    }).catch((err) => {
-      res.errMsg = err.message
-      fail && fail(res)
-      complete && complete(res)
+        resolve(res)
+      }).catch((err) => {
+        res.errMsg = err.message
+        fail && fail(res)
+        complete && complete(res)
 
-      return Promise.reject(err)
-    })
+        reject(err)
+      })
+  })
 }
 
 export function getStorage (opts = {}) {
   const { key, success, fail, complete } = opts
   const res = { errMsg: 'getStorage:ok' }
 
-  return AsyncStorage.getItem(key)
-    .then((data) => {
-      res.data = JSON.parse(data)
-      success && success(res)
-      complete && complete(res)
+  return new Promise((resolve, reject) => {
+    AsyncStorage.getItem(key)
+      .then((data) => {
+        res.data = JSON.parse(data)
+        success && success(res)
+        complete && complete(res)
 
-      return Promise.resolve(res)
-    }).catch((err) => {
-      res.errMsg = err.message
-      fail && fail(res)
-      complete && complete(res)
+        resolve(res)
+      }).catch((err) => {
+        res.errMsg = err.message
+        fail && fail(res)
+        complete && complete(res)
 
-      return Promise.reject(err)
-    })
+        reject(err)
+      })
+  })
 }
 
 export function getStorageInfo (opts = {}) {
   const { success, fail, complete } = opts
   const res = { errMsg: 'getStorageInfo:ok' }
 
-  return AsyncStorage.getAllKeys()
-    .then((data) => {
-      res.keys = data
-      res.currentSize = null
-      res.limitSize = null
-      success && success(res)
-      complete && complete(res)
+  return new Promise((resolve, reject) => {
+    AsyncStorage.getAllKeys()
+      .then((data) => {
+        res.keys = data
+        res.currentSize = null
+        res.limitSize = null
+        success && success(res)
+        complete && complete(res)
 
-      return Promise.resolve(res)
-    }).catch((err) => {
-      res.errMsg = err.message
-      fail && fail(res)
-      complete && complete(res)
+        resolve(res)
+      }).catch((err) => {
+        res.errMsg = err.message
+        fail && fail(res)
+        complete && complete(res)
 
-      return Promise.reject(err)
-    })
+        reject(err)
+      })
+  })
 }
 
 export function removeStorage (opts = {}) {
   const { key, success, fail, complete } = opts
   const res = { errMsg: 'removeStorage:ok' }
 
-  return AsyncStorage.removeItem(key)
-    .then(() => {
-      success && success(res)
-      complete && complete(res)
+  return new Promise((resolve, reject) => {
+    AsyncStorage.removeItem(key)
+      .then(() => {
+        success && success(res)
+        complete && complete(res)
 
-      return Promise.resolve(res)
-    }).catch((err) => {
-      res.errMsg = err.message
-      fail && fail(res)
-      complete && complete(res)
+        resolve(res)
+      }).catch((err) => {
+        res.errMsg = err.message
+        fail && fail(res)
+        complete && complete(res)
 
-      return Promise.reject(err)
-    })
+        reject(err)
+      })
+  })
 }
 
 export async function clearStorage () {
