e011610236dc28b43161bae7379f94b819145e88
Pines-Cheng <spider.cs.nuc@gmail.com>
2018-11-22 15:43:58 +0800

    fix: node-sass 编译有时候无输出的 bug

--- packages/taro-cli/src/rn/styleProcess.js
+++ packages/taro-cli/src/rn/styleProcess.js
@@ -97,6 +97,7 @@ function writeStyleFile ({css, tempFilePath}) {
   const fileContent = `import { StyleSheet } from 'react-native'\n\nexport default StyleSheet.create(${css})`
   fs.ensureDirSync(path.dirname(tempFilePath))
   fs.writeFileSync(tempFilePath, fileContent)
+  Util.printLog(Util.pocessTypeEnum.GENERATE, '生成文件', tempFilePath)
 }
 
 module.exports = {
