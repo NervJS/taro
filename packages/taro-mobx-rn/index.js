5dc7ae2b08c531a5c0e756afaa3b1a114d41a93d
Tom.Huang <hzlhu.dargon@gmail.com>
2018-11-28 11:50:03 +0800

    feat(mobx): 删除taro-mobx-h5 & taro-mobx-rn下无用逻辑

--- packages/taro-mobx-rn/index.js
+++ packages/taro-mobx-rn/index.js
@@ -1,12 +1,6 @@
-import { configure } from 'mobx'
 import { Component } from '@tarojs/taro-rn'
 import { Children, createElement } from 'react'
-import { unstable_batchedUpdates as rdBatched } from 'react-native'
 import { createProvider, inject as originInject } from '@tarojs/mobx-common'
-
-if (typeof rdBatched === 'function') {
-  configure({ reactionScheduler: rdBatched })
-}
 
 export function inject () {
   return originInject(...arguments, { Component, createElement })
