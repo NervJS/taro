5d01b3cc8396104c5ed7a0793b5387afc39683d8
yuche <i@yuche.me>
2018-12-03 20:36:31 +0800

    fix(taroize):  class 内部 wx. 开头的函数不会转换为 Taro.func()

--- packages/taroize/src/script.ts
+++ packages/taroize/src/script.ts
@@ -144,6 +144,25 @@ function parsePage (
   refId?: Set<string>
 ) {
   const stateKeys: string[] = []
+  path.traverse({
+    CallExpression (path) {
+      const callee = path.get('callee')
+      if (callee.isIdentifier()) {
+        const name = callee.node.name
+        if (name === 'getApp' || name === 'getCurrentPages') {
+          callee.replaceWith(
+            t.memberExpression(t.identifier('Taro'), callee.node)
+          )
+        }
+      }
+      if (callee.isMemberExpression()) {
+        const object = callee.get('object')
+        if (object.isIdentifier({ name: 'wx' })) {
+          object.replaceWith(t.identifier('Taro'))
+        }
+      }
+    }
+  })
   if (refId) {
     refId.forEach(id => {
       if (!stateKeys.includes(id)) {
