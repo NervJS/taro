c90f96c1bd7a6ad0573b557c4823913b962e386d
onelong <love@onelong.org>
2018-12-13 19:57:11 +0800

    refactor(eslint): 允许从 this.props 而来的函数名以`dispatch` 开头 (#1500)
    * 允许从 this.props 而来的函数名以`dispatch` 开头
    
    例如：
    
    ```javascript
    const mapDispatchToProps = dispatch =>({
      dispatchA(){
        dispatch(actions.Foo())
      },
      dispatchB(){}
    })
    ```
    
    * chore(test): 添加从 this.props 而来的函数名必须要以 on 或 dispatch 开头的测试
    
    * fix(test): 修复测试用例

--- packages/eslint-plugin-taro/rules/this-props-function.js
+++ packages/eslint-plugin-taro/rules/this-props-function.js
@@ -17,11 +17,11 @@ module.exports = {
             object.object.type === 'ThisExpression' &&
             object.property.name === 'props' &&
             property.type === 'Identifier' &&
-            !/^on[A-Z_]/.test(property.name) &&
+            !/^(on|dispatch)[A-Z_]/.test(property.name) &&
             property.name !== 'dispatch'
           ) {
             context.report({
-              message: '从 this.props 而来的函数名必须要以 `on` 开头',
+              message: '从 this.props 而来的函数名必须要以 `on` 或 `dispatch` 开头',
               node
             })
           }
