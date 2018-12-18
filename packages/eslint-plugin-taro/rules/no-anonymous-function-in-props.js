02f3fcc5a44046e6402d4837c13aa88a4400baa6
yuche <i@yuche.me>
2018-12-18 10:26:06 +0800

    fix(eslint): ref 可以使用匿名函数，#1560

--- packages/eslint-plugin-taro/rules/no-anonymous-function-in-props.js
+++ packages/eslint-plugin-taro/rules/no-anonymous-function-in-props.js
@@ -4,14 +4,14 @@ const ERROR_MESSAGE = '不能在 JSX 参数中使用匿名函数'
 
 module.exports = {
   meta: {
-    docs: buildDocsMeta(ERROR_MESSAGE, 'no-spread-in-props')
+    docs: buildDocsMeta(ERROR_MESSAGE, 'no-anonymous-function-in-props')
   },
 
   create (context) {
     const parentIsJSXAttribute = (node) => {
       const parents = context.getAncestors(node)
       const jsxAttr = parents.find(p => p.type === 'JSXAttribute')
-      if (jsxAttr) {
+      if (jsxAttr && jsxAttr.name.name !== 'ref') {
         context.report({
           message: ERROR_MESSAGE,
           node: jsxAttr
