95b5b39731a3473ec582ca70d769deaa0fdb4899
yuche <i@yuche.me>
2018-12-27 17:00:13 +0800

    feat(transformer): 支持类函数式组件
    feat(transformer): 支持类函数式组件
    
    feat(transformer): 支持类函数式组件

--- packages/taro-transformer-wx/src/class.ts
+++ packages/taro-transformer-wx/src/class.ts
@@ -325,10 +325,6 @@ class Transformer {
               }
             })
           }
-          if (name.startsWith('render')) {
-            self.renderJSX.set(name, path)
-            self.refIdMap.set(path, new Set([]))
-          }
           if (name === 'constructor') {
             path.traverse({
               AssignmentExpression (p) {
