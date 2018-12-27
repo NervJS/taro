26c7c15c1208871d9b9cb686115369ed902c9bfa
luckyadam <weitaozsh@gmail.com>
2018-12-27 11:22:32 +0800

    feat(taro): add FileSystemManager api, #1708

--- packages/taro/src/native-apis.js
+++ packages/taro/src/native-apis.js
@@ -29,7 +29,9 @@ const onAndSyncApis = {
   getLogManager: true,
   onMemoryWarning: true,
   reportAnalytics: true,
-  navigateToSmartGameProgram: true
+  navigateToSmartGameProgram: true,
+  // 文件
+  getFileSystemManager: true
 }
 const noPromiseApis = {
   // 媒体
