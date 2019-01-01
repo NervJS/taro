aa002bec958fc62b69e9e9f6132b096e139b8c89
luckyadam <weitaozsh@gmail.com>
2019-01-02 00:33:03 +0800

    feat(cli): 使用 typescript 重构 cli 代码

--- packages/taro-cli/src/doctor/index.ts
+++ packages/taro-cli/src/doctor/index.ts
@@ -1,13 +1,8 @@
-import configValidator from './configValidator';
-import packageValidator from './packageValidator';
-import recommandValidator from './recommandValidator';
-import eslintValidator from './eslintValidator';
-
 export default {
   validators: [
-    configValidator,
-    packageValidator,
-    recommandValidator,
-    eslintValidator
+    require('./configValidator'),
+    require('./packageValidator'),
+    require('./recommandValidator'),
+    require('./eslintValidator')
   ]
 }
