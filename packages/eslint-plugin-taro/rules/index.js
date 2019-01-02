f7780c9f2e45ba7e9404f2f9b09ae0d5541f8d9e
yuche <i@yuche.me>
2019-01-02 21:54:44 +0800

    refactor(eslint): 取消无法使用函数式组件的 eslint 规则

--- packages/eslint-plugin-taro/rules/index.js
+++ packages/eslint-plugin-taro/rules/index.js
@@ -6,7 +6,7 @@ const allRules = {
   //'no-anonymous-function-in-props': require('./no-anonymous-function-in-props'),
   // 'no-jsx-in-class-method': require('./no-jsx-in-class-method'),
   'no-spread-in-props': require('./no-spread-in-props'),
-  'no-stateless-component': require('./no-stateless-component'),
+  // 'no-stateless-component': require('./no-stateless-component'),
   // 'jsx-handler-names': require('./jsx-handler-names'),
   'reserve-class-properties': require('./reserve-class-properties'),
   // 'function-naming': require('./function-naming'),
