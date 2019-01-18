bb258ac2c6aac92bd82b685581b01cc88d4b2f15
范兴宇 <fanxingyu@fanxingyudeMacBook-Pro.local>
2019-01-18 12:13:56 +0800

    add:就打印一下

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -5,9 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require('react');
+const React = require("react");
 
-const CompLibrary = require('../../core/CompLibrary.js');
+const CompLibrary = require("../../core/CompLibrary.js");
 const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
@@ -19,11 +19,11 @@ function imgUrl(img) {
 }
 
 function docUrl(doc, language) {
-  return `${siteConfig.baseUrl}docs/${(language ? language + '/' : '')}${doc}`;
+  return `${siteConfig.baseUrl}docs/${language ? language + "/" : ""}${doc}`;
 }
 
 function pageUrl(page, language) {
-  return `${siteConfig.baseUrl}${(language ? language + '/' : '')}${page}}`;
+  return `${siteConfig.baseUrl}${language ? language + "/" : ""}${page}}`;
 }
 
 class Button extends React.Component {
@@ -39,7 +39,7 @@ class Button extends React.Component {
 }
 
 Button.defaultProps = {
-  target: '_self',
+  target: "_self"
 };
 
 const SplashContainer = props => (
@@ -73,7 +73,8 @@ const PromoSection = props => (
 
 class HomeSplash extends React.Component {
   render() {
-    let language = this.props.language || '';
+    let language = this.props.language || "";
+    console.log("taro");
     return (
       <SplashContainer>
         <div className="inner">
@@ -91,9 +92,10 @@ class HomeSplash extends React.Component {
 
 const Block = props => (
   <Container
-    padding={['bottom', 'top']}
+    padding={["bottom", "top"]}
     id={props.id}
-    background={props.background}>
+    background={props.background}
+  >
     <GridBlock align="center" contents={props.children} layout={props.layout} />
   </Container>
 );
@@ -102,17 +104,17 @@ const Features = props => (
   <Block layout="fourColumn">
     {[
       {
-        content: 'This is the content of my feature',
-        image: imgUrl('logo-taro.png'),
-        imageAlign: 'top',
-        title: 'Feature One',
+        content: "This is the content of my feature",
+        image: imgUrl("logo-taro.png"),
+        imageAlign: "top",
+        title: "Feature One"
       },
       {
-        content: 'The content of my second feature',
-        image: imgUrl('logo-taro.png'),
-        imageAlign: 'top',
-        title: 'Feature Two',
-      },
+        content: "The content of my second feature",
+        image: imgUrl("logo-taro.png"),
+        imageAlign: "top",
+        title: "Feature Two"
+      }
     ]}
   </Block>
 );
@@ -120,7 +122,8 @@ const Features = props => (
 const FeatureCallout = props => (
   <div
     className="productShowcaseSection paddingBottom"
-    style={{textAlign: 'center'}}>
+    style={{ textAlign: "center" }}
+  >
     <h2>Feature Callout</h2>
     <MarkdownBlock>These are features of this project</MarkdownBlock>
   </div>
@@ -130,11 +133,11 @@ const LearnHow = props => (
   <Block background="light">
     {[
       {
-        content: 'Talk about learning how to use this',
-        image: imgUrl('logo-taro.png'),
-        imageAlign: 'right',
-        title: 'Learn How',
-      },
+        content: "Talk about learning how to use this",
+        image: imgUrl("logo-taro.png"),
+        imageAlign: "right",
+        title: "Learn How"
+      }
     ]}
   </Block>
 );
@@ -143,11 +146,11 @@ const TryOut = props => (
   <Block id="try">
     {[
       {
-        content: 'Talk about trying this out',
-        image: imgUrl('logo-taro.png'),
-        imageAlign: 'left',
-        title: 'Try it Out',
-      },
+        content: "Talk about trying this out",
+        image: imgUrl("logo-taro.png"),
+        imageAlign: "left",
+        title: "Try it Out"
+      }
     ]}
   </Block>
 );
@@ -156,11 +159,11 @@ const Description = props => (
   <Block background="dark">
     {[
       {
-        content: 'This is another description of how this project is useful',
-        image: imgUrl('logo-taro.png'),
-        imageAlign: 'right',
-        title: 'Description',
-      },
+        content: "This is another description of how this project is useful",
+        image: imgUrl("logo-taro.png"),
+        imageAlign: "right",
+        title: "Description"
+      }
     ]}
   </Block>
 );
@@ -192,10 +195,10 @@ const Showcase = props => {
 
 class Index extends React.Component {
   render() {
-    let language = this.props.language || '';
+    let language = this.props.language || "";
 
     return (
-      <div className='home_container flex middle center'>
+      <div className="home_container flex middle center">
         <HomeSplash language={language} />
         {/* <div className="mainContainer">
           <Features />
