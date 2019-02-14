00ef7a2df285d75d698fd9aee6720c5dc3af6047
vinsonxiao(肖宇泽) <vinsonxiao@tencent.com>
2019-02-14 14:49:17 +0800

    fix(taro-cli+taro-plugin-babel):修复taro调用babel时未传递filename导致插件内无法访问编译文件路径问题(#2168) 
    1. 修复 taro-plugin-babel  中调用 babel.transform 未传递 config.filename 导致插件中无法访问 state.file.opts.filename问题
    2. 修复 taro-cli/weapp.js 中调用 plugin-babel 时 file 参数始终传递 entryFilePath 而非 sourceFilePath ，导致插件内无法正确拿到文件路径问题

--- packages/taro-plugin-babel/index.js
+++ packages/taro-plugin-babel/index.js
@@ -3,6 +3,8 @@ const { transform } = require('babel-core')
 module.exports = function babel (content, file, config) {
   let p
   try {
+    if (!config) config = {}
+    config.filename = file
     const res = transform(content, config)
     p = Promise.resolve(res)
   } catch (e) {
