e91c2c5ed8d7c07868980e41c7cb9d2a55967cda
jinjinjin0731 <709899428@qq.com>
2019-03-11 19:38:20 +0800

    fix(components): 回退 scrollView

--- packages/taro-components/src/components/scroll-view/index.js
+++ packages/taro-components/src/components/scroll-view/index.js
@@ -1,202 +1,180 @@
 import 'weui'
 import Nerv from 'nervjs'
 import omit from 'omit.js'
-import BScroll from 'better-scroll'
 import classNames from 'classnames'
-
 import './style/index.scss'
 
+function easeOutScroll (from, to, callback) {
+  if (from === to || typeof from !== 'number') {
+    return
+  }
+  let change = to - from
+  const dur = 500
+  const sTime = +new Date()
+  function linear (t, b, c, d) {
+    return c * t / d + b
+  }
+  const isLarger = to >= from
+
+  function step () {
+    from = linear(+new Date() - sTime, from, change, dur)
+    if ((isLarger && from >= to) || (!isLarger && to >= from)) {
+      callback(to)
+      return
+    }
+    callback(from)
+    requestAnimationFrame(step)
+  }
+  step()
+}
+function throttle (fn, delay) {
+  let timer = null
+  return function () {
+    clearTimeout(timer)
+    timer = setTimeout(function () {
+      fn()
+    }, delay)
+  }
+}
 class ScrollView extends Nerv.Component {
   constructor () {
     super(...arguments)
-    this._cacheLastX = 0
-    this._cacheLastY = 0
   }
-
   componentDidMount () {
-    this.initScroll()
-  }
-
-  componentDidUpdate () {
-    if (!this.refs.wrapperScroll && !this._scroll) {
-      return
-    }
-    this._scroll.destroy()
-    this.initScroll()
-  }
-
-  componentWillUnmount () {
-    this._scroll.destroy()
-  }
-
-  initScroll () {
-    if (!this.refs.wrapperScroll) {
-      return
-    }
-    const {
-      onScroll,
-      onScrollToUpper,
-      upperThreshold = 50,
-      onScrollToLower,
-      lowerThreshold = 50,
-      scrollWithAnimation,
-      scrollX,
-      scrollY,
-      scrollTop,
-      scrollLeft,
-      scrollIntoView
-    } = this.props
-    let opt = {
-      probeType: onScroll ? 3 : 0,
-      bounce: false,
-      click: true,
-      scrollX,
-      scrollY,
-      pullDownRefresh: {
-        threshold: 50,
-        stop: 20
+    setTimeout(() => {
+      const props = this.props
+      if (props.scrollY && typeof props.scrollTop === 'number') {
+        if ('scrollWithAnimation' in props) {
+          easeOutScroll(0, props.scrollTop, pos => {
+            this.container.scrollTop = pos
+          })
+        } else {
+          this.container.scrollTop = props.scrollTop
+        }
+        this._scrollTop = props.scrollTop
       }
-    }
-
-    if (scrollX) {
-      this.refs.contentScroll.style.width =
-        this.computedWidth(this.props.children) + 'px'
-    }
-
-    this._scroll = new BScroll(this.refs.wrapperScroll, opt)
-
-    if (onScroll) {
-      this._scroll.on('scroll', pos => {
-        const absX = Math.ceil(Math.abs(pos.x))
-        const absY = Math.ceil(Math.abs(pos.y))
-
-        const absMaxX = Math.abs(this._scroll.maxScrollX)
-        const absMaxY = Math.abs(this._scroll.maxScrollY)
-
-        const detail = {
-          scrollLeft: absX,
-          scrollTop: absY,
-          scrollHeight: this._scroll.scrollerHeight,
-          scrollWidth: this._scroll.scrollerWidth,
-          deltaX: this._scroll.directionX,
-          deltaY: this._scroll.directionY
+      if (props.scrollX && typeof props.scrollLeft === 'number') {
+        if ('scrollWithAnimation' in props) {
+          easeOutScroll(0, props.scrollLeft, pos => {
+            this.container.scrollLeft = pos
+          })
+        } else {
+          this.container.scrollLeft = props.scrollLeft
         }
-        onScroll({ detail })
-
-        // 滚动到顶部/左边触发
-        if (onScrollToUpper) {
-          // 顶部
-          if (upperThreshold > absY && this._cacheLastY !== absY) {
-            this._cacheLastY = absY
-            this.throttle(onScrollToUpper, 200)()
-          } else if (upperThreshold > absX && this._cacheLastX !== absX) {
-            // 左边
-            this._cacheLastX = absX
-            this.throttle(onScrollToUpper, 200)()
-          }
-        }
-
-        // 滚动到底部右边触发
-        if (onScrollToLower) {
-          // 底部
-          if (
-            lowerThreshold > Math.ceil(absMaxY) - absY &&
-            this._cacheLastY !== absY
-          ) {
-            this._cacheLastY = absY
-            this.throttle(onScrollToLower, 200)()
-          } else if (
-            lowerThreshold > Math.ceil(absMaxX) - absX &&
-            this._cacheLastX !== absX
-          ) {
-            // 右边
-            this._cacheLastX = absX
-            this.throttle(onScrollToLower, 200)()
-          }
-        }
-      })
-    }
-
-    const scrollWithAnimationTime = scrollWithAnimation ? 400 : 0
-    scrollTop &&
-      this._scroll.scrollTo(
-        0,
-        (scrollTop / this._scroll.scrollerHeight) * this._scroll.maxScrollY,
-        scrollWithAnimationTime
-      )
-    scrollLeft &&
-      this._scroll.scrollTo(
-        (scrollLeft / this._scroll.scrollerWidth) * this._scroll.maxScrollX,
-        0,
-        scrollWithAnimationTime
-      )
-
-    if (scrollIntoView) {
-      // 第一个子元素滚动不到，Hack 一下
-      const firstChildId = this.refs.contentScroll.firstChild && this.refs.contentScroll.firstChild.getAttribute('id')
-      const posX = scrollX ? 1 : 0
-      const posY = scrollY ? 1 : 0
-      firstChildId === scrollIntoView ? this._scroll.scrollTo(posX, posY, scrollWithAnimationTime) : this._scroll.scrollToElement(`#${scrollIntoView}`, scrollWithAnimationTime)
-    }
+        this._scrollLeft = props.scrollLeft
+      }
+    }, 10)
   }
 
-  computedWidth (children) {
-    let contentW = 0
-    if (Array.isArray(children) && children.length) {
-      contentW += this.parseComponentsArr(children)
-    } else if (!Array.isArray(children)) {
-      contentW += children.dom.getBoundingClientRect().width
+  componentWillReceiveProps (nextProps) {
+    const props = this.props
+    // Y 轴滚动
+    if (
+      nextProps.scrollY &&
+      typeof props.scrollTop === 'number' &&
+      nextProps.scrollTop !== this._scrollTop
+    ) {
+      if ('scrollWithAnimation' in nextProps) {
+        easeOutScroll(this._scrollTop, nextProps.scrollTop, pos => {
+          this.container.scrollTop = pos
+        })
+      } else {
+        this.container.scrollTop = nextProps.scrollTop
+      }
+      this._scrollTop = nextProps.scrollTop
     }
-    return contentW
-  }
-
-  parseComponentsArr (children) {
-    let contentW = 0
-    if (Array.isArray(children) && children.length) {
-      children.forEach(ele => {
-        contentW += this.parseComponentsArr(ele)
-      })
-    } else if (!Array.isArray(children)) {
-      contentW += children.dom.getBoundingClientRect().width
-    }
-    return contentW
-  }
-
-  throttle (fn, delay) {
-    if (this._timer) clearTimeout(this._timer)
-    this._timer = null
-    return () => {
-      this._timer = setTimeout(() => fn(), delay)
+    // X 轴滚动
+    if (
+      nextProps.scrollX &&
+      typeof props.scrollLeft === 'number' &&
+      nextProps.scrollLeft !== this._scrollLeft
+    ) {
+      if ('scrollWithAnimation' in nextProps) {
+        easeOutScroll(this._scrollLeft, nextProps.scrollLeft, pos => {
+          this.container.scrollLeft = pos
+        })
+      } else {
+        this.container.scrollLeft = nextProps.scrollLeft
+      }
+      this._scrollLeft = nextProps.scrollLeft
     }
   }
 
   render () {
-    const { className, style, scrollX } = this.props
-    const cls = classNames('taro-scroll', className)
-    const contentScroll = classNames('taro-scroll-content', {
-      'taro-scroll-content-x': scrollX
-    })
+    const {
+      className,
+      onScroll,
+      onScrollToUpper,
+      onScrollToLower,
+      scrollX,
+      scrollY
+    } = this.props
+    let { upperThreshold = 0, lowerThreshold = 0 } = this.props
+    const cls = classNames(
+      'taro-scroll',
+      {
+        [`taro-scroll-view__scroll-x`]: scrollX,
+        [`taro-scroll-view__scroll-y`]: scrollY
+      },
+      className
+    )
+    upperThreshold = parseInt(upperThreshold)
+    lowerThreshold = parseInt(upperThreshold)
+    const uperAndLower = () => {
+      const {
+        offsetWidth,
+        offsetHeight,
+        scrollLeft,
+        scrollTop,
+        scrollHeight,
+        scrollWidth
+      } = this.container
+      if (
+        onScrollToLower &&
+        ((this.props.scrollY &&
+          offsetHeight + scrollTop + lowerThreshold >= scrollHeight) ||
+          (this.props.scrollX &&
+            offsetWidth + scrollLeft + lowerThreshold >= scrollWidth))
+      ) {
+        onScrollToLower()
+      }
+      if (
+        onScrollToUpper &&
+        ((this.props.scrollY && scrollTop <= upperThreshold) ||
+          (this.props.scrollX && scrollLeft <= upperThreshold))
+      ) {
+        onScrollToUpper()
+      }
+    }
+    const uperAndLowerThrottle = throttle(uperAndLower, 200)
+    const _onScroll = e => {
+      const {
+        scrollLeft,
+        scrollTop,
+        scrollHeight,
+        scrollWidth
+      } = this.container
+      this._scrollLeft = scrollLeft
+      this._scrollTop = scrollTop
+      e.detail = {
+        scrollLeft,
+        scrollTop,
+        scrollHeight,
+        scrollWidth
+      }
+      uperAndLowerThrottle()
+      onScroll && onScroll(e)
+    }
     return (
       <div
-        ref='wrapperScroll'
-        {...omit(this.props, [
-          'className',
-          'scrollX',
-          'scrollY',
-          'scrollTop',
-          'scrollLeft',
-          'scrollWithAnimation',
-          'onScrollToUpper',
-          'onScrollToLower',
-          'onScroll',
-          'style'
-        ])}
-        style={style}
+        ref={container => {
+          this.container = container
+        }}
+        {...omit(this.props, ['className'])}
         className={cls}
+        onScroll={_onScroll}
       >
-        <div ref='contentScroll' className={contentScroll}>
-          {this.props.children}
-        </div>
+        {this.props.children}
       </div>
     )
   }
