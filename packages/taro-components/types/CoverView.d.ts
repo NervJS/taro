4e1516730fe3cdf0c8e0846fb0f7bce72f3a1123
Jay Fong <fjc0kb@gmail.com>
2019-03-14 14:29:49 +0800

    feat(taro-components): 完善 CoverView 的类型定义

--- packages/taro-components/types/CoverView.d.ts
+++ packages/taro-components/types/CoverView.d.ts
@@ -2,11 +2,38 @@ import { ComponentType } from 'react'
 import { StandardProps, CommonEventFunction } from './common'
 
 export interface CoverViewProps extends StandardProps {
-
   /**
    * 设置顶部滚动偏移量，仅在设置了 overflow-y: scroll 成为滚动元素后生效
    */
-  scrollTop?: number
+  scrollTop?: number,
+
+  /**
+   * 指定按下去的样式类。当 `hover-class="none"` 时，没有点击态效果
+   *
+   * 默认值：`none`
+   */
+  hoverClass?: string,
+
+  /**
+   * 指定是否阻止本节点的祖先节点出现点击态
+   *
+   * 默认值：`fasle`
+   */
+  hoverStopPropagation?: boolean,
+
+  /**
+   * 按住后多久出现点击态，单位毫秒
+   *
+   * 默认值：`50`
+   */
+  hoverStartTime?: number,
+
+  /**
+   * 手指松开后点击态保留时间，单位毫秒
+   *
+   * 默认值：`400`
+   */
+  hoverStayTime?: number,
 }
 
 export interface CoverImageProps extends StandardProps {
