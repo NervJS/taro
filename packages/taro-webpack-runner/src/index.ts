c288a45ae4e2774a686195370b043e0ef2f6ae94
littly <544028951@qq.com>
2019-03-15 21:25:46 +0800

    fix(webpack-runner): 修改了版本说明

--- packages/taro-webpack-runner/src/index.ts
+++ packages/taro-webpack-runner/src/index.ts
@@ -20,10 +20,10 @@ const customizeChain = (chain, customizeFunc: Function) => {
 }
 
 const warnConfigWebpack = () => {
-  console.log(chalk.yellow(`taro@1.3版本开始，h5.webpack配置项已经停止支持。作为代替，请使用配置项h5.webpackChain，文档：https://nervjs.github.io/taro/docs/config-detail.html#h5webpackchain`))
+  console.log(chalk.yellow(`taro@1.2.18版本开始，h5.webpack配置项已经停止支持。作为代替，请使用配置项h5.webpackChain，文档：https://nervjs.github.io/taro/docs/config-detail.html#h5webpackchain`))
 }
 const warnConfigEnableDll = () => {
-  console.log(chalk.yellow(`taro@1.3版本开始，taro在h5端加强了代码体积控制，抽离dll的收益已经微乎其微，同时也容易导致一些问题（#1800等）。所以h5.enableDll配置项暂时移除。`))
+  console.log(chalk.yellow(`taro@1.2.18版本开始，taro在h5端加强了代码体积控制，抽离dll的收益已经微乎其微，同时也容易导致一些问题（#1800等）。所以h5.enableDll配置项暂时移除。`))
 }
 
 const buildProd = (config: BuildConfig): Promise<void> => {
