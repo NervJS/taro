7384cfb99be65985ea6ec5920f44305f6a6155de
luckyadam <weitaozsh@gmail.com>
2019-03-21 15:07:37 +0800

    feat(taro): 补充 getMenuButtonBoundingClientRect API，close #2520

--- packages/taro/types/index.d.ts
+++ packages/taro/types/index.d.ts
@@ -7239,6 +7239,42 @@ declare namespace Taro {
    */
   function hideTabBar(OBJECT?: hideTabBar.Param): Promise<any>
 
+  namespace getMenuButtonBoundingClientRect {
+    type Return = {
+      /**
+       * 宽度，单位：px
+       */
+      width: number
+      /**
+       * 高度，单位：px
+       */
+      height: number
+      /**
+       * 	上边界坐标，单位：px
+       */
+      top: number
+      /**
+       * 	右边界坐标，单位：px
+       */
+      right: number
+      /**
+       * 	下边界坐标，单位：px
+       */
+      bottom: number
+      /**
+       * 	左边界坐标，单位：px
+       */
+      left: number
+    }
+  }
+  /**
+   * @since 2.1.0
+   *
+   * 获取菜单按钮（右上角胶囊按钮）的布局位置信息。坐标信息以屏幕左上角为原点
+   * @see https://developers.weixin.qq.com/miniprogram/dev/api/wx.getMenuButtonBoundingClientRect.html
+   */
+  function getMenuButtonBoundingClientRect(): getMenuButtonBoundingClientRect.Return
+
   namespace navigateTo {
     type Param = {
       /**
