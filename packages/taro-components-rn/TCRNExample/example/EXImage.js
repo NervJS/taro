7becd8e482b3a8d17f4f6e2d253d7a5898b1b9f0
Manjiz <25199230@qq.com>
2019-04-12 13:40:06 +0800

    feat(components-rn): 脱离expo, example 移至 react-native init 的项目下

---
+++ packages/taro-components-rn/TCRNExample/example/EXImage.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { StyleSheet } from 'react-native'
+import { View, Image } from '../../dist'
+
+const temp = StyleSheet.create({
+  img: {
+    width: 240,
+    height: 500
+  }
+})
+
+export default class EXImage extends Component {
+  constructor () {
+    super()
+    this.state = {
+      src: null
+    }
+  }
+
+  // componentDidMount () {
+  //   setTimeout(() => {
+  //     this.setState({
+  //       src: 'https://storage.360buyimg.com/mtd/home/jdlogo1529462435227.png'
+  //     })
+  //   }, 0)
+  // }
+
+  render () {
+    return (
+      <View style={{ width: 100 }}>
+        <Image
+          src={'https://storage.360buyimg.com/mtd/home/jdlogo1529462435227.png'}
+          // src={require('./jdlogo.png')}
+          mode="widthFix"
+          style={{
+            width: '100%',
+            // height: 500
+          }}
+        />
+      </View>
+    )
+  }
+}
---
+++ packages/taro-components-rn/TCRNExample/example/EXSlider.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { View, Slider } from '../../dist'
+
+export default class EXSlider extends Component {
+  state = {
+    progressPercent: 70
+  }
+
+  onSliderValueChange = (event) => {
+    // this.setState({ progressPercent: event.detail.value })
+  }
+
+  render () {
+    return (
+      <View style={{width: 300}}>
+        <Slider
+          value={this.state.progressPercent}
+          showValue={true}
+          onChange={this.onSliderValueChange}
+        />
+      </View>
+    )
+  }
+}
