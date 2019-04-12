2a0b21d72558eac34689a66e494b34de530e83e0
Manjiz <25199230@qq.com>
2019-04-12 13:37:46 +0800

    feat(components-rn): ts 改写 WebView 组件

--- packages/taro-components-rn/src/components/WebView/index.tsx
+++ packages/taro-components-rn/src/components/WebView/index.tsx
@@ -5,32 +5,22 @@
  * ✔ onError(binderror)
  *
  * 注意：onMessage 每次调用都会执行（小程序在特定时机触发），RN WebView 调用 window.postMessage 只接收一个字符串参数
- *
- * @flow
  */
 
 import * as React from 'react'
 import {
-  WebView,
-  StyleSheet,
+  WebView
 } from 'react-native'
-const utils = require('../../utils')
+import { WebViewProps } from './PropsType'
+import utils from '../../utils'
 
-type Props = {
-  style?: StyleSheet.Styles,
-  src?: boolean,
-  onMessage?: Function,
-  onLoad?: Function,
-  onError?: Function,
-}
-
-export default function _WebView ({
+const _WebView: React.SFC<WebViewProps> = ({
   style,
   src,
   onMessage = utils.noop,
   onLoad = utils.noop,
   onError = utils.noop,
-}: Props) {
+}) => {
   return (
     <WebView
       source={{ uri: src }}
@@ -55,3 +45,5 @@ export default function _WebView ({
     />
   )
 }
+
+export default _WebView
