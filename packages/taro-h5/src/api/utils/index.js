52703ac2d2d093329bbd9951dfe36e0be35d17a0
littly <544028951@qq.com>
2019-04-16 22:32:10 +0800

    feat(h5): h5增加api createInnerAudioContext

--- packages/taro-h5/src/api/utils/index.js
+++ packages/taro-h5/src/api/utils/index.js
@@ -84,23 +84,45 @@ const isValidColor = (color) => {
 const createCallbackManager = () => {
   const callbacks = []
 
+  /**
+   * 添加回调
+   * @param {{ callback: function, ctx: any } | function} opt 
+   */
   const add = (opt) => {
     callbacks.push(opt)
   }
 
+  /**
+   * 移除回调
+   * @param {{ callback: function, ctx: any } | function} opt 
+   */
   const remove = (opt) => {
-    const pos = callbacks.findIndex(({ callback }) => {
-      return callback === opt.callback
+    const pos = callbacks.findIndex(callback => {
+      return callback === opt
     })
     if (pos > -1) {
       callbacks.splice(pos, 1)
     }
   }
 
+  /**
+   * 获取回调函数数量
+   * @return {number}
+   */
   const count = () => callbacks.length
+
+  /**
+   * 触发回调
+   * @param  {...any} args 回调的调用参数
+   */
   const trigger = (...args) => {
-    callbacks.forEach(({ callback, ctx }) => {
-      callback.call(ctx, ...args)
+    callbacks.forEach(opt => {
+      if (typeof opt === 'function') {
+        opt(...args)
+      } else {
+        const { callback, ctx } = opt
+        callback.call(ctx, ...args)
+      }
     })
   }
 
