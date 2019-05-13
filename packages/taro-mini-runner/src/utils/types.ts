76373378b86da0fef75d3adf663d98ea8931e49a
luckyadam <weitaozsh@gmail.com>
2019-05-13 20:47:09 +0800

    feat(cli): 尝试接入 webpack 来编译各端小程序

---
+++ packages/taro-mini-runner/src/utils/types.ts
@@ -0,0 +1,72 @@
+import * as webpack from 'webpack'
+
+type FunctionLikeCustomWebpackConfig = (webpackConfig: webpack.Configuration, webpack) => webpack.Configuration
+
+export type CustomWebpackConfig = FunctionLikeCustomWebpackConfig | webpack.Configuration
+export interface IOption {
+  [key: string]: any
+}
+
+type TogglableOptions<T = IOption> = {
+  enable?: boolean
+  config?: T
+}
+
+export namespace PostcssOption {
+  export type cssModules = TogglableOptions<{
+    namingPattern: 'global' | string
+    generateScopedName: string | ((localName: string, absoluteFilePath: string) => string)
+  }>
+}
+
+export interface IPostcssOption {
+  autoprefixer?: TogglableOptions
+  pxtransform?: TogglableOptions
+  cssModules?: PostcssOption.cssModules
+}
+
+
+export interface IChain {
+  [key: string]: any
+}
+
+export interface ITaroMiniConfig {
+  entry: webpack.Entry
+  output: webpack.Output
+}
+
+export interface ITaroPlugins {
+  babel: IOption
+  csso?: TogglableOptions
+  uglify?: TogglableOptions
+}
+
+export interface ICopyOptions {
+  patterns: {
+    from: string
+    to: string
+    ignore: string[]
+  }[]
+  options: {
+    ignore: string[]
+  }
+}
+
+export interface ITaroBaseConfig {
+  outputDir: string
+  staticDirectory: string
+  chunkDirectory: string
+  copy: ICopyOptions
+
+  designWidth: number
+  deviceRatio?: number
+
+  defineConstants?: IOption
+  env?: IOption
+
+  plugins: ITaroPlugins
+}
+
+export interface IBuildConfig extends ITaroBaseConfig, ITaroMiniConfig {
+  isWatch: boolean
+}
