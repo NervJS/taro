03ae967034886e4f54baed45626d67d5520088b3
luckyadam <weitaozsh@gmail.com>
2019-05-15 22:27:57 +0800

    feat(cli): 拆分逻辑到 loader

--- packages/taro-mini-runner/src/index.ts
+++ packages/taro-mini-runner/src/index.ts
@@ -1,3 +1,4 @@
+import * as path from 'path'
 import * as webpack from 'webpack'
 
 import { IBuildConfig } from './utils/types'
@@ -5,15 +6,40 @@ import { printBuildError, bindProdLogger } from './utils/logHelper'
 import MiniPlugin from './plugins/miniPlugin'
 
 export default function build (config: IBuildConfig) {
+  const compilePlugins = config.plugins
+  const { babel } = compilePlugins
   const webpackConfig = {
+    mode: config.isWatch ? 'development' : 'production',
     entry: config.entry,
     output: {
       filename: '[name].js',
       publicPath: '/',
 			path: config.outputDir,
     },
+    module: {
+      rules: [
+        {
+					test: /\.(tsx?|jsx?)$/,
+					include: /src/,
+					exclude: /node_modules/,
+					use: [{
+            loader: path.resolve(__dirname, './loaders/fileParseLoader'),
+            options: {
+              babel
+            }
+          }, {
+            loader: path.resolve(__dirname, './loaders/wxTransformerLoader'),
+            options: {
+              buildAdapter: config.buildAdapter
+            }
+          }]
+				}
+      ]
+    },
     plugins: [
-      new MiniPlugin()
+      new MiniPlugin({
+        buildAdapter: config.buildAdapter
+      })
     ]
   }
 
