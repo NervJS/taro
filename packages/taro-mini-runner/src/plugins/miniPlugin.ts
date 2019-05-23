9d3a4499660de4b8e212f212cc6e42709771128a
luckyadam <weitaozsh@gmail.com>
2019-05-23 17:37:56 +0800

    feat(cli): 将 app/页面/组件 添加到入口文件进行编译

--- packages/taro-mini-runner/src/plugins/miniPlugin.ts
+++ packages/taro-mini-runner/src/plugins/miniPlugin.ts
@@ -3,6 +3,11 @@ import * as fs from 'fs-extra'
 
 import * as wxTransformer from '@tarojs/transformer-wx'
 import * as webpack from 'webpack'
+import * as SingleEntryPlugin from 'webpack/lib/SingleEntryPlugin'
+import * as FunctionModulePlugin from 'webpack/lib/FunctionModulePlugin'
+import * as NodeSourcePlugin from 'webpack/lib/node/NodeSourcePlugin'
+import * as JsonpTemplatePlugin from 'webpack/lib/JsonpTemplatePlugin'
+import * as LoaderTargetPlugin from 'webpack/lib/LoaderTargetPlugin'
 import { defaults } from 'lodash'
 import * as t from 'babel-types'
 import traverse from 'babel-traverse'
@@ -16,18 +21,54 @@ interface IMiniPluginOptions {
   buildAdapter: BUILD_TYPES
 }
 
+interface IComponent { name: string, path: string }
+
 const PLUGIN_NAME = 'MiniPlugin'
+
+const taroFileTypeMap = {}
+
+export enum TARO_FILE_TYPE {
+  APP = 'APP',
+  PAGE = 'PAGE',
+  COMPONENT = 'COMPONENT'
+}
+
+export const createTarget = function createTarget(name) {
+	const target = compiler => {
+		const { options } = compiler
+		compiler.apply(
+			new JsonpTemplatePlugin(options.output),
+			new FunctionModulePlugin(options.output),
+			new NodeSourcePlugin(options.node),
+			new LoaderTargetPlugin('web')
+		)
+	}
+
+	const creater = new Function(
+		`var t = arguments[0]; return function ${name}(c) { return t(c); }`
+	);
+	return creater(target)
+}
+
+export const Targets = {
+  [BUILD_TYPES.WEAPP]: createTarget(BUILD_TYPES.WEAPP),
+  [BUILD_TYPES.ALIPAY]: createTarget(BUILD_TYPES.ALIPAY),
+  [BUILD_TYPES.SWAN]: createTarget(BUILD_TYPES.SWAN),
+  [BUILD_TYPES.TT]: createTarget(BUILD_TYPES.TT),
+  [BUILD_TYPES.QQ]: createTarget(BUILD_TYPES.QQ),
+}
 
 export default class MiniPlugin {
   options: IMiniPluginOptions
   appEntry: string
-  pages: Set<string>
-  components: Set<string>
+  pages: Set<IComponent>
+  components: Set<IComponent>
   sourceDir: string
 
   constructor (options = {}) {
     this.options = defaults(options || {}, {
-      buildAdapter: BUILD_TYPES.WEAPP
+      buildAdapter: BUILD_TYPES.WEAPP,
+      commonLibName: 'lib.js'
     })
 
     this.pages = new Set()
@@ -68,6 +109,7 @@ export default class MiniPlugin {
     }
     const appEntryPath = getEntryPath(entry)
     this.sourceDir = path.dirname(appEntryPath)
+    taroFileTypeMap[appEntryPath] = TARO_FILE_TYPE.APP
     return appEntryPath
   }
 
@@ -147,30 +189,54 @@ export default class MiniPlugin {
     if (!appPages || appPages.length === 0) {
       throw new Error('缺少页面')
     }
-    this.pages = new Set([...appPages.map(item => resolveScriptPath(path.join(this.sourceDir, item)))])
+    this.pages = new Set([
+      ...appPages.map(item => {
+        const pagePath = resolveScriptPath(path.join(this.sourceDir, item))
+        taroFileTypeMap[pagePath] = TARO_FILE_TYPE.PAGE
+        return { name: item, path: pagePath }
+      })
+    ])
   }
 
-  getComponents (fileList: Set<string>, isRoot: boolean) {
+  getComponents (fileList: Set<IComponent>, isRoot: boolean) {
     const { buildAdapter } = this.options
     fileList.forEach(file => {
-      const code = fs.readFileSync(file).toString()
+      const code = fs.readFileSync(file.path).toString()
       const transformResult = wxTransformer({
         code,
-        sourcePath: file,
-        isTyped: REG_TYPESCRIPT.test(file),
+        sourcePath: file.path,
+        isTyped: REG_TYPESCRIPT.test(file.path),
         isRoot,
         adapter: buildAdapter
       })
+
       let depComponents = transformResult.components
       if (depComponents && depComponents.length) {
         depComponents.forEach(item => {
-          const componentPath = resolveScriptPath(path.resolve(path.dirname(file), item.path))
-          if (fs.existsSync(componentPath)) {
-            this.components.add(componentPath)
-            this.getComponents(new Set([componentPath]), false)
+          const componentPath = resolveScriptPath(path.resolve(path.dirname(file.path), item.path))
+          if (fs.existsSync(componentPath) && !Array.from(this.components).some(item => item.path === componentPath)) {
+            taroFileTypeMap[componentPath] = TARO_FILE_TYPE.COMPONENT
+            const componentName = componentPath.replace(this.sourceDir, '').replace(/\\/g, '/').replace(path.extname(componentPath), '')
+            const componentObj = { name: componentName, path: componentPath }
+            this.components.add(componentObj)
+            this.getComponents(new Set([componentObj]), false)
           }
         })
       }
+    })
+  }
+
+  addEntries (compiler: webpack.Compiler) {
+    const mainFiles = new Set([ ...this.pages, ...this.components ])
+    mainFiles.add({
+      name: 'app',
+      path: this.appEntry
+    })
+    mainFiles.forEach(item => {
+      compiler.hooks.make.tapAsync(PLUGIN_NAME, (compilation: webpack.compilation.Compilation, callback) => {
+        const dep = SingleEntryPlugin.createDependency(item.path, item.name)
+        compilation.addEntry(this.sourceDir, dep, item.name, callback)
+      })
     })
   }
 
@@ -178,5 +244,10 @@ export default class MiniPlugin {
     this.appEntry = this.getAppEntry(compiler)
     this.getPages()
     this.getComponents(this.pages, true)
+    this.addEntries(compiler)
+  }
+
+  static getTaroFileTypeMap () {
+    return taroFileTypeMap
   }
 }
