7856a48be264f402afae7057f74500a124f23db8
luckyadam <weitaozsh@gmail.com>
2019-05-23 17:37:56 +0800

    feat(cli): 将 app/页面/组件 添加到入口文件进行编译

--- packages/taro-mini-runner/src/loaders/wxTransformerLoader.ts
+++ packages/taro-mini-runner/src/loaders/wxTransformerLoader.ts
@@ -2,18 +2,23 @@ import { getOptions } from 'loader-utils'
 import * as wxTransformer from '@tarojs/transformer-wx'
 
 import { REG_TYPESCRIPT } from '../utils/constants'
+import { TARO_FILE_TYPE } from '../plugins/miniPlugin'
 
 export default function wxTransformerLoader (source) {
-  const options = getOptions(this)
+  const { buildAdapter, fileTypeMap } = getOptions(this)
   const filePath = this.resourcePath
-  const { buildAdapter } = options
-  const transformResult = wxTransformer({
+  const wxTransformerParams: any = {
     code: source,
     sourcePath: filePath,
     isTyped: REG_TYPESCRIPT.test(filePath),
     adapter: buildAdapter
-  })
+  }
+  if (fileTypeMap[filePath] === TARO_FILE_TYPE.APP) {
+    wxTransformerParams.isApp = true
+  } else if (fileTypeMap[filePath] === TARO_FILE_TYPE.PAGE) {
+    wxTransformerParams.isRoot = true
+  }
+  const transformResult = wxTransformer(wxTransformerParams)
   this.callback(null, transformResult.code, transformResult.ast)
-  console.log('wxTransformerLoader')
   return transformResult.code
 }
