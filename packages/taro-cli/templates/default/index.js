4310f625472827fe24ad00591e30b2993af74e10
zsir <zhenglfsir@gmail.com>
2019-05-27 17:35:48 +0800

    beautify log when developers init

--- packages/taro-cli/templates/default/index.js
+++ packages/taro-cli/templates/default/index.js
@@ -147,7 +147,7 @@ module.exports = function (creater, params, helper, cb) {
     } else {
       command = 'npm install'
     }
-    const installSpinner = ora(`执行安装项目依赖 ${chalk.cyan.bold(command)}, 需要一会儿...`).start()
+    const installSpinner = ora(`执行安装项目依赖 ${chalk.cyan.bold(command)}, 需要一会儿...\n`).start()
     const install = shelljs.exec(command, { silent: true })
     if (install.code === 0) {
       installSpinner.color = 'green'
@@ -158,8 +158,8 @@ module.exports = function (creater, params, helper, cb) {
       installSpinner.fail(chalk.red('安装项目依赖失败，请自行重新安装！'))
       console.log(`${install.stderr}${install.stdout}`)
     }
-    console.log(chalk.green(`创建项目 ${chalk.green.bold(projectName)} 成功！`))
-    console.log(chalk.green(`请进入项目目录 ${chalk.green.bold(projectName)} 开始工作吧！😝`))
+    console.log(chalk.green(`创建项目 ${chalk.green.bold(projectName)} 成功！`), '\n')
+    console.log(chalk.green(`请进入项目目录 ${chalk.green.bold(projectName)} 开始工作吧！😝`), '\n')
     if (typeof cb === 'function') {
       cb()
     }
