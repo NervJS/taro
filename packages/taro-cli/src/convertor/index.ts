096cef570cdb7e4da9b83b8a4219ea84c993355a
luckyadam <weitaozsh@gmail.com>
2019-05-29 16:13:15 +0800

    fix(cli): 普通文件经过编译器编译必须传入 isNormal

--- packages/taro-cli/src/convertor/index.ts
+++ packages/taro-cli/src/convertor/index.ts
@@ -407,6 +407,7 @@ export default class Convertor {
         const transformResult = wxTransformer({
           code,
           sourcePath: file,
+          isNormal: true,
           isTyped: REG_TYPESCRIPT.test(file)
         })
         const { ast, scriptFiles } = this.parseAst({
