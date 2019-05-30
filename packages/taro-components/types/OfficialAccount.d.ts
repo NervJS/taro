d3fd24feacfb42f9727a9349df86f42b61b93867
colder <colder@vitarn.com>
2019-05-30 10:53:44 +0800

    fix(components): 修复 OfficialAccount 事件 types

--- packages/taro-components/types/OfficialAccount.d.ts
+++ packages/taro-components/types/OfficialAccount.d.ts
@@ -1,16 +1,27 @@
 import { ComponentType } from 'react'
-import { StandardProps } from './common'
+import { StandardProps, CommonEventFunction } from './common'
+
+interface OfficialAccountEventDetail {
+  /**
+   * 状态码
+   */
+  status: number
+  /**
+   * 错误信息
+   */
+  errMsg: string
+}
 
 export interface OfficialAccountProps extends StandardProps {
 
   /**
    * 组件加载成功时触发
    */
-  onLoad?(): any,
+  onLoad?: CommonEventFunction<OfficialAccountEventDetail>,
   /**
    * 组件加载失败时触发
    */
-  onError?(): any
+  onError?: CommonEventFunction<OfficialAccountEventDetail>
 }
 
 declare const OfficialAccount: ComponentType<OfficialAccountProps>
