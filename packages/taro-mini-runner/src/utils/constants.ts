faf96dd8a5bc4293ef10e9ab7dab852154706c67
luckyadam <weitaozsh@gmail.com>
2019-06-05 16:42:23 +0800

    feat(taro-mini-runner): 生成多端类型文件

--- packages/taro-mini-runner/src/utils/constants.ts
+++ packages/taro-mini-runner/src/utils/constants.ts
@@ -20,6 +20,91 @@ export const enum BUILD_TYPES {
   QQ = 'qq'
 }
 
+export const enum TEMPLATE_TYPES {
+  WEAPP = '.wxml',
+  SWAN = '.swan',
+  ALIPAY = '.axml',
+  TT = '.ttml',
+  QUICKAPP = '.ux',
+  QQ = '.qml'
+}
+
+export const enum STYLE_TYPES {
+  WEAPP = '.wxss',
+  SWAN = '.css',
+  ALIPAY = '.acss',
+  TT = '.ttss',
+  QUICKAPP = '.css',
+  QQ = '.qss'
+}
+
+export const enum SCRIPT_TYPES {
+  WEAPP = '.js',
+  SWAN = '.js',
+  ALIPAY = '.js',
+  TT = '.js',
+  QUICKAPP = '.js',
+  QQ = '.js'
+}
+
+export const enum CONFIG_TYPES {
+  WEAPP = '.json',
+  SWAN = '.json',
+  ALIPAY = '.json',
+  TT = '.json',
+  QUICKAPP = '.json',
+  QQ = '.json'
+}
+
+export type IMINI_APP_FILE_TYPE = {
+  TEMPL: TEMPLATE_TYPES,
+  STYLE: STYLE_TYPES,
+  SCRIPT: SCRIPT_TYPES,
+  CONFIG: CONFIG_TYPES
+}
+
+export type IMINI_APP_FILES = {
+  [key: string]: IMINI_APP_FILE_TYPE
+}
+export const MINI_APP_FILES: IMINI_APP_FILES = {
+  [BUILD_TYPES.WEAPP]: {
+    TEMPL: TEMPLATE_TYPES.WEAPP,
+    STYLE: STYLE_TYPES.WEAPP,
+    SCRIPT: SCRIPT_TYPES.WEAPP,
+    CONFIG: CONFIG_TYPES.WEAPP
+  },
+  [BUILD_TYPES.SWAN]: {
+    TEMPL: TEMPLATE_TYPES.SWAN,
+    STYLE: STYLE_TYPES.SWAN,
+    SCRIPT: SCRIPT_TYPES.SWAN,
+    CONFIG: CONFIG_TYPES.SWAN
+  },
+  [BUILD_TYPES.ALIPAY]: {
+    TEMPL: TEMPLATE_TYPES.ALIPAY,
+    STYLE: STYLE_TYPES.ALIPAY,
+    SCRIPT: SCRIPT_TYPES.ALIPAY,
+    CONFIG: CONFIG_TYPES.ALIPAY
+  },
+  [BUILD_TYPES.TT]: {
+    TEMPL: TEMPLATE_TYPES.TT,
+    STYLE: STYLE_TYPES.TT,
+    SCRIPT: SCRIPT_TYPES.TT,
+    CONFIG: CONFIG_TYPES.TT
+  },
+  [BUILD_TYPES.QUICKAPP]: {
+    TEMPL: TEMPLATE_TYPES.QUICKAPP,
+    STYLE: STYLE_TYPES.QUICKAPP,
+    SCRIPT: SCRIPT_TYPES.QUICKAPP,
+    CONFIG: CONFIG_TYPES.QUICKAPP
+  },
+  [BUILD_TYPES.QQ]: {
+    TEMPL: TEMPLATE_TYPES.QQ,
+    STYLE: STYLE_TYPES.QQ,
+    SCRIPT: SCRIPT_TYPES.QQ,
+    CONFIG: CONFIG_TYPES.QQ
+  }
+}
+
 export const CONFIG_MAP = {
   [BUILD_TYPES.WEAPP]: {
     navigationBarTitleText: 'navigationBarTitleText',
