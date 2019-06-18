2f33f562b3ae97851729bf337ac3a7a1bc284413
Colder Xihk <cncolder@users.noreply.github.com>
2019-06-18 11:29:17 +0800

    fix(taro): 增加 Taro.getAccountInfoSync 类型定义 (#3433)

--- packages/taro/types/index.d.ts
+++ packages/taro/types/index.d.ts
@@ -9966,6 +9966,50 @@ declare namespace Taro {
   function reportAnalytics(eventName: string, data: any): void
 
   /**
+   * @since 2.2.2
+   *
+   * 获取当前帐号信息
+   *
+   * **示例代码：**
+   *
+   ```javascript
+   const accountInfo = wx.getAccountInfoSync();
+   console.log(accountInfo.miniProgram.appId) // 小程序 appId
+   console.log(accountInfo.plugin.appId) // 插件 appId
+   console.log(accountInfo.plugin.version) // 插件版本号， 'a.b.c' 这样的形式
+   ```
+   * @see https://developers.weixin.qq.com/miniprogram/dev/api/open-api/account-info/wx.getAccountInfoSync.html
+   */
+  function getAccountInfoSync(): getAccountInfoSync.Return
+
+  namespace getAccountInfoSync {
+    interface Return {
+      /**
+       * 小程序帐号信息
+       */
+      miniProgram: {
+        /**
+         * 小程序 appId
+         */
+        appId: string
+      }
+      /**
+       * 插件帐号信息（仅在插件中调用时包含这一项）
+       */
+      plugin?: {
+        /**
+         * 插件 appId
+         */
+        appId: string
+        /**
+         * 插件版本号
+         */
+        version: string
+      }
+    }
+  }
+
+  /**
    * @since 1.9.90
    *
    * 获取**全局唯一**的版本更新管理器，用于管理小程序更新。
