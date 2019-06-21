a6f4c0aece3ddfca11be80fb3a9210994a4f4be7
chenjiajian <798095202@qq.com>
2019-06-21 18:10:07 +0800

    fix(alipay): 修复在支付宝分包中状态管理工具不可用的问题，fix #3445

--- packages/taro-redux/src/utils/store.js
+++ packages/taro-redux/src/utils/store.js
@@ -4,6 +4,8 @@ const globalRef = Object.getPrototypeOf(appGlobal) || appGlobal
 export function getStore () {
   if (process.env.TARO_ENV === 'quickapp') {
     return globalRef.store || null
+  } else if (process.env.TARO_ENV === 'alipay') {
+    return my.taroReduxStore || null
   }
   return store
 }
@@ -11,6 +13,8 @@ export function getStore () {
 export function setStore (arg) {
   if (process.env.TARO_ENV === 'quickapp') {
     globalRef.store = arg
+  } else if (process.env.TARO_ENV === 'alipay') {
+    my.taroReduxStore = arg
   } else {
     store = arg
   }
