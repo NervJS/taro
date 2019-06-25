eb2511a0bf9bae823548cdf10a95dd23dd4ba34a
luckyadam <weitaozsh@gmail.com>
2019-06-25 17:12:49 +0800

    feat(taro): 调整小程序和 H5 的编译配置

--- packages/taro-cli/src/util/index.ts
+++ packages/taro-cli/src/util/index.ts
@@ -11,7 +11,8 @@ import * as minimatch from 'minimatch'
 import * as t from 'babel-types'
 import * as yauzl from 'yauzl'
 import * as findWorkspaceRoot from 'find-yarn-workspace-root'
-import * as chokidar from 'chokidar';
+import * as chokidar from 'chokidar'
+import { ICopyArgOptions, ICopyOptions, TogglableOptions } from '@tarojs/taro/types/compile'
 
 import defaultBabelConfig from '../config/babel'
 import defaultUglifyConfig from '../config/uglify'
@@ -30,7 +31,6 @@ import {
   UX_EXT,
   NODE_MODULES
 } from './constants'
-import { ICopyArgOptions, ICopyOptions, TogglableOptions } from './types'
 import { callPluginSync } from './npm'
 
 const execSync = child_process.execSync
@@ -585,7 +585,7 @@ export function copyFiles (appPath: string, copyConfig: ICopyOptions | void) {
             }
           }
           copyFileSync(from, to, copyOptions)
-	        if (pattern.watch){
+	        if (pattern.watch) {
 		        const watcher = chokidar.watch(from,{
 			        persistent: true,
 			        ignoreInitial: true
