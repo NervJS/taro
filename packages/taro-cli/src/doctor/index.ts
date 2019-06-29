a6ce5d59f4c31325ebf5e38966e96f3e6a33fcc5
Kan Chen <chenkan5@huawei.com>
2019-06-29 09:45:02 +0800

    检查原子服务定义文件前增加对应用类型的判断，只对快应用进行检查

--- packages/taro-cli/src/doctor/index.ts
+++ packages/taro-cli/src/doctor/index.ts
@@ -2,14 +2,12 @@ import configValidator from './configValidator';
 import packageValidator from './packageValidator';
 import recommandValidator from './recommandValidator';
 import eslintValidator from './eslintValidator';
-import abilityXMLValidator from './abilityXMLValidator';
 
 export default {
   validators: [
     configValidator,
     packageValidator,
     recommandValidator,
-    eslintValidator,
-    abilityXMLValidator
+    eslintValidator
   ]
 }
