16b261366dd754551233c906db5dadf9a80143e4
Jay Fong <fjc0kb@gmail.com>
2019-07-01 14:52:58 +0800

    feat(taro-components): 完善 Form 组件的类型定义 (#2951)

--- packages/taro-components/types/Form.d.ts
+++ packages/taro-components/types/Form.d.ts
@@ -3,21 +3,49 @@ import { StandardProps, CommonEventFunction } from './common'
 
 export interface FormProps extends StandardProps {
   /**
-   * 是否返回 formId 用于发送模板消息
+   * 是否返回 `formId` 用于发送模板消息。
+   *
+   * @default false
    */
   reportSubmit?: boolean,
+
+  /**
+   * 等待一段时间（毫秒数）以确认 `formId` 是否生效。
+   * 如果未指定这个参数，`formId` 有很小的概率是无效的（如遇到网络失败的情况）。
+   * 指定这个参数将可以检测 `formId` 是否有效，
+   * 以这个参数的时间作为这项检测的超时时间。
+   * 如果失败，将返回 `requestFormId:fail` 开头的 `formId`。
+   *
+   * @default 0
+   *
+   * @since 2.6.2
+   */
+  reportSubmitTimeout?: number,
 
   /**
    * 携带 form 中的数据触发 submit 事件
    *
    * event.detail = {value : {'name': 'value'} , formId: ''}
    */
-  onSubmit?: CommonEventFunction,
+  onSubmit?: CommonEventFunction<{
+    /**
+     * 当点击 `<form>` 表单中 `form-type` 为 `submit` 的 `<button>` 组件时，
+     * 会将表单组件中的 `value` 值进行提交，
+     * 需要在表单组件中加上 `name` 来作为 `key`。
+     */
+    value?: {
+      [formItemName: string]: any,
+    },
+    /**
+     * 当 `reportSubmit` 为 `true` 时，返回 `formId` 用于发送模板消息。
+     */
+    formId?: string,
+  }>,
 
   /**
    * 表单重置时会触发 reset 事件
    */
-  onReset?: CommonEventFunction
+  onReset?: CommonEventFunction,
 }
 
 declare const Form: ComponentType<FormProps>
