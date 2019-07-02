552e8688e127e4f96bb8e936beddb24078540320
onelong <caoyunlong@idaddy.cn>
2019-07-02 21:43:07 +0800

    fix: memo对state的判断应该用逻辑或 (#3600)

--- packages/taro/src/memo.js
+++ packages/taro/src/memo.js
@@ -3,8 +3,8 @@ import { isFunction, objectIs } from './util'
 export function memo (component, propsAreEqual) {
   component.prototype.shouldComponentUpdate = function (nextProps, nextState) {
     return (
-      (isFunction(propsAreEqual) ? !propsAreEqual(this.props, nextProps) : !objectIs(this.props, nextProps)) &&
-      !objectIs(this.state, nextState)
+      !objectIs(this.state, nextState) ||
+      (isFunction(propsAreEqual) ? !propsAreEqual(this.props, nextProps) : !objectIs(this.props, nextProps))
     )
   }
 
