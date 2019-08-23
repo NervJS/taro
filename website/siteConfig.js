80d432db24896e3b314ad2108acade42ff2b5db2
Pines-Cheng <spider.cs.nuc@gmail.com>
2019-08-23 18:56:50 +0800

    docs: add docs version

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -31,8 +31,8 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'TaroDocs',
-  organizationName: 'O2Team',
+  projectName: 'taro',
+  organizationName: 'NervJS',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
