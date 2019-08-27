d0e0343844a1fe16b6975f53c7de396aeb7036f1
yuche <i@yuche.me>
2019-08-27 13:35:51 +0800

    refactor(with-weapp): 在运行时处理所有选项

--- packages/taro-with-weapp/src/index.ts
+++ packages/taro-with-weapp/src/index.ts
@@ -1,7 +1,7 @@
-import { Component, ComponentLifecycle, getEnv, internal_safe_set as safeSet } from '@tarojs/taro'
-import isEqual from 'lodash/isEqual'
+import { Component, ComponentLifecycle, internal_safe_set as safeSet } from '@tarojs/taro'
+import { lifecycles, lifecycleMap, TaroLifeCycles } from './lifecycle'
+import { bind, proxy, isEqual } from './utils'
 
-type WeappLifeCycle = () => void
 type Observer = (newProps, oldProps, changePath: string) => void
 
 interface ObserverProperties {
@@ -9,24 +9,20 @@ interface ObserverProperties {
   observer: string | Observer
 }
 
-interface WeappComponent<P, S> extends Component<P, S> {
-  created?: WeappLifeCycle
-  ready?: WeappLifeCycle
-  detached?: WeappLifeCycle
-  attached?: WeappLifeCycle
-  moved?: WeappLifeCycle,
-  onLoad?: WeappLifeCycle,
-  onShow?: WeappLifeCycle,
-  onReady?: WeappLifeCycle,
-  onHide?: WeappLifeCycle,
-  onUnload?: WeappLifeCycle,
-  globalData?: any,
-  setData: Function
+interface ComponentClass<P = {}, S = {}> extends ComponentLifecycle<P, S> {
+  new (props: P): Component<P, S>
+  externalClasses: Record<string, unknown>
+  defaultProps?: Partial<P>
   _observeProps?: ObserverProperties[]
 }
 
-interface ComponentClass<P = {}, S = {}> extends ComponentLifecycle<P, S> {
-  new (props: P): WeappComponent<P, S>
+interface WxOptions {
+  methods?: {
+    [key: string]: Function;
+  }
+  properties?: Record<string, unknown>
+  props?: Record<string, unknown>
+  data?: Record<string, unknown>
 }
 
 function defineGetter (component: Component, key: string, getter: string) {
@@ -49,29 +45,142 @@ function isFunction (o): o is Function {
   return typeof o === 'function'
 }
 
-export default function withWeapp (componentType: string, weappConf?: Object) {
-  const isComponent = componentType === 'Component'
+export default function withWeapp (weappConf: WxOptions) {
 
   return (ConnectComponent: ComponentClass) => {
-    class BaseComponent<_ = {}, S = {}> extends ConnectComponent {
+    class BaseComponent<P = {}, S = {}> extends ConnectComponent {
+      private _observeProps: ObserverProperties[] = []
+
+      // mixins 可以多次调用生命周期
+      private willMounts: Function[] = []
+
+      private didMounts: Function[] = []
+
+      private didHides: Function[] = []
+
+      private didShows: Function[] = []
+
+      private willUnmounts: Function[] = []
+
       constructor (props) {
         super(props)
-        if (Array.isArray(super._observeProps)) {
-          this._observeProps = super._observeProps
-        }
+        this.init(weappConf)
         defineGetter(this, 'data', 'state')
-        if (isComponent) {
-          defineGetter(this, 'properties', 'props')
+        defineGetter(this, 'properties', 'props')
+      }
+
+      private initProps (props: Object) {
+        for (const propKey in props) {
+          if (props.hasOwnProperty(propKey)) {
+            const propValue = props[propKey]
+            if (isFunction(propValue)) {
+              if (propValue.observer) {
+                this._observeProps.push({
+                  name: propKey,
+                  observer: propValue.observer
+                })
+              }
+            }
+            proxy(this, 'props', propKey)
+          }
         }
       }
 
-      private safeExecute = (func?: Function, ...arg: any[]) => {
-        if (func) func.apply(this, arg)
+      private init (options: WxOptions) {
+        for (const confKey in options) {
+          const confValue = options[confKey]
+          switch (confKey) {
+            case 'externalClasses':
+              break
+            case 'data':
+              this.state = confKey
+              const keys = Object.keys(this.state)
+              let i = keys.length
+              while (i--) {
+                const key = keys[i]
+                proxy(this, `state`, key)
+              }
+              break
+            case 'properties':
+              this.initProps(confValue)
+              break
+            case 'methods':
+              for (const key in confValue) {
+                const method = confValue[key]
+                this[key] = bind(method, this)
+              }
+              break
+            case 'behaviors':
+              // this.initMixins(confValue, options);
+              break
+            case 'lifetimes':
+              for (const key in confValue) {
+                const lifecycle = confValue[key]
+                this.initLifeCycles(key, lifecycle)
+              }
+              break
+            default:
+              if (lifecycles.has(confKey)) {
+                const lifecycle = options[confKey]
+                this.initLifeCycles(confKey, lifecycle)
+              } else if (isFunction(confValue)) {
+                this[confKey] = bind(confValue, this)
+              } else {
+                this[confKey] = confValue
+              }
+              break
+          }
+        }
       }
 
-      private executeComponentFunc = (func?: Function, ...args: any[]) => {
-        if (isComponent) {
-          this.safeExecute(func, args)
+      private initLifeCycles (lifecycleName: string, lifecycle: Function) {
+        for (const lifecycleKey in lifecycleMap) {
+          const cycleNames = lifecycleMap[lifecycleKey]
+          if (cycleNames.indexOf(lifecycleName) !== -1) {
+            switch (lifecycleKey) {
+              case TaroLifeCycles.DidHide:
+                this.didHides.push(lifecycle)
+                break
+              case TaroLifeCycles.DidMount:
+                this.didMounts.push(lifecycle)
+                break
+              case TaroLifeCycles.DidShow:
+                this.didShows.push(lifecycle)
+                break
+              case TaroLifeCycles.WillMount:
+                this.willMounts.push(lifecycle)
+                break
+              case TaroLifeCycles.WillUnmount:
+                this.willUnmounts.push(lifecycle)
+                break
+              default:
+                break
+            }
+          }
+        }
+
+        // mixins 不会覆盖已经设置的生命周期，加入到 this 是为了形如 this.created() 的调用
+        if (!isFunction(this[lifecycleName])) {
+          this[lifecycleName] = lifecycle
+        }
+      }
+
+      private safeExecute = (func?: Function, ...args: unknown[]) => {
+        if (isFunction(func)) func.apply(this, args)
+      }
+
+      private executeLifeCycles (funcs: Function[], ...args: unknown[]) {
+        for (const func of funcs) {
+          this.safeExecute(func, ...args)
+        }
+      }
+
+      public selectComponent = (...args: unknown[]) => {
+        if (this.$scope && this.$scope.selectComponent) {
+          this.$scope.selectComponent(...args)
+        } else {
+          // tslint:disable-next-line: no-console
+          console.error(`this.$scope 下没有 selectComponent 方法`)
         }
       }
 
@@ -80,127 +189,72 @@ export default function withWeapp (componentType: string, weappConf?: Object) {
         Object.keys(obj).forEach(key => {
           safeSet(state, key, obj[key])
         })
-        Object.assign(this.state, state)
         this.setState(state, callback)
       }
 
-      triggerEvent = (eventName: string, ...args: any[]) => {
-        if (typeof eventName !== 'string') {
-          throw new Error('triggerEvent 第一个参数必须是字符串')
-        }
-        const fullEventName = `on${eventName}`
-        if (getEnv() === 'WEB') {
-          const func = this.props[`on${eventName[0].slice(0, 1)}${eventName.slice(1)}`]
-          if (typeof func === 'function') {
-            func(...args)
-          }
-        } else {
-          this.$scope.triggerEvent(fullEventName.toLowerCase(), ...args)
+      public triggerEvent = (eventName: string, ...args: unknown[]) => {
+        const func = this.props[`on${eventName[0].slice(0, 1).toUpperCase()}${eventName.slice(1)}`]
+        if (isFunction(func)) {
+          func.apply(this, args)
         }
       }
 
-      componentWillReceiveProps (nextProps: _) {
-        if (Array.isArray(this._observeProps)) {
-          this._observeProps.forEach(({ name: key, observer }) => {
-            const prop = this.props[key]
-            const nextProp = nextProps[key]
-            // 小程序是深比较不同之后才 trigger observer
-            if (!isEqual(prop, nextProp)) {
-              if (typeof observer === 'string') {
-                const ob = this[observer]
-                if (isFunction(ob)) {
-                  ob.call(this, nextProp, prop, key)
-                }
-              } else if (isFunction(observer)) {
-                observer.call(this, nextProp, prop, key)
-              }
+      public componentWillMount () {
+        this.safeExecute(super.componentWillMount)
+        this.executeLifeCycles(this.willMounts, this.$router.params || {})
+      }
+
+      public componentDidMount () {
+        this.safeExecute(super.componentDidMount)
+        this.executeLifeCycles(this.didMounts)
+      }
+
+      public componentWillUnmount () {
+        this._observeProps.forEach(({ name: key, observer }) => {
+          const prop = this.props[key]
+          if (typeof observer === 'string') {
+            const ob = this[observer]
+            if (isFunction(ob)) {
+              ob.call(this, prop, prop, key)
             }
-          })
-        }
-        this.executeComponentFunc(super.componentWillReceiveProps)
+          } else if (isFunction(observer)) {
+            observer.call(this, prop, prop, key)
+          }
+        })
+        this.safeExecute(super.componentWillUnmount)
+        this.executeLifeCycles(this.willUnmounts)
       }
 
-      componentWillMount () {
-        if (Array.isArray(this._observeProps)) {
-          this._observeProps.forEach(({ name: key, observer }) => {
-            const prop = this.props[key]
+      public componentDidHide () {
+        this.safeExecute(super.componentDidHide)
+        this.executeLifeCycles(this.didHides)
+      }
+
+      public componentDidShow () {
+        this.safeExecute(super.componentDidShow)
+        this.executeLifeCycles(this.didShows, this.$router.params || {})
+      }
+
+      public componentWillReceiveProps (nextProps: P) {
+        this._observeProps.forEach(({ name: key, observer }) => {
+          const prop = this.props[key]
+          const nextProp = nextProps[key]
+          // 小程序是深比较不同之后才 trigger observer
+          if (!isEqual(prop, nextProp)) {
             if (typeof observer === 'string') {
               const ob = this[observer]
               if (isFunction(ob)) {
-                ob.call(this, prop, prop, key)
+                ob.call(this, nextProp, prop, key)
               }
             } else if (isFunction(observer)) {
-              observer.call(this, prop, prop, key)
+              observer.call(this, nextProp, prop, key)
             }
-          })
-        }
-        this.executeComponentFunc(this.created)
-        this.safeExecute(this.onLoad)
-        if (super.componentWillMount) {
-          super.componentWillMount.call(this, this.$router.params || {})
-        }
-      }
-
-      componentDidHide () {
-        this.safeExecute(this.onHide)
-        this.safeExecute(super.componentDidHide)
-      }
-
-      componentDidShow () {
-        this.safeExecute(this.onShow)
-        if (super.componentDidShow) {
-          super.componentDidShow.call(this, this.$router.params || {})
-        }
-      }
-
-      componentDidMount () {
-        this.executeComponentFunc(this.attached)
-        this.safeExecute(super.componentDidMount)
-        this.executeComponentFunc(this.ready)
-        this.safeExecute(this.onReady)
-      }
-
-      componentWillUnmount () {
-        this.safeExecute(super.componentWillUnmount)
-        this.safeExecute(this.onUnload)
-        this.executeComponentFunc(this.detached)
-      }
-    }
-
-    if (weappConf) {
-      for (const confKey in weappConf) {
-        if (weappConf.hasOwnProperty(confKey)) {
-          const confValue = weappConf[confKey]
-          if (confKey === 'data') {
-            BaseComponent.prototype.state = confValue
-          } else if (confKey === 'properties') {
-            for (const propKey in confValue) {
-              if (confValue.hasOwnProperty(propKey)) {
-                const propValue = confValue[propKey]
-                if (typeof propValue !== 'function') {
-                  if (propValue.value) {
-                    (BaseComponent as any).defaultProps = {
-                      [propKey]: propValue.value
-                    }
-                  }
-                  if (propValue.observer) {
-                    if (!BaseComponent.prototype._observeProps) {
-                      BaseComponent.prototype._observeProps = []
-                    }
-                    BaseComponent.prototype._observeProps.push({
-                      name: propKey,
-                      observer: propValue.observer
-                    })
-                  }
-                }
-              }
-            }
-          } else {
-            BaseComponent.prototype[confKey] = confValue
           }
-        }
+        })
+        this.safeExecute(super.componentWillReceiveProps)
       }
     }
+
     return BaseComponent
   }
 }
