d12a02731339abd2652ab8f66ff8d8debbc2328d
luckyadam <weitaozsh@gmail.com>
2019-09-16 20:02:47 +0800

    feat(mini-runner): 保证快应用文件生成正确

--- packages/taro-mini-runner/src/plugins/TaroLoadChunksPlugin.ts
+++ packages/taro-mini-runner/src/plugins/TaroLoadChunksPlugin.ts
@@ -4,59 +4,74 @@ import webpack, { compilation } from 'webpack'
 import { ConcatSource } from 'webpack-sources'
 import { urlToRequest } from 'loader-utils'
 
-import { PARSE_AST_TYPE, REG_STYLE } from '../utils/constants'
-
-import { ITaroFileInfo } from './MiniPlugin'
+import { PARSE_AST_TYPE, REG_STYLE, BUILD_TYPES } from '../utils/constants'
+import { promoteRelativePath } from '../utils'
 
 const PLUGIN_NAME = 'TaroLoadChunksPlugin'
 
 interface IOptions {
   commonChunks: string[],
-  taroFileTypeMap: ITaroFileInfo
+  buildAdapter: BUILD_TYPES
 }
 
 export default class TaroLoadChunksPlugin {
   commonChunks: string[]
-  taroFileTypeMap: ITaroFileInfo
+  buildAdapter: BUILD_TYPES
 
   constructor (options: IOptions) {
     this.commonChunks = options.commonChunks
+    this.buildAdapter = options.buildAdapter
   }
 
   apply (compiler: webpack.Compiler) {
     compiler.hooks.thisCompilation.tap(PLUGIN_NAME, (compilation: compilation.Compilation) => {
       let commonChunks
       compilation.hooks.afterOptimizeChunks.tap(PLUGIN_NAME, (chunks: compilation.Chunk[]) => {
-        commonChunks = chunks.filter(chunk => this.commonChunks.includes(chunk.name))
+        commonChunks = chunks.filter(chunk => this.commonChunks.includes(chunk.name)).reverse()
       })
       compilation.chunkTemplate.hooks.renderWithEntry.tap(PLUGIN_NAME, (modules, chunk) => {
-        if (chunk.entryModule && chunk.entryModule.miniType === PARSE_AST_TYPE.ENTRY) {
-          compilation.hooks.afterOptimizeAssets.tap(PLUGIN_NAME, assets => {
-            const files = chunk.files
-            files.forEach(item => {
-              if (REG_STYLE.test(item)) {
-                const source = new ConcatSource()
-                const _source = assets[item]._source
-                Object.keys(assets).forEach(assetName => {
-                  const fileName = path.basename(assetName, path.extname(assetName))
-                  if (REG_STYLE.test(assetName) && this.commonChunks.includes(fileName)) {
-                    source.add(`@import ${JSON.stringify(urlToRequest(assetName))}`)
-                    source.add('\n')
-                    source.add(_source)
-                    assets[item]._source = source
-                  }
-                })
-              }
+        if (chunk.entryModule) {
+          if (chunk.entryModule.miniType === PARSE_AST_TYPE.ENTRY) {
+            compilation.hooks.afterOptimizeAssets.tap(PLUGIN_NAME, assets => {
+              const files = chunk.files
+              files.forEach(item => {
+                if (REG_STYLE.test(item)) {
+                  const source = new ConcatSource()
+                  const _source = assets[item]._source
+                  Object.keys(assets).forEach(assetName => {
+                    const fileName = path.basename(assetName, path.extname(assetName))
+                    if (REG_STYLE.test(assetName) && this.commonChunks.includes(fileName)) {
+                      source.add(`@import ${JSON.stringify(urlToRequest(assetName))}`)
+                      source.add('\n')
+                      source.add(_source)
+                      assets[item]._source = source
+                    }
+                  })
+                }
+              })
             })
-          })
-          const source = new ConcatSource()
-          commonChunks.reverse().forEach(chunkItem => {
-            source.add(`require(${JSON.stringify(urlToRequest(chunkItem.name))});\n`)
-          })
-          source.add('\n')
-          source.add(modules)
-          source.add(';')
-          return source
+            const source = new ConcatSource()
+            const id = chunk.id
+            commonChunks.forEach(chunkItem => {
+              source.add(`require(${JSON.stringify(promoteRelativePath(path.relative(id, chunkItem.name)))});\n`)
+            })
+            source.add('\n')
+            source.add(modules)
+            source.add(';')
+            return source
+          } else if (this.buildAdapter === BUILD_TYPES.QUICKAPP &&
+            (chunk.entryModule.miniType === PARSE_AST_TYPE.PAGE ||
+            chunk.entryModule.miniType === PARSE_AST_TYPE.COMPONENT)) {
+            const source = new ConcatSource()
+            const id = chunk.id
+            commonChunks.forEach(chunkItem => {
+              source.add(`require(${JSON.stringify(promoteRelativePath(path.relative(id, chunkItem.name)))});\n`)
+            })
+            source.add('\n')
+            source.add(modules)
+            source.add(';')
+            return source
+          }
         }
       })
     })
