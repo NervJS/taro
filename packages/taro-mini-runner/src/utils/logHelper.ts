62b3a1ca7c67787cbaabbd6277ccb9eb88c37663
luckyadam <weitaozsh@gmail.com>
2019-09-18 21:19:31 +0800

    fix(jd): 修复合并代码导致的京东小程序的编译问题

--- packages/taro-mini-runner/src/utils/logHelper.ts
+++ packages/taro-mini-runner/src/utils/logHelper.ts
@@ -14,13 +14,16 @@ const LOG_MAP = {
     OPEN: '请打开支付宝小程序开发者工具进行查看'
   },
   [BUILD_TYPES.QQ]: {
-    OPEN: '请打开QQ 小程序开发者工具进行查看'
+    OPEN: '请打开 QQ 小程序开发者工具进行查看'
   },
   [BUILD_TYPES.SWAN]: {
     OPEN: '请打开百度智能小程序开发者工具进行查看'
   },
   [BUILD_TYPES.TT]: {
     OPEN: '请打开字节跳动小程序开发者工具进行查看'
+  },
+  [BUILD_TYPES.JD]: {
+    OPEN: '请打开京东小程序开发者工具进行查看'
   },
   [BUILD_TYPES.QUICKAPP]: {
     OPEN: '请按快应用端开发流程 https://taro-docs.jd.com/taro/docs/quick-app.html 进行查看'
