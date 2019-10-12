3696650ab0cc7f359228653e3d858bd83cc0cd2b
yuche <i@yuche.me>
2019-10-12 11:03:17 +0800

    fix(runtime): 设置 nodeValue 不会触发更新

--- packages/taro-runtime/src/dom/text.ts
+++ packages/taro-runtime/src/dom/text.ts
@@ -2,19 +2,28 @@ import { TaroNode } from './node'
 import { NodeType } from './node_types'
 
 export class TaroText extends TaroNode {
-  public nodeValue: string
+  private _value: string
 
   public constructor (text: string) {
     super(NodeType.TEXT_NODE, '#text')
-    this.nodeValue = text
+    this._value = text
   }
 
   public set textContent (text: string) {
-    this.nodeValue = text
+    this._value = text
     this.enqueueUpdate()
   }
 
   public get textContent () {
-    return this.nodeValue
+    return this._value
+  }
+
+  public set nodeValue (text: string) {
+    this._value = text
+    this.enqueueUpdate()
+  }
+
+  public get nodeValue () {
+    return this._value
   }
 }
