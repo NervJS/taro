76d278fc7ef372bbe61603573cc4e90b85ef0258
yuche <i@yuche.me>
2019-10-14 20:12:55 +0800

    feat(taro): h5 和小程序端支持 Fragment

--- packages/taro-components/src/components/block/index.js
+++ packages/taro-components/src/components/block/index.js
@@ -1,11 +1,3 @@
 import 'weui'
 import Nerv from 'nervjs'
-export default class Block extends Nerv.Component {
-  constructor (props) {
-    super(props)
-  }
-
-  render () {
-    return <div>{this.props.children}</div>
-  }
-}
+export default Nerv.Fragment
