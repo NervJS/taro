d722f3454249469e8c48e18a4c443a37d556800b
tigerHee <316031602@qq.com>
2019-10-25 14:58:05 +0800

    Taro.chooseVideo的验证Param中缺少参数补齐
    Taro.chooseVideo的验证Param中缺少参数补齐

--- packages/taro/types/api/media/media.video.d.ts
+++ packages/taro/types/api/media/media.video.d.ts
@@ -179,7 +179,36 @@ declare namespace Taro {
        * 拍摄视频最长拍摄时间，单位秒。最长支持 60 秒
        */
       maxDuration?: number
+      /**
+       * 默认拉起的是前置或者后置摄像头。部分 Android 手机下由于系统 ROM 不支持无法生效, 默认值 'back'
+       * 合法值: 'back', 'front'
+       */
+      camera?: string
+      /**
+       * 接口调用成功的回调函数
+       */
+      success?: ParamPropSuccess
+      /**
+       * 接口调用失败的回调函数
+       */
+      fail?: ParamPropFail
+      /**
+       * 接口调用结束的回调函数（调用成功、失败都会执行）
+       */
+      complete?: ParamPropComplete
     }
+    /**
+     * 接口调用成功的回调函数
+     */
+    type ParamPropSuccess = (res: { tempFilePath: string; duration: number; size: number; height: number; width: number }) => any
+    /**
+     * 接口调用失败的回调函数
+     */
+    type ParamPropFail = (err: any) => any
+    /**
+     * 接口调用结束的回调函数（调用成功、失败都会执行）
+     */
+    type ParamPropComplete = () => any
   }
   /**
    * 拍摄视频或从手机相册中选视频，返回视频的临时文件路径。
