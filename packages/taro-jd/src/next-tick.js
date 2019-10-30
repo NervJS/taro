80f938c33fa61de0b76c9fb64256201f89897a3c
luckyadam <weitaozsh@gmail.com>
2019-10-30 15:06:43 +0800

    chore: merge feat_mini_webpack

--- packages/taro-jd/src/next-tick.js
+++
@@ -1,7 +0,0 @@
-const nextTick = (fn, ...args) => {
-  fn = typeof fn === 'function' ? fn.bind(null, ...args) : fn
-  const timerFunc = jd.nextTick ? jd.nextTick : setTimeout
-  timerFunc(fn)
-}
-
-export default nextTick
