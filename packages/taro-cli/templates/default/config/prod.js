4cb31daf99e8ea51fdeebe61a2aeae1f748ae81f
Garfield Lee <openpgp@550.moe>
2019-11-21 11:03:19 +0800

    fix(template): 更新 Taro 2.0 的模版和下载地址 #4837

--- packages/taro-cli/templates/default/config/prod.js
+++ packages/taro-cli/templates/default/config/prod.js
@@ -2,9 +2,8 @@ module.exports = {
   env: {
     NODE_ENV: '"production"'
   },
-  defineConstants: {
-  },
-  weapp: {},
+  defineConstants: {},
+  mini: {},
   h5: {
     /**
      * 如果h5端编译后体积过大，可以使用webpack-bundle-analyzer插件对打包体积进行分析。
