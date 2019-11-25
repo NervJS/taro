729bcb4398f16d728f73ef99f562c61bcecf2cd3
yuche <i@yuche.me>
2019-11-25 22:04:49 +0800

    fix(loader): vue-loader 重复调用

--- packages/taro-loader/src/page.ts
+++ packages/taro-loader/src/page.ts
@@ -1,13 +1,20 @@
 import * as webpack from 'webpack'
-// import { getOptions } from 'loader-utils'
+import { getOptions } from 'loader-utils'
+import * as path from 'path'
 // import { Loader } from './loader'
 // import * as t from '@babel/types'
 // import traverse, { NodePath } from '@babel/traverse'
 // import { INJECT_PAGE_INSTANCE, CREATE_PAGE_CONFIG } from './constants'
 
 export default function (this: webpack.loader.LoaderContext) {
+  const options = getOptions(this)
+  // raw is a placeholder loader to locate changed .vue resource
+  const raw = path.join(__dirname, 'raw.js')
+  const componentPath = options.framework === 'vue'
+    ? `${raw}!${this.resourcePath}`
+    : this.request.split('!').slice(1).join('!')
   return `import { createPageConfig } from '@tarojs/runtime'
-import component from '${this.request.split('!').slice(1).join('!')}'
+import component from '${componentPath}'
 Page(createPageConfig(component))
 `
 }
