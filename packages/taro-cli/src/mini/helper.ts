1dca9f7c0e36776fdac1f3d849c6180961f0b791
pengzhouhu <pengzhouhu@huawei.com>
2019-11-28 21:08:17 +0800

    fix(cli): 修正快应用依赖项安装命令在windows不兼容问题 (#4954)

--- packages/taro-cli/src/mini/helper.ts
+++ packages/taro-cli/src/mini/helper.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs-extra'
 import * as path from 'path'
+import * as os from 'os'
 import { execSync } from 'child_process'
 
 import * as _ from 'lodash'
@@ -153,13 +154,26 @@ export async function prepareQuickAppEnvironment (buildData: IBuildData) {
     needInstall = true
   }
   if (needInstall) {
+    const isWindows = os.platform() === 'win32'
     let command
     if (shouldUseYarn()) {
-      command = 'NODE_ENV=development yarn install'
+      if(!isWindows) {
+        command = 'NODE_ENV=development yarn install'
+      } else {
+        command = 'yarn install'
+      }
     } else if (shouldUseCnpm()) {
-      command = 'NODE_ENV=development cnpm install'
+      if(!isWindows) {
+        command = 'NODE_ENV=development cnpm install'
+      } else {
+        command = 'cnpm install'
+      }
     } else {
-      command = 'NODE_ENV=development npm install'
+      if(!isWindows) {
+        command = 'NODE_ENV=development npm install'
+      } else {
+        command = 'npm install'
+      }
     }
     const installSpinner = ora(`安装快应用依赖环境, 需要一会儿...`).start()
     try {
