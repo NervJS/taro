01ff618f9785fa27a5f2b088a628ca2939c95698
Garfield Lee <openpgp@550.moe>
2019-12-05 17:38:15 +0800

    feat(taro-runner-utils): 添加 @tarojs/runner-utils 包

---
+++ packages/taro-runner-utils/types/scss.d.ts
@@ -0,0 +1,24 @@
+import { BundleResult } from 'scss-bundle';
+interface LoaderOption {
+    data?: string;
+    [key: string]: any;
+}
+interface BuildConfig {
+    sass?: {
+        resource?: string | string[];
+        projectDirectory?: string;
+        data?: string;
+    };
+    sassLoaderOption?: LoaderOption;
+}
+/**
+ * Return bundled sass content.
+ *
+ * @param {string} url Absolute file path.
+ * @param {(string | undefined)} projectDirectory Absolute project location, where node_modules are located. Used for resolving tilde imports.
+ * @returns Bundle result.
+ */
+export declare function getBundleResult(url: string, projectDirectory?: string | undefined): Promise<BundleResult>;
+export declare function getBundleContent(resource: string | string[], projectDirectory?: string | undefined): Promise<string | undefined>;
+export declare function getSassLoaderOption<T extends BuildConfig>({ sass, sassLoaderOption }: T): Promise<LoaderOption>;
+export default getSassLoaderOption;
