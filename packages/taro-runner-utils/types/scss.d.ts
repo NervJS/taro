ba5902abf14f4e4032c068fca17ce4f836b209fd
Garfield Lee <openpgp@550.moe>
2019-12-06 13:15:18 +0800

    feat(taro-runner-utils): 添加单元测试和优化注释

--- packages/taro-runner-utils/types/scss.d.ts
+++ packages/taro-runner-utils/types/scss.d.ts
@@ -15,10 +15,23 @@ interface BuildConfig {
  * Return bundled sass content.
  *
  * @param {string} url Absolute file path.
- * @param {(string | undefined)} projectDirectory Absolute project location, where node_modules are located. Used for resolving tilde imports.
+ * @param {(string | undefined)} projectDirectory Absolute project location, where node_modules are located.
+ * Used for resolving tilde imports.
  * @returns Bundle result.
  */
 export declare function getBundleResult(url: string, projectDirectory?: string | undefined): Promise<BundleResult>;
+/**
+ * Return bundled sass content, but input resource can be a single string or an array.
+ * @param {(string | string[])} resource Input file path or a path array.
+ * @param {(string | undefined)} projectDirectory Absolute project location, where node_modules are located.
+ * Used for resolving tilde imports.
+ * @returns Bundle result.
+ */
 export declare function getBundleContent(resource: string | string[], projectDirectory?: string | undefined): Promise<string | undefined>;
+/**
+ * Return the merged sass loader option.
+ * @param {BuildConfig} param0 Build config.
+ * @returns Merged sass loader option.
+ */
 export declare function getSassLoaderOption({ sass, sassLoaderOption }: BuildConfig): Promise<LoaderOption>;
 export default getSassLoaderOption;
