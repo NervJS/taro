b0ec706fcbecedf65a2767876475a4671bea4a3f
ZakaryCode <zakarycode@gmail.com>
2019-12-15 21:28:12 +0800

    feat: checkout from auto-docs

--- scripts/docs-api.ts
+++ scripts/docs-api.ts
@@ -55,7 +55,7 @@ export default function docsAPI (
 
   if (diff) {
     const canges = spawn('git', ['status', '-z'])
-  
+
     canges.stdout.on('data', (data) => {
       const ss = data.toString().trim().split(/\u0000|\s+/ig)
       ss.forEach(s => {
