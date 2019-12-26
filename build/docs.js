edb057b7e8d76191f651f3290b11dbb33a4b5947
luckyadam <weitaozsh@gmail.com>
2019-12-26 16:07:28 +0800

    chore: 确保文档发布过程中持续输出内容，避免 ci 失败

--- build/docs.js
+++ build/docs.js
@@ -3,8 +3,18 @@ const ghPages = require('gh-pages')
 const ora = require('ora')
 const spinner = ora('Publishing gitbooks...').start()
 
-cp.exec('npm run docs', err => {
-  if (!err) {
+const docs = cp.spawn('npm', ['run', 'docs'])
+
+docs.stdout.on('data', data => {
+  console.log(data)
+})
+
+docs.stderr.on('data', data => {
+  console.error(data)
+})
+
+docs.on('close', code => {
+  if (code === 0) {
     ghPages.publish('./website/build/taro', err => {
       if (!err) {
         spinner.succeed('Publish successfully.')
@@ -13,6 +23,10 @@ cp.exec('npm run docs', err => {
       }
     })
   } else {
-    spinner.fail(err)
+    spinner.fail(`文档站点编译出错，出错码 ${code}`)
   }
+})
+
+docs.on('error', error => {
+  console.log(error)
 })
