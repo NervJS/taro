d15686e63c8622a635830fb8d2d624fa5b66d418
jsonz1993 <jsonz@qq.com>
2020-01-12 23:16:37 +0800

    style(taro-cli): 默认模板代码格式化

--- packages/taro-cli/templates/default/src/pages/index/index.jsx
+++ packages/taro-cli/templates/default/src/pages/index/index.jsx
@@ -18,18 +18,18 @@ export default class <%= _.capitalize(pageName) %> extends Component {
 
   componentDidHide () { }
 
-  <%if (locals.typescript) {-%>
-    /**
-     * 指定config的类型声明为: Taro.Config
-     *
-     * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-     * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-     * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-     */
-  <%}-%>
-    config<%if (locals.typescript) {%>: Config<%}%> = {
-      navigationBarTitleText: '首页'
-    }
+<%if (locals.typescript) {-%>
+  /**
+   * 指定config的类型声明为: Taro.Config
+   *
+   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+   */
+<%}-%>
+  config<%if (locals.typescript) {%>: Config<%}%> = {
+    navigationBarTitleText: '首页'
+  }
 
   render () {
     return (
