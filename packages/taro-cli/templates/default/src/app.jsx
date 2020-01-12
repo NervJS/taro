bfb0c2db4086b3b9cc459f2943130e80e4c5ac91
jsonz1993 <jsonz@qq.com>
2020-01-12 22:48:52 +0800

    style(taro-cli): 修复默认模板不符合eslint(react/sort-comp)规则(#5302)

--- packages/taro-cli/templates/default/src/app.jsx
+++ packages/taro-cli/templates/default/src/app.jsx
@@ -15,26 +15,6 @@ import './app.<%= cssExt %>'
 
 class App extends Component {
 
-<%if (locals.typescript) {-%>
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
-<%}-%>
-  config<%if (locals.typescript) {%>: Config<%}%> = {
-    pages: [
-      'pages/index/index'
-    ],
-    window: {
-      backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'WeChat',
-      navigationBarTextStyle: 'black'
-    }
-  }
 
   componentDidMount () {}
 
@@ -44,6 +24,26 @@ class App extends Component {
 
   componentDidCatchError () {}
 
+  <%if (locals.typescript) {-%>
+    /**
+     * 指定config的类型声明为: Taro.Config
+     *
+     * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+     * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+     * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+     */
+  <%}-%>
+    config<%if (locals.typescript) {%>: Config<%}%> = {
+      pages: [
+        'pages/index/index'
+      ],
+      window: {
+        backgroundTextStyle: 'light',
+        navigationBarBackgroundColor: '#fff',
+        navigationBarTitleText: 'WeChat',
+        navigationBarTextStyle: 'black'
+      }
+    }
   // 在 App 类中的 render() 函数没有实际作用
   // 请勿修改此函数
   render () {
--- packages/taro-cli/templates/default/src/pages/index/index.jsx
+++ packages/taro-cli/templates/default/src/pages/index/index.jsx
@@ -8,19 +8,6 @@ import './<%= pageName %>.<%= cssExt %>'
 
 export default class <%= _.capitalize(pageName) %> extends Component {
 
-<%if (locals.typescript) {-%>
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
-<%}-%>
-  config<%if (locals.typescript) {%>: Config<%}%> = {
-    navigationBarTitleText: '首页'
-  }
-
   componentWillMount () { }
 
   componentDidMount () { }
@@ -30,6 +17,19 @@ export default class <%= _.capitalize(pageName) %> extends Component {
   componentDidShow () { }
 
   componentDidHide () { }
+
+  <%if (locals.typescript) {-%>
+    /**
+     * 指定config的类型声明为: Taro.Config
+     *
+     * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+     * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+     * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+     */
+  <%}-%>
+    config<%if (locals.typescript) {%>: Config<%}%> = {
+      navigationBarTitleText: '首页'
+    }
 
   render () {
     return (
