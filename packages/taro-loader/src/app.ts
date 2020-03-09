5a199c4fc79e3065b4f5fec5491258ee740b06c9
yuche <i@yuche.me>
2020-03-09 14:09:02 +0800

    refactor(loader): 使用  loader-utils 自带的工具处理平台路径差异

--- packages/taro-loader/src/app.ts
+++ packages/taro-loader/src/app.ts
@@ -1,7 +1,9 @@
 import * as webpack from 'webpack'
-import { getOptions } from 'loader-utils'
+import { getOptions, stringifyRequest } from 'loader-utils'
 
 export default function (this: webpack.loader.LoaderContext) {
+  const stringify = (s: string): string => stringifyRequest(this, s)
+
   const options = getOptions(this)
   const method = options.framework === 'vue' ? 'createVueApp' : 'createReactApp'
   const prerender = `
@@ -9,7 +11,7 @@ if (typeof PRERENDER !== 'undefined') {
   global._prerender = inst
 }`
   return `import { ${method} } from '@tarojs/runtime'
-import component from '${this.request.replace(/\\/g, '/').split('!').slice(1).join('!')}'
+import component from '${stringify(this.request.split('!').slice(1).join('!'))}'
 var inst = App(${method}(component))
 ${options.prerender ? prerender : ''}
 `
--- packages/taro-loader/src/page.ts
+++ packages/taro-loader/src/page.ts
@@ -1,20 +1,21 @@
 import * as webpack from 'webpack'
-import { getOptions } from 'loader-utils'
+import { getOptions, stringifyRequest } from 'loader-utils'
 import * as path from 'path'
 
 export default function (this: webpack.loader.LoaderContext) {
   const options = getOptions(this)
+  const stringify = (s: string): string => stringifyRequest(this, s)
   // raw is a placeholder loader to locate changed .vue resource
   const raw = path.join(__dirname, 'raw.js')
   const componentPath = options.framework === 'vue'
     ? `${raw}!${this.resourcePath}`
-    : this.request.replace(/\\/g, '/').split('!').slice(1).join('!')
+    : this.request.split('!').slice(1).join('!')
   const prerender = `
 if (typeof PRERENDER !== 'undefined') {
   global._prerender = inst
 }`
   return `import { createPageConfig } from '@tarojs/runtime'
-import component from '${componentPath}'
+import component from '${stringify(componentPath)}'
 var inst = Page(createPageConfig(component, '${options.name}'))
 ${options.prerender ? prerender : ''}
 `
