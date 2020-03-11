0deed537c98e6f34969fd84ebe46faac6a34ab43
chenjiajian <798095202@qq.com>
2020-03-11 23:54:29 +0800

    fix(h5): input 支持 v-model

--- packages/taro-components/h5/vue/components/input.vue
+++ packages/taro-components/h5/vue/components/input.vue
@@ -1,34 +1,26 @@
 <script>
+import { listeners } from '../mixins/listeners'
 export default {
   name: 'taro-input',
+  mixins: [listeners],
+  model: {
+    event: 'model'
+  },
   props: {
     focus: Boolean
   },
   render (createElement) {
     const self = this
     return createElement('taro-input-core', {
-        'class': 'hydrated',
-        'attrs': {
+        class: 'hydrated',
+        attrs: {
           'auto-focus': self.focus
         },
         'on': {
+          ...this.listeners,
           input (e) {
             self.$emit('input', e)
-          },
-          focus (e) {
-            self.$emit('focus', e)
-          },
-          blur (e) {
-            self.$emit('blur', e)
-          },
-          confirm (e) {
-            self.$emit('confirm', e)
-          },
-          change (e) {
-            self.$emit('change', e)
-          },
-          keydown (e) {
-            self.$emit('keydown', e)
+            self.$emit('model', e.target.value)
           }
         }
       }, this.$slots.default)
