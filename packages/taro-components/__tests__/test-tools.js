351854866bd1fa112e006219ba6ab0f58970787e
chenjiajian <798095202@qq.com>
2020-03-18 10:58:53 +0800

    feat(h5): 加入 View 测试用例

---
+++ packages/taro-components/__tests__/test-tools.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { waitForChange } from './utils'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const h = React.createElement
+
+export async function mount (node, wrapper) {
+  const app = React.createRef()
+  const ref = React.createRef()
+
+  class App extends React.Component {
+    constructor () {
+      super(...arguments)
+      const { type, props } = node
+      this.state = {
+        Component: type,
+        props
+      }
+    }
+
+    setProps = async (nextProps) => {
+      this.setState(prev => ({
+        props: {
+          ...prev.props,
+          ...nextProps
+        }
+      }))
+
+      await waitForChange(ref.current)
+      return Promise.resolve()
+    }
+
+    render () {
+      const { Component, props } = this.state
+      return <Component ref={ref} {...props} />
+    }
+  }
+
+  ReactDOM.render(<App ref={app} />, wrapper)
+
+  await waitForChange(ref.current)
+
+  return Promise.resolve({
+    node: ref.current,
+    setProps: app.current.setProps
+  })
+}
