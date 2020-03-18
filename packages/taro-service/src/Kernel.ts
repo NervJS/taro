28632f61308b9b741e432a173baa9b7731c18e5b
luckyadam <weitaozsh@gmail.com>
2020-03-19 01:42:24 +0800

    feat(service): 实现注入/执行插件方法

--- packages/taro-service/src/Kernel.ts
+++ packages/taro-service/src/Kernel.ts
@@ -3,21 +3,26 @@ import * as fs from 'fs-extra'
 import { EventEmitter } from 'events'
 
 import { merge } from 'lodash'
-
+import { AsyncSeriesWaterfallHook } from 'tapable'
 import { IProjectConfig, PluginItem } from '@tarojs/taro/types/compile'
 
 import {
   IPreset,
   IPlugin,
-  IPaths
+  IPaths,
+  IHook
 } from './utils/types'
 import {
   CONFIG_DIR_NAME,
   DEFAULT_CONFIG_FILE,
-  PluginType
+  PluginType,
+  DEFAULT_SOURCE_ROOT,
+  DEFAULT_OUTPUT_ROOT,
+  IS_EVENT_HOOK
 } from './utils/constants'
 import { mergePlugins, resolvePresetsOrPlugins, convertPluginsToObject } from './utils'
 import createBabelRegister from './utils/babelRegister'
+import Plugin from './Plugin'
 
 interface IKernelOptions {
   appPath: string
@@ -27,22 +32,31 @@ interface IKernelOptions {
 
 export default class Kernel extends EventEmitter {
   appPath: string
-  presets: IPlugin[]
   plugins: IPlugin[]
   paths: IPaths
   config: IProjectConfig
   configPath: string
   extraPlugins: IPlugin[]
+  hooks: {
+    [name: string]: IHook[]
+  }
+  methods: {
+    [name: string]: Function
+  }
 
   constructor (options: IKernelOptions) {
     super()
     this.init(options)
   }
 
-  init (options: IKernelOptions) {
+  async init (options: IKernelOptions) {
     this.appPath = options.appPath || process.cwd()
+    this.hooks = {}
+    this.methods = {}
     this.initConfig()
+    this.initPaths()
     this.initPresetsAndPlugins(options)
+    await this.applyPlugins('onReady')
   }
 
   initConfig () {
@@ -52,6 +66,14 @@ export default class Kernel extends EventEmitter {
       process.exit(0)
     }
     this.config = require(this.configPath)(merge)
+  }
+
+  initPaths () {
+    this.paths = {} as IPaths
+    this.paths.appPath = this.appPath
+    this.paths.configPath = this.configPath
+    this.paths.sourcePath = path.join(this.appPath, this.config.sourceRoot || DEFAULT_SOURCE_ROOT)
+    this.paths.outputPath = path.join(this.appPath, this.config.outputRoot || DEFAULT_OUTPUT_ROOT)
   }
 
   initPresetsAndPlugins (options: IKernelOptions) {
@@ -85,7 +107,8 @@ export default class Kernel extends EventEmitter {
 
   initPreset (preset: IPreset) {
     const { id, path, opts, apply } = preset
-    const { presets, plugins } = apply()(this, opts) || {}
+    const pluginCtx = this.initPluginCtx({id, path, ctx: this})
+    const { presets, plugins } = apply()(pluginCtx, opts) || {}
     this.registerPlugin(preset)
     if (Array.isArray(presets)) {
       const _presets = resolvePresetsOrPlugins(convertPluginsToObject(presets)(PluginType.Preset), PluginType.Preset)
@@ -101,13 +124,74 @@ export default class Kernel extends EventEmitter {
   initPlugin (plugin: IPlugin) {
     const { id, path, opts, apply } = plugin
     this.registerPlugin(plugin)
-    apply()(this, opts)
+    const pluginCtx = this.initPluginCtx({id, path, ctx: this})
+    apply()(pluginCtx, opts)
   }
 
   registerPlugin (plugin: IPlugin) {
     if (this.plugins[plugin.id]) {
-      throw new Error('插件已被注册')
+      throw new Error(`插件 ${plugin.id} 已被注册`)
     }
     this.plugins[plugin.id] = plugin
+  }
+
+  initPluginCtx ({ id, path, ctx }: { id: string, path: string, ctx: Kernel }) {
+    const pluginCtx = new Plugin({ id, path, ctx })
+    const internalMethods = ['onReady', 'onStart']
+    const kernelApis = ['appPath', 'plugins', 'paths', 'apply']
+    internalMethods.forEach(name => {
+      pluginCtx.registerMethod(name)
+    })
+    kernelApis.forEach(name => {
+      pluginCtx[name] = typeof this[name] === 'function' ? this[name].bind(this) : this[name]
+    })
+    Object.keys(this.methods).forEach(name => {
+      if (typeof this.methods[name] === 'function') {
+        pluginCtx[name] = this.methods[name]
+      }
+    })
+    return pluginCtx
+  }
+
+  async applyPlugins (args: string | { name: string, initialVal?: any, opts?: any }) {
+    let name
+    let initialVal
+    let opts
+    if (typeof args === 'string') {
+      name = args
+    } else {
+      name = opts.name
+      initialVal = args.initialVal
+      opts = args.opts
+    }
+    if (typeof name !== 'string') {
+      throw new Error(`调用失败，未传入正确的名称！`)
+    }
+    const hooks = this.hooks[name] || []
+    const waterfall = new AsyncSeriesWaterfallHook(['arg'])
+    if (hooks.length) {
+      if (IS_EVENT_HOOK.test(name)) {
+        for (const hook of hooks) {
+          waterfall.tapPromise({
+            name,
+            stage: hook.stage || 0,
+            before: hook.before
+          }, async () => {
+            await hook.fn(opts)
+          })
+        }
+      } else {
+        for (const hook of hooks) {
+          waterfall.tapPromise({
+            name,
+            stage: hook.stage || 0,
+            before: hook.before
+          }, async (arg) => {
+            return await hook.fn(arg, opts)
+          })
+        }
+      }
+    }
+    await waterfall.promise(initialVal)
   }
 }
--- packages/taro-service/src/Plugin.ts
+++ packages/taro-service/src/Plugin.ts
@@ -1,9 +1,43 @@
-export class Plugin {
+import Kernel from './Kernel'
+import { IHook } from './utils/types'
+
+export default class Plugin {
   id: string
   path: string
-  args: any
-  apply: () => any
-  constructor () {
+  ctx: Kernel
+  constructor (opts) {
+    this.id = opts.id
+    this.path = opts.path
+    this.ctx = opts.ctx
+  }
 
+  register (hook: IHook) {
+    if (typeof hook.name !== 'string') {
+      throw new Error(`插件 ${this.id} 中注册 hook 失败， hook.name 必须是 string 类型`)
+    }
+    if (typeof hook.fn !== 'function') {
+      throw new Error(`插件 ${this.id} 中注册 hook 失败， hook.fn 必须是 function 类型`)
+    }
+    const hooks = this.ctx.hooks[hook.name] || []
+    this.ctx.hooks[hook.name] = hooks.concat(hook)
+  }
+
+  registerMethod (args: string | { name: string, fn?: Function }) {
+    let name, fn
+    if (typeof args === 'string') {
+      name = args
+    } else {
+      name = args.name
+      fn = args.fn
+    }
+    if (this.ctx.methods[name]) {
+      throw `已存在方法 ${name}`
+    }
+    this.ctx.methods[name] = fn || function (fn: Function) {
+      this.register({
+        name,
+        fn
+      })
+    }.bind(this)
   }
 }
