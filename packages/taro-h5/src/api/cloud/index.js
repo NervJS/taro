8d04530834b850d9955521b9697177d7d981a326
shenghanqin <shenghanqin@hujiang.com>
2020-03-19 10:41:18 +0800

    feat(taro-h5):  增加H5版本云开发的支持基于tcb-js-sdk

--- packages/taro-h5/src/api/cloud/index.js
+++ packages/taro-h5/src/api/cloud/index.js
@@ -1,27 +1,6 @@
+// 说明文档 https://github.com/TencentCloudBase/tcb-js-sdk
+// TCB 提供开发应用所需服务和基础设施。TCB JS SDK 让你可以在网页端使用 JavaScript 代码服务访问 TCB 的的服务。你可以使用该 SDK 构建自己的公众号页面或者独立的网站等 Web 服务。
+
 const TcbSdk = require('tcb-js-sdk')
 
-function wxCloud() {
-  const wxC = TcbSdk || {}
-  const wxcloud = {}
-  const apiList = [
-    'init',
-    'auth',
-    'database',
-    'uploadFile',
-    'downloadFile',
-    'getTempFileURL',
-    'deleteFile',
-    'callFunction'
-    // 'CloudID'
-  ]
-  apiList.forEach(v => {
-    wxcloud[v] = wxC[v]
-  })
-
-  console.log('wxcloud :', wxcloud);
-  return wxcloud
-}
-
-
-export const cloud = wxCloud()
-
+export const cloud = TcbSdk
