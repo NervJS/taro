615bea5c52f195a59fc91b72863afa5883342e1a
luckyadam <weitaozsh@gmail.com>
2020-03-24 15:36:19 +0800

    feat(service): 增加 Hooks 执行类型

--- packages/taro-service/src/utils/constants.ts
+++ packages/taro-service/src/utils/constants.ts
@@ -5,6 +5,8 @@ export const PRESET_PREFIX = '@tarojs/preset-'
 export const PLUGIN_PREFIX = '@tarojs/plugin-'
 
 export const IS_EVENT_HOOK = /^on/
+export const IS_ADD_HOOK = /^add/
+export const IS_MODIFY_HOOK = /^modify/
 
 export const presetOrPluginPrefixReg = new RegExp(`^${PRESET_PREFIX}|${PLUGIN_PREFIX}`)
 
