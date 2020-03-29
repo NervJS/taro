dacdef6d10181791a4a9065ea0ce3804305a334f
skywalker <houzhenhong@hotmail.com>
2020-03-30 00:43:04 +0800

    fix(mini-runner): windows 下的路径问题  (#5839)
    * fix(mini-runner): prerender app 装载时没有捕捉错误
    
    * fix(mini-runner): windows 下的路径问题
    
    在 windows 下 getRealPath 生成的路径为 \xx\xx 在转换字符串的时候会被转义

--- packages/taro-mini-runner/src/prerender/prerender.ts
+++ packages/taro-mini-runner/src/prerender/prerender.ts
@@ -112,10 +112,15 @@ export class Prerender {
     await this.writeScript('app')
 
     if (!this.appLoaded) {
-      this.vm.run(`
+      try {
+        this.vm.run(`
         const app = require('${this.getRealPath('app')}')
         app.onLaunch()
       `, this.outputPath)
+      } catch (error) {
+        printPrerenderFail('app')
+        console.error(error)
+      }
       this.appLoaded = true
       await Promise.resolve()
     }
@@ -134,7 +139,7 @@ export class Prerender {
   }
 
   private getRealPath (path: string, ext = '.js') {
-    return join(this.outputPath, path + ext)
+    return join(this.outputPath, path + ext).replace(/\\/g, '\\\\')
   }
 
   private buildSandbox () {
