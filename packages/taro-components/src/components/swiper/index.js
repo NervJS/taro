9bd9e0354e3f31a06a937880c906a6b257149ad7
ZakaryCode <zakarycode@gmail.com>
2020-04-01 14:36:55 +0800

    feat: swiper support 「pre/next」margin fix #1392

--- packages/taro-components/src/components/swiper/index.js
+++ packages/taro-components/src/components/swiper/index.js
@@ -59,7 +59,7 @@ class Swiper extends Nerv.Component {
     const that = this
     const opt = {
       // 指示器
-      pagination: { el: `.taro-swiper-${this._id} .swiper-pagination` },
+      pagination: { el: `.taro-swiper-${this._id} .swiper-container .swiper-pagination` },
       direction: vertical ? 'vertical' : 'horizontal',
       loop: circular,
       slidesPerView: parseFloat(displayMultipleItems, 10),
@@ -119,13 +119,15 @@ class Swiper extends Nerv.Component {
     if (this.mySwiper) {
       const nextCurrent = typeof nextProps.current === 'number' ? nextProps.current : this._$current || 0
 
-      // 是否衔接滚动模式
-      if (nextProps.circular) {
-        this.mySwiper.loopDestroy()
-        this.mySwiper.loopCreate()
-        this.mySwiper.slideToLoop(parseInt(nextCurrent, 10)) // 更新下标
-      } else {
-        this.mySwiper.slideTo(parseInt(nextCurrent, 10)) // 更新下标
+      if (!this.mySwiper.isBeginning && !this.mySwiper.isEnd) {
+        // 是否衔接滚动模式
+        if (nextProps.circular) {
+          this.mySwiper.loopDestroy()
+          this.mySwiper.loopCreate()
+          this.mySwiper.slideToLoop(parseInt(nextCurrent, 10)) // 更新下标
+        } else {
+          this.mySwiper.slideTo(parseInt(nextCurrent, 10)) // 更新下标
+        }
       }
 
       // 判断是否需要停止或开始自动轮播
@@ -168,11 +170,23 @@ class Swiper extends Nerv.Component {
     typeof func === 'function' && func(e)
   }
 
+  parsePX (s = '0px') {
+    return parseFloat(s.replace(/r*px/i, ''), 10)
+  }
+
   render () {
-    const { className, style, indicatorColor, indicatorActiveColor } = this.props
+    const { className, style, vertical, previousMargin, nextMargin, indicatorColor, indicatorActiveColor } = this.props
     let defaultIndicatorColor = indicatorColor || 'rgba(0, 0, 0, .3)'
     let defaultIndicatorActiveColor = indicatorActiveColor || '#000'
-    const cls = classNames(`taro-swiper-${this._id}`, 'swiper-container', className)
+    const cls = classNames(`taro-swiper-${this._id}`, className)
+    const sty = Object.assign({
+      paddingTop: vertical ? this.parsePX(previousMargin) : 0,
+      paddingRight: vertical ? 0 : this.parsePX(nextMargin),
+      paddingBottom: vertical ? this.parsePX(nextMargin) : 0,
+      paddingLeft: vertical ? 0 : this.parsePX(previousMargin),
+      overflow: 'hidden'
+    }, style)
+    console.log(sty)
     const paginationCls = classNames(
       'swiper-pagination',
       {
@@ -181,17 +195,19 @@ class Swiper extends Nerv.Component {
       }
     )
     return (
-      <div className={cls} style={style} ref={(el) => { this.$el = el }}>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `<style type='text/css'>
-            .taro-swiper-${this._id} .swiper-pagination-bullet { background: ${defaultIndicatorColor} }
-            .taro-swiper-${this._id} .swiper-pagination-bullet-active { background: ${defaultIndicatorActiveColor} }
-            </style>`
-          }}
-        />
-        <div className='swiper-wrapper'>{this.props.children}</div>
-        <div className={paginationCls} />
+      <div className={cls} style={sty}>
+        <div className='swiper-container' style={{ overflow: 'visible' }} ref={(el) => { this.$el = el }}>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `<style type='text/css'>
+              .taro-swiper-${this._id} .swiper-container .swiper-pagination-bullet { background: ${defaultIndicatorColor} }
+              .taro-swiper-${this._id} .swiper-container .swiper-pagination-bullet-active { background: ${defaultIndicatorActiveColor} }
+              </style>`
+            }}
+          />
+          <div className='swiper-wrapper'>{this.props.children}</div>
+          <div className={paginationCls} />
+        </div>
       </div>
     )
   }
