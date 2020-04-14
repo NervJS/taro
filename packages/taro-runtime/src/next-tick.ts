cc662a117e12e7da27ace7d45e8b6266429ada65
alexloading <guanmingzeng@gmail.com>
2020-04-14 11:47:05 +0800

    feat(runtime): nextTick适配小程序setData回调

--- packages/taro-runtime/src/next-tick.ts
+++ packages/taro-runtime/src/next-tick.ts
@@ -1,3 +1,32 @@
-export const nextTick = (cb: Function) => {
-  setTimeout(cb, 1)
+import { Current } from './current'
+import { getPath } from './dsl/common'
+import { TaroRootElement } from './dom/root'
+import { document } from './bom/document'
+import { ensure } from '@tarojs/shared'
+
+function removeLeadingSlash (path?: string) {
+  if (path == null) {
+    return ''
+  }
+  return path.charAt(0) === '/' ? path.slice(1) : path
+}
+
+export const nextTick = (cb: Function, ctx?: Record<string, any>) => {
+  const hasSetDataCallbacks = ['weapp', 'swan', 'qq', 'alipay', 'tt']
+  if (~hasSetDataCallbacks.indexOf(process.env.TARO_ENV || '')) {
+    let pageElement: TaroRootElement | null = null
+    const router = Current.router
+    if (router) {
+      const path = getPath(removeLeadingSlash(router.path), router.params)
+      pageElement = document.getElementById<TaroRootElement>(path)
+      ensure(pageElement !== null, '没有找到页面实例。')
+      if (pageElement) {
+        pageElement.enqueueUpdateCallbak(cb, ctx)
+      }
+    }
+  } else {
+    setTimeout(function () {
+      ctx ? cb.call(ctx) : cb()
+    }, 1)
+  }
 }
