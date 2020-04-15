9be1fc4e99b9fad69a5bc4e6dc7412a5d897fe08
yuche <i@yuche.me>
2020-04-15 19:08:06 +0800

    fix(runtime): 解析 html 属性有多个 = 号时出错，close #6003

--- packages/taro-runtime/src/dom/html/parser.ts
+++ packages/taro-runtime/src/dom/html/parser.ts
@@ -86,6 +86,13 @@ function getTagName (tag: string) {
   return 'view'
 }
 
+function splitEqual (str: string) {
+  const sep = '='
+  const idx = str.indexOf(sep)
+  if (idx === -1) return [str]
+  return [str.slice(0, idx), str.slice(idx + sep.length)]
+}
+
 function format (children: ChildNode[]) {
   return children.filter(child => {
     if (child.type === 'comment') {
@@ -107,13 +114,13 @@ function format (children: ChildNode[]) {
     el.className = child.tagName
     for (let i = 0; i < child.attributes.length; i++) {
       const attr = child.attributes[i]
-      const [key, ...value] = attr.split('=')
+      const [key, value] = splitEqual(attr)
       if (key === 'class') {
         el.className += el.className
       } else if (key[0] === 'o' && key[1] === 'n') {
         continue
       } else {
-        el.setAttribute(key, value == null ? true : unquote(value.join('')))
+        el.setAttribute(key, value == null ? true : unquote(value))
       }
     }
 
