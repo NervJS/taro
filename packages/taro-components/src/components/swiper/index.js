43bc09129f276314be3e2630d82d7247d5c3e228
ZakaryCode <zakarycode@gmail.com>
2020-04-20 15:15:14 +0800

    fix: autoplay 非中断条件下，自动重启

--- packages/taro-components/src/components/swiper/index.js
+++ packages/taro-components/src/components/swiper/index.js
@@ -69,7 +69,7 @@ class Swiper extends Nerv.Component {
       observeParents: true,
       on: {
         slideChange () {
-          let e = createEvent('touchend')
+          const e = createEvent('touchend')
           try {
             Object.defineProperty(e, 'detail', {
               enumerable: true,
@@ -82,7 +82,7 @@ class Swiper extends Nerv.Component {
           that.handleOnChange(e)
         },
         transitionEnd () {
-          let e = createEvent('touchend')
+          const e = createEvent('touchend')
           try {
             Object.defineProperty(e, 'detail', {
               enumerable: true,
@@ -149,7 +149,7 @@ class Swiper extends Nerv.Component {
           autoplay.stop()
         }
       }
-      if (!autoplay.paused) {
+      if (nextProps.autoplay && !autoplay.paused) {
         autoplay.run()
         autoplay.paused = false
       }
@@ -190,8 +190,8 @@ class Swiper extends Nerv.Component {
 
   render () {
     const { className, style, vertical, previousMargin, nextMargin, indicatorColor, indicatorActiveColor } = this.props
-    let defaultIndicatorColor = indicatorColor || 'rgba(0, 0, 0, .3)'
-    let defaultIndicatorActiveColor = indicatorActiveColor || '#000'
+    const defaultIndicatorColor = indicatorColor || 'rgba(0, 0, 0, .3)'
+    const defaultIndicatorActiveColor = indicatorActiveColor || '#000'
     const cls = classNames(`taro-swiper-${this._id}`, className)
     const sty = Object.assign({
       paddingTop: vertical ? this.parsePX(previousMargin) : 0,
