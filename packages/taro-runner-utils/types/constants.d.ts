65263a45e3dd68165933e1ccc726dfa98f3a68ba
chenjiajian <798095202@qq.com>
2020-05-09 11:36:12 +0800

    fix(webpack-runner): fix ci
    * babel-preset-taro 为 babel runtime 设置了绝对路径，这会影响 webpack 的 moduleName，从而影响测试用例通过。

--- packages/taro-runner-utils/types/constants.d.ts
+++ packages/taro-runner-utils/types/constants.d.ts
@@ -189,12 +189,12 @@ export declare enum META_TYPE {
     CONFIG = "CONFIG"
 }
 export declare enum PARSE_AST_TYPE {
-  ENTRY = 'ENTRY',
-  PAGE = 'PAGE',
-  COMPONENT = 'COMPONENT',
-  NORMAL = 'NORMAL',
-  STATIC = 'STATIC',
-  EXPORTS = 'EXPORTS'
+    ENTRY = "ENTRY",
+    PAGE = "PAGE",
+    COMPONENT = "COMPONENT",
+    NORMAL = "NORMAL",
+    STATIC = "STATIC",
+    EXPORTS = "EXPORTS"
 }
 export declare const enum processTypeEnum {
     START = "start",
