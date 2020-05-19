771bd51bb3c81d9f6d8037aa3bcf80b71b9f4030
luckyadam <weitaozsh@gmail.com>
2020-05-19 11:52:41 +0800

    refactor(cli): 更新配置检测 && 配置检测出错时不中断编译

--- packages/taro-cli/src/presets/commands/build.ts
+++ packages/taro-cli/src/presets/commands/build.ts
@@ -43,7 +43,6 @@ export default (ctx) => {
             lineChalk(line.desc)
           )
         })
-        return
       }
       if (typeof platform !== 'string') {
         console.log(chalk.red('请传入正确的编译类型！'))
