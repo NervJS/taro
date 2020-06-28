ea9bd13ffd32fb594869162198bdbb4e666a0f27
colder <colder@vitarn.com>
2020-06-28 17:06:10 +0800

    fix: expose useReady hook

--- packages/taro-api/src/index.js
+++ packages/taro-api/src/index.js
@@ -24,6 +24,7 @@ import {
   useTitleClick,
   useOptionMenuClick,
   usePullIntercept,
+  useReady,
   useRouter,
   options,
   nextTick,
@@ -56,6 +57,7 @@ const Taro = {
   useTitleClick,
   useOptionMenuClick,
   usePullIntercept,
+  useReady,
   useRouter,
   options,
   nextTick
