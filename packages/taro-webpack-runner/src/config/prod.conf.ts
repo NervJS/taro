f34c3a3f431644dc963e69626ffe9403f5ca2bf1
chenjiajian <798095202@qq.com>
2020-07-01 16:26:56 +0800

    fix(h5): 增加dist-h5导出，修复h5项目编译报错，fix #6843
    by @w91

--- packages/taro-webpack-runner/src/config/prod.conf.ts
+++ packages/taro-webpack-runner/src/config/prod.conf.ts
@@ -125,7 +125,7 @@ export default function (appPath: string, config: Partial<BuildConfig>): any {
   }
 
   if (config.framework === FRAMEWORK_MAP.REACT || config.framework === FRAMEWORK_MAP.NERV) {
-    alias['@tarojs/components$'] = '@tarojs/components/h5/react}'
+    alias['@tarojs/components$'] = '@tarojs/components/dist-h5/react'
   }
 
   chain.merge({
