2aa8d47de18836ad54befc2a1c3fd952d15c86c6
Spencer17x <1253478653@qq.com>
2020-07-03 16:10:13 +0800

    fix: 修复getCurrentInstance方法类型声明#6904

--- packages/taro/types/taro.extend.d.ts
+++ packages/taro/types/taro.extend.d.ts
@@ -106,7 +106,7 @@ declare namespace Taro {
 
   function setIsUsingDiff (flag: boolean)
 
-  const Current: {
+  interface Current {
     app: AppInstance | null,
     router: RouterInfo | null,
     page: PageInstance | null,
@@ -114,4 +114,6 @@ declare namespace Taro {
     onHide: string,
     onShow: string
   }
+
+  function getCurrentInstance(): Current
 }
