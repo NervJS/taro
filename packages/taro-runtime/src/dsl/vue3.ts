ba247ebd57b50a8091f91f5f1a2c3c093f136838
chenjiajian <798095202@qq.com>
2020-07-28 14:14:10 +0800

    fix(runtime): 修复字节跳动小程序白屏(vue3)

--- packages/taro-runtime/src/dsl/vue3.ts
+++ packages/taro-runtime/src/dsl/vue3.ts
@@ -1,5 +1,5 @@
 import { isFunction, isArray, ensure, capitalize, toCamelCase, internalComponents, hasOwn, isBooleanStringLiteral } from '@tarojs/shared'
-import { AppInstance } from './instance'
+import { AppInstance, Instance, PageProps } from './instance'
 import { Current } from '../current'
 import { injectPageInstance } from './common'
 import { isBrowser } from '../env'
@@ -18,6 +18,8 @@ import type { Reconciler } from '../reconciler'
 
 function createVue3Page (h: typeof createElement, id: string) {
   return function (component): VNode {
+    // vue3 组件 created 时机比小程序页面 onShow 慢，这里先插入一个实例以响应初始化时的小程序生命周期调用
+    injectPageInstance(({ $options: component } as Instance<PageProps>), id)
     const inject = {
       props: {
         tid: String
@@ -114,51 +116,65 @@ export function createVue3App (app: App<TaroElement>, h: typeof createElement, c
     return pages.slice()
   }
 
-  class AppConfig implements AppInstance {
-    config = config
-
-    onLaunch (options) {
-      Current.router = {
-        params: options?.query,
-        ...options
-      }
-      appInstance = app.mount('#app')
-      const onLaunch = appInstance?.$options?.onLaunch
-      isFunction(onLaunch) && onLaunch.call(appInstance, options)
-    }
-
-    onShow (options) {
-      Current.router = {
-        params: options?.query,
-        ...options
-      }
-      const onShow = appInstance?.$options?.onShow
-      isFunction(onShow) && onShow.call(appInstance, options)
-    }
-
-    onHide (options: unknown) {
-      const onHide = appInstance?.$options?.onHide
-      isFunction(onHide) && onHide.call(appInstance, options)
-    }
-
-    mount = (component: Component, id: string, cb: () => void) => {
+  const appConfig: AppInstance = Object.create({
+    mount (component: Component, id: string, cb: () => void) {
       const page = createVue3Page(h, id)(component)
       pages.push(page)
       this.updateAppInstance(cb)
-    }
+    },
 
-    unmount = (id: string, cb: () => void) => {
+    unmount (id: string, cb: () => void) {
       pages = pages.filter(page => page.key !== id)
       this.updateAppInstance(cb)
-    }
+    },
 
     updateAppInstance (cb?: (() => void | undefined)) {
       appInstance.$forceUpdate()
       appInstance.$nextTick(cb)
     }
-  }
+  }, {
+    config: {
+      writable: true,
+      enumerable: true,
+      configurable: true,
+      value: config
+    },
 
-  Current.app = new AppConfig()
+    onLaunch: {
+      enumerable: true,
+      value (options) {
+        Current.router = {
+          params: options?.query,
+          ...options
+        }
+        appInstance = app.mount('#app')
+        const onLaunch = appInstance?.$options?.onLaunch
+        isFunction(onLaunch) && onLaunch.call(appInstance, options)
+      }
+    },
+
+    onShow: {
+      enumerable: true,
+      value (options) {
+        Current.router = {
+          params: options?.query,
+          ...options
+        }
+        const onShow = appInstance?.$options?.onShow
+        isFunction(onShow) && onShow.call(appInstance, options)
+      }
+    },
+
+    onHide: {
+      enumerable: true,
+      value (options) {
+        const onHide = appInstance?.$options?.onHide
+        isFunction(onHide) && onHide.call(appInstance, options)
+      }
+    }
+  })
+
+  Current.app = appConfig
 
   return Current.app
 }
