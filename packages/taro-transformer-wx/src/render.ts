e3cc4d497e351404d6c8b6aeab1d6571e4268fa8
luckyadam <weitaozsh@gmail.com>
2020-07-30 16:39:28 +0800

    fix(transformer-wx): 修复 render.ts ts 报错

--- packages/taro-transformer-wx/src/render.ts
+++ packages/taro-transformer-wx/src/render.ts
@@ -2145,12 +2145,12 @@ export class RenderParser {
                 }
                 setJSXAttr(component.node, Adapter.for, t.stringLiteral(`{{${forExpr}}}`))
               }
-            } if (Adapters.swan === Adapter.type) {
+            } else if (Adapters.swan === Adapter.type) {
               const attributes = component.node.openingElement.attributes
-              const keyAttribute: t.JSXAttribute | undefined = attributes.find(a => t.isJSXIdentifier(a.name, {name: 'key'}))
+              const keyAttribute: t.JSXAttribute | undefined = attributes.find(a => t.isJSXIdentifier(a.name, { name: 'key' }))
               if (keyAttribute && t.isJSXExpressionContainer(keyAttribute.value)) {
-                let itemName = itemId!.name;
-                const expressionArray =  generateMemberExpressionArray(keyAttribute.value.expression as t.MemberExpression)
+                let itemName = itemId!.name
+                const expressionArray = generateMemberExpressionArray(keyAttribute.value.expression as t.MemberExpression)
                 expressionArray[0] = itemName // 将key属性的值MemberExpression的首位（对象）替换成forItem
                 const memberExpressionString = expressionArray.join('.')
                 const forExpr = `${stateName} trackBy ${memberExpressionString}`
