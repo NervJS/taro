fc4786d1c20112530c1f002ad98654c831a08111
zhuxianguo <zhuxianguo.cn@outlook.com>
2020-08-12 10:48:40 +0800

    Update tabbar.ts
    修正tabbar在mode=hash模式下无法显示的问题

--- packages/taro-router/src/tabbar.ts
+++ packages/taro-router/src/tabbar.ts
@@ -8,6 +8,8 @@ export function initTabbar (config: AppConfig) {
   // TODO: 找到 tabbar 的类型
   const tabbar = document.createElement('taro-tabbar') as any
   tabbar.conf = config.tabBar
+  const routerConfig = (config as any).router
+  tabbar.conf.mode = routerConfig && routerConfig.mode ? routerConfig.mode : 'hash'
   const container = document.getElementById('container')
   // eslint-disable-next-line no-unused-expressions
   container?.appendChild(tabbar)
