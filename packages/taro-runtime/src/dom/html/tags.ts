6b351dbcbf244566daf9cc0f57512f7689689651
pengtikui <949828390@qq.com>
2020-08-21 00:45:13 +0800

    fix(runtime): 行内元素添加 <b> 标签

--- packages/taro-runtime/src/dom/html/tags.ts
+++ packages/taro-runtime/src/dom/html/tags.ts
@@ -25,7 +25,7 @@ const internalCompsList = Object.keys(internalComponents)
 export const isMiniElements = makeMap(internalCompsList, true)
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Inline_elements
-export const isInlineElements = makeMap('a,i,abbr,iframe,select,acronym,slot,small,bdi,kbd,strong,big,map,sub,sup,br,mark,mark,meter,template,canvas,textarea,cite,object,time,code,output,u,data,picture,tt,datalist,var,dfn,del,q,em,s,embed,samp', true)
+export const isInlineElements = makeMap('a,i,abbr,iframe,select,acronym,slot,small,bdi,kbd,strong,big,map,sub,sup,br,mark,mark,meter,template,canvas,textarea,cite,object,time,code,output,u,data,picture,tt,datalist,var,dfn,del,q,em,s,embed,samp,b', true)
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Block-level_elements
 export const isBlockElements = makeMap('address,fieldset,li,article,figcaption,main,aside,figure,nav,blockquote,footer,ol,details,form,p,dialog,h1,h2,h3,h4,h5,h6,pre,dd,header,section,div,hgroup,table,dl,hr,ul,dt', true)
