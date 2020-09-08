69ba5b89eed3f29858fdace7deacc9c85f013de6
Kai <kai.yang@yunzhanghu.com>
2020-09-08 09:52:46 +0800

    refactor: babel-plugin-transform-jsx-to-stylesheet插件升级到babel7

--- packages/babel-plugin-transform-jsx-to-stylesheet/__tests__/index.spec.js
+++ packages/babel-plugin-transform-jsx-to-stylesheet/__tests__/index.spec.js
@@ -1,18 +1,15 @@
 import jSXStylePlugin from '../src/index'
-import syntaxJSX from 'babel-plugin-syntax-jsx'
-import { transform } from 'babel-core'
+import syntaxJSX from '@babel/plugin-syntax-jsx'
+import { transform } from '@babel/core'
 
 const mergeStylesFunctionTemplate = `function _mergeStyles() {
   var newTarget = {};
-
   for (var index = 0; index < arguments.length; index++) {
     var target = arguments[index];
-
     for (var key in target) {
       newTarget[key] = Object.assign(newTarget[key] || {}, target[key]);
     }
   }
-
   return newTarget;
 }`
 
@@ -40,86 +37,77 @@ const getClassNameFunctionTemplate = `function _getClassName() {
   }
 
   return className.join(' ').trim();
-}`
+}
+`
 
-const getStyleFunctionTemplete = `function _getStyle(classNameExpression) {
-  var cache = _styleSheet.__cache || (_styleSheet.__cache = {});
-
+const getStyleFunctionTemplate = `function _getStyle(classNameExpression) {
   var className = _getClassName(classNameExpression);\n
   var classNameArr = className.split(/\\s+/);
-  var style = cache[className];
+  var style = [];
 
-  if (!style) {
-    style = {};
-
-    if (classNameArr.length === 1) {
-      style = _styleSheet[classNameArr[0].trim()];
-    } else {
-      classNameArr.forEach(function (cls) {
-        style = Object.assign(style, _styleSheet[cls.trim()]);
-      });
-    }
-
-    cache[className] = style;
+  if (classNameArr.length === 1) {
+    style.push(_styleSheet[classNameArr[0].trim()]);
+  } else {
+    classNameArr.forEach(function (cls) {
+      style.push(_styleSheet[cls.trim()]);
+    });
   }
 
   return style;
-}`
+}
+`
 
 describe('jsx style plugin', () => {
-  function getTransfromCode (code) {
+  function getTransformCode (code) {
     return transform(code, {
       plugins: [jSXStylePlugin, syntaxJSX]
     }).code
   }
 
   it('transform only one className to style as member', () => {
-    expect(getTransfromCode(`
+    expect(getTransformCode(`
 import { createElement, Component } from 'rax';
 import './app.css';
-
 class App extends Component {
   render() {
     return <div className="header" />;
   }
-}`)).toBe(`
-import { createElement, Component } from 'rax';
-import appStyleSheet from './app.css';
-
+}`)).toBe(`import { createElement, Component } from 'rax';
+import appStyleSheet from './app_styles';
 var _styleSheet = appStyleSheet;
+
 class App extends Component {
   render() {
     return <div style={_styleSheet["header"]} />;
   }
+
 }`)
-  })
+})
 
   it('transform multiple classNames to style as array', () => {
-    expect(getTransfromCode(`
+    expect(getTransformCode(`
 import { createElement, Component } from 'rax';
-import './app.css';
-
+import "./app.css";
 class App extends Component {
   render() {
     return <div className="header1 header2" />;
   }
-}`)).toBe(`
-import { createElement, Component } from 'rax';
-import appStyleSheet from './app.css';
-
+}`)).toBe(`import { createElement, Component } from 'rax';
+import appStyleSheet from "./app_styles";
 var _styleSheet = appStyleSheet;
+
 class App extends Component {
   render() {
     return <div style={[_styleSheet["header1"], _styleSheet["header2"]]} />;
   }
+
 }`)
   })
 
   it('transform array, object and expressions', () => {
-    expect(getTransfromCode(`
+    expect(getTransformCode(`
 import { createElement, Component } from 'rax';
 import './app.css';
-
 class App extends Component {
   render() {
     return <div className={'header'}>
@@ -129,182 +117,172 @@ class App extends Component {
       <div className={getClassName()} />
     </div>;
   }
-}`)).toBe(`
-import { createElement, Component } from 'rax';
-import appStyleSheet from './app.css';
-
+}`)).toBe(`import { createElement, Component } from 'rax';
+import appStyleSheet from './app_styles';
 var _styleSheet = appStyleSheet;
 
 ${getClassNameFunctionTemplate}
-
-${getStyleFunctionTemplete}
-
+${getStyleFunctionTemplate}
 class App extends Component {
   render() {
     return <div style={_styleSheet["header"]}>
-      <div style={_getStyle({ active: props.isActive })} />
-      <div style={_getStyle(['header1 header2', 'header3', { active: props.isActive }])} />
+      <div style={_getStyle({
+        active: props.isActive
+      })} />
+      <div style={_getStyle(['header1 header2', 'header3', {
+        active: props.isActive
+      }])} />
       <div style={_getStyle(props.visible ? 'show' : 'hide')} />
       <div style={_getStyle(getClassName())} />
     </div>;
   }
+
 }`)
   })
 
   it('combine one style and className', () => {
-    expect(getTransfromCode(`
+    expect(getTransformCode(`
 import { createElement, Component } from 'rax';
 import './app.css';
 import style from './style.css';
-
 class App extends Component {
   render() {
     return <div className="header2" style={styles.header1} />;
   }
-}`)).toBe(`${mergeStylesFunctionTemplate}
-
-import { createElement, Component } from 'rax';
-import appStyleSheet from './app.css';
-import styleStyleSheet from './style.css';
-
-var _styleSheet = _mergeStyles(appStyleSheet, styleStyleSheet);
+}`)).toBe(`import { createElement, Component } from 'rax';
+import appStyleSheet from './app_styles';
+var _styleSheet = appStyleSheet;
 
 class App extends Component {
   render() {
     return <div style={[_styleSheet["header2"], styles.header1]} />;
   }
+
 }`)
   })
 
   it('combine inline style object and className', () => {
-    expect(getTransfromCode(`
+    expect(getTransformCode(`
 import { createElement, Component } from 'rax';
 import './app.css';
-
 class App extends Component {
   render() {
     return <div className="header" style={{
       height: 100
     }} />;
   }
-}`)).toBe(`
-import { createElement, Component } from 'rax';
-import appStyleSheet from './app.css';
-
+}`)).toBe(`import { createElement, Component } from 'rax';
+import appStyleSheet from './app_styles';
 var _styleSheet = appStyleSheet;
+
 class App extends Component {
   render() {
     return <div style={[_styleSheet["header"], {
       height: 100
     }]} />;
   }
+
 }`)
   })
 
   it('combine multiple styles and className', () => {
-    expect(getTransfromCode(`
+    expect(getTransformCode(`
 import { createElement, Component } from 'rax';
 import './app.css';
 import style from './style.css';
-
 class App extends Component {
   render() {
     return <div className="header2" style={[styles.header1, styles.header3]} />;
   }
-}`)).toBe(`${mergeStylesFunctionTemplate}
-
-import { createElement, Component } from 'rax';
-import appStyleSheet from './app.css';
-import styleStyleSheet from './style.css';
-
-var _styleSheet = _mergeStyles(appStyleSheet, styleStyleSheet);
+}`)).toBe(`import { createElement, Component } from 'rax';
+import appStyleSheet from './app_styles';
+var _styleSheet = appStyleSheet;
 
 class App extends Component {
   render() {
     return <div style={[_styleSheet["header2"], styles.header1, styles.header3]} />;
   }
+
 }`)
   })
 
-  it('do not transfrom code when no css file', () => {
+  it('do not transform code when no css file', () => {
     const code = `
 import { createElement, Component } from 'rax';
-
 class App extends Component {
   render() {
     return <div className="header" />;
   }
 }`
-
-    expect(getTransfromCode(code)).toBe(code)
-  })
-
-  it('transform scss file', () => {
-    expect(getTransfromCode(`
-import { createElement, Component } from 'rax';
-import './app.scss';
+    expect(getTransformCode(code))
+    .toBe(`import { createElement, Component } from 'rax';
 
 class App extends Component {
   render() {
     return <div className="header" />;
   }
-}`)).toBe(`
-import { createElement, Component } from 'rax';
-import appStyleSheet from './app.scss';
 
+}`)
+})
+
+  it('transform scss file', () => {
+    expect(getTransformCode(`
+import { createElement, Component } from 'rax';
+import './app.scss';
+class App extends Component {
+  render() {
+    return <div className="header" />;
+  }
+}`)).toBe(`import { createElement, Component } from 'rax';
+import appStyleSheet from './app_styles';
 var _styleSheet = appStyleSheet;
+
 class App extends Component {
   render() {
     return <div style={_styleSheet["header"]} />;
   }
+
 }`)
   })
 
   it('transform scss file with hyphen(-) in the filename', () => {
-    expect(getTransfromCode(`
+    expect(getTransformCode(`
 import { createElement, Component } from 'rax';
 import './app-style.scss';
-
 class App extends Component {
   render() {
     return <div className="header" />;
   }
-}`)).toBe(`
-import { createElement, Component } from 'rax';
-import app_styleStyleSheet from './app-style.scss';
-
+}`)).toBe(`import { createElement, Component } from 'rax';
+import app_styleStyleSheet from './app-style_styles';
 var _styleSheet = app_styleStyleSheet;
+
 class App extends Component {
   render() {
     return <div style={_styleSheet["header"]} />;
   }
+
 }`)
   })
 
   it('transform constant elements in render', () => {
-    expect(getTransfromCode(`
+    expect(getTransformCode(`
 import { createElement, render } from 'rax';
 import './app.css';
-
 render(<div className="header" />);
-`)).toBe(`
-import { createElement, render } from 'rax';
-import appStyleSheet from './app.css';
-
+`)).toBe(`import { createElement, render } from 'rax';
+import appStyleSheet from './app_styles';
 var _styleSheet = appStyleSheet;
 render(<div style={_styleSheet["header"]} />);`)
   })
 
   it('transform stylus in render', () => {
-    expect(getTransfromCode(`
+    expect(getTransformCode(`
 import { createElement, render } from 'rax';
 import './app.styl';
-
 render(<div className="header" />);
-`)).toBe(`
-import { createElement, render } from 'rax';
+`)).toBe(`import { createElement, render } from 'rax';
 import appStyleSheet from './app_styles';
-
 var _styleSheet = appStyleSheet;
 render(<div style={_styleSheet["header"]} />);`)
   })
