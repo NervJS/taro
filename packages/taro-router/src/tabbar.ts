d08f5f6d28c3b82ccf4853529c091e374f569600
ZakaryCode <zakarycode@gmail.com>
2020-09-15 14:37:26 +0800

    fix(h5): tabbar init error #7290

--- packages/taro-router/src/tabbar.ts
+++ packages/taro-router/src/tabbar.ts
@@ -1,4 +1,5 @@
 import { AppConfig } from '@tarojs/taro'
+import { history } from './history'
 
 export function initTabbar (config: AppConfig) {
   if (config.tabBar == null) {
@@ -7,7 +8,9 @@ export function initTabbar (config: AppConfig) {
 
   // TODO: 找到 tabbar 的类型
   const tabbar = document.createElement('taro-tabbar') as any
+  const homePage = config.pages ? config.pages[0] : ''
   tabbar.conf = config.tabBar
+  tabbar.conf.homePage = history.location.pathname === '/' ? homePage : history.location.pathname
   const routerConfig = (config as any).router
   tabbar.conf.mode = routerConfig && routerConfig.mode ? routerConfig.mode : 'hash'
   const container = document.getElementById('container')
