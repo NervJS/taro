875a7e3af26c8b527245f556cfd82c795c0259ee
chenjiajian <798095202@qq.com>
2020-09-15 16:59:07 +0800

    chore(shared): 抽取出小程序平台插件的基类，放在 shared 包里

---
+++ packages/shared/src/platform-plugin-base.ts
@@ -0,0 +1,115 @@
+import type { IPluginContext } from '@tarojs/service'
+import type { RecursiveTemplate, UnRecursiveTemplate } from './template'
+
+interface IFileType {
+  templ: string
+  style: string
+  config: string
+  script: string
+  xs?: string
+}
+
+export class TaroPlatformBase {
+  ctx: IPluginContext
+  helper: IPluginContext['helper']
+  config: any
+  platform: string
+  globalObject: string
+  fileType: IFileType
+  template: RecursiveTemplate | UnRecursiveTemplate
+
+  constructor (ctx: IPluginContext, config) {
+    this.ctx = ctx
+    this.helper = ctx.helper
+    this.config = config
+  }
+
+  /**
+   * 1. 清空 dist 文件夹
+   * 2. 输出提示
+   */
+  setup () {
+    this.emptyOutputDir()
+    this.printDevelopmentTip(this.platform)
+  }
+
+  emptyOutputDir () {
+    const { outputPath } = this.ctx.paths
+    this.helper.emptyDirectory(outputPath)
+  }
+
+  printDevelopmentTip (platform: string) {
+    if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') return
+
+    const { isWindows, chalk } = this.helper
+    let exampleCommand
+
+    if (isWindows) {
+      exampleCommand = `$ set NODE_ENV=production && taro build --type ${platform} --watch`
+    } else {
+      exampleCommand = `$ NODE_ENV=production taro build --type ${platform} --watch`
+    }
+
+    console.log(chalk.yellowBright(`Tips: 预览模式生成的文件较大，设置 NODE_ENV 为 production 可以开启压缩。
+Example:
+${exampleCommand}
+`))
+  }
+
+  /**
+   * 返回当前项目内的 @tarojs/mini-runner 包
+   */
+  async getRunner () {
+    const { appPath } = this.ctx.paths
+    const { npm } = this.helper
+    const runner = await npm.getNpmPkg('@tarojs/mini-runner', appPath)
+    return runner.bind(null, appPath)
+  }
+
+  /**
+   * 准备 mini-runner 参数
+   */
+  getBaseOptions () {
+    const { ctx, config, globalObject, fileType, template } = this
+
+    return {
+      ...config,
+      nodeModulesPath: ctx.paths.nodeModulesPath,
+      buildAdapter: config.platform,
+      globalObject,
+      fileType,
+      template
+    }
+  }
+
+  /**
+   * 生成 project.config.json
+   * @param src 项目源码中配置文件的名称
+   * @param dist 编译后配置文件的名称，默认为 'project.config.json'
+   */
+  generateProjectConfig (src: string, dist = 'project.config.json') {
+    this.ctx.generateProjectConfig({
+      srcConfigName: src,
+      distConfigName: dist
+    })
+  }
+
+  /**
+   * 递归替换对象的 key 值
+   */
+  recursiveReplaceObjectKeys (obj, keyMap) {
+    Object.keys(obj).forEach(key => {
+      if (keyMap[key]) {
+        obj[keyMap[key]] = obj[key]
+        if (typeof obj[key] === 'object') {
+          this.recursiveReplaceObjectKeys(obj[keyMap[key]], keyMap)
+        }
+        delete obj[key]
+      } else if (keyMap[key] === false) {
+        delete obj[key]
+      } else if (typeof obj[key] === 'object') {
+        this.recursiveReplaceObjectKeys(obj[key], keyMap)
+      }
+    })
+  }
+}
