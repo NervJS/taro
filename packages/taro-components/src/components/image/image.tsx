2f094abd8fb75245467c5b1cf75ad2454847bc2e
zhuxianguo <zhuxianguo.cn@outlook.com>
2020-09-21 14:53:38 +0800

    Image组件mode选项增加默认值
    Image组件mode选项增加默认值

--- packages/taro-components/src/components/image/image.tsx
+++ packages/taro-components/src/components/image/image.tsx
@@ -77,7 +77,7 @@ export class Image implements ComponentInterface {
   render () {
     const {
       src,
-      mode,
+      mode = 'scaleToFill',
       lazyLoad,
       aspectFillMode,
       imageOnLoad,
