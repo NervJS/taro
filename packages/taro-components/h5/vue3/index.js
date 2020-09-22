f590027dc87b7c6ab17aa7a062a85c4f6d8ac5e8
b2nil <kentsu.ho@gmail.com>
2020-09-22 21:29:15 +0800

    fix(h5): 修复 #7329：Vue3 H5 中使用 render 函数渲染节点时, onTap 不能触发点击事件

--- packages/taro-components/h5/vue3/index.js
+++ packages/taro-components/h5/vue3/index.js
@@ -6,23 +6,71 @@ import Image from './components/image'
 import Icon from './components/icon'
 import ScrollView from './components/scroll-view'
 
-export function initVue3Components (app) {
+const componentMap = {}
+
+function genSimpleComponents(components) {
+  components.map(component => {
+    if (typeof component === 'string') {
+      componentMap[component] = createComponent(component)
+    } else {
+      const { name, classNames } = component
+      componentMap[name] = createComponent(name, classNames)
+    }
+  })
+}
+
+genSimpleComponents(simpleComponents)
+
+// simple components
+export const View = componentMap['taro-view']
+export const RichText = componentMap['taro-rich-text']
+export const Button = componentMap['taro-button']
+export const CheckboxGroup = componentMap['taro-checkbox-group']
+export const Editor = componentMap['taro-editor']
+export const Form = componentMap['taro-form']
+export const Label = componentMap['taro-label']
+export const PickerView = componentMap['taro-picker-view']
+export const PickerViewColumn = componentMap['taro-picker-view-column']
+export const CoverImage = componentMap['taro-cover-image']
+export const CoverView = componentMap['taro-cover-view']
+export const MoveableArea = componentMap['taro-moveable-area']
+export const MoveableView = componentMap['taro-moveable-view']
+export const Swiper = componentMap['taro-swiper']
+export const FunctionalPageNavigator = componentMap['taro-functional-page-navigator']
+export const Navigator = componentMap['taro-navigator']
+export const Audio = componentMap['taro-audio']
+export const Camera = componentMap['taro-camera']
+export const LivePlayer = componentMap['taro-live-player']
+export const Map = componentMap['taro-map']
+export const Ad = componentMap['taro-ad']
+export const OfficialAccount = componentMap['taro-official-account']
+export const OpenData = componentMap['taro-open-data']
+export const WebView = componentMap['taro-web-view']
+export const NavigationBar = componentMap['taro-navigation-bar']
+export const Block = componentMap['taro-block']
+export const Canvas = componentMap['taro-canvas']
+
+// simple components with classNames
+export const Checkbox = componentMap['taro-checkbox']
+export const Progress = componentMap['taro-progress']
+export const RadioGroup = componentMap['taro-radio-group']
+export const Radio = componentMap['taro-radio']
+export const SwiperItem = componentMap['taro-swiper-item']
+export const Video = componentMap['taro-video']
+
+// Form components
+export const Input = createFormsComponent('taro-input', 'input')
+export const Textarea = createFormsComponent('taro-textarea', 'input')
+export const Picker = createFormsComponent('taro-picker', 'change')
+export const Switch = createFormsComponent('taro-switch', 'change', 'checked')
+export const Slider = createFormsComponent('taro-slider', 'change', 'value', ['weui-slider-box'])
+
+export function initVue3Components(app) {
   app.config.isCustomElement = tag => /^taro-/.test(tag) || tag === 'root' || tag === 'block'
 
   simpleComponents.map(component => {
-    if (typeof component === 'string') {
-      app.component(component, createComponent(component))
-    } else {
-      const { name, classNames } = component
-      app.component(name, createComponent(name, classNames))
-    }
+    app.component(component, componentMap[component])
   })
-
-  const Input = createFormsComponent('taro-input', 'input')
-  const Textarea = createFormsComponent('taro-textarea', 'input')
-  const Picker = createFormsComponent('taro-picker', 'change')
-  const Switch = createFormsComponent('taro-switch', 'change', 'checked')
-  const Slider = createFormsComponent('taro-slider', 'change', 'value', ['weui-slider-box'])
 
   app.component('taro-input', Input)
   app.component('taro-textarea', Textarea)
@@ -33,4 +81,12 @@ export function initVue3Components (app) {
   app.component('taro-image', Image)
   app.component('taro-icon', Icon)
   app.component('taro-scroll-view', ScrollView)
+}
+
+export {
+  // others
+  Text,
+  Image,
+  Icon,
+  ScrollView
 }
