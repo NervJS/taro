67d2a7735df56c978ef9a7b0f9904aeddb595162
ZakaryCode <zakarycode@gmail.com>
2020-09-22 14:26:52 +0800

    fix(cli): build ui with plugins

--- packages/taro-cli/src/util/types.ts
+++ packages/taro-cli/src/util/types.ts
@@ -1,61 +1,61 @@
 export interface IInstallOptions {
-  dev: boolean,
+  dev: boolean
   peerDependencies?: boolean
 }
 
 export interface INpmConfig {
-  dir: string,
+  dir: string
   name: string
 }
 
 export interface IResolvedCache  {
   [key: string]: {
-    main: string,
+    main: string
     files: string[]
   }
 }
 
 export interface IPrettierConfig {
-  printWidth?: number,
-  tabWidth?: number,
-  useTabs?: boolean,
-  semi?: boolean,
-  singleQuote?: boolean,
-  jsxSingleQuote?: boolean,
-  trailingComma?: 'none' | 'es5' | 'all',
-  bracketSpacing?: boolean,
-  jsxBracketSameLine?: boolean,
-  arrowParens?: 'avoid' | 'always',
-  rangeStart?: number,
-  rangeEnd?: number,
-  parser?: 'babel' | 'babylon' | 'flow' | 'typescript' | 'css' | 'scss' | 'less' | 'json' | 'json5' | 'json-stringify' | 'graphql' | 'markdown' | 'mdx' | 'html' | 'vue' | 'angular' | 'yaml',
-  filepath?: string,
-  requirePragma?: boolean,
-  insertPragma?: boolean,
-  proseWrap?: 'always' | 'never' | 'preserve',
-  htmlWhitespaceSensitivity?: 'css' | 'strict' | 'ignore',
+  printWidth?: number
+  tabWidth?: number
+  useTabs?: boolean
+  semi?: boolean
+  singleQuote?: boolean
+  jsxSingleQuote?: boolean
+  trailingComma?: 'none' | 'es5' | 'all'
+  bracketSpacing?: boolean
+  jsxBracketSameLine?: boolean
+  arrowParens?: 'avoid' | 'always'
+  rangeStart?: number
+  rangeEnd?: number
+  parser?: 'babel' | 'babylon' | 'flow' | 'typescript' | 'css' | 'scss' | 'less' | 'json' | 'json5' | 'json-stringify' | 'graphql' | 'markdown' | 'mdx' | 'html' | 'vue' | 'angular' | 'yaml'
+  filepath?: string
+  requirePragma?: boolean
+  insertPragma?: boolean
+  proseWrap?: 'always' | 'never' | 'preserve'
+  htmlWhitespaceSensitivity?: 'css' | 'strict' | 'ignore'
   endOfLine?: 'auto' | 'lf' | 'crlf' | 'cr'
 }
 
 export interface IBuildOptions {
-  type?: string,
-  watch?: boolean,
-  platform?: string,
-  port?: number,
-  release?: boolean,
-  envHasBeenSet?: boolean,
-  page?: string,
-  component?: string,
+  type?: string
+  watch?: boolean
+  platform?: string
+  port?: number
+  release?: boolean
+  envHasBeenSet?: boolean
+  page?: string
+  component?: string
   uiIndex?: string
 }
 
 export interface IMiniAppBuildConfig {
-  adapter: string,
-  watch?: boolean,
-  envHasBeenSet?: boolean,
-  port?: number,
-  release?: boolean,
-  page?: string,
+  adapter: string
+  watch?: boolean
+  envHasBeenSet?: boolean
+  port?: number
+  release?: boolean
+  page?: string
   component?: string
 }
 
@@ -64,8 +64,9 @@ export interface IOption {
 }
 
 export interface IBuildHooks {
-  onBuildStart?: Function,
-  modifyWebpackChain?: Function,
-  modifyBuildAssets?: Function,
+  onBuildStart?: Function
+  modifyBuildAssets?: Function
+  modifyWebpackChain?: Function
+  modifyBuildTempFileContent?: Function
   onBuildFinish?: Function
 }
