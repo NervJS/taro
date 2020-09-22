e5c54a7dd80e63c374bc4848d7553a8ba37e1822
ZakaryCode <zakarycode@gmail.com>
2020-09-22 19:01:36 +0800

    fix(components): remove unnecessary assignment

--- packages/taro-components/src/components/image/image.tsx
+++ packages/taro-components/src/components/image/image.tsx
@@ -77,7 +77,7 @@ export class Image implements ComponentInterface {
   render () {
     const {
       src,
-      mode = 'scaleToFill',
+      mode,
       lazyLoad,
       aspectFillMode,
       imageOnLoad,
