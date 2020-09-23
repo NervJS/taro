6ee01239231ec9737072c38383fa0f04895371f6
ZakaryCode <zakarycode@gmail.com>
2020-09-23 19:27:58 +0800

    revert(webpack-runner): wrong change

--- packages/taro-webpack-runner/src/util/chain.ts
+++ packages/taro-webpack-runner/src/util/chain.ts
@@ -327,11 +327,9 @@ export const getModule = (appPath: string, {
   const styleLoader = getStyleLoader([defaultStyleLoaderOption, styleLoaderOption])
   const topStyleLoader = getStyleLoader([defaultStyleLoaderOption, {
     insert: function insertAtTop (element) {
-      // eslint-disable-next-line no-var
-      var parent = document.querySelector('head')
+      const parent = document.querySelector('head')
       if (parent) {
-        // eslint-disable-next-line no-var
-        var lastInsertedElement = window._lastElementInsertedByStyleLoader
+        const lastInsertedElement = (window as any)._lastElementInsertedByStyleLoader
         if (!lastInsertedElement) {
           parent.insertBefore(element, parent.firstChild)
         } else if (lastInsertedElement.nextSibling) {
@@ -339,7 +337,7 @@ export const getModule = (appPath: string, {
         } else {
           parent.appendChild(element)
         }
-        window._lastElementInsertedByStyleLoader = element
+        (window as any)._lastElementInsertedByStyleLoader = element
       }
     }
   }, styleLoaderOption])
