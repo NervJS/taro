4159d7915c6f3123db5d22c3dfde27c5556a0f95
chenjiajian <798095202@qq.com>
2020-11-02 10:59:22 +0800

    fix(loader): 修复分享 API 在使用 redux 时无法生效的问题，fix #7232
    page loader 处理页面时先分析页面代码 AST，识别出是否有调用分享 API，再决定是否自动添加 enableShareAppMessage、enableShareTimeline 配置

--- packages/taro-loader/src/page.ts
+++ packages/taro-loader/src/page.ts
@@ -1,6 +1,8 @@
 import * as webpack from 'webpack'
 import { getOptions, stringifyRequest } from 'loader-utils'
 import * as path from 'path'
+import * as acorn from 'acorn'
+import * as walk from 'acorn-walk'
 import { frameworkMeta } from './utils'
 
 interface PageConfig {
@@ -8,12 +10,13 @@ interface PageConfig {
   path: string
 }
 
-export default function (this: webpack.loader.LoaderContext) {
+export default function (this: webpack.loader.LoaderContext, source: string) {
   const options = getOptions(this)
-  const config = getPageConfig(options.config, this.resourcePath)
+  const { framework, config: loaderConfig } = options
+  const config = getPageConfig(loaderConfig, this.resourcePath)
   const configString = JSON.stringify(config)
   const stringify = (s: string): string => stringifyRequest(this, s)
-  const { isNeedRawLoader } = frameworkMeta[options.framework]
+  const { isNeedRawLoader } = frameworkMeta[framework]
   // raw is a placeholder loader to locate changed .vue resource
   const raw = path.join(__dirname, 'raw.js')
   const componentPath = isNeedRawLoader
@@ -23,6 +26,11 @@ export default function (this: webpack.loader.LoaderContext) {
 if (typeof PRERENDER !== 'undefined') {
   global._prerender = inst
 }`
+
+  if (framework === 'react' || framework === 'nerv') {
+    Object.assign(config, addConfig(source))
+  }
+
   return `import { createPageConfig } from '@tarojs/runtime'
 import component from ${stringify(componentPath)}
 var config = ${configString};
@@ -46,4 +54,51 @@ function getPageConfig (configs: Record<string, PageConfig>, resourcePath: strin
 
 function removeExt (file: string) {
   return path.join(path.dirname(file), path.basename(file, path.extname(file)))
+}
+
+function addConfig (source) {
+  const configsMap = {
+    enableShareAppMessage: ['onShareAppMessage', 'useShareAppMessage'],
+    enableShareTimeline: ['onShareTimeline', 'useShareTimeline']
+  }
+  const ast = acorn.parse(source, {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  })
+
+  const additionConfig: Record<string, any> = {}
+
+  function check (name: string) {
+    Object.keys(configsMap).forEach(configName => {
+      const apis: string[] = configsMap[configName]
+      if (apis.includes(name)) {
+        additionConfig[configName] = true
+      }
+    })
+  }
+
+  walk.simple(ast, {
+    FunctionExpression (node: any) {
+      if (!node.id || !node.id.name) return
+      check(node.id.name)
+    },
+    FunctionDeclaration (node: any) {
+      if (!node.id || !node.id.name) return
+      check(node.id.name)
+    },
+    CallExpression (node: any) {
+      const { callee } = node
+      if (callee.type === 'Identifier') {
+        check(callee.name)
+      } else if (callee.type === 'MemberExpression') {
+        if (callee.property.type === 'Identifier') {
+          check(callee.property.name)
+        } else if (callee.property.type === 'Literal') {
+          check(callee.property.value)
+        }
+      }
+    }
+  })
+
+  return additionConfig
 }
