b4d5bce2ace9b195e4ca4aa56966ec90cddcd009
yechunxi <yechunxi00@163.com>
2020-11-20 11:51:38 +0800

    feat(runtime-rn): add 3 taro3 rountime-rn

---
+++ packages/taro-runtime-rn/src/index.ts
@@ -0,0 +1,35 @@
+export {
+  createPageConfig
+} from './page'
+export {
+  createReactNativeApp,
+  getApp
+} from './app'
+export {
+  Current,
+  getCurrentInstance
+} from './current'
+export * from './hooks'
+export * from './emmiter'
+export {
+  navigateTo,
+  redirectTo,
+  navigateBack,
+  switchTab,
+  reLaunch,
+  showTabBar,
+  hideTabBar,
+  showTabBarRedDot,
+  hideTabBarRedDot,
+  setTabBarBadge,
+  removeTabBarBadge,
+  setTabBarItem,
+  setNavigationBarTitle,
+  setNavigationBarColor,
+  showNavigationBarLoading,
+  hideNavigationBarLoading,
+  setTabBarStyle
+} from '@tarojs/router-rn'
+export {
+  pxTransform
+} from './compute'
