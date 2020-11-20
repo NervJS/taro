3f044a5f3cb7b24f6bab4dcfe453b83368d50d31
yechunxi <yechunxi00@163.com>
2020-11-20 11:51:38 +0800

    feat(runtime-rn): add 3 taro3 rountime-rn

---
+++ packages/taro-runtime-rn/src/utils.ts
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const incrementId = () => {
+  let id = 0
+  return () => (id++).toString()
+}
+export function isFunction (o: unknown):boolean {
+  return typeof o === 'function'
+}
+
+export const EMPTY_OBJ: any = {}
+
+export const isArray = Array.isArray
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function isClassComponent (component): boolean {
+  return isFunction(component.render) ||
+    !!component.prototype?.isReactComponent ||
+    component.prototype instanceof React.Component
+}
