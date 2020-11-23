731f09e571076b198dfd9ae4e6d08db80d055467
ZeroToOne <ydreammer@gmail.com>
2020-11-23 15:11:45 +0800

    feat(taro-h5): support fileName parameter when using Taro.uploadFile api(#7377)

--- packages/taro-h5/src/api/fileTransfer/uploadFile.js
+++ packages/taro-h5/src/api/fileTransfer/uploadFile.js
@@ -6,7 +6,7 @@ import {
   XHR_STATS
 } from './utils'
 
-const createUploadTask = ({ url, filePath, formData, name, header, success, error }) => {
+const createUploadTask = ({ url, filePath, formData, name, header, fileName, success, error }) => {
   let timeout
   let formKey
   const apiName = 'uploadFile'
@@ -81,7 +81,7 @@ const createUploadTask = ({ url, filePath, formData, name, header, success, erro
 
   convertObjectUrlToBlob(filePath)
     .then(fileObj => {
-      form.append(name, fileObj, fileObj.name || `file-${Date.now()}`)
+      form.append(name, fileObj, fileName || fileObj.name || `file-${Date.now()}`)
       send()
     })
     .catch(e => {
@@ -137,12 +137,13 @@ const createUploadTask = ({ url, filePath, formData, name, header, success, erro
  * @param {string} object.name 文件对应的 key，开发者在服务端可以通过这个 key 获取文件的二进制内容
  * @param {Object} [object.header] HTTP 请求 Header，Header 中不能设置 Referer
  * @param {Object} [object.formData] HTTP 请求中其他额外的 form data
+ * @param {string} object.fileName 上传的文件名
  * @param {function} [object.success] 接口调用成功的回调函数
  * @param {function} [object.fail] 接口调用失败的回调函数
  * @param {function} [object.complete] 接口调用结束的回调函数（调用成功、失败都会执行）
  * @returns {UploadTask}
  */
-const uploadFile = ({ url, filePath, name, header, formData, success, fail, complete }) => {
+const uploadFile = ({ url, filePath, name, header, formData, fileName, success, fail, complete }) => {
   let task
   const promise = new Promise((resolve, reject) => {
     task = createUploadTask({
@@ -151,6 +152,7 @@ const uploadFile = ({ url, filePath, name, header, formData, success, fail, comp
       name,
       filePath,
       formData,
+      fileName,
       success: res => {
         success && success(res)
         complete && complete()
