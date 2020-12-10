12df287d433bc81283046a15e0c2c7579e51bd3c
Damo <daiguimao@sightp.com>
2020-12-10 12:28:37 +0800

    fix(diff): view层级过多会导致栈溢出

--- packages/taro-with-weapp/src/diff.js
+++ packages/taro-with-weapp/src/diff.js
@@ -11,84 +11,92 @@ export function diff (current, pre) {
 }
 
 function syncKeys (current, pre) {
-  if (current === pre) return
-  const rootCurrentType = type(current)
-  const rootPreType = type(pre)
-  if (rootCurrentType == OBJECTTYPE && rootPreType == OBJECTTYPE) {
-    // if(Object.keys(current).length >= Object.keys(pre).length){
-    for (let key in pre) {
-      const currentValue = current[key]
-      if (currentValue === undefined) {
-        current[key] = null
-      } else {
-        syncKeys(currentValue, pre[key])
+  const stack = [];
+  function _syncKeys ([current, pre]) {
+    if (current === pre) return
+    const rootCurrentType = type(current)
+    const rootPreType = type(pre)
+    if (rootCurrentType == OBJECTTYPE && rootPreType == OBJECTTYPE) {
+      // if(Object.keys(current).length >= Object.keys(pre).length){
+      for (let key in pre) {
+        const currentValue = current[key]
+        if (currentValue === undefined) {
+          current[key] = null
+        } else {
+          stack.push([currentValue, pre[key]])
+        }
+      }
+      // }
+    } else if (rootCurrentType == ARRAYTYPE && rootPreType == ARRAYTYPE) {
+      if (current.length >= pre.length) {
+        pre.forEach((item, index) => {
+          stack.push([current[index], item])
+        })
       }
     }
-    // }
-  } else if (rootCurrentType == ARRAYTYPE && rootPreType == ARRAYTYPE) {
-    if (current.length >= pre.length) {
-      pre.forEach((item, index) => {
-        syncKeys(current[index], item)
-      })
-    }
   }
+  while (stack.length > 0) { _syncKeys(stack.shift()) }
 }
 
 function _diff (current, pre, path, result) {
-  if (current === pre) return
-  const rootCurrentType = type(current)
-  const rootPreType = type(pre)
-  if (rootCurrentType == OBJECTTYPE) {
-    if (rootPreType != OBJECTTYPE || Object.keys(current).length < Object.keys(pre).length) {
-      setResult(result, path, current)
-    } else {
-      for (let key in current) {
-        const currentValue = current[key]
-        const preValue = pre[key]
-        const currentType = type(currentValue)
-        const preType = type(preValue)
-        if (currentType != ARRAYTYPE && currentType != OBJECTTYPE) {
-          if (currentValue != pre[key]) {
-            setResult(result, (path == '' ? '' : path + '.') + key, currentValue)
-          }
-        } else if (currentType == ARRAYTYPE) {
-          if (preType != ARRAYTYPE) {
-            setResult(result, (path == '' ? '' : path + '.') + key, currentValue)
-          } else {
-            if (currentValue.length < preValue.length) {
+  const stack = [];
+  function __diff ([current, pre, path, result]) {
+    if (current === pre) return
+    const rootCurrentType = type(current)
+    const rootPreType = type(pre)
+    if (rootCurrentType == OBJECTTYPE) {
+      if (rootPreType != OBJECTTYPE || Object.keys(current).length < Object.keys(pre).length) {
+        setResult(result, path, current)
+      } else {
+        for (let key in current) {
+          const currentValue = current[key]
+          const preValue = pre[key]
+          const currentType = type(currentValue)
+          const preType = type(preValue)
+          if (currentType != ARRAYTYPE && currentType != OBJECTTYPE) {
+            if (currentValue != pre[key]) {
+              setResult(result, (path == '' ? '' : path + '.') + key, currentValue)
+            }
+          } else if (currentType == ARRAYTYPE) {
+            if (preType != ARRAYTYPE) {
               setResult(result, (path == '' ? '' : path + '.') + key, currentValue)
             } else {
-              currentValue.forEach((item, index) => {
-                _diff(item, preValue[index], (path == '' ? '' : path + '.') + key + '[' + index + ']', result)
-              })
+              if (currentValue.length < preValue.length) {
+                setResult(result, (path == '' ? '' : path + '.') + key, currentValue)
+              } else {
+                currentValue.forEach((item, index) => {
+                  stack.push([item, preValue[index], (path == '' ? '' : path + '.') + key + '[' + index + ']', result])
+                })
+              }
             }
-          }
-        } else if (currentType == OBJECTTYPE) {
-          if (preType != OBJECTTYPE || Object.keys(currentValue).length < Object.keys(preValue).length) {
-            setResult(result, (path == '' ? '' : path + '.') + key, currentValue)
-          } else {
-            for (let subKey in currentValue) {
-              _diff(currentValue[subKey], preValue[subKey], (path == '' ? '' : path + '.') + key + '.' + subKey, result)
+          } else if (currentType == OBJECTTYPE) {
+            if (preType != OBJECTTYPE || Object.keys(currentValue).length < Object.keys(preValue).length) {
+              setResult(result, (path == '' ? '' : path + '.') + key, currentValue)
+            } else {
+              for (let subKey in currentValue) {
+                stack.push([currentValue[subKey], preValue[subKey], (path == '' ? '' : path + '.') + key + '.' + subKey, result])
+              }
             }
           }
         }
       }
-    }
-  } else if (rootCurrentType == ARRAYTYPE) {
-    if (rootPreType != ARRAYTYPE) {
-      setResult(result, path, current)
-    } else {
-      if (current.length < pre.length) {
+    } else if (rootCurrentType == ARRAYTYPE) {
+      if (rootPreType != ARRAYTYPE) {
         setResult(result, path, current)
       } else {
-        current.forEach((item, index) => {
-          _diff(item, pre[index], path + '[' + index + ']', result)
-        })
+        if (current.length < pre.length) {
+          setResult(result, path, current)
+        } else {
+          current.forEach((item, index) => {
+            stack.push([item, pre[index], path + '[' + index + ']', result])
+          })
+        }
       }
+    } else {
+      setResult(result, path, current)
     }
-  } else {
-    setResult(result, path, current)
   }
+  while (stack.length > 0) { __diff(stack.shift()) }
 }
 
 function setResult (result, k, v) {
