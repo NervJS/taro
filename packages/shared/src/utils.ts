44a05a769abb0e88cdd3ec5b7b203db7281cd18c
chenjiajian <798095202@qq.com>
2021-01-07 09:57:10 +0800

    fix: ci

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -53,7 +53,6 @@ export const toKebabCase = function (string) {
   return string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
 }
 
-
 export function capitalize (s: string) {
   return s.charAt(0).toUpperCase() + s.slice(1)
 }
