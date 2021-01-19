3484050b725a033f99980cdf4c39b3b1fd84425a
luckyadam <weitaozsh@gmail.com>
2021-01-19 16:24:06 +0800

    fix(loader): 修正 taro-loader 处理

--- packages/taro-loader/src/app.ts
+++ packages/taro-loader/src/app.ts
@@ -1,5 +1,7 @@
 import * as webpack from 'webpack'
 import { getOptions, stringifyRequest } from 'loader-utils'
+import { normalizePath } from '@tarojs/helper'
+
 import { frameworkMeta } from './utils'
 
 export default function (this: webpack.loader.LoaderContext) {
@@ -8,12 +10,14 @@ export default function (this: webpack.loader.LoaderContext) {
   const options = getOptions(this)
   const { importFrameworkStatement, frameworkArgs, creator } = frameworkMeta[options.framework]
   const config = JSON.stringify(options.config)
+  const loaders = this.loaders
+  const thisLoaderIndex = loaders.findIndex(item => normalizePath(item.path).indexOf('@tarojs/taro-loader') >= 0)
   const prerender = `
 if (typeof PRERENDER !== 'undefined') {
   global._prerender = inst
 }`
   return `import { ${creator}, window } from '@tarojs/runtime'
-import component from ${stringify(this.request.split('!').slice(1).join('!'))}
+import component from ${stringify(this.request.split('!').slice(thisLoaderIndex + 1).join('!'))}
 ${importFrameworkStatement}
 var config = ${config};
 window.__taroAppConfig = config
