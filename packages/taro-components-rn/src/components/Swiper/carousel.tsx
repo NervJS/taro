5c6f5f0b4b4299685c385dd66cd0ec4b66cccc1b
iChengbo <chengbo_x@163.com>
2021-01-19 11:25:53 +0800

    fix: 修复 swiper infinite 切换导致自动播放停止的问题

--- packages/taro-components-rn/src/components/Swiper/carousel.tsx
+++ packages/taro-components-rn/src/components/Swiper/carousel.tsx
@@ -112,8 +112,8 @@ class Carousel extends React.Component<CarouselProps, CarouselState> {
 
   componentDidUpdate(prevProps: CarouselProps) {
     if (
-      prevProps.autoplay !== undefined &&
-      prevProps.autoplay !== this.props.autoplay
+      (prevProps.autoplay !== undefined && prevProps.autoplay !== this.props.autoplay) ||
+      (prevProps.infinite !== undefined && prevProps.infinite !== this.props.infinite)
     ) {
       this.autoplay(!this.props.autoplay)
     }
