301ec66d9103b172e050e0ae546840d4fb9a2744
吴晓曦(寿险总部科技研发部) <WUXIAOXI924@pingan.com.cn>
2021-01-20 09:40:04 +0800

    feat(taro-react): 添加props.spec的测试用例

--- packages/taro-react/__tests__/props.spec.js
+++ packages/taro-react/__tests__/props.spec.js
@@ -94,6 +94,15 @@ describe('Context', () => {
       expect(container.firstChild.getAttribute('b')).toBe('b')
     })
 
+    it('not remove null properties', () => {
+      const container = document.createElement('div')
+      render(<div id='1' a={null} />, container)
+      render(<div id='1' a='a' b='b' />, container)
+      expect(container.firstChild.id).toBe('1')
+      expect(container.firstChild.getAttribute('a')).toBe('a')
+      expect(container.firstChild.getAttribute('b')).toBe('b')
+    })
+
     it('should ignore ref', () => {
       const container = document.createElement('div')
       render(<div ref={React.createRef} />, container)
