0ecff10921c035b3ad17ae49620cf3e5ec4d4a70
Roger Leung <zerotoherorg@gmail.com>
2021-02-03 11:15:50 +0800

    fix(runtime): 修复custom-wrapper优化失效

--- packages/taro-mini-runner/src/template/custom-wrapper.ts
+++ packages/taro-mini-runner/src/template/custom-wrapper.ts
@@ -2,4 +2,4 @@
 import { createRecursiveComponentConfig } from '@tarojs/runtime'
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
-Component(createRecursiveComponentConfig())
+Component(createRecursiveComponentConfig('custom-wrapper'))
--- packages/taro-runtime/src/dsl/common.ts
+++ packages/taro-runtime/src/dsl/common.ts
@@ -313,7 +313,7 @@ export function createComponentConfig (component: React.ComponentClass, componen
   return config
 }
 
-export function createRecursiveComponentConfig () {
+export function createRecursiveComponentConfig (componentName?: string) {
   return {
     properties: {
       i: {
@@ -337,7 +337,7 @@ export function createRecursiveComponentConfig () {
     },
     options: {
       addGlobalClass: true,
-      virtualHost: true
+      virtualHost: componentName !== 'custom-wrapper'
     },
     methods: {
       eh: eventHandler
