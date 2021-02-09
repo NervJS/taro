ff4a10dcb9165fdc64dba19835f521413e48afd2
chenjiajian <798095202@qq.com>
2021-02-09 17:30:37 +0800

    fix(loader): 修复百度小程序渲染问题，#7293

--- packages/taro-cli/src/doctor/configSchema.ts
+++ packages/taro-cli/src/doctor/configSchema.ts
@@ -31,7 +31,7 @@ const schema = Joi.object().keys({
       Joi.object().keys({
         from: Joi.string().required(),
         to: Joi.string().required(),
-        ignore: Joi.string()
+        ignore: Joi.array().items(Joi.string())
       })
     ),
 
