451a28d44f80c236485f429e5b1deb2b12fec52e
chenjiajian <798095202@qq.com>
2021-02-23 22:04:55 +0800

    fix(runtime): 修复H5使用类组件刷新时报错的问题。

--- packages/taro-runtime/src/dsl/react.ts
+++ packages/taro-runtime/src/dsl/react.ts
@@ -104,6 +104,10 @@ function setReconciler () {
       if (!prev || !next) return
 
       // 子组件使用 lifecycle hooks 注册了生命周期后，会存在 prev，里面是注册的生命周期回调。
+
+      // prev 使用 Object.create(null) 创建，H5 的 fast-refresh 可能也会导致存在 prev，要排除这些意外产生的 prev
+      if ('constructor' in prev) return
+
       Object.keys(prev).forEach(item => {
         if (isFunction(next[item])) {
           next[item] = [next[item], ...prev[item]]
