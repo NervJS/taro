3b182868f81e6360e7a66556be8b500f991c826d
chenjiajian <798095202@qq.com>
2021-05-27 23:00:16 +0800

    refactor(plugin-html): 使用 rollup 打包，为了可以摇树

---
+++ packages/taro-plugin-html/rollup.config.js
@@ -0,0 +1,33 @@
+const { join } = require('path')
+const typescript = require('rollup-plugin-typescript2')
+const cwd = __dirname
+
+const base = {
+  external: ['@tarojs/shared'],
+  plugins: [typescript()]
+}
+
+// 供 CLI 编译时使用的 Taro 插件入口
+const comileConfig = {
+  input: join(cwd, 'src/index.ts'),
+  output: {
+    file: join(cwd, 'dist/index.js'),
+    format: 'cjs',
+    sourcemap: true,
+    exports: 'named'
+  },
+  ...base
+}
+
+// 运行时入口
+const runtimeConfig = {
+  input: join(cwd, 'src/runtime.ts'),
+  output: {
+    file: join(cwd, 'dist/runtime.js'),
+    format: 'es',
+    sourcemap: true
+  },
+  ...base
+}
+
+module.exports = [comileConfig, runtimeConfig]
