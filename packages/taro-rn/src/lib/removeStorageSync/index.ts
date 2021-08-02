dddc9b1204f09f62e62b76c4e34034db8c77f4b7
iChengbo <chengbo_x@163.com>
2021-08-03 01:35:24 +0800

    refactor(rn): refactor unsupported api

--- packages/taro-rn/src/lib/removeStorageSync/index.ts
+++ packages/taro-rn/src/lib/removeStorageSync/index.ts
@@ -1,7 +1,1 @@
-/**
- * 暂不支持
- * @param key 
- */
-export function removeStorageSync(key: String) {
-    throw Error('RN 端暂不支持 removeStorageSync，请使用 removeStorage')
-}
+export { removeStorageSync } from '../unsupportedApi'
