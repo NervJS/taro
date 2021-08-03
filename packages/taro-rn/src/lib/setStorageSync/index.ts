0d02c29162bf72c8fcfa0c0e3abd858126875819
iChengbo <chengbo_x@163.com>
2021-08-03 12:23:41 +0800

    refactor(rn): refactor unsupported api (#9955)

--- packages/taro-rn/src/lib/setStorageSync/index.ts
+++ packages/taro-rn/src/lib/setStorageSync/index.ts
@@ -1,8 +1,1 @@
-/**
- * 暂不支持
- * @param key 
- * @param data 
- */
-export function setStorageSync(key: String, data: any) {
-    throw Error('RN 端暂不支持 setStorageSync，请使用 setStorage')
-}
+export { setStorageSync } from '../unsupportedApi'
