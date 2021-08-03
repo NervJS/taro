eb6daa38f31925fec711bbc40eafc01c05a27f7a
iChengbo <chengbo_x@163.com>
2021-08-03 18:04:06 +0800

    refactor(rn): replace deprecated keyboard api

--- packages/taro-rn/src/lib/keyboard.ts
+++ packages/taro-rn/src/lib/keyboard.ts
@@ -6,19 +6,20 @@ const hideKeyboard = (opts: Taro.hideKeyboard.Option = {}): void => {
   try {
     Keyboard.dismiss()
     const res = { errMsg: 'hideKeyboard:ok' }
-    success && success(res)
-    complete && complete(res)
+    success?.(res)
+    complete?.(res)
   } catch (err) {
     const res = { errMsg: err.message }
-    fail && fail(res)
-    complete && complete(res)
+    fail?.(res)
+    complete?.(res)
   }
 }
 
-const callbackManager = createCallbackManager()
+const _cbManager = createCallbackManager()
+let _hasListener = false
 
 const keyboardHeightListener = (e) => {
-  callbackManager.trigger(e.endCoordinates.height)
+  _cbManager.trigger({ height: e.endCoordinates.height })
 }
 
 /**
@@ -26,10 +27,11 @@ const keyboardHeightListener = (e) => {
  * @param {(height: number) => void} callback 键盘高度变化事件的回调函数
  */
 const onKeyboardHeightChange = (callback: Taro.onKeyboardHeightChange.Callback): void => {
-  callbackManager.add(callback)
-  if (callbackManager.count() === 1) {
-    Keyboard.addListener('keyboardDidShow', keyboardHeightListener)
-    Keyboard.addListener('keyboardDidHide', keyboardHeightListener)
+  _cbManager.add(callback)
+  if (!_hasListener) {
+    Keyboard.addListener("keyboardDidShow", keyboardHeightListener)
+    Keyboard.addListener("keyboardDidHide", keyboardHeightListener)
+    _hasListener = true
   }
 }
 
@@ -37,11 +39,18 @@ const onKeyboardHeightChange = (callback: Taro.onKeyboardHeightChange.Callback):
  * 取消监听键盘高度变化事件
  * @param {(height: number) => void} callback 键盘高度变化事件的回调函数
  */
-const offKeyboardHeightChange = (callback: Taro.onKeyboardHeightChange.Callback): void => {
-  callbackManager.remove(callback)
-  if (callbackManager.count() === 0) {
-    Keyboard.removeListener('keyboardDidShow', keyboardHeightListener)
-    Keyboard.removeListener('keyboardDidHide', keyboardHeightListener)
+const offKeyboardHeightChange = (callback?: Taro.onKeyboardHeightChange.Callback): void => {
+  if (callback && typeof callback === 'function') {
+    _cbManager.remove(callback)
+  } else if (callback === undefined) {
+    _cbManager.clear()
+  } else {
+    console.warn('offKeyboardHeightChange failed')
+  }
+  if (_cbManager.count() === 0) {
+    Keyboard.removeAllListeners('keyboardDidShow')
+    Keyboard.removeAllListeners('keyboardDidHide')
+    _hasListener = false
   }
 }
 
