23bc50960342b90772e2f3775bfd92665e521d1f
zhiqingchen <329604487@qq.com>
2021-08-20 15:34:56 +0800

    feat(license): add license header

--- packages/taro-components-qa/src/components/taro-audio/img.js
+++ packages/taro-components-qa/src/components/taro-audio/img.js
@@ -1,3 +1,22 @@
+/*
+* Licensed to the O2Team under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
 export const PAUSE_IMG = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAABMklEQVRoQ+2Z0Y3CQAxE7QqOEqAjbwd0QAmEDijhOliXBBVAB0YrATqdkGxl10SRZn/XmXiek/kx08oPr7x/goGlJ4gJrGICIrIhoiMR7YnoTkS/qnoa1XyPfugTEpEzMx/+NmxmJ1WdRpjo0Y8auDFzm8L7mNlFVXeDDMzWDxkopdinRmutoec9kz36oQZ6XuA13+579GEgQtirwQQyCXna+AeyCWXrI4UihL0apFAmIU8bKZRNKFsfKRQh7NUghTIJedpIoWxC2fpIoQhhrwYplEnI00YKZRPK1kcKRQh7NUihTEKe9rdSqC01fv41c621biMNejWllNn6oZ9YRCZmbhua9xm84JitHzLQun6aaCsmMrO2YhqynXkRmasfNuB9Bkvdw8BS5F/vxQQwgU4CD9RcRkCIkNZgAAAAAElFTkSuQmCC'
 export const START_IMG = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAB2klEQVRoQ9WZfVEEMQzFEwXgACSAk+IAHIAC7hRwKAAJwQE4AAWAA1DwmDdTZjo3cNtuu023f3d282s+XtKqrHzpyu2XIoAQwqmZfYwEnQUQQggi8qCqxwC+RGRjZvcjgEwC8NRV9X3fWADPInJhZgRyWzkA16p695eF0RuEIIzLygHYqOrtIesA7MzsxoOgCQANB/AaQ6prkjcDiBDMhxsze+zljaYAv0YDMBG56pHgiwBEbzCUmOAMrcXWYgCJN6gZ26UIFgeI3lhMM7oAJAnOvGB+NFvdAJKQaqoZ3QESzaA3qhPcBaClZrgBtNIMd4BazRgCoEYzhgKYoxnDASQJnjVnDAmQQJxPzeDDAkSIrZltDsn26ACcLXZrBfgGcLbWEPoEEHJajRFD6AnAZe40NxIAQ4bDz8GY38+HUQDe4qkXd6fuAAB4RcmTn3XD5wnAkGGsV01oXgAv0fjqS7DuAAAm1bVkYO4JwNrOkGl6EdwLoKi2j+SBWbV9FIDZtd0doLa2ewI0qe1eAKzt7CBnKWqJ0eneJlWodW0vgckB+PeRT0Sy+/YSo0r25gDwmZVd4tHehxer7U0B+DE+dKsq+/QTEemeqFUzcclpeOydDCEPo0r+uXqAHzf0dkAxk0BxAAAAAElFTkSuQmCC'
 
--- packages/taro/types/api/alipay/index.d.ts
+++ packages/taro/types/api/alipay/index.d.ts
@@ -1,3 +1,22 @@
+/*!
+* Licensed to the O2Team under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
 declare namespace Taro {
   /**
    * 此接口可获取支付宝会员的基础信息（头像图片地址、昵称、性别、国家码、省份、所在市区），接入方法请参考 获取会员基础信息介绍。如需获取支付宝会员标识（user_id），请调用 my.getAuthCode 和 alipay.system.oauth.token 接口。
