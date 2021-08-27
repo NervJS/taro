17e5ce43dc52587d32e2dfa4fdbb55df3f9bd893
chenjiajian <798095202@qq.com>
2021-08-27 15:38:45 +0800

    fix(runtime): 修复 H5 编译报错，fix #10124

--- packages/taro-runtime/src/container/index.ts
+++ packages/taro-runtime/src/container/index.ts
@@ -14,36 +14,37 @@ import processPluginHooks from './plugin-hooks'
 
 const container = new Container()
 
-container.bind<TaroElement>(SERVICE_IDENTIFIER.TaroElement).to(TaroElement).whenTargetNamed(ElementNames.Element)
-container.bind<TaroDocument>(SERVICE_IDENTIFIER.TaroElement).to(TaroDocument).inSingletonScope().whenTargetNamed(ElementNames.Document)
-container.bind<TaroRootElement>(SERVICE_IDENTIFIER.TaroElement).to(TaroRootElement).whenTargetNamed(ElementNames.RootElement)
-container.bind<FormElement>(SERVICE_IDENTIFIER.TaroElement).to(FormElement).whenTargetNamed(ElementNames.FormElement)
-container.bind<InstanceNamedFactory>(SERVICE_IDENTIFIER.TaroElementFactory).toFactory<TaroElement>((context: interfaces.Context) => {
-  return (named: ElementNames) => (nodeName?: string) => {
-    const el = context.container.getNamed<TaroElement>(SERVICE_IDENTIFIER.TaroElement, named)
-    if (nodeName) {
-      el.nodeName = nodeName
+if (process.env.TARO_ENV !== 'h5') {
+  container.bind<TaroElement>(SERVICE_IDENTIFIER.TaroElement).to(TaroElement).whenTargetNamed(ElementNames.Element)
+  container.bind<TaroDocument>(SERVICE_IDENTIFIER.TaroElement).to(TaroDocument).inSingletonScope().whenTargetNamed(ElementNames.Document)
+  container.bind<TaroRootElement>(SERVICE_IDENTIFIER.TaroElement).to(TaroRootElement).whenTargetNamed(ElementNames.RootElement)
+  container.bind<FormElement>(SERVICE_IDENTIFIER.TaroElement).to(FormElement).whenTargetNamed(ElementNames.FormElement)
+  container.bind<InstanceNamedFactory>(SERVICE_IDENTIFIER.TaroElementFactory).toFactory<TaroElement>((context: interfaces.Context) => {
+    return (named: ElementNames) => (nodeName?: string) => {
+      const el = context.container.getNamed<TaroElement>(SERVICE_IDENTIFIER.TaroElement, named)
+      if (nodeName) {
+        el.nodeName = nodeName
+      }
+      el.tagName = el.nodeName.toUpperCase()
+      return el
     }
-    el.tagName = el.nodeName.toUpperCase()
-    return el
-  }
-})
+  })
 
-container.bind<TaroText>(SERVICE_IDENTIFIER.TaroText).to(TaroText)
-container.bind<InstanceFactory<TaroText>>(SERVICE_IDENTIFIER.TaroTextFactory).toFactory<TaroText>((context: interfaces.Context) => {
-  return (text: string) => {
-    const textNode = context.container.get<TaroText>(SERVICE_IDENTIFIER.TaroText)
-    textNode._value = text
-    return textNode
-  }
-})
+  container.bind<TaroText>(SERVICE_IDENTIFIER.TaroText).to(TaroText)
+  container.bind<InstanceFactory<TaroText>>(SERVICE_IDENTIFIER.TaroTextFactory).toFactory<TaroText>((context: interfaces.Context) => {
+    return (text: string) => {
+      const textNode = context.container.get<TaroText>(SERVICE_IDENTIFIER.TaroText)
+      textNode._value = text
+      return textNode
+    }
+  })
 
-container.bind<TaroNodeImpl>(SERVICE_IDENTIFIER.TaroNodeImpl).to(TaroNodeImpl).inSingletonScope()
-container.bind<TaroElementImpl>(SERVICE_IDENTIFIER.TaroElementImpl).to(TaroElementImpl).inSingletonScope()
+  container.bind<TaroNodeImpl>(SERVICE_IDENTIFIER.TaroNodeImpl).to(TaroNodeImpl).inSingletonScope()
+  container.bind<TaroElementImpl>(SERVICE_IDENTIFIER.TaroElementImpl).to(TaroElementImpl).inSingletonScope()
+}
+
 container.bind<Hooks>(SERVICE_IDENTIFIER.Hooks).to(Hooks).inSingletonScope()
-
 container.load(DefaultHooksContainer)
-
 processPluginHooks(container)
 
 export default container
