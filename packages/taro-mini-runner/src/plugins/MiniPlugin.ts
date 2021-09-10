135ee67356c1e036239af316e5a4649315071c9e
Ken Ho <kentsu.ho@gmail.com>
2021-09-10 20:36:49 +0800

    fix(mini-runner): should get pages again if the configs are modified

--- packages/taro-mini-runner/src/plugins/MiniPlugin.ts
+++ packages/taro-mini-runner/src/plugins/MiniPlugin.ts
@@ -894,6 +894,8 @@ export default class TaroMiniPlugin {
     const customWrapperName = 'custom-wrapper'
     if (typeof modifyMiniConfigs === 'function') {
       await modifyMiniConfigs(this.filesConfig)
+      // get pages based on the modified configs
+      this.getPages()
     }
     if (!this.options.blended && !isBuildPlugin) {
       const appConfigPath = this.getConfigFilePath(this.appEntry)
