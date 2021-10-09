354d197b40f851aecdf121b13cd15a8d4aa213bb
早道 <shinken008@gmail.com>
2021-10-09 19:28:35 +0800

    taro react native 0.66.0 版本升级 (#10394)
    * chore: react-native 版本升级到 0.65.1 以及 expo-依赖更新
    
    * chore: expo 版本更新
    
    * chore(taro-rn/taro-component-rn): 升级依赖
    
    * chore(taro-compnents-rn): @unimodules/react-native-adapter @unimodules/core 使用事件 remove 不报错版本
    
    * chore(babel-preset-taro/rn-runner/rn-supporter): 升级 metro 相关依赖
    
    * chore: 更新依赖
    
    * feat(taro-rn/taro-runtime-rn): 修改 eventEmitter remove 事件方法
    
    * fix: 修复 hoverStartTime 失效情况
    
    * chore: react native 升级到 0.66.0 版本
    
    * chore: react native 升级到 0.66.0 版本, 修复测试报错
    
    chore: react native 升级到 0.66.0 版本
    
    * fix(taro-components): add rn types
    
    * fix(taro-components): skiplibcheck
    
    * fix(taro-components): 解决引入 react native build ts 报错
    
    * fix(taro-rn): netinfo bug
    
    https://github.com/react-native-netinfo/react-native-netinfo/issues/486
    
    * fix(components): rm Duplicate skipLibCheck
    
    Co-authored-by: zhiqingchen <329604487@qq.com>
    Co-authored-by: tony chen <zombieczq@gmail.com>

--- packages/taro-rn/src/lib/permission.ts
+++ packages/taro-rn/src/lib/permission.ts
@@ -1,4 +1,4 @@
-import { Linking, AppState } from 'react-native';
+import { Linking, AppState, NativeEventSubscription } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { errorHandler, successHandler } from '../utils';
 
@@ -16,6 +16,7 @@ const scopeMap = {
 }
 
 let stateListener // 缓存监听函数
+let appStateSubscription: NativeEventSubscription | undefined
 
 const getAuthSetting = async () => {
   const keyArr = Object.keys(scopeMap)
@@ -42,8 +43,8 @@ const handleAppStateChange = async (nextAppState, resolve, reject, opts) => {
       res.errMsg = 'openSetting:ok'
       success?.(res)
       complete?.(res)
-
-      AppState.removeEventListener('change', stateListener as any);
+  
+      appStateSubscription?.remove()
       resolve(res)
     } catch (error) {
       res.errMsg = 'openSetting:fail'
@@ -92,7 +93,7 @@ export async function getSetting(opts: Taro.getSetting.Option = {}): Promise<Tar
 export function openSetting(opts: Taro.openSetting.Option = {}): Promise<Taro.openSetting.SuccessCallbackResult> {
   return new Promise((resolve, reject) => {
     stateListener = (next) => handleAppStateChange(next, resolve, reject, opts)
-    AppState.addEventListener('change', stateListener)
+    appStateSubscription = AppState.addEventListener('change', stateListener)
     Linking.openSettings()
   })
 }
