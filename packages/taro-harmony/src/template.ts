10c7e546cec24b3aff08078024eecb6a598638bc
= <=>
2021-10-18 20:43:53 +0800

    feat(harmony): 修改组件label组件 pull规则

--- packages/taro-harmony/src/template.ts
+++ packages/taro-harmony/src/template.ts
@@ -123,10 +123,10 @@ ${elements}
       .join('')
   }
 
-  replacePropName (name: string, value: string, _componentName?: string) {
+  replacePropName (name: string, value: string, componentName?: string) {
     if (value === 'eh') return name.toLowerCase().replace(/^bind/, '@')
     // 由于鸿蒙不支持for属性 需要修改for属性，需要改名
-    if (_componentName === 'label' && name === 'for') return 'target'
+    if (componentName === 'label' && name === 'for') return 'target'
     return name
   }
 
