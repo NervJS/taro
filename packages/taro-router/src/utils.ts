4e92441924f393b7cb6e7fe57c40505174fa15b8
liuchuzhang <liuchuzhang1@gmail.com>
2021-10-25 15:45:06 +0800

    chore: add type

--- packages/taro-router/src/utils.ts
+++ packages/taro-router/src/utils.ts
@@ -32,7 +32,7 @@ export const throttle = (fn: Function, threshold: number) => {
   }
 }
 
-export const stripBasename = (path, basename = '') => path.startsWith(basename) ? path.replace(basename, '') : path
+export const stripBasename = (path: string, basename: string): string => path.startsWith(basename) ? path.replace(basename, '') : path
 
 export const isTabBar = (config: RouterConfig): boolean => {
   const { customRoutes = {}, basename = '', pathname } = config.router
