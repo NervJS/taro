8982e50bba01ed28c0e41e1b08dcbed8998c4561
huozhongyi <huozhongyi@jd.com>
2021-11-02 11:15:07 +0800

    feat: change textare component

--- packages/taro-harmony/src/components/components-harmony/textarea/index.js
+++ packages/taro-harmony/src/components/components-harmony/textarea/index.js
@@ -1,61 +1,84 @@
+import window from '@ohos.window'
+
 export default {
   data: {
-    textareaValue: ''
+    textareaValue: '',
+    height: 0
   },
+
   props: [
     'id',
     'cls',
     'value',
     'placeholder',
-    'placeholderStyle',
-    'placeholderClass',
-    'disabled',
     'maxlength',
     'autoFocus',
-    'focus',
-    'fixed',
-    'cursorSpacing',
-    'cursor',
-    'showConfirmBar',
     'selectionStart',
     'selectionEnd',
-    'adjustPosition',
-    'holdKeyboard',
-    'disableDefaultPadding',
     'headerIcon',
     'showcounter',
     'menuoptions',
     'softkeyboardenabled'
   ],
+
+  onInit () {
+    this.windowClass = window.getTopWindow()
+    this.windowClass.then(win => {
+      win.on('keyboardHeightChange', (err, data) => {
+        if (!err) {
+          if (this.height !== data.height) {
+            this.onKeyboardHeightChange(data.height)
+          }
+          this.height = data.height
+        }
+      })
+    })
+  },
+
   onInput (e) {
     this.textareaValue = e.value
     this.$emit('input', {
       id: this.id, value: e.text, cursor: e.text.length, keyCode: null
     })
   },
+
   onFocus () {
     this.$emit('focus', {
-      id: this.id, value: this.textareaValue
+      id: this.id, value: this.textareaValue, height: this.height
     })
   },
+
   onBlur () {
     this.$emit('blur', {
-      id: this.id, value: this.textareaValue
+      id: this.id, value: this.textareaValue, height: this.height
     })
   },
+
   onTranslate (e) {
     this.$emit('translate', e)
   },
+
   onShare (e) {
     this.$emit('share', e)
   },
+
   onSearch (e) {
-    this.$emit('search', e)
+    this.$emit('confirm', {
+      id: this.id, ...e
+    })
   },
+
   onOptionSelect (e) {
     this.$emit('optionselect', e)
   },
+
   onSelectChange (e) {
     this.$emit('selectchange', e)
+  },
+
+  onKeyboardHeightChange (height) {
+    this.$emit('keyboardheightchange', {
+      id: this.id, height, duration: 0
+    })
   }
 }
