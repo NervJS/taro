212137577dd579bf99f25367f119c13220e32f57
chenjiajian <798095202@qq.com>
2021-11-18 19:44:30 +0800

    chore(vue3): 代码解耦

--- packages/taro-plugin-vue3/src/index.ts
+++ packages/taro-plugin-vue3/src/index.ts
@@ -1,66 +1,44 @@
-import { REG_VUE, chalk } from '@tarojs/helper'
-import { DEFAULT_Components } from '@tarojs/runner-utils'
-import { internalComponents, toCamelCase, capitalize } from '@tarojs/shared/dist/template'
-import { getLoaderMeta } from './loader-meta'
+import { chalk } from '@tarojs/helper'
+import { modifyMiniWebpackChain } from './webpack.mini'
+import { modifyH5WebpackChain } from './webpack.h5'
 
 import type { IPluginContext } from '@tarojs/service'
-import type { RootNode, TemplateChildNode, ElementNode, AttributeNode, DirectiveNode, SimpleExpressionNode } from '@vue/compiler-core'
-
-const CUSTOM_WRAPPER = 'custom-wrapper'
-let isBuildH5
 
 export default (ctx: IPluginContext) => {
   const { framework } = ctx.initialConfig
   if (framework !== 'vue3') return
 
-  isBuildH5 = process.env.TARO_ENV === 'h5'
-
   ctx.modifyWebpackChain(({ chain, webpack, data }) => {
-    customVueChain(chain, webpack, data)
-    setLoader(chain)
+    // 通用
+    setAlias(chain)
+    setDefinePlugin(chain, webpack)
 
-    if (isBuildH5) {
-      setStyleLoader(ctx, chain)
-
-      chain.merge({
-        module: {
-          rule: {
-            'process-import-taro': {
-              test: /taro-h5[\\/]src[\\/]index/,
-              loader: require.resolve('./api-loader')
-            }
-          }
-        }
-      })
+    if (process.env.TARO_ENV === 'h5') {
+      // H5
+      modifyH5WebpackChain(ctx, chain)
     } else {
-      chain
-        .plugin('definePlugin')
-        .tap(args => {
-          args[0].ENABLE_ADJACENT_HTML = true
-          args[0].ENABLE_CLONE_NODE = true
-          args[0].ENABLE_TEMPLATE_CONTENT = true
-          return args
-        })
+      // 小程序
+      modifyMiniWebpackChain(ctx, chain, data)
     }
   })
 }
 
-function customVueChain (chain, webpack, data) {
+function setAlias (chain) {
   // 避免 npm link 时，taro composition apis 使用的 vue 和项目使用的 vue 实例不一致。
   chain.resolve.alias
     .set('vue', require.resolve('vue'))
+}
 
+function setDefinePlugin (chain, webpack) {
   chain
     .plugin('defined')
     .use(webpack.DefinePlugin, [{
       __VUE_OPTIONS_API__: JSON.stringify(true),
       __VUE_PROD_DEVTOOLS__: JSON.stringify(false)
     }])
-
-  setVueLoader(chain, data)
 }
 
-function getVueLoaderPath (): string {
+export function getVueLoaderPath (): string {
   try {
     return require.resolve('vue-loader', {
       paths: [process.cwd()]
@@ -69,142 +47,5 @@ function getVueLoaderPath (): string {
     // eslint-disable-next-line no-console
     console.log(chalk.yellow('找不到 vue-loader，请先安装。'))
     process.exit(1)
-  }
-}
-
-function setVueLoader (chain, data) {
-  const vueLoaderPath = getVueLoaderPath()
-
-  // plugin
-  const { VueLoaderPlugin } = require(vueLoaderPath)
-  chain
-    .plugin('vueLoaderPlugin')
-    .use(VueLoaderPlugin)
-
-  // loader
-  let vueLoaderOption
-
-  if (isBuildH5) {
-    // H5
-    vueLoaderOption = {
-      transformAssetUrls: {
-        video: ['src', 'poster'],
-        'live-player': 'src',
-        audio: 'src',
-        source: 'src',
-        image: 'src',
-        'cover-image': 'src',
-        'taro-video': ['src', 'poster'],
-        'taro-live-player': 'src',
-        'taro-audio': 'src',
-        'taro-source': 'src',
-        'taro-image': 'src',
-        'taro-cover-image': 'src'
-      },
-      compilerOptions: {
-        // https://github.com/vuejs/vue-next/blob/master/packages/compiler-core/src/options.ts
-        nodeTransforms: [(node: RootNode | TemplateChildNode) => {
-          if (node.type === 1 /* ELEMENT */) {
-            node = node as ElementNode
-            const nodeName = node.tag
-            if (DEFAULT_Components.has(nodeName)) {
-              node.tag = `taro-${nodeName}`
-              node.tagType = 1 /* 0: ELEMENT, 1: COMPONENT */
-            }
-          }
-        }]
-      }
-    }
-  } else {
-    // 小程序
-    vueLoaderOption = {
-      optimizeSSR: false,
-      transformAssetUrls: {
-        video: ['src', 'poster'],
-        'live-player': 'src',
-        audio: 'src',
-        source: 'src',
-        image: 'src',
-        'cover-image': 'src'
-      },
-      compilerOptions: {
-        // https://github.com/vuejs/vue-next/blob/master/packages/compiler-core/src/options.ts
-        nodeTransforms: [(node: RootNode | TemplateChildNode) => {
-          if (node.type === 1 /* ELEMENT */) {
-            node = node as ElementNode
-            const nodeName = node.tag
-
-            if (capitalize(toCamelCase(nodeName)) in internalComponents) {
-              // change only ElementTypes.COMPONENT to ElementTypes.ELEMENT
-              // and leave ElementTypes.SLOT untouched
-              if (node.tagType === 1 /* COMPONENT */) {
-                node.tagType = 0 /* ELEMENT */
-              }
-              data.componentConfig.includes.add(nodeName)
-            }
-
-            if (nodeName === CUSTOM_WRAPPER) {
-              node.tagType = 0 /* ELEMENT */
-              data.componentConfig.thirdPartyComponents.set(CUSTOM_WRAPPER, new Set())
-            }
-
-            const usingComponent = data.componentConfig.thirdPartyComponents.get(nodeName)
-            if (usingComponent != null) {
-              node.props.forEach(prop => {
-                if (prop.type === 6 /* ATTRIBUTE */) {
-                  usingComponent.add((prop as AttributeNode).name)
-                } else if ((prop as any).type === 7 /* DIRECTIVE */) {
-                  prop = prop as DirectiveNode
-                  if (prop.arg?.type === 4 /* SimpleExpression */) {
-                    let value = (prop.arg as SimpleExpressionNode).content
-                    if (prop.name === 'on') {
-                      value = `on${value}`
-                    }
-                    usingComponent.add(value)
-                  }
-                }
-              })
-            }
-          }
-        }]
-      }
-    }
-  }
-
-  chain.module
-    .rule('vue')
-    .test(REG_VUE)
-    .use('vueLoader')
-    .loader(vueLoaderPath)
-    .options(vueLoaderOption)
-}
-
-function setStyleLoader (ctx: IPluginContext, chain) {
-  const config = ctx.initialConfig.h5 || {}
-
-  const { styleLoaderOption = {} } = config
-  chain.module
-    .rule('customStyle')
-    .merge({
-      use: [{
-        loader: 'style-loader',
-        options: styleLoaderOption
-      }]
-    })
-}
-
-function setLoader (chain) {
-  if (isBuildH5) {
-    chain.plugin('mainPlugin')
-      .tap(args => {
-        args[0].loaderMeta = getLoaderMeta()
-        return args
-      })
-  } else {
-    chain.plugin('miniPlugin')
-      .tap(args => {
-        args[0].loaderMeta = getLoaderMeta()
-        return args
-      })
   }
 }
