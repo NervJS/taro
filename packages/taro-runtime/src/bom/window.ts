0e140c4581c637c819bd6366c98161c83ca02635
chenjiajian <798095202@qq.com>
2021-11-22 10:58:38 +0800

    fix(runtime): 修复使用 recoil 报错的问题，fix #10119
    封装 window.setTimeout、window.clearTimeout，避免第三方库使用 window.setTimeout 调用时，Taro 封装的 window 对象和真正挂载 setTimeout 的 global 对象不一样时，报 Illegal invocation 的问题

--- packages/taro-runtime/src/bom/window.ts
+++ packages/taro-runtime/src/bom/window.ts
@@ -3,7 +3,7 @@ import { document } from './document'
 import { isBrowser, win } from '../env'
 import { raf, caf } from './raf'
 import { getComputedStyle } from './getComputedStyle'
-import { DATE, SET_TIMEOUT } from '../constants'
+import { DATE } from '../constants'
 
 export const window = isBrowser ? win : {
   navigator,
@@ -35,7 +35,10 @@ if (process.env.TARO_ENV && process.env.TARO_ENV !== 'h5') {
   if (!(DATE in window)) {
     (window as any).Date = Date
   }
-  if (!(SET_TIMEOUT in window)) {
-    (window as any).setTimeout = setTimeout
+  (window as any).setTimeout = function (cb, delay) {
+    setTimeout(cb, delay)
+  }
+  ;(window as any).clearTimeout = function (seed) {
+    clearTimeout(seed)
   }
 }
