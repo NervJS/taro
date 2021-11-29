ffed3ef8cde4834a1450b794255d498235cb9d9d
hawleyHuo <18611784832@163.com>
2021-11-29 13:43:50 +0800

    feat(harmony): add navigator component (#10664)
    * feat(harmony): add navigator component
    
    * feat(harmony): add hover navigator component
    
    * feat: change harmony com some quest
    
    Co-authored-by: huozhongyi <huozhongyi@jd.com>

--- packages/taro-harmony/src/components/components-harmony/utils/index.js
+++ packages/taro-harmony/src/components/components-harmony/utils/index.js
@@ -8,3 +8,34 @@ export function createOption (option) {
   }
   return option
 }
+
+export function queryToJson (str) {
+  const dec = decodeURIComponent
+  const qp = str.split('&')
+  const ret = {}
+  let name
+  let val
+  for (let i = 0, l = qp.length, item; i < l; ++i) {
+    item = qp[i]
+    if (item.length) {
+      const s = item.indexOf('=')
+      if (s < 0) {
+        name = dec(item)
+        val = ''
+      } else {
+        name = dec(item.slice(0, s))
+        val = dec(item.slice(s + 1))
+      }
+      if (typeof ret[name] === 'string') {
+        ret[name] = [ret[name]]
+      }
+
+      if (Array.isArray(ret[name])) {
+        ret[name].push(val)
+      } else {
+        ret[name] = val
+      }
+    }
+  }
+  return ret
+}
