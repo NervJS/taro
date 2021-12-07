b2499914fd50a76f5a13574025d8f7dd1deb0c66
caishuai6 <caishuai6@jd.com>
2021-12-07 14:01:01 +0800

    feat: 添加 name 属性到表单组件
    Signed-off-by: caishuai6 <caishuai6@jd.com>

--- packages/taro-harmony/src/components/components-harmony/checkbox/index.js
+++ packages/taro-harmony/src/components/components-harmony/checkbox/index.js
@@ -2,6 +2,7 @@ export default {
   props: [
     'id',
     'value',
+    'name',
     'disabled',
     'checked',
     'groupId',
--- packages/taro-harmony/src/components/components-harmony/radio/index.js
+++ packages/taro-harmony/src/components/components-harmony/radio/index.js
@@ -1,6 +1,7 @@
 export default {
   props: [
     'id',
+    'name',
     'groupId',
     'value',
     'checked',
