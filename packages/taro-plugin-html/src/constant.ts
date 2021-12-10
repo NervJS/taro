21e2e0548592a491d5afa2ed153bb1c042e9ee28
Yang <yangpan@zhongan.com>
2021-12-10 14:12:23 +0800

    fix: taro-plugin-html genAttrMapFnFromDir

--- packages/taro-plugin-html/src/constant.ts
+++ packages/taro-plugin-html/src/constant.ts
@@ -17,9 +17,9 @@ export namespace SpecialMaps {
 
 function genAttrMapFnFromDir (dir: Record<string, string | [string, Record<string, any>]>): SpecialMaps.MapAttrFn {
   const fn: SpecialMaps.MapAttrFn = function (key, value) {
-    key = key.toLowerCase();
-    if (key in dir) {
-      const res = dir[key]
+    const lowerKey = key.toLowerCase()
+    if (lowerKey in dir) {
+      const res = dir[lowerKey]
       if (isString(res)) {
         key = res
       } else {
