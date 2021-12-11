280b7b804cd200b3be0d359d16d1b21ce5ddcfc2
chenjiajian <798095202@qq.com>
2021-12-12 01:56:36 +0800

    fix(runtime): 修复 Vue3 首次进入页面会触发两次 onShow 的问题，fix #10728

--- packages/taro-runtime/src/dsl/vue3.ts
+++ packages/taro-runtime/src/dsl/vue3.ts
@@ -2,7 +2,7 @@ import { isFunction, isArray, ensure } from '@tarojs/shared'
 import container from '../container'
 import SERVICE_IDENTIFIER from '../constants/identifiers'
 import { Current } from '../current'
-import { injectPageInstance, safeExecute } from './common'
+import { injectPageInstance } from './common'
 import { isBrowser } from '../env'
 
 import type {
@@ -25,10 +25,6 @@ function createVue3Page (h: typeof createElement, id: string) {
       },
       created () {
         injectPageInstance(this, id)
-        // vue3 组件 created 时机比小程序页面 onShow 慢，因此在 created 后再手动触发一次 onShow。
-        this.$nextTick(() => {
-          safeExecute(id, 'onShow')
-        })
       }
     }
 
