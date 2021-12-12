db6dc2c41832115e0761a2f40161f20f57abd9ce
xueshuai7 <xueshuai@jd.com>
2021-12-12 17:15:43 +0800

    feat: media开发

---
+++ packages/taro-harmony/src/apis/media/image.ts
@@ -0,0 +1,11 @@
+// HarmonyOS 图片模块首批接口从API version 7开始支持。
+// HarmonyOS 文档链接：https://developer.harmonyos.com/cn/docs/documentation/doc-references/js-apis-image-0000001122977382
+// WX 文档链接：https://developers.weixin.qq.com/miniprogram/dev/api/media/image/wx.saveImageToPhotosAlbum.html
+// ❌ wx.saveImageToPhotosAlbum(Object object)
+// ❌ wx.previewMedia(Object object)
+// ❌ wx.previewImage(Object object)
+// ❌ wx.getImageInfo(Object object)
+// ❌ wx.compressImage(Object object)
+// ❌ wx.chooseMessageFile(Object object)
+// ❌ wx.chooseMessageFile(Object object)
+// ❌ wx.chooseImage(Object object)
