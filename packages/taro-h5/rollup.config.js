565a5e6ee05390e76e21811c10b946f84c4a0235
Zakary <zakarycode@gmail.com>
2021-12-28 16:34:59 +0800

    fix(h5): external router

--- packages/taro-h5/rollup.config.js
+++ packages/taro-h5/rollup.config.js
@@ -13,7 +13,7 @@ const babel = require('@rollup/plugin-babel').default
 const cwd = __dirname
 const baseConfig = {
   external: d => {
-    return /^@tarojs\/(runtime|taro)$/.test(d) || d.includes('@babel/runtime')
+    return /^@tarojs\/(router|runtime|taro)$/.test(d) || d.includes('@babel/runtime')
   },
   output: {
     format: 'cjs',
