7b03b1023388883694a43e044046186934bdff42
miles <36622981+rquanx@users.noreply.github.com>
2022-01-06 21:32:53 +0800

    perf(mini-runner): 优化 MiniSplitChunksPlugin 的运行性能 (#10986)
    * refactor: 减少不必要的chunkNames计算
    
    * refactor: 去除多余的Array.from
    
    * refactor: 去除多余的转数组
    
    * refactor: 修改forEach为find，符合条件时退出
    
    * feat: 增加正则缓存，进一步加快MiniSplitChunksPlugin
    
    * fix: modulesIterable没有map，使用forEach
    
    * fix: 缓存判断修正
    
    Co-authored-by: chenjiajian <798095202@qq.com>
    Co-authored-by: tanrunquan <tanrunquan@corp.netease.com>

---
+++ packages/taro-mini-runner/src/utils/cacheMap.ts
@@ -0,0 +1,16 @@
+export class CacheMap {
+  map: Map<string, boolean>;
+  constructor () {
+    this.map = new Map()
+  }
+
+  test (regex: RegExp, str: string) {
+    const key = `${regex.source}--${str}`
+    if (this.map.has(key)) {
+      return this.map.get(key)
+    }
+    const result = regex.test(str)
+    this.map.set(key, result)
+    return result
+  }
+}
