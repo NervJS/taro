62e00fc575b12e9bac2388fe07f07bfd537afdd7
chenjiajian <798095202@qq.com>
2022-01-10 10:32:31 +0800

    chore: fix ci

--- packages/taro-runtime/src/bom/window.ts
+++ packages/taro-runtime/src/bom/window.ts
@@ -35,10 +35,10 @@ if (process.env.TARO_ENV && process.env.TARO_ENV !== 'h5') {
   if (!(DATE in window)) {
     (window as any).Date = Date
   }
-  (window as any).setTimeout = function (handler: string | (...args: any[]) => any, timeout?: number, ...args: any[]) {
-    return setTimeout(handler, timeout, ...args)
+  (window as any).setTimeout = function (...args: Parameters<typeof setTimeout>) {
+    return setTimeout(...args)
   }
-  ;(window as any).clearTimeout = function (handle?: number) {
-    return clearTimeout(handle)
+  ;(window as any).clearTimeout = function (...args: Parameters<typeof clearTimeout>) {
+    return clearTimeout(...args)
   }
 }
