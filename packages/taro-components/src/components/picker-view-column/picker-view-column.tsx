6318c721da7a61b5c635ddbe78c62502f70588b3
huwei161 <huwei161@jd.com>
2022-01-14 10:34:02 +0800

    feat(h5): 添加H5 pickerView和pickerColumn支持

--- packages/taro-components/src/components/picker-view-column/picker-view-column.tsx
+++ packages/taro-components/src/components/picker-view-column/picker-view-column.tsx
@@ -1,17 +1,90 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Component, h, Host } from '@stencil/core'
+import { Component, h, Host, Listen, Element, Event, EventEmitter, Prop } from '@stencil/core'
 
 @Component({
-  tag: 'taro-picker-view-column-core'
+  tag: 'taro-picker-view-column-core',
+  styleUrl: './style/index.scss'
 })
 export class PickerViewColumn {
-  componentDidLoad () {
-    console.error('H5 暂不支持 PickerViewColumn 组件！')
+
+  @Element() el: HTMLElement
+
+  @Prop() tag: string
+
+  @Prop() initselectindex: string
+
+  @Event({ eventName: 'col-select' }) onChange: EventEmitter
+
+  private isMove: boolean = false
+
+  @Prop() paddingtop: string
+
+  @Listen('mousedown')
+  onMouseDown() {
+    this.isMove = true
   }
 
-  render () {
+  @Listen('mouseleave')
+  onMouseMoveEnd() {
+    if (!this.isMove) return
+
+    this.isMove = false
+    this.handleMoveEnd()
+  }
+
+  @Listen('touchend')
+  onTouchEnd() {
+    this.handleMoveEnd()
+  }
+
+  handleMoveEnd() {
+    const childList = this.el.querySelectorAll('taro-view-core')
+    let element = this.el
+    let scrollTop = element.scrollTop
+    let sum = 0
+    let selectedIndex: string = '0'
+    for (const index in childList) {
+      let item = element.childNodes[index];
+      let itemElm = item as Element
+      let itemHeight = itemElm.clientHeight
+      if ((sum + itemHeight / 2.0) > scrollTop) {
+        selectedIndex = index
+        break
+      }
+      sum += itemHeight
+    }
+    element.scrollTo({
+      top: sum,
+      behavior: 'smooth'
+    })
+    this.onChange.emit({
+      curIndex: this.tag,
+      selectedIndex: selectedIndex
+    })
+  }
+  /// 过滤非PickerViewColumn组件
+  componentDidRender() {
+    if (!!this.initselectindex) {
+      const childList = this.el.querySelectorAll('taro-view-core')
+      let element = this.el
+      let sum = 0
+      for (const index in childList) {
+        let item = element.childNodes[index];
+        let itemElm = item as Element
+        let itemHeight = itemElm.clientHeight
+        if (this.initselectindex === index) {
+          break
+        }
+        sum += itemHeight
+      }
+      element.scrollTo({ top: sum })
+    }
+  }
+
+  render() {
+    const { paddingtop } = this;
     return (
-      <Host />
+      <Host class="_picker-view-column-container" style={{ 'paddingTop': `${paddingtop}px` }} />
     )
   }
 }
--- packages/taro-components/src/components/picker-view/picker-view.tsx
+++ packages/taro-components/src/components/picker-view/picker-view.tsx
@@ -1,17 +1,75 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Component, h, Host } from '@stencil/core'
+import { Component, h, Host, Element, Prop, Event, EventEmitter, Listen } from '@stencil/core'
 
 @Component({
-  tag: 'taro-picker-view-core'
+  tag: 'taro-picker-view-core',
+  styleUrl: './style/index.scss'
 })
+
 export class PickerView {
-  componentDidLoad () {
-    console.error('H5 暂不支持 PickerView 组件！')
+  private indicator: HTMLDivElement | undefined
+
+  @Element() el: HTMLElement
+
+  @Prop() indicatorStyle: string
+  @Prop() indicatorClass: string
+
+  @Prop() value: number[]
+
+  @Prop() maskStyle: string
+  @Prop() maskClass: string
+
+  @Event({
+    eventName: 'change'
+  })
+  onChange: EventEmitter
+
+  @Event() onPickStart: EventEmitter
+  @Event() onPickEnd: EventEmitter
+
+  @Listen('col-select')
+  function(e: CustomEvent<{ curIndex: string, selectedIndex: string }>) {
+    e.stopPropagation()
+    if ((e.target as Element).tagName !== 'TARO-PICKER-VIEW-COLUMN-CORE') return
+    let _curIndex: number = +e.detail.curIndex
+    let _selectedIndex = +e.detail.selectedIndex
+    this.value[_curIndex] = _selectedIndex
+    this.onChange.emit({ value: this.value })
+  }
+  componentDidLoad() {
+    const childList = this.el.querySelectorAll('taro-picker-view-column-core')
+    childList.forEach((element, index) => {
+      element.setAttribute('tag', `${index}`)
+      let selectindex = '0'
+      if (!!this.value && this.value.length > index) {
+        selectindex = `${this.value[index]}`
+      }
+      let indicatorHeight: number = this.indicator?.clientHeight || 0
+      let paddingtop = (this.el.clientHeight - indicatorHeight) / 2.0
+      element.setAttribute('initselectindex', `${selectindex}`)
+      element.setAttribute('paddingtop', `${paddingtop}`)
+    })
   }
 
-  render () {
+  /// 过滤非PickerViewColumn组件
+  componentDidRender() {
+    this.el.childNodes.forEach(item => {
+      let childEle = (item as Element)
+      if ('TARO-PICKER-VIEW-COLUMN-CORE' !== childEle.tagName && childEle.className !== '_picker-view-mask-container') {
+        this.el.removeChild(item)
+      }
+    })
+  }
+  render() {
     return (
-      <Host></Host>
+      <Host class="_picker-view-container">
+        <slot />
+        <div class='_picker-view-mask-container'>
+          <div class='_picker-view-mask-top' />
+          <div class='_picker-view-mask-indicator' ref={indicator => this.indicator = indicator} />
+          <div class='_picker-view-mask-bottom' />
+        </div>
+      </Host>
     )
   }
 }
