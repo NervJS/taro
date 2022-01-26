9849696c52305d73a9e7c3ab6b0d404875eb7122
xiaxinyao1 <xiaxinyao1@jd.com>
2022-01-26 16:37:01 +0800

    chore: 模块依赖升级；更新业务数据

--- docs/news.js
+++ docs/news.js
@@ -18,9 +18,9 @@ function Index() {
   useEffect(() => {
     // 注册资源位
     register({
-      zoneId: '1480870218102263809',  // 羚珑专区id
+      zoneId: '1485579794201481218',  // 羚珑专区id
       debug: false, // 是否开启debug模式
-      positionId: '1480873921702068225', // 必传参数，资源位id
+      positionId: '1485796590212263937', // 必传参数，资源位id
       type: 'img', // 资源位类型
       containerId: 'news', // 注册资源位成功后，资源位预览图的 DOM 节点
       backupUrl: 'https://img20.360buyimg.com/img/jfs/t1/220737/13/10934/81315/61dd7009Ead9f1ca0/81f28ce8f80dce8e.jpg',  // 兜底图
