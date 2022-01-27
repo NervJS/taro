e6f7ea3cf1758160d7bee6e1e0466b2b46f50f5b
xiaxinyao1 <xiaxinyao1@jd.com>
2022-01-27 14:18:25 +0800

    fix: incorrect backup link

--- docs/news.js
+++ docs/news.js
@@ -23,7 +23,7 @@ function Index() {
       type: 'img', // 资源位类型
       containerId: 'news', // 注册资源位成功后，资源位预览图的 DOM 节点
       backupUrl: 'https://img20.360buyimg.com/img/jfs/t1/220737/13/10934/81315/61dd7009Ead9f1ca0/81f28ce8f80dce8e.jpg',  // 兜底图
-      backupLink: 'www.baidu.com',  // 兜底链接
+      backupLink: 'https://mp.weixin.qq.com/s/Hud405mLileIEzHX3Z-ueQ',  // 兜底链接
       onClick (e, { url }) {
         // 点击时的回调
         window.open(url);
