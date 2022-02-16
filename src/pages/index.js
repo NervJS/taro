01bcfa4b936cd88bb746a3ba4b760126e4f452ac
huangli47 <huangli@jd.com>
2022-02-16 16:10:08 +0800

    fix: home page 404

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import {Redirect} from '@docusaurus/router';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function Home() {
-  const url = useBaseUrl('/docs/README')
+  const url = useBaseUrl('/docs')
   return <Redirect to={url} />;
 }
 
