f6ab94b0bce94e0b106e891101397d69d852cc02
chenjiajian <798095202@qq.com>
2022-02-16 10:00:55 +0800

    docs(examples): 增加开发微信小程序插件的例子，fix #10796

---
+++ examples/build-weapp-plugin/src/component/comp.js
@@ -0,0 +1,23 @@
+// component/comp.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
