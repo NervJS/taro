eea79ebd7f7513d7a0f5d41ca907f193f1d351ff
chenjiajian <798095202@qq.com>
2022-02-17 19:20:40 +0800

    chore: add example blended-basic

---
+++ examples/blended-basic/miniapp/utils/util.js
@@ -0,0 +1,24 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : `0${n}`
+}
+
+const logRaw = () => {
+  console.log('I m raw')
+}
+
+module.exports = {
+  formatTime,
+  logRaw
+}
