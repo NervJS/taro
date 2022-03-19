9a5eb39f0c4560536ea045280a843c5fc7deffcf
bingtsingw <bingtsingw@gmail.com>
2022-03-19 11:26:50 +0800

    feat(shared): 创建component template支持'eh'

--- packages/shared/src/template.ts
+++ packages/shared/src/template.ts
@@ -122,7 +122,7 @@ export class BaseTemplate {
         for (let prop in component) {
           if (hasOwn(component, prop)) {
             let propValue = component[prop]
-            if (prop.startsWith('bind')) {
+            if (prop.startsWith('bind') || propValue === 'eh') {
               propValue = 'eh'
             } else if (propValue === '') {
               propValue = `i.${toCamelCase(prop)}`
