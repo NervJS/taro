3c45f3c374018b571f7ad2986ae8f25d2ba0cfad
advancedcat <wshx1938@gmail.com>
2022-04-16 14:08:04 +0800

    fix(api): 修复调用request后onHeadersReceived未定义的问题, fix #11224

--- packages/shared/src/native-apis.ts
+++ packages/shared/src/native-apis.ts
@@ -287,8 +287,9 @@ function processApis (taro, global, config: IProcessApisIOptions = {}) {
         })
 
         // 给 promise 对象挂载属性
-        if (key === 'uploadFile' || key === 'downloadFile') {
-          task && ['abort', 'offHeadersReceived', 'offProgressUpdate', 'onHeadersReceived', 'onProgressUpdate'].forEach(method => {
+        if (['request', 'uploadFile', 'downloadFile'].includes(key)) {
+          const taskMethods = ['abort', 'onHeadersReceived', 'offHeadersReceived', 'onProgressUpdate', 'offProgressUpdate', 'onChunkReceived', 'offChunkReceived']
+          task && taskMethods.forEach(method => {
             if (method in task) {
               p[method] = task[method].bind(task)
             }
