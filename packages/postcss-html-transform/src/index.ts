db866eb142f632710724828fdf2f64b57ea2a4bf
Zakary <zakarycode@gmail.com>
2022-05-07 15:27:05 +0800

    feat(template): 支持 pnmp for h5

--- packages/postcss-html-transform/src/index.ts
+++ packages/postcss-html-transform/src/index.ts
@@ -3,15 +3,15 @@ import postcss from 'postcss'
 export default postcss.plugin('postcss-html-transform', plugin)
 
 const htmlTags = ['html', 'body', 'a', 'audio', 'button', 'canvas', 'form', 'iframe', 'img', 'input', 'label', 'progress', 'select', 'slot', 'textarea', 'video', 'abbr', 'area', 'b', 'bdi', 'big', 'br', 'cite', 'code', 'data', 'datalist', 'del', 'dfn', 'em', 'i', 'ins', 'kbd', 'map', 'mark', 'meter', 'output', 'picture', 'q', 's', 'samp', 'small', 'span', 'strong', 'sub', 'sup', 'td', 'template', 'th', 'time', 'tt', 'u', 'var', 'wbr', 'address', 'article', 'aside', 'blockquote', 'caption', 'dd', 'details', 'dialog', 'div', 'dl', 'dt', 'fieldset', 'figcaption', 'figure', 'footer', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'hgroup', 'hr', 'legend', 'li', 'main', 'nav', 'ol', 'p', 'pre', 'section', 'summary', 'table', 'tbody', 'tfoot', 'thead', 'tr', 'ul', 'svg']
-const miniAppTag = ['cover-image', 'cover-image', 'match-media', 'movable-area', 'movable-view', 'page-container', 'scroll-view', 'share-element', 'swiper', 'swiper-item', 'view', 'icon', 'progress', 'rich-text', 'text', 'button', 'checkbox', 'checkbox-group', 'editor', 'form', 'input', 'keyboard-accessory', 'label', 'picker', 'picker-view', 'picker-view-column', 'radio', 'radio-group', 'slider', 'switch', 'textarea', 'functional-page-navigator', 'navigator', 'audio', 'camera', 'image', 'live-player', 'live-pusher', 'video', 'voip-room', 'map', 'canvas', 'web-view', 'ad', 'ad-custom', 'official-account', 'open-data', 'native-component', 'aria-component', 'navigation-bar', 'page-meta']
-const tag2Rgx = (tags: string[] = []) => new RegExp(`(^| |\\+|,|~|>|\\n)(${tags.join('|')})\\b(?=$| |\\.|\\+|,|~|:|\\[)`, 'g')
+const miniAppTags = ['cover-image', 'cover-view', 'match-media', 'movable-area', 'movable-view', 'page-container', 'scroll-view', 'share-element', 'swiper', 'swiper-item', 'view', 'icon', 'progress', 'rich-text', 'text', 'button', 'checkbox', 'checkbox-group', 'editor', 'form', 'input', 'keyboard-accessory', 'label', 'picker', 'picker-view', 'picker-view-column', 'radio', 'radio-group', 'slider', 'switch', 'textarea', 'functional-page-navigator', 'navigator', 'audio', 'camera', 'image', 'live-player', 'live-pusher', 'video', 'voip-room', 'map', 'canvas', 'web-view', 'ad', 'ad-custom', 'official-account', 'open-data', 'navigation-bar', 'page-meta']
+const tags2Rgx = (tags: string[] = []) => new RegExp(`(^| |\\+|,|~|>|\\n)(${tags.join('|')})\\b(?=$| |\\.|\\+|,|~|:|\\[)`, 'g')
 
 function plugin (options) {
   let selectorFilter
   let walkRules
   switch (options.platform) {
     case 'h5': {
-      selectorFilter = tag2Rgx(miniAppTag)
+      selectorFilter = tags2Rgx(miniAppTags)
       walkRules = (rule: postcss.Rule) => {
         rule.selector = rule.selector.replace(selectorFilter, '$1taro-$2-core')
       }
@@ -25,7 +25,7 @@ function plugin (options) {
     }
     default: {
       // mini-program
-      const selector = tag2Rgx(htmlTags)
+      const selector = tags2Rgx(htmlTags)
       walkRules = (rule: postcss.Rule) => {
         if (/(^| )\*(?![=/*])/.test(rule.selector)) {
           rule.remove()
