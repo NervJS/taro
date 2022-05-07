20a3500bc66007787172d785f18c52b700bdf64b
Zakary <zakarycode@gmail.com>
2022-05-07 16:49:54 +0800

    fix(vue3): 修复 vue3 组件导出警告

--- packages/taro-components/h5/vue3/index.js
+++ packages/taro-components/h5/vue3/index.js
@@ -1,17 +1,14 @@
 import * as components from './components'
 
-export * from './components'
-
 export function initVue3Components (app) {
   app.config.isCustomElement = tag => /^taro-/.test(tag) || tag === 'root' || tag === 'block'
 
-  Object.keys(components).forEach(name => {
-    if (components.hasOwnProperty(name)) {
+  Object.entries(components).forEach(([name, component]) => {
+    if (component) {
       const tagName = 'taro-' + name.replace(/(?<=[a-z])([A-Z])/g, '-$1').toLowerCase()
-      const component = components[name]
-      if (component) {
-        app.component(tagName, component)
-      }
+      app.component(tagName, component)
     }
   })
 }
+
+export * from './components'
