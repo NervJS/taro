c8dde7219f7e557fa241b930314014f239a9ccf8
kingzez <vincent.mr.w@gmail.com>
2022-06-01 15:51:49 +0800

    refactor(cli): init method optimize

--- packages/create-app/createApp.ts
+++ packages/create-app/createApp.ts
@@ -1,8 +1,14 @@
+import * as minimist from 'minimist'
 import { Project } from '@tarojs/cli'
 
-export function init (options) {
+export function init () {
+  const argv = minimist(process.argv.slice(2), {
+    boolean: ['typescript', 'ts']
+  })
+  const projectName: string | undefined = argv._[0]
+  const typescript: boolean | undefined = argv.typescript || argv.ts || undefined
   const appPath = process.cwd()
-  const { projectName, typescript } = options
+
   const project = new Project({
     projectName,
     projectDir: appPath,
--- packages/create-app/index.ts
+++ packages/create-app/index.ts
@@ -1,17 +1,7 @@
 #!/usr/bin/env node
 
-import * as minimist from 'minimist'
 import { init } from './createApp'
 import { printPkgVersion } from './util'
 
-const argv = minimist(process.argv.slice(2), {
-  boolean: ['typescript', 'ts']
-})
-const projectName: string | undefined = argv._[0]
-const typescript: boolean | undefined = argv.typescript || argv.ts || undefined
-
 printPkgVersion()
-init({
-  projectName,
-  typescript
-})
+init()
