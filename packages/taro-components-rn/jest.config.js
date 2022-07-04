7ba71ff6a26fa07f77e3a8513952faee4ddad589
zhiqingchen <329604487@qq.com>
2022-07-04 18:24:57 +0800

    fix: components-rn & taro-rn test case

--- packages/taro-components-rn/jest.config.js
+++ packages/taro-components-rn/jest.config.js
@@ -7,6 +7,6 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest'
   },
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+    'node_modules/(?!(.*(jest-)?react-native|.*@react-native(-community)?)|.*expo|.*@react-navigation/.*|.*react-native-svg)'
   ]
 }
