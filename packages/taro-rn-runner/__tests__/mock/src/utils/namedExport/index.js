a9242901187fdac614d852e81f4c69ceb0e53fb6
zhiqingchen <329604487@qq.com>
2022-07-11 13:07:58 +0800

    feat(rn): mv deprecated packages

--- packages/taro-rn-runner/__tests__/mock/src/utils/namedExport/index.js
+++ packages/taro-rn-runner/__tests__/mock/src/utils/namedExport/index.js
@@ -1,3 +1,2 @@
-// import { v4 as uuidv4 } from 'uuid';
 import * as fileBuild from 'expo-file-system/build/FileSystem'
 console.log({fileBuild})
--- packages/taro-rn-runner/__tests__/mock/src/utils/requireReactNative/index.js
+++ packages/taro-rn-runner/__tests__/mock/src/utils/requireReactNative/index.js
@@ -1,3 +1,2 @@
-// import { v4 as uuidv4 } from 'uuid';
 import silblings from 'react-native-root-siblings'
 console.log({silblings})
