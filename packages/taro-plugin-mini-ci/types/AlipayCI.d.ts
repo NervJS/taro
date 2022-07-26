27ffbc462c0baac29572accaaab68e1b489b0bd1
beezen <1184031131@qq.com>
2022-07-26 17:10:56 +0800

    feat(plugin-mini-ci): 新增支付宝小程序支持cli --open 打开开发者工具

--- packages/taro-plugin-mini-ci/types/AlipayCI.d.ts
+++ packages/taro-plugin-mini-ci/types/AlipayCI.d.ts
@@ -2,6 +2,8 @@ import BaseCI from './BaseCi';
 /** 文档地址： https://opendocs.alipay.com/mini/miniu/api */
 export default class AlipayCI extends BaseCI {
     miniu: any;
+    /** 小程序开发者工具安装路径 */
+    private devToolsInstallPath;
     protected _init(): void;
     open(): void;
     upload(): Promise<void>;
