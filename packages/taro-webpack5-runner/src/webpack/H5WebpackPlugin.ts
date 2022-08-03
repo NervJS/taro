4c8acb978e9bf21a3f6950e870521af71c481f16
Zakary <zakarycode@gmail.com>
2022-08-03 13:52:42 +0800

    fix(postcss): pxtransform 语法优化

--- packages/taro-webpack5-runner/src/webpack/H5WebpackPlugin.ts
+++ packages/taro-webpack5-runner/src/webpack/H5WebpackPlugin.ts
@@ -83,7 +83,7 @@ export class H5WebpackPlugin {
     const options = this.pxtransformOption?.config || {}
     const max = options?.maxRootSize ?? 40
     const min = options?.minRootSize ?? 20
-    const baseFontSize = options?.baseFontSize ?? min > 1 ? min : 20
+    const baseFontSize = options?.baseFontSize || min > 1 ? min : 20
     const designWidth = (input => typeof options.designWidth === 'function'
       ? options.designWidth(input)
       : options.designWidth)(baseFontSize)
