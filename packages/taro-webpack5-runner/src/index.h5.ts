0a15c32e60c18c7d1a025e56ba04f407530340de
Zakary <zakarycode@gmail.com>
2022-08-11 16:51:08 +0800

    fix(runner): 修复 proxy 合并错误问题 fix #12265

--- packages/taro-webpack5-runner/src/index.h5.ts
+++ packages/taro-webpack5-runner/src/index.h5.ts
@@ -132,11 +132,11 @@ export default async function build (appPath: string, rawConfig: H5BuildConfig):
 async function getDevServerOptions (appPath: string, config: H5BuildConfig): Promise<WebpackDevServer.Configuration> {
   const publicPath = parsePublicPath(config.publicPath)
   const outputPath = path.join(appPath, config.outputRoot || 'dist')
-  const customDevServerOption = config.devServer || {}
+  const { proxy: customProxy = [], ...customDevServerOption } = config.devServer || {}
   const routerConfig = config.router || {}
   const routerMode = routerConfig.mode || 'hash'
   const isMultiRouterMode = routerMode === 'multi'
-  const proxy = {}
+  const proxy: WebpackDevServer.Configuration['proxy'] = []
   if (isMultiRouterMode) {
     const app = new H5AppInstance(config.entry as EntryNormalized, {
       sourceDir: path.join(appPath, config.sourceRoot || SOURCE_DIR),
@@ -180,7 +180,24 @@ async function getDevServerOptions (appPath: string, config: H5BuildConfig): Pro
         }
       }
     }
-    proxy[routerBasename] = { bypass }
+    proxy.push({
+      context: [routerBasename],
+      bypass
+    })
+  }
+
+  if (!(customProxy instanceof Array)) {
+    proxy.push(...Object.entries(customProxy).map(([url, options = {}]) => {
+      const item: WebpackDevServer.ProxyConfigArrayItem = {
+        context: [url]
+      }
+      if (typeof options === 'string') {
+        item.target = options
+      } else {
+        Object.assign(item, options)
+      }
+      return item
+    }))
   }
 
   const chunkFilename = config.output.chunkFilename ?? `${config.chunkDirectory || 'chunk'}/[name].js`
