bbd27dbe75ddd6305bb075be157edfdf8ddd3780
Aliom252181 <13674546193@163.com>
2022-08-20 12:03:22 +0800

    Update multiSelector.tsx

--- packages/taro-components-rn/src/components/Picker/multiSelector.tsx
+++ packages/taro-components-rn/src/components/Picker/multiSelector.tsx
@@ -137,7 +137,7 @@ export default class MultiSelector extends React.Component<MultiSelectorProps, M
   }
 
   render(): JSX.Element {
-    const { children, disabled, itemStyle,indicatorStyle } = this.props
+    const { children, disabled, itemStyle, indicatorStyle } = this.props
     const { cols, range, value } = this.state
 
     return (
