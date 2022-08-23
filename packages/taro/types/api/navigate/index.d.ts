554d01bbf85b1fff9018763efcb9158df369d2e0
sushi-su <s.xiuxing@foxmail.com>
2022-08-23 18:21:18 +0800

    feat(weapp): 补充 openBusinessView 的类型定义

--- packages/taro/types/api/navigate/index.d.ts
+++ packages/taro/types/api/navigate/index.d.ts
@@ -85,6 +85,81 @@ declare module '../../index' {
     }
   }
 
+  namespace openBusinessView {
+    /**
+     * 业务参数：需要传递给支付分的业务数据
+     * @interface ExtraData
+     */
+    interface ExtraData {
+      /**
+       * 商户号：微信支付分配的商户号。
+       * 示例值：1230000109
+       * @type {string[1,32]}
+       * @memberof ExtraData
+       */
+      mch_id: string
+      /**
+       * 服务ID
+       * 示例值：88888888000011
+       * @type {string[1,32]}
+       * @memberof ExtraData
+       */
+      service_id: string
+      /**
+       * 商户服务订单号：商户系统内部服务订单号（不是交易单号）。
+       * 示例值：234323JKHDFE1243252
+       * @type {string[1,32]}
+       * @memberof ExtraData
+       */
+      out_order_no: string
+      /**
+       * 时间戳：生成签名时间戳，单位秒。
+       * 示例值：1530097563
+       * @type {string[1,32]}
+       * @memberof ExtraData
+       */
+      timestamp: string
+      /**
+       * 随机字符串：生成签名随机串。由数字、大小写字母组成，长度不超过32位。
+       * 示例值：zyx53Nkey8o4bHpxTQvd8m7e92nG5mG2
+       * @type {string[1,32]}
+       * @memberof ExtraData
+       */
+      nonce_str: string
+      /**
+       * 签名方式：签名类型，仅支持HMAC-SHA256。
+       * 示例值：HMAC-SHA256
+       * @type {string[1,32]}
+       * @memberof ExtraData
+       */
+      sign_type: string
+      /**
+       * 签名：使用字段mch_id、service_id、out_order_no、timestamp、nonce_str、sign_type按照签名生成算法计算得出的签名值。
+       * 示例值：029B52F67573D7E3BE74904BF9AEA
+       * @type {string[1,64]}
+       * @memberof ExtraData
+       */
+      sign: string
+    }
+
+    interface Option {
+      /**
+       * 跳转类型：固定配置：wxpayScoreDetail
+       * 示例值：wxpayScoreDetail
+       * @type {string[1,16]}
+       * @memberof Option
+       */
+      businessType: string
+      extraData: ExtraData
+      /** 接口调用成功的回调函数 */
+      success?: (res: TaroGeneral.CallbackResult) => void
+      /** 接口调用失败的回调函数 */
+      fail?: (res: TaroGeneral.CallbackResult) => void
+      /** 接口调用结束的回调函数（调用成功、失败都会执行） */
+      complete?: (res: TaroGeneral.CallbackResult) => void
+    }
+  }
+
   interface TaroStatic {
     /** 打开半屏小程序。接入指引请参考 [半屏小程序能力](https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/openEmbeddedMiniProgram.html)。
      * @supported weapp
@@ -153,5 +228,40 @@ declare module '../../index' {
      * @see https://developers.weixin.qq.com/miniprogram/dev/api/navigate/wx.exitMiniProgram.html
      */
     exitMiniProgram(option?: exitMiniProgram.Option): Promise<TaroGeneral.CallbackResult>
+
+    /** 商户通过调用订单详情接口打开微信支付分小程序，引导用户查看订单详情（小程序端）
+     * @supported weapp
+     * @example
+     * ```tsx
+     * if (wx.openBusinessView) {
+     *   wx.openBusinessView({
+     *     businessType: 'wxpayScoreDetail',
+     *     extraData: {
+     *       mch_id: '1230000109',
+     *       service_id: '88888888000011',
+     *       out_order_no: '1234323JKHDFE1243252',
+     *       timestamp: '1530097563',
+     *       nonce_str: 'zyx53Nkey8o4bHpxTQvd8m7e92nG5mG2',
+     *       sign_type: 'HMAC-SHA256',
+     *       sign: '029B52F67573D7E3BE74904BF9AEA'
+     *     },
+     *     success() {
+     *       //dosomething
+     *     },
+     *     fail() {
+     *       //dosomething
+     *     },
+     *     complete() {
+     *       //dosomething
+     *     }
+     *   });
+     * } else {
+     *   //引导用户升级微信版本
+     * }
+     * ```
+     *
+     * @see https://pay.weixin.qq.com/wiki/doc/apiv3/apis/chapter6_1_25.shtml
+     */
+    openBusinessView(option: openBusinessView.Option): Promise<TaroGeneral.CallbackResult>
   }
 }
