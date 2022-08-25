5b48533210ef2f65b6ec2847cb83e8252c9683c0
bigMeow <running@vip.163.com>
2022-08-25 23:52:11 +0800

    feat(taro-plugin-mini-ci): 微信小程序预览、上传后统一输出二维码地址和内容

--- packages/taro-plugin-mini-ci/src/WeappCI.ts
+++ packages/taro-plugin-mini-ci/src/WeappCI.ts
@@ -8,6 +8,7 @@ import * as os from 'os'
 import * as path from 'path'
 
 import BaseCI from './BaseCi'
+import { generateQrcodeImageFile, printQrcode2Terminal, readQrcodeImageContent } from './utils/qrcode'
 
 export default class WeappCI extends BaseCI {
   private instance: Project
@@ -82,29 +83,40 @@ export default class WeappCI extends BaseCI {
 
   async preview () {
     const { chalk, printLog, processTypeEnum } = this.ctx.helper
+    const { outputPath } = this.ctx.paths
     try {
       printLog(processTypeEnum.START, '上传开发版代码到微信后台并预览')
+      const previewQrcodePath = path.join(outputPath, 'preview.jpg')
       const uploadResult = await ci.preview({
         project: this.instance,
         version: this.version,
         desc: this.desc,
         onProgressUpdate: undefined,
-        robot: this.pluginOpts.weapp!.robot
+        robot: this.pluginOpts.weapp!.robot,
+        qrcodeFormat: 'image',
+        qrcodeOutputDest: previewQrcodePath
       })
-
       if (uploadResult.subPackageInfo) {
         const allPackageInfo = uploadResult.subPackageInfo.find((item) => item.name === '__FULL__')
         const mainPackageInfo = uploadResult.subPackageInfo.find((item) => item.name === '__APP__')
         const extInfo = `本次上传${allPackageInfo!.size / 1024}kb ${mainPackageInfo ? ',其中主包' + mainPackageInfo.size + 'kb' : ''}`
-        console.log(chalk.green(`上传成功 ${new Date().toLocaleString()} ${extInfo}`))
+        console.log(chalk.green(`开发版上传成功 ${new Date().toLocaleString()} ${extInfo}\n`))
+      }
+      try {
+        const qrContent = await readQrcodeImageContent(previewQrcodePath)
+        await printQrcode2Terminal(qrContent)
+        printLog(processTypeEnum.REMIND, `预览二维码已生成，存储在:"${previewQrcodePath}",二维码内容是：${qrContent}`)
+      } catch (error) {
+        printLog(processTypeEnum.ERROR, chalk.red(`获取预览二维码失败：${error.message}`))
       }
     } catch (error) {
-      console.log(chalk.red(`上传失败 ${new Date().toLocaleString()} \n${error.message}`))
+      printLog(processTypeEnum.ERROR, chalk.red(`上传失败 ${new Date().toLocaleString()} \n${error.message}`))
     }
   }
 
   async upload () {
     const { chalk, printLog, processTypeEnum } = this.ctx.helper
+    const { outputPath } = this.ctx.paths
     try {
       printLog(processTypeEnum.START, '上传体验版代码到微信后台')
       printLog(processTypeEnum.REMIND, `本次上传版本号为："${this.version}"，上传描述为：“${this.desc}”`)
@@ -120,10 +132,23 @@ export default class WeappCI extends BaseCI {
         const allPackageInfo = uploadResult.subPackageInfo.find((item) => item.name === '__FULL__')
         const mainPackageInfo = uploadResult.subPackageInfo.find((item) => item.name === '__APP__')
         const extInfo = `本次上传${allPackageInfo!.size / 1024}kb ${mainPackageInfo ? ',其中主包' + mainPackageInfo.size + 'kb' : ''}`
-        console.log(chalk.green(`上传成功 ${new Date().toLocaleString()} ${extInfo}`))
+        console.log(chalk.green(`上传成功 ${new Date().toLocaleString()} ${extInfo}\n`))
+      }
+
+      try {
+        const uploadQrcodePath = path.join(outputPath, 'upload.png')
+        // 体验码规则： https://open.weixin.qq.com/sns/getexpappinfo?appid=xxx&path=入口路径.html#wechat-redirect
+        const qrContent = `https://open.weixin.qq.com/sns/getexpappinfo?appid=${this.pluginOpts.weapp!.appid}#wechat-redirect`
+        await printQrcode2Terminal(qrContent)
+        await generateQrcodeImageFile(uploadQrcodePath, qrContent)
+        printLog(processTypeEnum.REMIND, `体验版二维码已生成，存储在:"${uploadQrcodePath}",二维码内容是："${qrContent}"`)
+        printLog(processTypeEnum.REMIND, `可能需要您前往微信后台，将当前上传版本设置为“体验版”`)
+        printLog(processTypeEnum.REMIND, `若本次上传的robot机器人和上次一致，并且之前已经在微信后台设置其为“体验版”，则本次无需再次设置`)
+      } catch (error) {
+        printLog(processTypeEnum.ERROR, chalk.red(`体验二维码生成失败：${error.message}`))
       }
     } catch (error) {
-      console.log(chalk.red(`上传失败 ${new Date().toLocaleString()} \n${error.message}`))
+      printLog(processTypeEnum.ERROR, chalk.red(`上传失败 ${new Date().toLocaleString()} \n${error.message}`))
     }
   }
 }
