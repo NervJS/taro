229bfc2bcee6599bdffda62aa8cfb31bcb1d8033
bigMeow <running@vip.163.com>
2022-08-26 16:30:13 +0800

    feat(taro-plugin-mini-ci): 百度小程序CI预览统一输出预览码路径和内容

--- packages/taro-plugin-mini-ci/src/SwanCI.ts
+++ packages/taro-plugin-mini-ci/src/SwanCI.ts
@@ -3,7 +3,8 @@ import * as path from 'path'
 import * as shell from 'shelljs'
 
 import BaseCI from './BaseCi'
-import generateQrCode from './QRCode'
+import {  resolveNpmSync } from './utils/npm'
+import { generateQrcodeImageFile, printQrcode2Terminal } from './utils/qrcode'
 
 export default class SwanCI extends BaseCI {
   private swanBin
@@ -12,16 +13,38 @@ export default class SwanCI extends BaseCI {
     if (this.pluginOpts.swan == null) {
       throw new Error('请为"@tarojs/plugin-mini-ci"插件配置 "swan" 选项')
     }
+    const { chalk, printLog, processTypeEnum } = this.ctx.helper
     try {
-      this.swanBin = path.resolve(require.resolve('swan-toolkit'), '../../.bin/swan')
+      this.swanBin = resolveNpmSync('swan-toolkit/bin/swan',process.cwd())
     } catch (error) {
-      throw new Error('请安装依赖：swan-toolkit')
+      printLog(processTypeEnum.ERROR, chalk.red('请安装依赖：swan-toolkit'))
+      process.exit(1)
     }
   }
 
   open () {
     const { printLog, processTypeEnum } = this.ctx.helper
     printLog(processTypeEnum.WARNING, '百度小程序不支持 "--open" 参数打开开发者工具')
+  }
+
+  async preview () {
+    const { outputPath } = this.ctx.paths
+    const { printLog, processTypeEnum } = this.ctx.helper
+    const previewQrcodePath = path.join(outputPath, 'preview.png')
+    printLog(processTypeEnum.START, '预览百度小程序')
+    shell.exec(`${this.swanBin} preview --project-path ${outputPath} --token ${this.pluginOpts.swan!.token} --min-swan-version ${this.pluginOpts.swan!.minSwanVersion || '3.350.6'} --json`, async (_code, stdout, stderr) => {
+      if (!stderr) {
+        stdout = JSON.parse(stdout)
+        const qrContent = stdout.list[0].url
+        // console.log('预览图片：', stdout.list[0].urlBase64)
+        await printQrcode2Terminal(qrContent)
+        await generateQrcodeImageFile(previewQrcodePath, qrContent)
+        printLog(
+          processTypeEnum.REMIND,
+          `预览二维码已生成，存储在:"${ previewQrcodePath }",二维码内容是：${ qrContent }`
+        )
+      }
+    })
   }
 
   async upload () {
@@ -31,24 +54,11 @@ export default class SwanCI extends BaseCI {
     printLog(processTypeEnum.REMIND, `本次上传版本号为："${this.version}"，上传描述为：“${this.desc}”`)
     shell.exec(`${this.swanBin} upload --project-path ${outputPath} --token ${this.pluginOpts.swan!.token} --release-version ${this.version} --min-swan-version ${this.pluginOpts.swan!.minSwanVersion || '3.350.6'} --desc ${this.desc} --json`, (_code, _stdout, stderr) => {
       if (!stderr) {
+        // TODO 没有百度小程序账号， 不知道它的上传码规则
         // stdout = JSON.parse(stdout)
         console.log(chalk.green(`上传成功 ${new Date().toLocaleString()}`))
       }
     })
   }
-
-  async preview () {
-    const { outputPath } = this.ctx.paths
-    const { printLog, processTypeEnum } = this.ctx.helper
-    printLog(processTypeEnum.START, '预览百度小程序')
-    shell.exec(`${this.swanBin} preview --project-path ${outputPath} --token ${this.pluginOpts.swan!.token} --min-swan-version ${this.pluginOpts.swan!.minSwanVersion || '3.350.6'} --json`, (_code, stdout, stderr) => {
-      if (!stderr) {
-        stdout = JSON.parse(stdout)
-        console.log('在线预览地址：', stdout.list[0].url)
-        // console.log('预览图片：', stdout.list[0].urlBase64)
-        // 需要自己将预览二维码打印到控制台
-        generateQrCode(stdout.list[0].url)
-      }
-    })
-  }
+  
 }
