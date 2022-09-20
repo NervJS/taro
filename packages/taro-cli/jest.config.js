99eb448f3c1c870380bd119da1e7ebf7493eb665
Zakary <zakarycode@gmail.com>
2022-09-20 13:59:31 +0800

    fix(test): update cli babel check

--- packages/taro-cli/jest.config.js
+++ packages/taro-cli/jest.config.js
@@ -1,11 +1,22 @@
+const { jsWithTs: tsjPreset } = require('ts-jest/presets')
+
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx', 'node'],
-  testMatch: ['**/__tests__/?(*.)+(spec|test).[jt]s?(x)'],
   globals: {
     'ts-jest': {
       diagnostics: false
-    }
-  }
+    },
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx', 'node'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/?(*.)+(spec|test).[jt]s?(x)'],
+  testTimeout: 60000,
+  transform: {
+    '^.+\\.jsx?$': [ require.resolve('babel-jest'), { rootMode: 'upward' } ],
+    '^.+\\.tsx?$': 'ts-jest',
+    ...tsjPreset.transform
+  },
+  transformIgnorePatterns: [
+    'node_modules',
+  ],
 }
