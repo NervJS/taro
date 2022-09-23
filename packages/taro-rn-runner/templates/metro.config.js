b2979467004200476ab9d8fa340c2519c5f9bf8f
zhiqingchen <329604487@qq.com>
2022-09-23 14:03:30 +0800

    feat: refactory qrcode print

--- packages/taro-rn-runner/templates/metro.config.js
+++ packages/taro-rn-runner/templates/metro.config.js
@@ -5,7 +5,4 @@ module.exports = mergeConfig({
   // custom your metro config here
   // https://facebook.github.io/metro/docs/configuration
   resolver: {}
-}, getMetroConfig({
-  // whether to print the preview QR code
-  qr: process.env.NODE_ENV !== 'production'
-}))
+}, getMetroConfig())
