f56b832a9eeafee926badb381bf6006013b4f548
Zakary <zakarycode@gmail.com>
2022-10-17 20:08:11 +0800

    fix(events): add method & component not support events

--- packages/taro-components/src/components/map/map.tsx
+++ packages/taro-components/src/components/map/map.tsx
@@ -1,12 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Component, h, Host } from '@stencil/core'
+import { Component, ComponentInterface, h, Host } from '@stencil/core'
+
+import { notSupport } from '../../utils'
 
 @Component({
   tag: 'taro-map-core'
 })
-export class Map {
+export class Map implements ComponentInterface {
   componentDidLoad () {
-    console.error('H5 暂不支持 Map 组件！')
+    notSupport('Map', this)
   }
 
   render () {
