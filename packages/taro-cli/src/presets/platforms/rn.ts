92fe3271df210b44ccc245a8cd3b859026890386
zhiqingchen <329604487@qq.com>
2022-10-28 15:10:59 +0800

    feat: expo 47

--- packages/taro-cli/src/presets/platforms/rn.ts
+++ packages/taro-cli/src/presets/platforms/rn.ts
@@ -31,7 +31,7 @@ function makeSureReactNativeInstalled (workspaceRoot: string): Promise<void> {
       // 便于开发时切换版本
       const devTag = process.env.DEVTAG || ''
       console.log('Installing React-Native related packages:')
-      let packages = `react@^18.1.0 react-dom@^18.1.0 react-native@^0.70.1 expo@~46.0.10 @tarojs/taro-rn${devTag} @tarojs/components-rn${devTag} @tarojs/rn-runner${devTag}`
+      let packages = `react@^18.1.0 react-dom@^18.1.0 react-native@^0.70.4 expo@~47.0.0-beta.3 @tarojs/taro-rn${devTag} @tarojs/components-rn${devTag} @tarojs/rn-runner${devTag}`
       console.log(packages)
       // windows下不加引号的话，package.json中添加的依赖不会自动带上^
       packages = packages.split(' ').map(str => `"${str}"`).join(' ')
