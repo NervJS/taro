b5ca4d82197958ca352dad7f755b59e0c43aab2e
pengboUESTC <pengbotju@163.com>
2022-10-31 10:40:01 +0800

    feat: 正则调整

--- packages/taro-webpack-runner/src/util/chain.ts
+++ packages/taro-webpack-runner/src/util/chain.ts
@@ -512,9 +512,7 @@ export const parseModule = (appPath: string, {
        */
       if (/css-loader/.test(filename)) return true
       // 若包含 @tarojs/components，则跳过 babel-loader 处理
-
-      // eslint-disable-next-line no-useless-escape
-      if (/@tarojs[\\|\/]components/.test(filename)) return true
+      if (/@tarojs[\\/]components/.test(filename)) return true
 
       // 非 node_modules 下的文件直接走 babel-loader 逻辑
       if (!(/node_modules/.test(filename))) return false
