a0e9ee6036e67b7b27e4569d4516ca28cfc9ec25
pengmao <pengmao@kezaihui.com>
2022-12-08 18:53:20 +0800

    (chore):增加api注释

--- packages/taro/types/api/navigate/index.d.ts
+++ packages/taro/types/api/navigate/index.d.ts
@@ -9,16 +9,27 @@ declare module '../../index' {
       path?: string
       /** 需要传递给目标小程序的数据，目标小程序可在 `App.onLaunch`，`App.onShow` 中获取到这份数据。如果跳转的是小游戏，可以在 [Taro.onShow](#)、[Taro.getLaunchOptionsSync](/docs/apis/base/weapp/life-cycle/getLaunchOptionsSync) 中可以获取到这份数据数据。 */
       extraData?: TaroGeneral.IAnyObject
-      /** 要打开的小程序版本。仅在当前小程序为开发版或体验版时此参数有效。如果当前小程序是正式版，则打开的小程序必定是正式版。*/
+      /** 要打开的小程序版本。仅在当前小程序为开发版或体验版时此参数有效。如果当前小程序是正式版，则打开的小程序必定是正式版。 */
       envVersion?: keyof EnvVersion
       /** 小程序链接，当传递该参数后，可以不传 appId 和 path。链接可以通过【小程序菜单】->【复制链接】获取。 */
       shortLink?: string
+      /** 校验方式 。默认为binding */
+      verify?: keyof Verify
+      /** 不 reLaunch 目标小程序，直接打开目标跳转的小程序退后台时的页面，需满足以下条件：1. 目标跳转的小程序生命周期未被销毁；2. 且目标当次启动的path、query、apiCategory与上次启动相同。默认值为 false 。 */
+      noRelaunchIfPathUnchanged?: boolean
       /** 接口调用成功的回调函数 */
       success?: (res: TaroGeneral.CallbackResult) => void
       /** 接口调用失败的回调函数 */
       fail?: (res: TaroGeneral.CallbackResult) => void
       /** 接口调用结束的回调函数（调用成功、失败都会执行） */
       complete?: (res: TaroGeneral.CallbackResult) => void
+    }
+
+    interface Verify {
+      /** 校验小程序管理后台的绑定关系 */
+      binding
+      /** 校验目标打开链接是否为小程序联盟商品。 */
+      unionProduct
     }
 
     interface EnvVersion {
@@ -39,7 +50,7 @@ declare module '../../index' {
       path?: string
       /** 需要传递给目标小程序的数据，目标小程序可在 `App.onLaunch`，`App.onShow` 中获取到这份数据。如果跳转的是小游戏，可以在 [Taro.onShow](#)、[Taro.getLaunchOptionsSync](/docs/apis/base/weapp/life-cycle/getLaunchOptionsSync) 中可以获取到这份数据数据。 */
       extraData?: TaroGeneral.IAnyObject
-      /** 要打开的小程序版本。仅在当前小程序为开发版或体验版时此参数有效。如果当前小程序是正式版，则打开的小程序必定是正式版。*/
+      /** 要打开的小程序版本。仅在当前小程序为开发版或体验版时此参数有效。如果当前小程序是正式版，则打开的小程序必定是正式版。 */
       envVersion?: keyof EnvVersion
       /** 小程序链接，当传递该参数后，可以不传 appId 和 path。链接可以通过【小程序菜单】->【复制链接】获取。 */
       shortLink?: string
