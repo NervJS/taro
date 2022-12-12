08785cc680f8e7ebb10f7284e0499fde7794f2e6
bigMeow <lizong9527@gmail.com>
2022-12-12 14:07:13 +0800

    feat(taro-runtime): 支持创建anchorElement元素

--- packages/taro-runtime/src/dom/document.ts
+++ packages/taro-runtime/src/dom/document.ts
@@ -1,6 +1,7 @@
 import { controlledComponent, isUndefined } from '@tarojs/shared'
 
 import {
+  A,
   COMMENT,
   DOCUMENT_ELEMENT_NAME,
   ROOT_STR
@@ -13,6 +14,7 @@ import { NodeType } from '../dom/node_types'
 import { TaroRootElement } from '../dom/root'
 import { TaroText } from '../dom/text'
 import env from '../env'
+import { AnchorElement } from './anchorElement'
 
 export class TaroDocument extends TaroElement {
   public documentElement: TaroElement
@@ -27,15 +29,25 @@ export class TaroDocument extends TaroElement {
   }
 
   public createElement (type: string): TaroElement | TaroRootElement | FormElement {
-    if (type === ROOT_STR) {
+    const nodeName = type.toLocaleLowerCase()
+    if (nodeName === ROOT_STR) {
       return new TaroRootElement()
     }
 
-    const element = controlledComponent.has(type)
-      ? new FormElement()
-      : new TaroElement()
+    let element: TaroElement
+    switch (true) {
+      case controlledComponent.has(nodeName):
+        element = new FormElement()
+        break
+      case nodeName === A:
+        element = new AnchorElement()
+        break
+      default:
+        element = new TaroElement()
+        break
+    }
 
-    element.nodeName = type
+    element.nodeName = nodeName
     element.tagName = type.toUpperCase()
 
     return element
