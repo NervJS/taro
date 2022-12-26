4f6e0f55293d388719b135167f4ed9f9ef64967e
bigMeow <lizong9527@gmail.com>
2022-12-26 12:44:54 +0800

    feat(taro-plugin-http): 限定插件只对小程序生效

--- packages/taro-plugin-http/src/index.ts
+++ packages/taro-plugin-http/src/index.ts
@@ -15,36 +15,38 @@ interface IOptions {
 }
 
 export default (ctx: IPluginContext, options: IOptions) => {
-  ctx.modifyWebpackChain(({ chain }) => {
-    chain.plugin('definePlugin').tap((args) => {
-      args[0].ENABLE_COOKIE = options.enableCookie ?? false
-      return args
+  if (!['h5', 'rn'].includes(ctx.runOpts.options.platform)) {
+    ctx.modifyWebpackChain(({ chain }) => {
+      chain.plugin('definePlugin').tap((args) => {
+        args[0].ENABLE_COOKIE = options.enableCookie ?? false
+        return args
+      })
+
+      const runtimeAlia = `${packageName}/dist/runtime`
+      chain.resolve.alias.set(runtimeAlia, path.join(__dirname, 'runtime.js'))
+      // 注入相关全局BOM对象
+      chain.plugin('providerPlugin').tap((args) => {
+        args[0].XMLHttpRequest = [runtimeAlia, 'XMLHttpRequest']
+
+        // 实际上本runtime 没有实现 FormData 和 Blob 对象， 所以第三方库中的这2个对象会被替换成 undefined
+        // （axios这类请求库用到了这2个对象，所以要么实现它要么把它替换掉, 这里我们选择把它替换掉，这样可以确保除了上传以外的功能可以继续使用）
+        ;(options.disabledFormData ?? true) && (args[0].FormData ||= [runtimeAlia, 'FormData'])
+        ;(options.disabledBlob ?? true) && (args[0].Blob ||= [runtimeAlia, 'Blob'])
+
+        return args
+      })
     })
 
-    const runtimeAlia = `${packageName}/dist/runtime`
-    chain.resolve.alias.set(runtimeAlia, path.join(__dirname, 'runtime.js'))
-    // 注入相关全局BOM对象
-    chain.plugin('providerPlugin').tap((args) => {
-      args[0].XMLHttpRequest = [runtimeAlia, 'XMLHttpRequest']
-
-      // 实际上本runtime 没有实现 FormData 和 Blob 对象， 所以第三方库中的这2个对象会被替换成 undefined
-      // （axios这类请求库用到了这2个对象，所以要么实现它要么把它替换掉, 这里我们选择把它替换掉，这样可以确保除了上传以外的功能可以继续使用）
-      ;(options.disabledFormData ?? true) && (args[0].FormData ||= [runtimeAlia, 'FormData'])
-      ;(options.disabledBlob ?? true) && (args[0].Blob ||= [runtimeAlia, 'Blob'])
-
-      return args
+    ctx.registerMethod({
+      name: 'onSetupClose',
+      fn (platform: TaroPlatformBase) {
+        const injectedPath = `${packageName}/dist/runtime`
+        if (isArray(platform.runtimePath)) {
+          platform.runtimePath.push(injectedPath)
+        } else if (isString(platform.runtimePath)) {
+          platform.runtimePath = [platform.runtimePath, injectedPath]
+        }
+      },
     })
-  })
-
-  ctx.registerMethod({
-    name: 'onSetupClose',
-    fn (platform: TaroPlatformBase) {
-      const injectedPath = `${packageName}/dist/runtime`
-      if (isArray(platform.runtimePath)) {
-        platform.runtimePath.push(injectedPath)
-      } else if (isString(platform.runtimePath)) {
-        platform.runtimePath = [platform.runtimePath, injectedPath]
-      }
-    },
-  })
+  }
 }
