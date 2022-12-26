4676894a48614029209f3f8dc052e0c8cc58ebed
bigMeow <lizong9527@gmail.com>
2022-12-26 12:29:02 +0800

    fix(taro-plugin-http): 默认值调整

--- packages/taro-plugin-http/src/index.ts
+++ packages/taro-plugin-http/src/index.ts
@@ -6,25 +6,18 @@ import { name as packageName } from '../package.json'
 import type { IPluginContext, TaroPlatformBase } from '@tarojs/service'
 
 interface IOptions {
-  /** 支持 document.cookie 和 http 设置 cookie */
+  /** 支持 document.cookie 和 http 设置 cookie (默认false) */
   enableCookie?: boolean
-  /** 禁用掉 FormData 全局对象  */
+  /** 禁用掉 FormData 全局对象 (默认true禁用) */
   disabledFormData?: boolean
-  /** 禁用掉 Blob 全局对象 */
+  /** 禁用掉 Blob 全局对象 (默认true禁用) */
   disabledBlob?: boolean
 }
 
-export default (
-  ctx: IPluginContext,
-  options: IOptions = {
-    enableCookie: false,
-    disabledFormData: true,
-    disabledBlob: true,
-  }
-) => {
+export default (ctx: IPluginContext, options: IOptions) => {
   ctx.modifyWebpackChain(({ chain }) => {
     chain.plugin('definePlugin').tap((args) => {
-      args[0].ENABLE_COOKIE = options.enableCookie ?? true
+      args[0].ENABLE_COOKIE = options.enableCookie ?? false
       return args
     })
 
@@ -33,10 +26,12 @@ export default (
     // 注入相关全局BOM对象
     chain.plugin('providerPlugin').tap((args) => {
       args[0].XMLHttpRequest = [runtimeAlia, 'XMLHttpRequest']
+
       // 实际上本runtime 没有实现 FormData 和 Blob 对象， 所以第三方库中的这2个对象会被替换成 undefined
       // （axios这类请求库用到了这2个对象，所以要么实现它要么把它替换掉, 这里我们选择把它替换掉，这样可以确保除了上传以外的功能可以继续使用）
-      !options.disabledFormData && (args[0].FormData ||= [runtimeAlia, 'FormData'])
-      !options.disabledBlob && (args[0].Blob ||= [runtimeAlia, 'Blob'])
+      ;(options.disabledFormData ?? true) && (args[0].FormData ||= [runtimeAlia, 'FormData'])
+      ;(options.disabledBlob ?? true) && (args[0].Blob ||= [runtimeAlia, 'Blob'])
+
       return args
     })
   })
