10ff4a6f3471bcc94a21206636f58c7860108604
ZakaryCode <ZakaryCode@users.noreply.github.com>
2023-01-07 00:07:34 +0000

    refactor(types): sync components types

--- packages/taro-components/types/Video.d.ts
+++ packages/taro-components/types/Video.d.ts
@@ -352,6 +352,11 @@ interface VideoProps extends StandardProps {
    */
   preferredPeakBitRate?: number
 
+  /** 是否为直播源
+   * @supported weapp
+   */
+  isLive?: boolean
+
   /** 当开始/继续播放时触发 play 事件
    * @supported weapp, alipay, swan, tt, qq, jd, h5, rn
    */
