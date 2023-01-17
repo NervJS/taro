ea724bdf1eadabd333e1efb8782b2e61d4eef684
Chen Xianmin <xianmin12@gmail.com>
2023-01-17 16:23:30 +0800

    fix(h5): request 更严谨的判断 options.data 为 Object #9786

--- packages/taro-h5/src/api/network/request/index.ts
+++ packages/taro-h5/src/api/network/request/index.ts
@@ -56,7 +56,7 @@ function _request (options) {
   params.cache = options.cache || 'default'
   if (methodUpper === 'GET' || methodUpper === 'HEAD') {
     url = generateRequestUrlWithParams(url, options.data)
-  } else if (typeof options.data === 'object') {
+  } else if (Object.prototype.toString.call(options.data) === '[object Object]') {
     options.header = options.header || {}
 
     const keyOfContentType = Object.keys(options.header).find(item => item.toLowerCase() === 'content-type')
