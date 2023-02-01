4ad7956bc7f7f44295226ababe4e47efcf1cc148
dongji136 <dongji136@jd.com>
2023-02-01 14:58:06 +0800

    fix(runtime): fix vue3 createTaroHook

--- packages/taro-plugin-vue3/src/runtime/composition-functions.ts
+++ packages/taro-plugin-vue3/src/runtime/composition-functions.ts
@@ -8,6 +8,7 @@ import { isArray, isFunction, isUndefined } from '@tarojs/shared'
 import {
   inject,
   onMounted,
+  onUnmounted,
   ref
 } from 'vue'
 
@@ -16,8 +17,10 @@ function createTaroHook (lifecycle: keyof PageLifeCycle | keyof AppInstance) {
     const id = inject<string>('id')!
     const fnRef = ref(fn)
 
+    let inst: any
+    let callback
     onMounted(() => {
-      let inst: any = getPageInstance(id)
+      inst = getPageInstance(id)
       if (inst === undefined) {
         inst = Object.create({
           $options: {}
@@ -26,7 +29,7 @@ function createTaroHook (lifecycle: keyof PageLifeCycle | keyof AppInstance) {
       }
       inst = inst.$options
 
-      const callback = (...args: any) => fnRef.value(...args)
+      callback = (...args: any) => fnRef.value(...args)
       const currentCallback = inst[lifecycle]
 
       if (isUndefined(currentCallback)) {
@@ -35,6 +38,18 @@ function createTaroHook (lifecycle: keyof PageLifeCycle | keyof AppInstance) {
         inst[lifecycle] = [inst[lifecycle], callback]
       } else if (isArray(currentCallback)) {
         inst[lifecycle] = [...currentCallback, callback]
+      }
+    })
+
+    onUnmounted(() => {
+      if (!inst || !callback) {
+        return
+      }
+      const list = inst![lifecycle]
+      if (list === callback) {
+        inst[lifecycle] = undefined
+      } else if (isArray(list)) {
+        inst[lifecycle] = list.filter(item => item !== callback)
       }
     })
   }
