35dee7dbef85691e8d0a5799c7f523911256ecee
Zakary <zakarycode@gmail.com>
2023-02-07 21:30:30 +0800

    build(deps-dev): update vue deps-dev

--- packages/taro-plugin-vue3/src/index.ts
+++ packages/taro-plugin-vue3/src/index.ts
@@ -119,15 +119,3 @@ function setDefinePlugin (chain) {
       return args
     })
 }
-
-export function getVueLoaderPath (): string {
-  try {
-    return require.resolve('vue-loader', {
-      paths: [process.cwd()]
-    })
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(chalk.yellow('找不到 vue-loader，请先安装。'))
-    process.exit(1)
-  }
-}
