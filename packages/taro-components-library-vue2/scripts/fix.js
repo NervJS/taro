b2934d234e6b2a14e6a9bfc55a6e39868831aa04
Zakary <zakarycode@gmail.com>
2023-02-16 20:02:26 +0800

    feat(h5): 更改 stencil 组件注册方案

--- packages/taro-components-library-vue2/scripts/fix.js
+++ packages/taro-components-library-vue2/scripts/fix.js
@@ -6,11 +6,12 @@ const utilsPath = path.resolve(__dirname, '..', 'src/vue-component-lib/utils.ts'
 
 if (fs.existsSync(componentsPath)) {
   const codeBuffer = fs.readFileSync(componentsPath)
-  let code = codeBuffer.toString().replace(/const\sTaro([A-Za-z]+)\s=/g, 'const $1 =').replace(/const\s([A-Za-z]+)Core\s=/g, 'const $1 =')
+  let code = codeBuffer.toString().replace(/import\stype\s\{\s([^}]*)\s\}\sfrom\s'@tarojs\/components[^']*';/ig, `import type { $1 } from '@tarojs/components/dist/types/components';`)
+  code = code.replace(/const\sTaro([A-Za-z]+)\s=/g, 'const $1 =').replace(/const\s([A-Za-z]+)Core\s=/g, 'const $1 =')
 
   if (!code.includes('fragment')) {
     const comps = ['block', 'custom-wrapper']
-    code = code.replace(new RegExp(`taro-(${comps.join('|')})-core`, 'ig'), 'fragment')
+    code = code.replace(new RegExp(`'taro-(${comps.join('|')})-core'`, 'ig'), `'fragment'`)
   }
 
   fs.writeFileSync(componentsPath, code)
--- packages/taro-components-library-vue3/scripts/fix.js
+++ packages/taro-components-library-vue3/scripts/fix.js
@@ -6,9 +6,10 @@ const utilsPath = path.resolve(__dirname, '..', 'src/vue-component-lib/utils.ts'
 
 if (fs.existsSync(componentsPath)) {
   const codeBuffer = fs.readFileSync(componentsPath)
-  let code = codeBuffer.toString().replace(/const\sTaro([A-Za-z]+)\s=/g, 'const $1 =').replace(/const\s([A-Za-z]+)Core\s=/g, 'const $1 =')
+  let code = codeBuffer.toString().replace(/import\stype\s\{\s([^}]*)\s\}\sfrom\s'@tarojs\/components[^']*';/ig, `import type { $1 } from '@tarojs/components/dist/types/components';`)
+  code = code.replace(/const\sTaro([A-Za-z]+)\s=/g, 'const $1 =').replace(/const\s([A-Za-z]+)Core\s=/g, 'const $1 =')
 
-  if (!code.includes('Fragment')) {
+  if (!code.includes('slots.default')) {
     const comps = ['Block', 'CustomWrapper']
     code = code.replace('/* auto-generated vue proxies */', `/* auto-generated vue proxies */\nimport { defineComponent } from 'vue'`)
     code = code.replace(new RegExp(`export const (${comps.join('|')}) = \\/\\*\\@__PURE__\\*\\/ defineContainer[^;]*;`, 'ig'), 'export const $1 = defineComponent((__props, { slots }) => slots.default);')
