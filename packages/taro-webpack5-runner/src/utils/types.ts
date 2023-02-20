80837fd4acef24eaa1b5919ac34989dc30d6c8a4
Zakary <zakarycode@gmail.com>
2023-02-20 21:59:15 +0800

    feat(h5): 为 web 端新增组件使用遍历方案

--- packages/taro-webpack5-runner/src/utils/types.ts
+++ packages/taro-webpack5-runner/src/utils/types.ts
@@ -2,7 +2,7 @@ import type { RecursiveTemplate, UnRecursiveTemplate } from '@tarojs/shared/dist
 import type { IH5Config, IMiniAppConfig, IProjectBaseConfig } from '@tarojs/taro/types/compile'
 import type Webpack from 'webpack'
 import type { PrerenderConfig } from '../prerender/prerender'
-import type { IComponentConfig } from '../template/component'
+import type { IComponentConfig } from './component'
 
 export interface IOption {
   [key: string]: any
@@ -37,6 +37,9 @@ export interface IFileType {
 export interface CommonBuildConfig extends IProjectBaseConfig {
   entry?: Webpack.EntryObject
   mode: 'production' | 'development' | 'none'
+  /** hooks */
+  onCompilerMake: (compilation) => Promise<any>
+  onParseCreateElement: (nodeName, componentConfig) => Promise<any>
 }
 
 export interface MiniBuildConfig extends CommonBuildConfig, IMiniAppConfig {
@@ -56,8 +59,6 @@ export interface MiniBuildConfig extends CommonBuildConfig, IMiniAppConfig {
   hot?: boolean
   /** hooks */
   modifyComponentConfig: (componentConfig: IComponentConfig, config: Partial<MiniBuildConfig>) => Promise<any>
-  onCompilerMake: (compilation) => Promise<any>
-  onParseCreateElement: (nodeName, componentConfig) => Promise<any>
 }
 
 export interface H5BuildConfig extends CommonBuildConfig, IH5Config {
