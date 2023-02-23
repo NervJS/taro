5277a6de9a6ffd5b2eb9a42f535ca915a830dd47
TheKonka <1094918@gmail.com>
2023-02-23 17:18:33 +0800

    chore: revert bindFollowLifestyle

--- packages/taro-alipay/src/components.ts
+++ packages/taro-alipay/src/components.ts
@@ -41,7 +41,6 @@ export const components = {
     scope: '',
     'public-id': '',
     bindGetAuthorize: '',
-    bindFollowLifestyle: '',
     bindError: '',
     bindGetUserInfo: '',
     bindGetPhoneNumber: ''
