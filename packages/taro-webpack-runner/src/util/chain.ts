d9acf1bcad6a29e85da229238d4d8fb870fae413
Zakary <zakarycode@gmail.com>
2023-02-24 19:19:28 +0800

    fix(h5): react 组件适配器 style 属性对齐旧版特性 fix #13293 (#13305)
    * fix(scrips): update dev scripts
    
    * fix(h5): stencil tips error
    
    * chore(deps): 更新适配器版本
    
    * fix(h5): style 属性对齐旧版本适配器特性 fix #13293
    
    * fix(h5): 优化组件库 sourceMap 加载方案 fix #13301
    
    * fix(runner): remove useless vars
    
    * test: 更新snap
    
    ---------
    
    Co-authored-by: AzuFF <47267778+heiazu@users.noreply.github.com>
    Co-authored-by: zhuminghui2 <zhuminghui2@jd.com>

--- packages/taro-webpack-runner/src/util/chain.ts
+++ packages/taro-webpack-runner/src/util/chain.ts
@@ -83,52 +83,6 @@ const getPlugin = (plugin: any, args: Option[]) => {
   }
 }
 
-export const DEFAULT_Components = new Set<string>([
-  'view',
-  'scroll-view',
-  'swiper',
-  'cover-view',
-  'cover-image',
-  'icon',
-  'text',
-  'rich-text',
-  'progress',
-  'button',
-  'checkbox',
-  'form',
-  'input',
-  'label',
-  'picker',
-  'picker-view',
-  'picker-view-column',
-  'radio',
-  'radio-group',
-  'checkbox-group',
-  'slider',
-  'switch',
-  'textarea',
-  'navigator',
-  'audio',
-  'image',
-  'video',
-  'camera',
-  'live-player',
-  'live-pusher',
-  'map',
-  'canvas',
-  'open-data',
-  'web-view',
-  'swiper-item',
-  'movable-area',
-  'movable-view',
-  'functional-page-navigator',
-  'ad',
-  'block',
-  'import',
-  'official-account',
-  'editor'
-])
-
 const mergeOption = ([...options]: Option[]): Option => {
   return recursiveMerge({}, ...options)
 }
