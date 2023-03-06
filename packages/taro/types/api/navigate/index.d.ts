f83ce3805b5020aa66275619c11f475cbc1a8b19
ruidoc <2218466341@qq.com>
2023-03-06 14:33:31 +0800

    chore(type): 打开小程序，根据条件判断appId是否必传

--- packages/taro/types/api/navigate/index.d.ts
+++ packages/taro/types/api/navigate/index.d.ts
@@ -1,18 +1,28 @@
 import Taro from '../../index'
 
 declare module '../../index' {
+  /** BasicOption 用于控制 appId 和 shortLink 任意一项必传 */
+  type BasicOption =
+    | {
+        /** 要打开的小程序 appId */
+        appId?: string
+        /** 小程序链接，当传递该参数后，可以不传 appId 和 path。链接可以通过【小程序菜单】->【复制链接】获取。 */
+        shortLink: string
+      }
+    | {
+        /** 要打开的小程序 appId */
+        appId: string
+        /** 小程序链接，当传递该参数后，可以不传 appId 和 path。链接可以通过【小程序菜单】->【复制链接】获取。 */
+        shortLink?: string
+      }
   namespace openEmbeddedMiniProgram {
-    interface Option {
-      /** 要打开的小程序 appId */
-      appId: string
+    type Option = {
       /** 打开的页面路径，如果为空则打开首页。path 中 ? 后面的部分会成为 query，在小程序的 `App.onLaunch`、`App.onShow` 和 `Page.onLoad` 的回调函数或小游戏的 [Taro.onShow](#) 回调函数、[Taro.getLaunchOptionsSync](/docs/apis/base/weapp/life-cycle/getLaunchOptionsSync) 中可以获取到 query 数据。对于小游戏，可以只传入 query 部分，来实现传参效果，如：传入 "?foo=bar"。 */
       path?: string
       /** 需要传递给目标小程序的数据，目标小程序可在 `App.onLaunch`，`App.onShow` 中获取到这份数据。如果跳转的是小游戏，可以在 [Taro.onShow](#)、[Taro.getLaunchOptionsSync](/docs/apis/base/weapp/life-cycle/getLaunchOptionsSync) 中可以获取到这份数据数据。 */
       extraData?: TaroGeneral.IAnyObject
       /** 要打开的小程序版本。仅在当前小程序为开发版或体验版时此参数有效。如果当前小程序是正式版，则打开的小程序必定是正式版。 */
       envVersion?: keyof EnvVersion
-      /** 小程序链接，当传递该参数后，可以不传 appId 和 path。链接可以通过【小程序菜单】->【复制链接】获取。 */
-      shortLink?: string
       /** 校验方式 。默认为binding */
       verify?: keyof Verify
       /** 不 reLaunch 目标小程序，直接打开目标跳转的小程序退后台时的页面，需满足以下条件：1. 目标跳转的小程序生命周期未被销毁；2. 且目标当次启动的path、query、apiCategory与上次启动相同。默认值为 false 。 */
@@ -23,7 +33,7 @@ declare module '../../index' {
       fail?: (res: TaroGeneral.CallbackResult) => void
       /** 接口调用结束的回调函数（调用成功、失败都会执行） */
       complete?: (res: TaroGeneral.CallbackResult) => void
-    }
+    } & BasicOption
 
     interface Verify {
       /** 校验小程序管理后台的绑定关系 */
@@ -43,24 +53,20 @@ declare module '../../index' {
   }
 
   namespace navigateToMiniProgram {
-    interface Option {
-      /** 要打开的小程序 appId */
-      appId: string
+    type Option = {
       /** 打开的页面路径，如果为空则打开首页。path 中 ? 后面的部分会成为 query，在小程序的 `App.onLaunch`、`App.onShow` 和 `Page.onLoad` 的回调函数或小游戏的 [Taro.onShow](#) 回调函数、[Taro.getLaunchOptionsSync](/docs/apis/base/weapp/life-cycle/getLaunchOptionsSync) 中可以获取到 query 数据。对于小游戏，可以只传入 query 部分，来实现传参效果，如：传入 "?foo=bar"。 */
       path?: string
       /** 需要传递给目标小程序的数据，目标小程序可在 `App.onLaunch`，`App.onShow` 中获取到这份数据。如果跳转的是小游戏，可以在 [Taro.onShow](#)、[Taro.getLaunchOptionsSync](/docs/apis/base/weapp/life-cycle/getLaunchOptionsSync) 中可以获取到这份数据数据。 */
       extraData?: TaroGeneral.IAnyObject
       /** 要打开的小程序版本。仅在当前小程序为开发版或体验版时此参数有效。如果当前小程序是正式版，则打开的小程序必定是正式版。 */
       envVersion?: keyof EnvVersion
-      /** 小程序链接，当传递该参数后，可以不传 appId 和 path。链接可以通过【小程序菜单】->【复制链接】获取。 */
-      shortLink?: string
       /** 接口调用成功的回调函数 */
       success?: (res: TaroGeneral.CallbackResult) => void
       /** 接口调用失败的回调函数 */
       fail?: (res: TaroGeneral.CallbackResult) => void
       /** 接口调用结束的回调函数（调用成功、失败都会执行） */
       complete?: (res: TaroGeneral.CallbackResult) => void
-    }
+    } & BasicOption
 
     interface EnvVersion {
       /** 开发版 */
