773d19cb8cf5e8a22d20e77bb19fafc14869485e
Zakary <zakarycode@gmail.com>
2023-03-07 18:28:44 +0800

    feat(components): 移除 react 适配组件库新架构下冗余加载器

--- packages/taro-components-react/src/components/lottie/index.tsx
+++
@@ -1,3 +0,0 @@
-import './loader'
-
-export { Lottie as default } from '@tarojs/components/lib/react'
--- packages/taro-components-react/src/components/movable-area/index.tsx
+++
@@ -1,3 +0,0 @@
-import './loader'
-
-export { MovableArea, MovableView } from '@tarojs/components/lib/react'
