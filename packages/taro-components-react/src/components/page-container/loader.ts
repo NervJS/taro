c553095e31677709138be477688dc855a759e024
Zakary <zakarycode@gmail.com>
2023-03-07 18:28:44 +0800

    feat(components): 移除 react 适配组件库新架构下冗余加载器

--- packages/taro-components-react/src/components/page-container/loader.ts
+++
@@ -1,3 +0,0 @@
-import { defineCustomElement } from '@tarojs/components/dist/components/taro-page-container-core'
-
-defineCustomElement()
--- packages/taro-components-react/src/components/tabs/loader.ts
+++
@@ -1,3 +0,0 @@
-import { defineCustomElement } from '@tarojs/components/dist/components/taro-tabs-core'
-
-defineCustomElement()
