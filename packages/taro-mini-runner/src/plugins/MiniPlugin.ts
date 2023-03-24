e73b8b7198918f78fcbf810d1935ca59167d9a92
xuanzebin <xuanzebin3@jd.com>
2023-03-24 14:08:38 +0800

    fix(plugin): 修复插件编译后模版缺失小程序自定义组件的问题

--- packages/taro-mini-runner/src/plugins/MiniPlugin.ts
+++ packages/taro-mini-runner/src/plugins/MiniPlugin.ts
@@ -676,10 +676,22 @@ export default class TaroMiniPlugin {
     const filePath = file.path
     const fileConfigPath = file.isNative ? this.replaceExt(filePath, '.json') : this.getConfigFilePath(filePath)
     const fileConfig = readConfig(fileConfigPath)
+    const { componentGenerics } = fileConfig
+
     // 修复百度小程序内容服务组件使用新的引入方式"usingSwanComponents"导致的无法编译到页面配置json的问题
     // 获取 fileConfig 里面的匹配 "/^using[A-Za-z]*Components$/"的字段，之后合并到 usingComponents 中
     const usingArray = Object.keys(fileConfig).filter(item => /^using[A-Za-z]*Components$/.test(item)).map(item => fileConfig[item])
     const usingComponents = usingArray.length < 1 ? undefined : Object.assign({}, ...usingArray)
+
+    if (this.options.isBuildPlugin && componentGenerics) {
+      Object.keys(componentGenerics).forEach(component => {
+        if (componentGenerics[component]) {
+          if (!componentConfig.thirdPartyComponents.has(component)) {
+            componentConfig.thirdPartyComponents.set(component, new Set())
+          }
+        }
+      })
+    }
 
     // 递归收集依赖的第三方组件
     if (usingComponents) {
