2fcb01782cfdc0e140fecac57a34fd96144a4262
xuanzebin <xuanzebin3@jd.com>
2023-03-27 14:23:13 +0800

    test(jd-plugin): 更新京东小程序插件编译的测试 demo 代码

--- examples/build-weapp-plugin/src/plugin/components/avatar/avatar.tsx
+++ examples/build-weapp-plugin/src/plugin/components/avatar/avatar.tsx
@@ -3,14 +3,24 @@ import { View, Image } from '@tarojs/components'
 import './avatar.scss'
 
 export default class Avatar extends Component<{ mode: any, onAvatarClick: any }, null> {
+  node: { ctx: any }
+
+  handleClick () {
+    if (process.env.TARO_ENV === 'jd') {
+      this.node.ctx.triggerEvent('avatarClick')
+    } else {
+      this.node.ctx.triggerEvent('avatar-click')
+    }
+  }
+
   render () {
     return (
-      <View>
+      <View ref={node => this.node = node} >
         <Image
           className='logo'
           src='http://storage.360buyimg.com/taro-static/static/images/logo.png'
           mode={this.props.mode}
-          onClick={this.props.onAvatarClick}
+          onClick={this.handleClick.bind(this)}
         />
       </View>
     )
