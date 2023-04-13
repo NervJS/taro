06a76da037fb4bedc8b650452d89caabb0d68a05
shiyangzhang <shiyangzhang@whale.im>
2023-04-13 09:09:58 +0800

    清理不要代码

--- packages/taro-plugin-solid/src/webpack.h5.ts
+++ packages/taro-plugin-solid/src/webpack.h5.ts
@@ -1,45 +1,30 @@
-import type { IPluginContext } from "@tarojs/service";
+import type { IPluginContext } from '@tarojs/service'
 import { mergeWith } from 'lodash'
 
-import type { Frameworks } from "./index";
-import { getLoaderMeta } from "./loader-meta";
+import type { Frameworks } from './index'
+import { getLoaderMeta } from './loader-meta'
 
-export function modifyH5WebpackChain(
-  ctx: IPluginContext,
-  framework: Frameworks,
-  chain
-) {
-  setLoader(framework, chain);
-  setPlugin(ctx, framework, chain);
+export function modifyH5WebpackChain(ctx: IPluginContext, framework: Frameworks, chain) {
+  setLoader(framework, chain)
 
   chain.merge({
     module: {
       rule: {
-        "process-import-taro": {
+        'process-import-taro': {
           test: /taro-h5[\\/]dist[\\/]index/,
-          loader: require.resolve("./api-loader"),
+          loader: require.resolve('./api-loader'),
         },
       },
     },
-  });
+  })
 }
-
 
 function setLoader(framework: Frameworks, chain) {
-  function customizer (object = '', sources = '') {
-    if ([object, sources].every(e => typeof e === 'string')) return object + sources
+  function customizer(object = '', sources = '') {
+    if ([object, sources].every((e) => typeof e === 'string')) return object + sources
   }
-  chain.plugin('mainPlugin')
-    .tap(args => {
-      args[0].loaderMeta = mergeWith(
-        getLoaderMeta(framework), args[0].loaderMeta, customizer
-      )
-      return args
-    })
-}
-
-function setPlugin(_ctx: IPluginContext, _framework: Frameworks, _chain) {
-  // const config = ctx.initialConfig;
-
-  // @TODO 没有找到这个地方应该怎么做
+  chain.plugin('mainPlugin').tap((args) => {
+    args[0].loaderMeta = mergeWith(getLoaderMeta(framework), args[0].loaderMeta, customizer)
+    return args
+  })
 }
--- packages/taro-plugin-solid/src/webpack.mini.ts
+++ packages/taro-plugin-solid/src/webpack.mini.ts
@@ -1,29 +1,16 @@
-import { fs } from "@tarojs/helper";
-import type { IPluginContext } from "@tarojs/service";
+import { fs } from '@tarojs/helper'
+import type { IPluginContext } from '@tarojs/service'
 
-import type { Frameworks } from "./index";
-import { getLoaderMeta } from "./loader-meta";
+import type { Frameworks } from './index'
+import { getLoaderMeta } from './loader-meta'
 
-export function modifyMiniWebpackChain(
-  ctx: IPluginContext,
-  framework: Frameworks,
-  chain
-) {
-  setAlias(ctx, framework, chain);
-  setLoader(framework, chain);
-}
-
-function setAlias(ctx: IPluginContext, framework: Frameworks, chain) {
-  const config = ctx.initialConfig;
-  const alias = chain.resolve.alias;
-
-  [fs, framework, config];
-  alias.set("@tarojs/components$", "@/components");
+export function modifyMiniWebpackChain(ctx: IPluginContext, framework: Frameworks, chain) {
+  setLoader(framework, chain)
 }
 
 function setLoader(framework: Frameworks, chain) {
-  chain.plugin("miniPlugin").tap((args) => {
-    args[0].loaderMeta = getLoaderMeta(framework);
-    return args;
-  });
+  chain.plugin('miniPlugin').tap((args) => {
+    args[0].loaderMeta = getLoaderMeta(framework)
+    return args
+  })
 }
