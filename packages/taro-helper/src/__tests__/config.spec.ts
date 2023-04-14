40dfc267dcf2cf9853e4891598f26fd5c1489fb1
ZakaryCode <zakarycode@gmail.com>
2023-04-14 11:25:14 +0800

    feat(helper): readConfig 支持读取 json

--- packages/taro-helper/src/__tests__/config.spec.ts
+++ packages/taro-helper/src/__tests__/config.spec.ts
@@ -15,13 +15,28 @@ describe('readConfig', () => {
     }
   }
 
-  test('read config with tips', async () => {
+  test('read config without tips', async () => {
     const result = readConfig(path.join(__dirname, './__mocks__/app.config.ts'))
     expect(result).toEqual(config)
   })
 
   test('read config with tips', async () => {
-    const result = readConfig(path.join(__dirname, './__mocks__/app.config.ts'))
+    const result = readConfig(path.join(__dirname, './__mocks__/app.define.config.ts'))
+    expect(result).toEqual(config)
+  })
+
+  test('read config with import', async () => {
+    const result = readConfig(path.join(__dirname, './__mocks__/app.import.config.ts'))
+    expect(result).toEqual(config)
+  })
+
+  test('read config with require', async () => {
+    const result = readConfig(path.join(__dirname, './__mocks__/app.require.config.ts'))
+    expect(result).toEqual(config)
+  })
+
+  test('read json config', async () => {
+    const result = readConfig(path.join(__dirname, './__mocks__/app.config.json'))
     expect(result).toEqual(config)
   })
 })
