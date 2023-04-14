c0e72418f478c30bf97e7dbfa2c98c739b2a47ab
bin <linhaobin28@qq.com>
2023-04-14 14:42:31 +0800

    fix: taro-loader

--- packages/taro-loader/src/app.ts
+++ packages/taro-loader/src/app.ts
@@ -60,7 +60,7 @@ ${instantiateApp}
 ${options.prerender ? prerender : ''}
 initPxTransform({
   designWidth: ${pxTransformConfig.designWidth},
-  deviceRatio: ${JSON.stringify(pxTransformConfig.deviceRatio)}
+  deviceRatio: ${JSON.stringify(pxTransformConfig.deviceRatio)},
   baseFontSize: ${pxTransformConfig.baseFontSize || (pxTransformConfig.minRootSize >= 1 ? pxTransformConfig.minRootSize : 20)},
   unitPrecision: ${pxTransformConfig.unitPrecision},
   targetUnit: ${JSON.stringify(pxTransformConfig.targetUnit)}
