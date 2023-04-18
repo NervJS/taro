a8d0a3f639f542dc1a1b15bbb9058055bd9b4a01
xuanzebin <xuanzebin3@jd.com>
2023-04-18 14:08:44 +0800

    perf(runtime): 优化小程序 runtime 性能

--- packages/taro-runtime/src/dom/class-list.ts
+++ packages/taro-runtime/src/dom/class-list.ts
@@ -1,57 +1,114 @@
 import type { TaroElement } from './element'
 
-export class ClassList extends Set<string> {
+export class ClassList {
   private el: TaroElement
 
+  private _value: string[] = []
+
   constructor (className: string, el: TaroElement) {
-    super()
-    className.trim().split(/\s+/).forEach(super.add.bind(this))
     this.el = el
+    className.trim().split(/\s+/).forEach(token => this._value.push(token))
   }
 
   public get value () {
-    return [...this].filter(v => v !== '').join(' ')
+    return this.toString()
   }
 
-  public add (s: string) {
-    super.add(s)
-    this._update()
-
-    return this
+  public get length () {
+    return this._value.length
   }
 
-  public get length (): number {
-    return this.size
+  add () {
+    let i = 0
+    let updated = false
+
+    const tokens = arguments
+    const l = tokens.length
+    const value = this._value
+
+    do {
+      const token: string = tokens[i]
+
+      if (this.checkTokenIsValid(token) && !~value.indexOf(token)) {
+        value.push(token)
+
+        updated = true
+      }
+    } while (++i < l)
+
+    if (updated) {
+      this._update()
+    }
   }
 
-  public remove (s: string) {
-    super.delete(s)
-    this._update()
+  remove () {
+    let i = 0
+    let updated = false
+
+    const tokens = arguments
+    const l = tokens.length
+    const value = this._value
+
+    do {
+      const token = tokens[i] + ''
+
+      if (!this.checkTokenIsValid(token)) continue
+    
+      const index = value.indexOf(token)
+
+      if (~index) {
+        value.splice(index, 1)
+
+        updated = true
+      }
+    } while (++i < l)
+
+    if (updated) {
+      this._update()
+    }
   }
 
-  public toggle (s: string) {
-    if (super.has(s)) {
-      super.delete(s)
-    } else {
-      super.add(s)
+  contains (token: string) {
+    if (!this.checkTokenIsValid(token)) return false
+
+    return !!~this._value.indexOf(token)
+  }
+
+  toggle (token: string, force: boolean) {
+    const result = this.contains(token)
+    const method = result ? force !== true && 'remove' : force !== false && 'add'
+
+    if (method) {
+      // @ts-ignore
+      this[method](token)
     }
 
-    this._update()
+    if (force === true || force === false) {
+      return force
+    } else {
+      return !result
+    }
   }
 
-  public replace (s1: string, s2: string) {
-    super.delete(s1)
-    super.add(s2)
+  replace (token: string, replacement_token: string) {
+    if (!this.checkTokenIsValid(token) || !this.checkTokenIsValid(replacement_token)) return
+  
+    const index = this._value.indexOf(token)
 
-    this._update()
+    if (~index) {
+      this._value.splice(index, 1, replacement_token)
+		  this._update()
+    }
   }
 
-  public contains (s: string) {
-    return super.has(s)
+  toString () {
+	  return this._value.filter(v => v !== '').join(' ')
   }
 
-  public toString () {
-    return this.value
+  private checkTokenIsValid (token: string) {
+    if (token === '' || /\s/.test(token)) return false
+  
+    return true
   }
 
   private _update () {
