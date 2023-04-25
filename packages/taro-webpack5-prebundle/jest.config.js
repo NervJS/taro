c63c58008ffdbf59078848ac6e5db230a06265d8
ZakaryCode <zakarycode@gmail.com>
2023-04-25 15:00:12 +0800

    feat(runner): 限制 jest 进程和同时测试个数

--- packages/taro-webpack5-prebundle/jest.config.js
+++ packages/taro-webpack5-prebundle/jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   testEnvironmentOptions: {},
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
-  setupFilesAfterEnv: ['./src/__tests__/setup/index.ts'],
   testMatch: ['**/__tests__/?(*.)+(spec|test).[jt]s?(x)'],
   testTimeout: 30000
 }
