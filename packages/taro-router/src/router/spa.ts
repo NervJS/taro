9ef4a7ef7a6858484db8916a66d286a895abda35
ZakaryCode <zakarycode@gmail.com>
2023-04-27 16:26:34 +0800

    feat(h5): 补充 onPageNotFound 返回参数

--- packages/taro-router/src/router/spa.ts
+++ packages/taro-router/src/router/spa.ts
@@ -68,9 +68,13 @@ export function createRouter (
       ;[element, , params] = await Promise.all(result)
     } catch (error) {
       if (error.status === 404) {
-        app.onPageNotFound?.({
-          path: handler.pathname
-        })
+        const notFoundEvent = {
+          isEntryPage: stacks.length === 0,
+          path: handler.pathname,
+          query: handler.getQuery(createStampId()),
+        }
+        app.onPageNotFound?.(notFoundEvent)
+        eventCenter.trigger('__taroRouterNotFound', notFoundEvent)
       } else if (/Loading hot update .* failed./.test(error.message)) {
         // NOTE: webpack5 与 prebundle 搭配使用时，开发环境下初次启动时偶发错误，由于 HMR 加载 chunk hash 错误，导致热更新失败
         window.location.reload()
