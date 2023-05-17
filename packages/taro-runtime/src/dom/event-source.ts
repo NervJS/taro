0a0495e0c9d2031d869a432f4c4d139757916ee8
xuanzebin <xuanzebin3@jd.com>
2023-05-17 10:01:55 +0800

    perf(runtime): 优化 eventsource 性能

--- packages/taro-runtime/src/dom/event-source.ts
+++ packages/taro-runtime/src/dom/event-source.ts
@@ -1,11 +1,58 @@
 import type { TaroNode } from './node'
 
-interface IEventSource extends Map<string | undefined | null, TaroNode> {
+type TaroID = string | undefined | null
+
+type IEventSourceData = [TaroID, TaroNode][]
+
+interface IEventSource {
+  data: IEventSourceData
+  clear (): void
+  size (): number
+  has (id: TaroID): boolean
+  delete (id: TaroID): void
+  get (id: TaroID): TaroNode | undefined
+  set (id: TaroID, value: TaroNode): void
   removeNode (child: TaroNode): void
   removeNodeTree (child: TaroNode): void
 }
 
-class EventSource extends Map {
+class EventSource {
+  data: IEventSourceData = []
+
+  clear () {
+    this.data.length = 0
+  }
+  
+  size () {
+    return this.data.length
+  }
+
+  has (id: TaroID) {
+    return this.data.some(([sid]) => sid === id)
+  }
+
+  delete (id: TaroID) {
+    const index = this.data.findIndex(([sid]) => sid === id)
+
+    if (index !== -1) {
+      this.data.splice(index, 1)
+    }
+  }
+
+  get (id: TaroID) {
+    return this.data.find(([sid]) => sid === id)?.[1]
+  }
+
+  set (id: TaroID, value: TaroNode) {
+    const index = this.data.findIndex(([sid]) => sid === id)
+  
+    if (index !== -1) {
+      this.data[index] = [id, value]
+    } else {
+      this.data.push([id, value])
+    }
+  }
+
   removeNode (child: TaroNode) {
     const { sid, uid } = child
     this.delete(sid)
