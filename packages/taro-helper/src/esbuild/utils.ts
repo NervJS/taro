9d797ce609977121b7f5cccfbfd01e00edf84184
yoyo837 <yoyo837@hotmail.com>
2023-05-29 11:01:06 +0800

    chore: shaking by args

--- packages/taro-helper/src/esbuild/utils.ts
+++ packages/taro-helper/src/esbuild/utils.ts
@@ -5,17 +5,8 @@ import type { OnResolveArgs, OnResolveResult } from 'esbuild'
 export function externalEsbuildModule (args: Partial<OnResolveArgs>): OnResolveResult {
   return {
     ...pick(args, [
-      'errors',
-      'external',
-      'namespace',
-      'path',
-      'pluginData',
-      'pluginName',
-      'sideEffects',
-      'suffix',
-      'warnings',
-      'watchDirs',
-      'watchFiles'
+      'resolveDir',
+      'kind',
     ]),
     external: true
   }
