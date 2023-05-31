5d8e716659c0e133c8ccf2a13e0ee44cf45e6b90
luckyadam <weitaozsh@gmail.com>
2023-05-31 11:12:50 +0800

    chore: 修复测试用例

--- packages/taro-cli/src/doctor/index.ts
+++ packages/taro-cli/src/doctor/index.ts
@@ -10,7 +10,27 @@ import {
 } from '@tarojs/plugin-doctor/js-binding'
 
 export default {
-  validators: {
-    validateEnv, validateConfig, validateEslint, validatePackage, validateRecommend
-  }
+  validators: [
+    () => {
+      return validateEnv.call(this)
+    },
+    (args) => {
+      const configStr = JSON.stringify(args.projectConfig, (_, v) => {
+        if (typeof v === 'function') {
+          return '__function__'
+        }
+        return v
+      })
+      return validateConfig.call(this, configStr)
+    },
+    (args) => {
+      return validatePackage.call(this, args.appPath, args.nodeModulesPath)
+    },
+    (args) => {
+      return validateRecommend.call(this, args.appPath)
+    },
+    async (args) => {
+      return await validateEslint.call(this, args.projectConfig, args.chalk)
+    }
+  ]
 }
