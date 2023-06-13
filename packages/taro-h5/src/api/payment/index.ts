c54603abcdf37ef8b7263f659dd3242f97e1a665
smileying <smileying_haha@163.com>
2023-06-13 21:45:10 +0800

    fix(h5): remove 重复添加的faceVerifyForPay

--- packages/taro-h5/src/api/payment/index.ts
+++ packages/taro-h5/src/api/payment/index.ts
@@ -4,4 +4,3 @@ import { temporarilyNotSupport } from '../../utils'
 export const requestPayment = /* @__PURE__ */ temporarilyNotSupport('requestPayment')
 export const requestPluginPayment = /* @__PURE__ */ temporarilyNotSupport('requestPluginPayment')
 export const requestOrderPayment = /* @__PURE__ */ temporarilyNotSupport('requestOrderPayment')
-export const faceVerifyForPay = /* @__PURE__ */ temporarilyNotSupport('faceVerifyForPay')
