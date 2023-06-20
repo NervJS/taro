67bcc1905fbfd7872f1ec56cb3b41509acb6390c
bigMeow <running@vip.163.com>
2023-06-20 18:42:28 +0800

    perf: webpack5默认开启详细日志

--- packages/taro-cli/templates/default/config/dev.js
+++ packages/taro-cli/templates/default/config/dev.js
@@ -1,6 +1,10 @@
 <% if (typescript) {%>import { UserConfigExport } from "@tarojs/cli";
 <%}%>
 export default {
+  <% if (compiler === 'webpack5') {%>logger: {
+    quiet: false,
+    stats: true
+  },<%}%>
   mini: {},
   h5: {}
 }<% if (typescript) {%> satisfies UserConfigExport<%}%>
