3959f7bafc0e93b85cb48d3f95f1d3ea1ad526fd
huangzhipeng02 <huangzhipeng02@58.com>
2023-07-05 14:52:08 +0800

    feat: 整理example工程，分页面显示demo

--- examples/mini-program-example/src/app.config.ts
+++ examples/mini-program-example/src/app.config.ts
@@ -1,6 +1,10 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/grid-view/index',
+    'pages/list-view/index',
+    'pages/page-container/index',
+    'pages/match-media/index'
   ],
   window: {
     backgroundTextStyle: 'light',
