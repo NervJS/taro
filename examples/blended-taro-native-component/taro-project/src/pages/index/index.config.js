cf11bf48b35afbb61408b7eff37f99a555d16e2a
laiyonghong3 <laiyonghong@jd.com>
2023-07-24 15:46:03 +0800

    feat: 修改融合编译的demo程序

--- examples/blended-taro-native-component/taro-project/src/pages/index/index.config.js
+++ examples/blended-taro-native-component/taro-project/src/pages/index/index.config.js
@@ -1,6 +1,6 @@
 export default {
   navigationBarTitleText: '测试页',
   usingComponents: {
-    "c-picker": "../../components/picker/index",
+    // "c-picker": "../../components/picker/index",
   }
 }
