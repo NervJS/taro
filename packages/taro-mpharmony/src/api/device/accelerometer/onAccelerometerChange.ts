1a2ada3e2a20492f220a80a34717c54c463987b1
xiaoyan428820 <121045285@qq.com>
2023-08-07 16:28:19 +0800

    fix: 修复网络相关接口实现及加速计接口桥接问题

---
+++ packages/taro-mpharmony/src/api/device/accelerometer/onAccelerometerChange.ts
@@ -0,0 +1,20 @@
+import Taro from '@tarojs/taro'
+import { shouldBeFunction } from 'src/utils'
+
+/**
+ * 监听加速度数据事件。频率根据 Taro.startAccelerometer() 的 interval 参数。可使用 Taro.stopAccelerometer() 停止监听。
+ */
+export const onAccelerometerChange: typeof Taro.onAccelerometerChange = (callback) => {
+  const name = 'onAccelerometerChange'
+
+  // callback must be an Function
+  const isFunction = shouldBeFunction(callback)
+  if (!isFunction.flag) {
+    const res = { errMsg: `${name}:fail ${isFunction.msg}` }
+    console.error(res.errMsg)
+    return
+  }
+
+  // @ts-ignore
+  native.onAccelerometerChange(callback)
+}
