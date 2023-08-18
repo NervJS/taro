6d39af1e3205fff800dd32604a56043eb6e68242
xuanzebin <xuanzebin3@jd.com>
2023-08-18 19:16:49 +0800

    fix(vue): 修复 vue 框架使用 h() 导致的报错问题

--- packages/taro-webpack5-runner/src/plugins/TaroNormalModulesPlugin.ts
+++ packages/taro-webpack5-runner/src/plugins/TaroNormalModulesPlugin.ts
@@ -95,6 +95,10 @@ export default class TaroNormalModulesPlugin {
               }
 
               const [type, prop] = node.arguments
+              
+              // 防止 vue2 中类似 h() 的定义报错
+              if (!type) return
+
               const componentName = type.name
 
               if (type.value) {
