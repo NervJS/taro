fd76fba3ddaafa4a1631dd60dddd163d29fa6375
guoenxuan <genxuan_gdx@163.com>
2023-08-23 15:12:32 +0800

    fix: 修正websocket接口并优化测试用例

--- examples/mini-program-example/src/pages/api/network/webSocket/index.tsx
+++ examples/mini-program-example/src/pages/api/network/webSocket/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Taro from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View } from '@tarojs/components'
 import ButtonList from '@/components/buttonList'
+import { TestConsole } from '@/util/util'
 import './index.scss'
 
 /**
@@ -9,135 +10,218 @@ import './index.scss'
  * @returns
  */
 
+const wsConnectConfig = {
+  url: 'ws://192.168.48.245:3000',
+  header: {
+    'content-type': 'application/json',
+  },
+  protocols: [],
+  tcpNoDelay: true,
+}
+
 export default class Index extends React.Component {
   state = {
+    task: {},
     list: [
       {
-        id: 'sendSocketMessage',
-        func: (apiIndex) => {
-          Taro.sendSocketMessage({
-            data: 'test sendSocketMessage',
-            success: (res) => {
-              console.log('sendSocketMessage success ', res)
-            },
-            fail: (res) => {
-              console.log('sendSocketMessage fail ', res)
-            },
-            complete: (res) => {
-              console.log('sendSocketMessage complete ', res)
-            },
-          })
-        },
-      },
-      {
         id: 'onSocketOpen',
-        func: (apiIndex) => {
-          Taro.connectSocket({
-            url: 'www.baidu.com',
-          })
+        func: () => {
+          TestConsole.consoleTest('Taro.onSocketOpen')
+          console.log('注册onSocketOpen, 需要websocket连接后才能触发回调')
           Taro.onSocketOpen((res) => {
-            console.log('onSocketOpen ', res)
+            console.log('Taro.onSocketOpen:', res)
           })
         },
       },
       {
         id: 'onSocketMessage',
-        func: (apiIndex) => {
-          Taro.connectSocket({
-            url: 'www.baidu.com',
-          })
-          Taro.onSocketMessage(function (res) {
-            console.log('onSocketMessage ' + res.data)
+        func: () => {
+          TestConsole.consoleTest('Taro.onSocketMessage')
+          console.log('注册onSocketMessage, 需要websocket连接收到信息后才能触发回调')
+          Taro.onSocketMessage((res) => {
+            console.log('Taro.onSocketMessage:', res)
+            Taro.showToast({
+              title: res.data,
+            })
           })
         },
       },
       {
         id: 'onSocketClose',
-        func: (apiIndex) => {
-          Taro.connectSocket({
-            url: 'www.baidu.com',
+        func: () => {
+          TestConsole.consoleTest('Taro.onSocketClose')
+          console.log('注册onSocketClose, 需要websocket关闭才能触发回调')
+          Taro.onSocketClose((res) => {
+            console.log('Taro.onSocketClose:', res)
           })
-          Taro.onSocketOpen((res) => {
-            console.log('onSocketOpen ', res)
-            Taro.closeSocket()
-          })
-          Taro.onSocketClose(function (res) {
-            console.log('onSocketClose ', res)
+        },
+      },
+      {
+        id: 'onSocketError',
+        func: () => {
+          TestConsole.consoleTest('Taro.onSocketError')
+          console.log('注册onSocketError, 需要websocket连接后发生错误才能触发回调')
+          Taro.onSocketError((res) => {
+            console.log('Taro.onSocketError:', res)
           })
         },
       },
       {
         id: 'connectSocket',
-        func: (apiIndex) => {
+        inputData: wsConnectConfig,
+        func: (apiIndex, data) => {
+          TestConsole.consoleTest('Taro.connectSocket')
           Taro.connectSocket({
-            url: 'www.baidu.com',
-            header: {
-              'content-type': 'application/json',
-            },
-            protocols: ['protocol1'],
-            tcpNoDelay: false,
+            ...data,
             success: (res) => {
-              console.log('connectSocket success ', res)
+              TestConsole.consoleSuccess.call(this, res, apiIndex)
             },
             fail: (res) => {
-              console.log('connectSocket fail ', res)
+              TestConsole.consoleFail.call(this, res, apiIndex)
             },
             complete: (res) => {
-              console.log('connectSocket complete ', res)
+              TestConsole.consoleComplete.call(this, res, apiIndex)
+            },
+          })
+        },
+      },
+      {
+        id: 'sendSocketMessage',
+        inputData: {
+          data: 'Taro三方框架',
+        },
+        func: (apiIndex, data) => {
+          TestConsole.consoleTest('Taro.sendSocketMessage')
+          Taro.sendSocketMessage({
+            ...data,
+            success: (res) => {
+              TestConsole.consoleSuccess.call(this, res, apiIndex)
+            },
+            fail: (res) => {
+              TestConsole.consoleFail.call(this, res, apiIndex)
+            },
+            complete: (res) => {
+              TestConsole.consoleComplete.call(this, res, apiIndex)
+            },
+          })
+        },
+      },
+      {
+        id: 'closeSocket',
+        inputData: {
+          code: 1000,
+          reason: '主动关闭',
+        },
+        func: (apiIndex, data) => {
+          TestConsole.consoleTest('Taro.closeSocket')
+          Taro.closeSocket({
+            ...data,
+            success: (res) => {
+              TestConsole.consoleSuccess.call(this, res, apiIndex)
+            },
+            fail: (res) => {
+              TestConsole.consoleFail.call(this, res, apiIndex)
+            },
+            complete: (res) => {
+              TestConsole.consoleComplete.call(this, res, apiIndex)
             },
           })
         },
       },
       {
         id: 'SocketTask',
-        func: (apiIndex) => {
+        inputData: wsConnectConfig,
+        func: (apiIndex, data) => {
+          TestConsole.consoleTest('Taro.SocketTask')
           Taro.connectSocket({
-            url: 'www.baidu.com',
-            success: function () {
-              console.log('connect success')
+            ...data,
+            success: (res) => {
+              TestConsole.consoleSuccess.call(this, res, apiIndex)
             },
-            fail: () => {
-              console.log('connect fail')
+            fail: (res) => {
+              TestConsole.consoleFail.call(this, res, apiIndex)
+            },
+            complete: (res) => {
+              TestConsole.consoleComplete.call(this, res, apiIndex)
             },
           }).then((task) => {
-            task.send({
-              data: 'test send',
-              success: (res) => {
-                console.log('send success ', res)
-              },
-              fail: (res) => {
-                console.log('send fail ', res)
-              },
-              complete: (res) => {
-                console.log('send complete ', res)
-              },
+            this.setState({ task })
+            task.onOpen((res) => {
+              console.log('SocketTask.onOpen:', res)
             })
-            task.onOpen(function (res) {
-              console.log('onOpen', res)
-              task.send({ data: 'xxx' })
+            task.onMessage((res) => {
+              console.log('SocketTask.onMessage:', res)
+              Taro.showToast({
+                title: res.data,
+              })
             })
-            task.onMessage(function (msg) {
-              console.log('onMessage: ', msg)
+            task.onClose((res) => {
+              console.log('SocketTask.onClose:', res)
             })
-            task.onError(function (res) {
-              console.log('onError', res)
+            task.onError((res) => {
+              console.log('SocketTask.onError:', res)
             })
-            task.close({
-              code: 200,
-              reason: 'test close',
-              success: (res) => {
-                console.log('close success ', res)
-              },
-              fail: (res) => {
-                console.log('close fail ', res)
-              },
-              complete: (res) => {
-                console.log('close complete ', res)
-              },
+          })
+        },
+      },
+      {
+        id: 'SocketTask.send',
+        inputData: {
+          data: 'Taro三方框架',
+        },
+        func: (apiIndex, data) => {
+          TestConsole.consoleTest('SocketTask.send')
+          const task = this.state.task as Taro.SocketTask
+          if (task.readyState === undefined || task.readyState !== task.OPEN) {
+            console.log('先点击SocketTask')
+            Taro.showToast({
+              title: 'Click SocketTask',
+              icon: 'error',
             })
-            task.onClose(function (e) {
-              console.log('onClose: ', e)
+            return
+          }
+          task.send({
+            ...data,
+            success: (res) => {
+              TestConsole.consoleSuccess.call(this, res, apiIndex)
+            },
+            fail: (res) => {
+              TestConsole.consoleFail.call(this, res, apiIndex)
+            },
+            complete: (res) => {
+              TestConsole.consoleComplete.call(this, res, apiIndex)
+            },
+          })
+        },
+      },
+      {
+        id: 'SocketTask.close',
+        inputData: {
+          code: 1000,
+          reason: '主动关闭',
+        },
+        func: (apiIndex, data) => {
+          TestConsole.consoleTest('SocketTask.close')
+          const task = this.state.task as Taro.SocketTask
+          if (task.readyState === undefined || task.readyState !== task.OPEN) {
+            console.log('先点击SocketTask')
+            Taro.showToast({
+              title: 'Click SocketTask',
+              icon: 'error',
             })
+            return
+          }
+          task.close({
+            ...data,
+            success: (res) => {
+              TestConsole.consoleSuccess.call(this, res, apiIndex)
+            },
+            fail: (res) => {
+              TestConsole.consoleFail.call(this, res, apiIndex)
+            },
+            complete: (res) => {
+              TestConsole.consoleComplete.call(this, res, apiIndex)
+            },
           })
         },
       },
