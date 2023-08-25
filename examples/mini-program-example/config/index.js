e6c4555f824d51c070aa58460a86af7c318f37cc
guoenxuan <genxuan_gdx@163.com>
2023-08-25 09:36:10 +0800

    fix: 优化界面交互api测试用例

--- examples/mini-program-example/config/index.js
+++ examples/mini-program-example/config/index.js
@@ -10,7 +10,7 @@ const config = {
     828: 1.81 / 2
   },
   sourceRoot: 'src',
-  outputRoot: 'dist',
+  outputRoot: `dist/${process.env.TARO_ENV}`,
   plugins: [],
   alias: {
     '@/util': path.resolve(__dirname, '..', 'src/util'),
