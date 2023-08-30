98b10a18cde2dc3918bb0dc6039497c56048b68f
xuanzebin <xuanzebin3@jd.com>
2023-08-30 17:33:28 +0800

    feat: 完成 env 相关 api 的适配

--- packages/taro-platform-harmony/src/apis/runtime/index.ts
+++ packages/taro-platform-harmony/src/apis/runtime/index.ts
@@ -1,6 +1,25 @@
 import { eventCenter, getCurrentInstance } from '@tarojs/runtime/dist/runtime.esm'
 
+const ENV_TYPE = {
+  WEAPP: 'WEAPP',
+  SWAN: 'SWAN',
+  ALIPAY: 'ALIPAY',
+  TT: 'TT',
+  QQ: 'QQ',
+  JD: 'JD',
+  WEB: 'WEB',
+  RN: 'RN',
+  HARMONY: 'HARMONY',
+  QUICKAPP: 'QUICKAPP'
+}
+
+function getEnv () {
+  return ENV_TYPE.HARMONY
+}
+
 export {
+  ENV_TYPE,
   eventCenter,
   getCurrentInstance,
+  getEnv,
 }
