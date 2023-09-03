208997a0f9c16d92a0d3a94990704c3b27754b85
konka <1094918@gmail.com>
2023-09-03 16:19:04 +0800

    fix(style): use 100% instandof 100vh
    There will be scrollbar if use min-height:100vh on Chrome. Better choice is use height:100%.

--- packages/taro-router/src/style.ts
+++ packages/taro-router/src/style.ts
@@ -35,7 +35,6 @@ export function loadRouterStyle (usingWindowScroll) {
     position: relative;
     width: 100%;
     height: 100%;
-    min-height: 100vh;
   }
 
   .taro-tabbar__container .taro_router {
