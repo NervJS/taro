f96ee721d041d72134bc76f51dedd7c8800dfb15
liuzejia <790868516@qq.com>
2023-09-03 18:33:44 +0800

    fix: 修复vite模版的问题

--- packages/taro-cli/templates/default/template_creator.js
+++ packages/taro-cli/templates/default/template_creator.js
@@ -13,25 +13,25 @@ const handler = {
   '/types/vue.d.ts' ({ framework, typescript }) {
     return ['vue', 'vue3'].includes(framework) && !!typescript
   },
-  '/src/pages/index/index.jsx' ({ pageName, pageDir, subPkg }) {
+  '/src/pages/index/index.jsx' ({ pageName, pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, 'index.jsx'),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.jsx')
     }
   },
-  '/src/pages/index/index.css' ({ pageName, pageDir, subPkg }) {
+  '/src/pages/index/index.css' ({ pageName, pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, 'index.css'),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.css')
     }
   },
-  '/src/pages/index/index.vue' ({ pageName, pageDir, subPkg }) {
+  '/src/pages/index/index.vue' ({ pageName, pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, 'index.vue'),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.vue')
     }
   },
-  '/src/pages/index/index.config.js' ({ pageName, pageDir, subPkg }) {
+  '/src/pages/index/index.config.js' ({ pageName, pageDir = '', subPkg = '' }) {
     return {
       setPageName: path.join(PAGES_ENTRY, pageDir, pageName, 'index.config.js'),
       setSubPkgName: path.join(SOURCE_ENTRY, subPkg, pageDir, pageName, 'index.config.js')
