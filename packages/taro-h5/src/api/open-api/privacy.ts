3697b29f4c86243df703bf7dec12338469b078b7
ZakaryCode <zakarycode@gmail.com>
2023-09-04 18:57:11 +0800

    feat(h5): add privacy api tips

---
+++ packages/taro-h5/src/api/open-api/privacy.ts
@@ -0,0 +1,7 @@
+import { temporarilyNotSupport } from '../../utils'
+
+// 隐私信息授权
+export const requirePrivacyAuthorize = /* @__PURE__ */ temporarilyNotSupport('requirePrivacyAuthorize')
+export const openPrivacyContract = /* @__PURE__ */ temporarilyNotSupport('openPrivacyContract')
+export const onNeedPrivacyAuthorization = /* @__PURE__ */ temporarilyNotSupport('onNeedPrivacyAuthorization')
+export const getPrivacySetting = /* @__PURE__ */ temporarilyNotSupport('getPrivacySetting')
