f8c2195a9487db1d88b2b002e3507ee85bbd1d04
Philly Cai <philly.cai@storehub.com>
2023-09-10 01:41:38 +0800

    fix(type): add checkIsAddedToMyMiniProgram type

--- packages/taro-h5/src/api/open-api/favorites.ts
+++ packages/taro-h5/src/api/open-api/favorites.ts
@@ -3,4 +3,3 @@ import { temporarilyNotSupport } from '../../utils'
 // 收藏
 export const addVideoToFavorites = /* @__PURE__ */ temporarilyNotSupport('addVideoToFavorites')
 export const addFileToFavorites = /* @__PURE__ */ temporarilyNotSupport('addFileToFavorites')
-export const checkIsAddedToMyMiniProgram = /* @__PURE__ */ temporarilyNotSupport('checkIsAddedToMyMiniProgram')
