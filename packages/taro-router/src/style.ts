fbc4d2dbad9935fb734b5641fdf0e9204fa1f9ea
Zakary <zakarycode@gmail.com>
2023-09-25 14:22:13 +0800

    fix(route): normal page max-height fix #14592

--- packages/taro-router/src/style.ts
+++ packages/taro-router/src/style.ts
@@ -34,7 +34,7 @@ export function loadAnimateStyle (ms = 300) {
 /**
  * 插入路由相关样式
  */
-export function loadRouterStyle (usingWindowScroll) {
+export function loadRouterStyle (usingWindowScroll: boolean) {
   const css = `
   .taro_router {
     position: relative;
@@ -53,8 +53,11 @@ export function loadRouterStyle (usingWindowScroll) {
   `}
   }
 
-  .taro-tabbar__container .taro-tabbar__panel {
+  .taro-tabbar__container > .taro-tabbar__panel {
     overflow: hidden;
+  }
+
+  .taro-tabbar__container > .taro-tabbar__panel > .taro_page.taro_tabbar_page {
     max-height: calc(100vh - var(--taro-tabbar-height) - constant(safe-area-inset-bottom));
     max-height: calc(100vh - var(--taro-tabbar-height) - env(safe-area-inset-bottom));
   }
