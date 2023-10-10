9e83d3ac91279144596ac6ad51198b8e17686698
Chen-jj <798095202@qq.com>
2023-10-10 10:57:58 +0800

    feat(compile-mode): 新增编译模式的 webpack loader

--- packages/taro-webpack5-runner/src/loaders/miniCompilerLoader.ts
+++ packages/taro-webpack5-runner/src/loaders/miniCompilerLoader.ts
@@ -1,55 +1,76 @@
-// import { swc } from '@tarojs/helper'
-// import { parse } from "@babel/parser"
-// import { transformFromAstSync } from "@babel/core"
-// import { getComponentsAlias } from '@tarojs/shared'
-// import { getOptions } from 'loader-utils'
+import { fs, swc } from '@tarojs/helper'
+import { getComponentsAlias } from '@tarojs/shared'
+import { getOptions } from 'loader-utils'
+import * as path from 'path'
+
+let FILE_COUNTER = 0
+const FILE_COUNTER_MAP = new Map<string, number>()
+
+const COMPILE_MODE = 'compileMode'
 
 export default async function (source) {
   const callback = this.async()
-  // const options = getOptions(this)
+  const options = getOptions(this)
+  const resourcePath = this.resourcePath
 
-  if (!(/\.[tj]sx$/.test(this.resourcePath))) {
+  // @TODO 思考非 JSX 文件应该如何处理 p3
+  if (!((/\.[tj]sx$/.test(resourcePath)) && source.includes(COMPILE_MODE))) {
     return callback(null, source)
   }
-  // swc
-  //   .transform(source, {
-  //     filename: this.resourcePath,
-  //     sourceMaps: false,
-  //     jsc: {
-  //       target: 'es2020'
-  //     },
-  //   })
-  //   .then((output) => {
-  //     console.log('output.code: ', output.code)
-  //     callback(null, source)
-  //   })
-  //   .catch(err => callback(err))
 
-  // const parsedAst = await parse(source, {
-  //   sourceType: 'module',
-  //   sourceFilename: this.resourcePath,
-  //   plugins: [
-  //     'classProperties',
-  //     'jsx',
-  //     'flowComments',
-  //     'asyncGenerators',
-  //     'objectRestSpread',
-  //     ['decorators', { decoratorsBeforeExport: true }],
-  //     'dynamicImport',
-  //     'doExpressions',
-  //     'typescript',
-  //   ],
-  // })
-  // const { code } = transformFromAstSync(
-  //   parsedAst,
-  //   source,
-  //   {
-  //     // configFile: false,
-  //     filename: this.resourcePath,
-  //     plugins: [],
-  //     presets: []
-  //   }
-  // )!
-  // console.log('output.code: ', code)
-  callback(null, source)
+  const { template, outputDir, fileType } = options
+
+  template.componentsAlias = getComponentsAlias(template.internalComponents)
+  const components = template.createMiniComponents(template.internalComponents)
+
+  if (!FILE_COUNTER_MAP.has(resourcePath)) {
+    FILE_COUNTER_MAP.set(resourcePath, FILE_COUNTER++)
+  }
+
+  // @TODO 保留 JSX p2
+  try {
+    const { code } = await swc
+      .transform(source, {
+        filename: resourcePath,
+        sourceMaps: false,
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+            decorators: true
+          },
+          transform: {
+            legacyDecorator: true
+          },
+          experimental: {
+            plugins: [
+              [
+                path.join(path.dirname(require.resolve('@tarojs/helper')), './swc/plugin-compile-mode/target/wasm32-wasi/release/swc_plugin_compile_mode.wasm'),
+                {
+                  tmpl_prefix: `f${FILE_COUNTER_MAP.get(resourcePath)}`,
+                  components,
+                  adapter: template.Adapter,
+                }
+              ]
+            ]
+          }
+        }
+      })
+
+    const regExp = /var\s+TARO_TEMPLATES_(\w+)\s*=\s*'(.+)';/g
+    let res
+    while((res = regExp.exec(code)) !== null) {
+      const [, $0, $1] = res
+      const outputPath = path.join(outputDir, 'taro_xmls', `${$0}${fileType.templ}`)
+      // 小程序 xml 不支持 unescape，在此处对被 SWC 转义后的字符作还原
+      const content = $1.replace(/\\[xu]([a-fA-F0-9]{2,4})/g, (_, $1) => {
+        const charCode = parseInt($1, 16)
+        return String.fromCharCode(charCode)
+      })
+      await fs.outputFile(outputPath, content)
+    }
+    callback(null, code.replace(regExp, ''))
+  } catch (err) {
+    callback(err)
+  }
 }
