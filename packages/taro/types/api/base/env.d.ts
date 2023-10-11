c008851f4b63e5b564046f9171e006884808d046
kongxiaojun <kongxiaojun@58.com>
2023-10-11 14:23:01 +0800

    fix: 添加mpharmony环境变量

--- packages/taro/types/api/base/env.d.ts
+++ packages/taro/types/api/base/env.d.ts
@@ -11,7 +11,7 @@ declare module '../../index' {
       /** 框架 */
       FRAMEWORK: 'react' | 'preact' | 'nerv' | 'vue' | 'vue3'
       /** Taro 环境变量 */
-      TARO_ENV: 'weapp' | 'h5' | 'rn' | 'swan' | 'alipay' | 'tt' | 'qq' | 'jd' | 'quickapp'
+      TARO_ENV: 'weapp' | 'h5' | 'rn' | 'swan' | 'alipay' | 'tt' | 'qq' | 'jd' | 'quickapp' | 'mpharmony'
       /** 文件系统中的用户目录路径 (本地路径) */
       USER_DATA_PATH?: string
     }
