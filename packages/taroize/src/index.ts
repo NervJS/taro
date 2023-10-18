b27ac077885d978e5740b5b9fc0e35b1a86a01d7
qican777 <18362991637@163.com>
2023-10-18 20:44:33 +0800

    fix: 代码回退#313

--- packages/taroize/src/index.ts
+++ packages/taroize/src/index.ts
@@ -11,7 +11,6 @@ interface Option {
   script?: string
   scriptPath?: string
   wxml?: string
-  wxmlPath?: string
   path: string
   rootPath: string
   framework: 'react' | 'vue'
@@ -41,7 +40,7 @@ export function parse (option: Option) {
     }
   }
 
-  const { wxml, wxses, imports, refIds } = parseWXML(option.path, option.wxml, false, option.wxmlPath)
+  const { wxml, wxses, imports, refIds } = parseWXML(option.path, option.wxml)
   setting.sourceCode = option.script!
   const ast = parseScript(option.script, wxml as t.Expression, wxses, refIds, option.isApp)
 
