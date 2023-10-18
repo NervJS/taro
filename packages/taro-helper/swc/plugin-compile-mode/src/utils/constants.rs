44cf325a20763abef4586b2817a29631b7d11a01
Chen-jj <798095202@qq.com>
2023-10-18 09:49:55 +0800

    chore: 整理代码结构

--- packages/taro-helper/swc/plugin-compile-mode/src/utils/constants.rs
+++ packages/taro-helper/swc/plugin-compile-mode/src/utils/constants.rs
@@ -1,1 +1,5 @@
 pub const COMPILE_MODE: &str = "compileMode";
+pub const EVENT_HANDLER: &str = "eh";
+pub const DATA_SID: &str = "data-sid";
+pub const TMPL_DATA_ROOT: &str = "i.";
+pub const ID: &str = "id";
