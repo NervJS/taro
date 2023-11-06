6137b0db3cdb6c6529fa80bb434b295b9cb9f676
smileying <smileying_haha@163.com>
2023-11-06 16:21:46 +0800

    feat(types): update storage types

--- packages/taro/types/api/storage/index.d.ts
+++ packages/taro/types/api/storage/index.d.ts
@@ -49,6 +49,10 @@ declare module '../../index' {
       keys: string[]
       /** 限制的空间大小，单位 KB */
       limitSize: number
+      /** 是否执行成功
+       * @supported alipay
+       */
+      success: boolean
     }
   }
 
@@ -102,9 +106,53 @@ declare module '../../index' {
     }
   }
 
+  namespace batchSetStorageSync {
+    interface Option {
+      /** [{ key, value }] */
+      kvList: kv[]
+      /** 接口调用结束的回调函数（调用成功、失败都会执行） */
+      complete?: (res: TaroGeneral.CallbackResult) => void
+      /** 接口调用失败的回调函数 */
+      fail?: (res: TaroGeneral.CallbackResult) => void
+      /** 接口调用成功的回调函数 */
+      success?: (result: SuccessCallbackResult<T>) => void
+    }
+    interface kv {
+      /** key 本地缓存中指定的 key */
+      key: string
+      /** data 需要存储的内容。只支持原生类型、Date、及能够通过JSON.stringify序列化的对象。*/
+      value: any
+    }
+  }
+
+  namespace batchSetStorage {
+    interface Option {
+      kvList: kv[]
+    }
+    interface kv {
+      /** key 本地缓存中指定的 key */
+      key: string
+      /** data 需要存储的内容。只支持原生类型、Date、及能够通过JSON.stringify序列化的对象。*/
+      value: any
+    }
+  }
+
+  namespace batchGetStorage {
+    interface Option {
+      /** 本地缓存中指定的 keyList */
+      keyList: string[]
+      /** 接口调用结束的回调函数（调用成功、失败都会执行） */
+      complete?: (res: TaroGeneral.CallbackResult) => void
+      /** 接口调用失败的回调函数 */
+      fail?: (res: TaroGeneral.CallbackResult) => void
+      /** 接口调用成功的回调函数 */
+      success?: (result: SuccessCallbackResult<T>) => void
+    }
+  }
+
   interface TaroStatic {
     /** Taro.setStorage 的同步版本
-     * @supported weapp, h5, tt
+     * @supported weapp, alipay, swan, jd, qq, tt, h5
      * @example
      * ```tsx
      * Taro.setStorage({
@@ -128,7 +176,7 @@ declare module '../../index' {
     ): void
 
     /** 将数据存储在本地缓存中指定的 key 中。会覆盖掉原来该 key 对应的内容。除非用户主动删除或因存储空间原因被系统清理，否则数据都一直可用。单个 key 允许存储的最大数据长度为 1MB，所有数据存储上限为 10MB。
-     * @supported weapp, h5, rn, tt
+     * @supported weapp, alipay, swan, jd, qq, tt, rn
      * @example
      * ```tsx
      * Taro.setStorage({
@@ -155,7 +203,7 @@ declare module '../../index' {
     ): void
 
     /** Taro.removeStorage 的同步版本
-     * @supported weapp, h5, tt
+     * @supported weapp, alipay, swan, jd, qq, tt, h5
      * @example
      * ```tsx
      * try {
@@ -172,7 +220,7 @@ declare module '../../index' {
     ): void
 
     /** 从本地缓存中移除指定 key
-     * @supported weapp, h5, rn, tt
+     * @supported weapp, alipay, swan, jd, qq, tt, h5, rn
      * @example
      * ```tsx
      * Taro.removeStorage({
@@ -187,7 +235,7 @@ declare module '../../index' {
     removeStorage(option: removeStorage.Option): Promise<TaroGeneral.CallbackResult>
 
     /** Taro.getStorage 的同步版本
-     * @supported weapp, h5, tt
+     * @supported weapp, alipay, swan, jd, qq, tt, h5
      * @example
      * ```tsx
      * try {
@@ -207,7 +255,7 @@ declare module '../../index' {
     ): T
 
     /** Taro.getStorageInfo 的同步版本
-     * @supported weapp, h5, rn, tt
+     * @supported weapp, alipay, swan, jd, qq, tt, h5, rn
      * @example
      * ```tsx
      * try {
@@ -224,7 +272,7 @@ declare module '../../index' {
     getStorageInfoSync(): getStorageInfoSync.Option
 
     /** 异步获取当前storage的相关信息
-     * @supported weapp, h5, rn, tt
+     * @supported weapp, alipay, swan, jd, qq, tt, h5, rn
      * @example
      * ```tsx
      * Taro.getStorageInfo({
@@ -240,7 +288,7 @@ declare module '../../index' {
     getStorageInfo(option?: getStorageInfo.Option): Promise<TaroGeneral.CallbackResult>
 
     /** 从本地缓存中异步获取指定 key 的内容
-     * @supported weapp, h5, rn, tt
+     * @supported weapp, alipay, swan, jd, qq, tt, h5, rn
      * @example
      * ```tsx
      * Taro.getStorage({
@@ -264,7 +312,7 @@ declare module '../../index' {
     ): void
 
     /** Taro.clearStorage 的同步版本
-     * @supported weapp, h5, tt
+     * @supported weapp, alipay, swan, jd, qq, tt, h5
      * @example
      * ```tsx
      * try {
@@ -278,7 +326,7 @@ declare module '../../index' {
     clearStorageSync(): void
 
     /** 清理本地数据缓存
-     * @supported weapp, h5, rn, tt
+     * @supported weapp, alipay, swan, jd, qq, tt, h5, rn
      * @example
      * ```tsx
      * Taro.clearStorage()
@@ -286,5 +334,71 @@ declare module '../../index' {
      * @see https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.clearStorage.html
      */
     clearStorage(option?: clearStorage.Option): Promise<TaroGeneral.CallbackResult>
+
+    /** 将数据批量存储在本地缓存中指定的 key 中。
+     * 会覆盖掉原来该 key 对应的内容。除非用户主动删除或因存储空间原因被系统清理，否则数据都一直可用。
+     * 单个 key 允许存储的最大数据长度为 1MB，所有数据存储上限为 10MB。
+     * @supported weapp
+     * @example
+     * ```tsx
+     * try {
+     *   Taro.batchSetStorageSync([{key: 'key', value: 'value'}])
+     * } catch (e) { }
+     * ```
+     * @see https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.batchGetStorageSync.html
+     */
+    batchSetStorageSync(option: batchSetStorageSync.Option): void
+
+    /** 将数据批量存储在本地缓存中指定的 key 中。会覆盖掉原来该 key 对应的内容。
+     * 除非用户主动删除或因存储空间原因被系统清理，否则数据都一直可用。
+     * 单个 key 允许存储的最大数据长度为 1MB，所有数据存储上限为 10MB。
+     * @supported weapp
+     * @example
+     * ```tsx
+     * Taro.batchGetStorage({
+     *   keyList: ['key']
+     *   success(res) {
+     *     console.log(res)
+     *   }
+     * })
+     * ```
+     * @see https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.batchGetStorage.html
+     */
+    batchSetStorage(option: batchSetStorage.Option): Promise<TaroGeneral.CallbackResult>
+
+    /** 从本地缓存中同步批量获取指定 key 的内容。
+     * @supported weapp
+     * @example
+     * ```tsx
+     * try {
+     *   var valueList = Taro.batchGetStorageSync(['key'])
+     *   if (valueList) {
+     *     // Do something with return value
+     *   }
+     * } catch (e) {
+     *   // Do something when catch error
+     * }
+     * ```
+     * @see https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.batchGetStorageSync.html
+     */
+    batchGetStorageSync<T = any>(
+      /** 本地缓存中指定的 key 数组 */
+      keyList: string[]
+    ): T[]
+
+    /** 从本地缓存中异步批量获取指定 key 的内容。
+     * @supported weapp
+     * @example
+     * ```tsx
+     * Taro.batchGetStorage({
+     *   keyList: ['key'],
+     *   success(res) {
+     *     console.log(res)
+     *   }
+     * })
+     * ```
+     * @see https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.batchGetStorage.html
+     */
+    batchGetStorage(option: batchGetStorage.Option): Promise<TaroGeneral.CallbackResult>
   }
 }
