37004139fc2f68e38516364a90c49ca5ac8f7a13
luckyadam <weitaozsh@gmail.com>
2023-11-09 21:10:56 +0800

    feat: 实现创建项目逻辑

--- crates/native_binding/src/lib.rs
+++ crates/native_binding/src/lib.rs
@@ -1,9 +1,38 @@
 #![deny(clippy::all)]
-
 #[macro_use]
 extern crate napi_derive;
 
+use std::collections::HashMap;
+
+use napi::threadsafe_function::ThreadsafeFunction;
+use taro_init::{creator::CreateOptions, project::Project};
+
 #[napi]
-pub fn add(i: i32, j: i32) -> i32 {
-  i + j
+pub async fn create_project(
+  conf: Project,
+  handlers: HashMap<String, ThreadsafeFunction<CreateOptions>>,
+) {
+  let project = Project::new(
+    conf.name,
+    conf.dir,
+    conf.npm,
+    conf.description,
+    conf.typescript,
+    conf.template,
+    conf.css,
+    conf.framework,
+    conf.auto_install,
+    conf.template_root,
+    conf.version,
+    conf.date,
+    conf.compiler,
+  );
+  let mut thread_safe_functions = HashMap::new();
+  for (key, callback) in handlers {
+    thread_safe_functions.insert(key, callback);
+  }
+  if let Err(e) = project.create(thread_safe_functions).await {
+    println!("创建项目错误，原因如下：");
+    println!("{:?}", e);
+  }
 }
