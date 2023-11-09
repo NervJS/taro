3cffef1568d9093a71f44ea3343eed46169f6e1a
Chen-jj <798095202@qq.com>
2023-11-09 10:44:27 +0800

    perf(loader): 缓存所有页面加载 loader

--- packages/taro-loader/src/entry-cache.ts
+++ packages/taro-loader/src/entry-cache.ts
@@ -1,7 +1,9 @@
+import { getOptions } from 'loader-utils'
+
 export const entryCache = new Map<string, string>()
 
 export default function () {
-  const { name } = this.getOptions()
+  const { name } = getOptions(this)
   if (name && entryCache.has(name)) {
     const content = entryCache.get(name)
     entryCache.delete(name)
