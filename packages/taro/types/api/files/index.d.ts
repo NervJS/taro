dca0c6678406b352dc8cee4e2ed334442516919c
ZakaryCode <ZakaryCode@users.noreply.github.com>
2023-11-11 00:14:49 +0000

    refactor(types): sync components types

--- packages/taro/types/api/files/index.d.ts
+++ packages/taro/types/api/files/index.d.ts
@@ -664,7 +664,7 @@ declare module '../../index' {
       /** 创建的目录路径 */
       dirPath: string
       /** 是否在递归创建该目录的上级目录后再创建该目录。如果对应的上级目录已经存在，则不创建该上级目录。
-       *  如 dirPath 为 a/b/c/d 且 recursive 为 true，将创建 a 目录，再在 a 目录下创建 b 目录，以此类推直至创建 a/b/c 目录下的 d 目录。 
+       *  如 dirPath 为 a/b/c/d 且 recursive 为 true，将创建 a 目录，再在 a 目录下创建 b 目录，以此类推直至创建 a/b/c 目录下的 d 目录。
        *  @default false
        */
       recursive?: boolean
