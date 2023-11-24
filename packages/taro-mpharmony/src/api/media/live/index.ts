464541ebc99001e3cac338ec8da82e7e3fd0b0ac
xiaoyan428820 <121045285@qq.com>
2023-11-24 10:29:10 +0800

    fix: 修改API注释及harmony-definition文件

--- packages/taro-mpharmony/src/api/media/live/index.ts
+++ packages/taro-mpharmony/src/api/media/live/index.ts
@@ -6,8 +6,7 @@ import { LivePlayerContext } from './LivePlayerContext'
 /**
  * 创建 live-player 上下文 LivePlayerContext 对象
  * 
- * @canUse createLivePlayerContext 
- * @__object [id, inst]
+ * @canUse createLivePlayerContext
  */
 export const createLivePlayerContext: typeof Taro.createLivePlayerContext = (id, inst) => {
   const el = findDOM(inst) as HTMLElement
