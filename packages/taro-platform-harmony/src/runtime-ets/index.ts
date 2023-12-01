e024631fd033b57f89b55a1fca48fb1b2d6f55ef
zhuminghui2 <zhuminghui2@jd.com>
2023-12-01 10:50:19 +0800

    fix: 修改current引入顺序

--- packages/taro-platform-harmony/src/runtime-ets/index.ts
+++ packages/taro-platform-harmony/src/runtime-ets/index.ts
@@ -1,10 +1,11 @@
 // Current必须放在前面初始化
+// eslint-disable-next-line simple-import-sort/exports
+export * from './current'
 export * from './bom/document'
 export * from './bom/getComputedStyle'
 export { nav as navigator } from './bom/navigator'
 export { caf as cancelAnimationFrame, now, raf as requestAnimationFrame } from './bom/raf'
 export * from './bom/window'
-export * from './current'
 export * from './dom/element'
 export * from './dom/event'
 export * from './dom/node'
