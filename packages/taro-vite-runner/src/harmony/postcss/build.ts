b33ef01aaf8d6fc354bbcd5c140df937317f7e8e
ZakaryCode <zakarycode@gmail.com>
2023-12-11 15:25:25 +0800

    fix(ets): 移除 postcss 模块冗余函数方法

--- packages/taro-vite-runner/src/harmony/postcss/build.ts
+++ packages/taro-vite-runner/src/harmony/postcss/build.ts
@@ -1,72 +1,15 @@
 import path from 'path'
 
 import { addLeadingSlash, stripTrailingSlash } from '../../utils'
+import { getResolveUrl, relativeUrlMechanisms } from '../../utils/rollup'
 
 import type { InternalModuleFormat } from 'rollup'
 import type { ResolvedConfig } from 'vite'
-
-/*
-  The following functions are copied from rollup
-  https://github.com/rollup/rollup/blob/0bcf0a672ac087ff2eb88fbba45ec62389a4f45f/src/ast/nodes/MetaProperty.ts#L145-L193
-
-  https://github.com/rollup/rollup
-  The MIT License (MIT)
-  Copyright (c) 2017 [these people](https://github.com/rollup/rollup/graphs/contributors)
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-const needsEscapeRegEx = /[\n\r'\\\u2028\u2029]/
-const quoteNewlineRegEx = /([\n\r'\u2028\u2029])/g
-const backSlashRegEx = /\\/g
-
-function escapeId(id: string): string {
-  if (!needsEscapeRegEx.test(id)) return id
-  return id.replace(backSlashRegEx, '\\\\').replace(quoteNewlineRegEx, '\\$1')
-}
 
 function joinUrlSegments(a: string, b: string): string {
   if (!a || !b) return a || b || ''
   return stripTrailingSlash(a) + addLeadingSlash(b)
 }
-
-const getResolveUrl = (path: string, URL = 'URL') => `new ${URL}(${path}).href`
-
-const getRelativeUrlFromDocument = (relativePath: string, umd = false) =>
-  getResolveUrl(
-    `'${escapeId(relativePath)}', ${
-      umd ? `typeof document === 'undefined' ? location.href : ` : ''
-    }document.currentScript && document.currentScript.src || document.baseURI`,
-  )
-
-const getFileUrlFromFullPath = (path: string) =>
-  `require('u' + 'rl').pathToFileURL(${path}).href`
-
-const getFileUrlFromRelativePath = (path: string) =>
-  getFileUrlFromFullPath(`__dirname + '/${path}'`)
-
-const relativeUrlMechanisms: Record<
-InternalModuleFormat,
-(relativePath: string) => string
-> = {
-  amd: (relativePath) => {
-    if (relativePath[0] !== '.') relativePath = './' + relativePath
-    return getResolveUrl(`require.toUrl('${relativePath}'), document.baseURI`)
-  },
-  cjs: (relativePath) =>
-    `(typeof document === 'undefined' ? ${getFileUrlFromRelativePath(
-      relativePath,
-    )} : ${getRelativeUrlFromDocument(relativePath)})`,
-  es: (relativePath) => getResolveUrl(`'${relativePath}', import.meta.url`),
-  iife: (relativePath) => getRelativeUrlFromDocument(relativePath),
-  // NOTE: make sure rollup generate `module` params
-  system: (relativePath) => getResolveUrl(`'${relativePath}', module.meta.url`),
-  umd: (relativePath) =>
-    `(typeof document === 'undefined' && typeof location === 'undefined' ? ${getFileUrlFromRelativePath(
-      relativePath,
-    )} : ${getRelativeUrlFromDocument(relativePath, true)})`,
-}
-/* end of copy */
 
 const customRelativeUrlMechanisms = {
   ...relativeUrlMechanisms,
