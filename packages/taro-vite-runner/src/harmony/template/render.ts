551f3dee3fc7106867fb0cae28326c4470c2cc12
xuanzebin <xuanzebin3@jd.com>
2023-12-12 20:40:06 +0800

    feat: 增加 button 组件

--- packages/taro-vite-runner/src/harmony/template/render.ts
+++ packages/taro-vite-runner/src/harmony/template/render.ts
@@ -8,12 +8,16 @@ export default class RenderParser extends BaseParser {
   }
 
   generate () {
-    const renderContent = `// import TaroIcon from './icon'
-
+    const renderContent = `import TaroImage from './image'
+import TaroText from './text'
+import TaroView from './view'
+// import TaroIcon from './icon'
+// import TaroForm from './form'
+import TaroButton from './button'
 // import TaroLabel from './label'
 // import TaroInput from './input'
-// import { TaroSwiper, TaroSwiperItem } from './swiper'
-// import TaroButton from './button'
+// import TaroVideo from './video'
+// import TaroPicker from './picker'
 // import TaroSlider from './slider'
 // import TaroSwitch from './switch'
 // import TaroTextArea from './textArea'
@@ -21,90 +25,83 @@ export default class RenderParser extends BaseParser {
 // import TaroInnerHtml from './innerHtml'
 // import TaroScrollView from './scrollView'
 // import { TaroRadio, TaroRadioGroup } from './radio'
+// import { TaroSwiper, TaroSwiperItem } from './swiper'
 // import { TaroCheckboxGroup, TaroCheckbox } from './checkbox'
-// import TaroPicker from './picker'
-// import TaroVideo from './video'
-// import TaroForm from './form'
-import TaroImage from './image'
-import TaroText from './text'
-import TaroView from './view'
+
 ${this.generateRenderImport()}
 import { NodeType, convertNumber2VP } from '../runtime'
 import { AttributeManager } from './utils/AttributeManager'
-import type { TaroViewElement, TaroElement, TaroImageElement, TaroTextElement } from '../runtime'
-
-// import type {
-//   TaroCheckboxElement,
-//   TaroFormElement,
-//   TaroIconElement,
-//   TaroLabelElement, 
-//   TaroPickerElement, 
-//   TaroRadioElement,
-//   TaroRichTextElement,
-//   TaroRadioGroupElement, 
-//   TaroInputElement, 
-//   TaroCheckboxGroupElement,
-//   TaroTextAreaElement,
-//   TaroVideoElement,
-//   TaroSwiperItemElement,
-//   TaroSwiperElement,
-//   TaroSwitchElement,
-//   TaroSliderElement,
-//   TaroScrollViewElement,
-//   TaroButtonElement
-// } from './element'
-
-// @Builder
-// function createNode (node: TaroElement) {
-  // ${this.generateRenderCondition()}if (node.tagName === 'BUTTON') {
-  //   TaroButton({ node: node as TaroButtonElement })
-  // } else if (node.tagName === 'SCROLL-VIEW') {
-  //   TaroScrollView({ node: node as TaroScrollViewElement })
-  // } else if (node.tagName === 'SLIDER') {
-  //   TaroSlider({ node: node as TaroSliderElement })
-  // } else if (node.tagName === 'SWITCH') {
-  //   TaroSwitch({ node: node as TaroSwitchElement })
-  // } else if (node.tagName === 'INPUT') {
-  //   TaroInput({ node: node as TaroInputElement })
-  // } else if (node.tagName === 'SWIPER') {
-  //   TaroSwiper({ node: node as  TaroSwiperElement })
-  // } else if (node.tagName === 'SWIPER-ITEM') {
-  //   TaroSwiperItem({ node: node as  TaroSwiperItemElement })
-  // } else if (node.tagName === 'INNER-HTML') {
-  //   TaroInnerHtml({ node })
-  // } else if (node.tagName === 'RICH-TEXT') {
-  //   TaroRichText({ node: node as  TaroRichTextElement })
-  // } else if (node.tagName === 'ICON') {
-  //   TaroIcon({ node: node as  TaroIconElement })
-  // } else if (node.tagName === 'TEXTAREA') {
-  //   TaroTextArea({ node: node as  TaroTextAreaElement })
-  // } else if (node.tagName === 'CHECKBOX-GROUP') {
-  //   TaroCheckboxGroup({ node: node as TaroCheckboxGroupElement })
-  // } else if (node.tagName === 'CHECKBOX') {
-  //   TaroCheckbox({ node: node as  TaroCheckboxElement })
-  // } else if (node.tagName === 'RADIO-GROUP') {
-  //   TaroRadioGroup({ node: node as TaroRadioGroupElement })
-  // } else if (node.tagName === 'RADIO') {
-  //   TaroRadio({ node: node as  TaroRadioElement })
-  // } else if (node.tagName === 'LABEL') {
-  //   TaroLabel({ node: node as  TaroLabelElement })
-  // } else if (node.tagName === 'PICKER') {
-  //   TaroPicker({ node: node as  TaroPickerElement })
-  // } else if (node.tagName === 'FORM') {
-  //   TaroForm({ node: node as  TaroFormElement })
-  // } else if (node.tagName === 'VIDEO') {
-  //   TaroVideo({ node: node as TaroVideoElement })
-  // }
-// }
+import type {
+  TaroViewElement,
+  TaroElement,
+  TaroImageElement,
+  TaroButtonElement
+  TaroTextElement,
+  TaroCheckboxElement,
+  TaroFormElement,
+  TaroIconElement,
+  TaroLabelElement, 
+  TaroPickerElement, 
+  TaroRadioElement,
+  TaroRichTextElement,
+  TaroRadioGroupElement, 
+  TaroInputElement, 
+  TaroCheckboxGroupElement,
+  TaroTextAreaElement,
+  TaroVideoElement,
+  TaroSwiperItemElement,
+  TaroSwiperElement,
+  TaroSwitchElement,
+  TaroSliderElement,
+  TaroScrollViewElement,
+} from '../runtime'
 
 @Builder
 function createChildItem (item: TaroElement) {
-  if (item.tagName === 'VIEW') {
+  ${this.generateRenderCondition()}if (item.tagName === 'VIEW') {
     TaroView(item as TaroViewElement)
   } else if (item.tagName === 'TEXT' || item.nodeType === NodeType.TEXT_NODE) {
     TaroText(item as TaroTextElement)
   } else if (item.tagName === 'IMAGE') {
     TaroImage(item as TaroImageElement)
+  } else if (item.tagName === 'BUTTON') {
+    TaroButton(item as TaroButtonElement)
+  } else if (node.tagName === 'SCROLL-VIEW') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'SLIDER') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'SWITCH') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'INPUT') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'SWIPER') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'SWIPER-ITEM') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'INNER-HTML') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'RICH-TEXT') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'ICON') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'TEXTAREA') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'CHECKBOX-GROUP') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'CHECKBOX') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'RADIO-GROUP') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'RADIO') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'LABEL') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'PICKER') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'FORM') {
+    TaroView(item as TaroViewElement)
+  } else if (node.tagName === 'VIDEO') {
+    TaroView(item as TaroViewElement)
   } else {
     TaroView(item as TaroViewElement)
   }
@@ -146,7 +143,7 @@ function createLazyChildren (node: TaroElement) {
   }, (item: TaroElement) => item._nid)
 }
 
-export { createChildItem, createLazyChildren }
+export { createChildItem, createChildItemWithPosition, createLazyChildren }
 `
 
     return renderContent
