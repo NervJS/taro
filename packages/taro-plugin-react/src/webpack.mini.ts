e0eb073b1f95a7d192ee27b8dcce9db3c94bf014
liuzejia <liuzejia@SZMAC-FV0MR4G7.local>
2023-12-19 21:00:45 +0800

    feat: mobx-react 不设置别名

--- packages/taro-plugin-react/src/webpack.mini.ts
+++ packages/taro-plugin-react/src/webpack.mini.ts
@@ -27,7 +27,7 @@ function setAlias (ctx: IPluginContext, framework: Frameworks, chain) {
       const newFilePath = path.join(path.dirname(reactModulePath), 'cjs', 'react.production.min.js')
 
       alias.set('react-reconciler$', 'react-reconciler/cjs/react-reconciler.production.min.js')
-      alias.set('react$', newFilePath)
+      alias.set(/^(?!.*mobx-react$).*react$/, newFilePath)
       alias.set('react/jsx-runtime$', 'react/cjs/react-jsx-runtime.production.min.js')
 
       // 在React18中，使用了exports字段约定了模块暴露路径，其中并未暴露 ./cjs/ 。这将使上面的alias在编译时报错。相当的tricky。
