faf2996f483058bffae2f24cc580e8cbe2f61698
ZakaryCode <zakarycode@gmail.com>
2023-12-19 19:27:55 +0800

    feat(ets): 移除 vibrate 弃用方法

--- packages/taro-platform-harmony/src/apis/device/vibrate.ts
+++ packages/taro-platform-harmony/src/apis/device/vibrate.ts
@@ -1,25 +1,32 @@
 import vibrator from '@ohos.vibrator'
 
-import { callAsyncFail, callAsyncSuccess } from '../utils'
+import { MethodHandler } from '../utils/handler'
 
 import type Taro from '@tarojs/taro/types'
 
 const VIBRATOR_LONG_TIME = 400
 const VIBRATOR_SHORT_TIME = 15
 
-function vibrateBaseGenerator (time: number) {
-  return (options: Taro.vibrateLong.Option) => {
+function vibrateBaseGenerator (duration: number, name = '') {
+  return ({ success, fail, complete }: Taro.vibrateLong.Option = {}) => {
+    const handle = new MethodHandler({ name, success, fail, complete })
     return new Promise((resolve, reject) => {
-      vibrator.vibrate(time).then(() => {
-        const res = { errMsg: 'vibrateShort:ok' }
-        callAsyncSuccess(resolve, res, options)
+      vibrator.startVibration({
+        type: 'time',
+        duration,
+      }, {
+        usage: 'unknown'
       }, (error) => {
-        const res = { errMsg: 'vibrateShort:fail' + error }
-        callAsyncFail(reject, res, options)
+        if (error) {
+          return handle.fail({
+            errMsg: `vibrate fail, error.code: ${error.code}; error.message: ${error.message}`
+          }, { resolve, reject })
+        }
+        return handle.success({}, { resolve, reject })
       })
     })
   }
 }
 
-export const vibrateLong = vibrateBaseGenerator(VIBRATOR_LONG_TIME)
-export const vibrateShort = vibrateBaseGenerator(VIBRATOR_SHORT_TIME)
+export const vibrateLong = vibrateBaseGenerator(VIBRATOR_LONG_TIME, 'vibrateLong')
+export const vibrateShort = vibrateBaseGenerator(VIBRATOR_SHORT_TIME, 'vibrateShort')
