161c8acd53c57fcefc9243387b9c7ed4a1ac1d45
chenjiajian <798095202@qq.com>
2023-12-19 15:21:58 +0800

    fix(miniapp): 修复小程序Input组件使用async onInput回调时输入框闪烁的问题 (#14998)
    * fix(miniapp): 修复小程序Input组件使用async onInput回调时输入框闪烁的问题
    
    * fix: ci

--- packages/taro-tt/src/runtime-utils.ts
+++ packages/taro-tt/src/runtime-utils.ts
@@ -5,9 +5,16 @@ export * from './apis-list'
 export * from './components'
 export const hostConfig = {
   initNativeApi,
-  modifyMpEventImpl: (event) => {
+  modifyMpEventImpl (event) {
     if (event.type === 'regionchange') {
       event.type = event.detail.type
+    }
+  },
+  modifyTaroEventReturn (node, event, returnVal) {
+    if (node.nodeName === 'pay-button') {
+      if (event.type === 'applyrefund' || event.type === 'getgoodsinfo') {
+        return returnVal
+      }
     }
   }
 }
