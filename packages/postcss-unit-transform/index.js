5dfa518dedc7ddcb27f7f835b5aaaa7dbf51ae58
fahasikei <1299484460@qq.com>
2023-12-21 17:28:30 +0800

    feat: 修复convert-lab转换样式问题，postcss-unit-transform包添加单元测试，补充使用工具函数初始化page页面

--- packages/postcss-unit-transform/index.js
+++ packages/postcss-unit-transform/index.js
@@ -6,9 +6,9 @@ function plugin (opts) {
   return function (root) {
     root.walkDecls(function (decl) {
       let value = decl.value
-      value = value.replace(/\b-?(\d+(\.\d+)?)px\b/ig, function (size) {
+      value = value.replace(/\b-?(\d+(\.\d+)?)px\b/ig, function (match, size) {
         return Number(size) === 0 ? '0px': parseFloat(size) * 2 + 'px'
-      }).replace(/\b-?(\d+(\.\d+)?)rpx\b/ig, function (size) {
+      }).replace(/\b-?(\d+(\.\d+)?)rpx\b/ig, function (match, size) {
         return size + 'px'
       })
       decl.value = value
