3a4999a9b8dcddb0d1646af39ab50bad9caed573
tony chen <tony.zq.chen@gmail.com>
2023-12-21 11:47:16 +0800

    feat: 0.73

--- packages/taro-components-rn/src/setup.ts
+++ packages/taro-components-rn/src/setup.ts
@@ -1,1 +1,13 @@
 import '@testing-library/jest-native/extend-expect'
+
+jest.mock('react-native-webview', () => {
+  const React = jest.requireActual('react')
+  const { View } = jest.requireActual('react-native')
+  const WebView = (props) => React.createElement(View, props)
+
+  return {
+    WebView,
+    default: WebView,
+    __esModule: true,
+  }
+})
--- packages/taro-rn-runner/templates/metro.config.js
+++ packages/taro-rn-runner/templates/metro.config.js
@@ -1,10 +1,14 @@
-const { mergeConfig } = require('metro-config')
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const { getMetroConfig } = require('@tarojs/rn-supporter')
 
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+
 module.exports = (async function (){
-  return mergeConfig({
-  // custom your metro config here
-  // https://facebook.github.io/metro/docs/configuration
-    resolver: {}
-  }, await getMetroConfig())
+    return mergeConfig(getDefaultConfig(__dirname), await getMetroConfig(), config)
 })()
