4d9be6a85d66a593516db3ad1df3618725f730c8
Chen-jj <798095202@qq.com>
2023-12-21 21:08:19 +0800

    chore: update workflow nodejs

---
+++ packages/taro-helper/scripts/artifacts.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const fs = require('fs')
+
+const plugins = [
+  'swc_plugin_compile_mode',
+]
+
+plugins.forEach(plugin => {
+  const srcPath = path.join(__dirname, `../../../../crates/native_binding/artifacts/${plugin}.wasm`)
+  const destPath = path.join(__dirname, `../swc/${plugin}.wasm`)
+  fs.copyFile(srcPath, destPath, err => err && console.log('[@tarojs/helper] artifacts error: ', err))
+})
