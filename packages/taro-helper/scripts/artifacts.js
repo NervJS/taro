8f7cedfacc6d2b7e2f74beb8234c6ebccc2747f1
Chen-jj <798095202@qq.com>
2023-12-21 22:23:11 +0800

    chore(release): publish 3.6.22-beta.0 --tag=beta

--- packages/taro-helper/scripts/artifacts.js
+++ packages/taro-helper/scripts/artifacts.js
@@ -6,7 +6,7 @@ const plugins = [
 ]
 
 plugins.forEach(plugin => {
-  const srcPath = path.join(__dirname, `../../../../crates/native_binding/artifacts/wasm-wasi-swc_plugins/${plugin}.wasm`)
+  const srcPath = path.join(__dirname, `../../../crates/native_binding/artifacts/wasm-wasi-swc_plugins/${plugin}.wasm`)
   const destPath = path.join(__dirname, `../swc/${plugin}.wasm`)
   fs.copyFile(srcPath, destPath, err => err && console.log('[@tarojs/helper] artifacts error: ', err))
 })
