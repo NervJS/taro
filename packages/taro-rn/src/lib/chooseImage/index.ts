a466f47e183342d8997cc22f303ffff9cdbc5c3f
tony chen <tony.zq.chen@gmail.com>
2023-12-27 11:43:50 +0800

    fix: chooseImage & chooseVideo

--- packages/taro-rn/src/lib/chooseImage/index.ts
+++ packages/taro-rn/src/lib/chooseImage/index.ts
@@ -2,44 +2,32 @@ import { chooseMedia, MEDIA_TYPE } from '../media'
 import { showActionSheet } from '../showActionSheet'
 
 export function chooseImage(opts: Taro.chooseImage.Option): Promise<any> {
-  const { sourceType } = opts
-  switch (JSON.stringify(sourceType)) {
-    case '[\'camera\']':
-    case '["camera"]':
-      return chooseMedia({...opts, sourceType: ['camera']}, MEDIA_TYPE.IMAGES)
-    case '[\'album\']':
-    case '["album"]':
-      return chooseMedia({...opts, sourceType: ['album']}, MEDIA_TYPE.IMAGES)
-    default:
-      return showImagePicker(opts)
+  const {
+    sourceType = ['album', 'camera'],
+  } = opts
+  if(sourceType?.includes('camera') && sourceType?.includes('album')) {
+    return showImagePicker(opts)
+  } else if (sourceType?.includes('camera')) {
+    return chooseMedia({...opts, sourceType: ['camera']}, MEDIA_TYPE.IMAGES)
   }
+  return chooseMedia({...opts, sourceType: ['album']}, MEDIA_TYPE.IMAGES)
 }
 
-function showImagePicker(options: Taro.chooseImage.Option):any {
+async function showImagePicker(options: Taro.chooseImage.Option):Promise<any> {
   const { fail, complete } = options
-  return new Promise((resolve, reject) => { 
-    showActionSheet({
+  try {
+    const res = await showActionSheet({
       itemList: ['拍照', '从手机相册选择'],
-      success: function (res) {
-        if (res.tapIndex === 0) {
-          resolve(chooseMedia({...options, sourceType: ['camera']}, MEDIA_TYPE.IMAGES))
-        }
-        if (res.tapIndex === 1) {
-          resolve(chooseMedia({...options, sourceType: ['album']}, MEDIA_TYPE.IMAGES))
-        }
-      },
-      fail: function () {
-        const res = { errMsg: 'chooseImage:fail cancel' }
-        reject(res)
-      }
-    }).catch(err => {
-      console.log('error: ', err.errMsg)
-      if (/showActionSheet:fail cancel/.test(err.errMsg)) {
-        const res = { errMsg: 'chooseImage:fail cancel' }
-        fail?.(res)
-        complete?.(res)
-        reject(res)
-      }
     })
-  })
+    if (res.tapIndex === 0) {
+      return chooseMedia({...options, sourceType: ['camera']}, MEDIA_TYPE.IMAGES)
+    }
+    if (res.tapIndex === 1) {
+      return chooseMedia({...options, sourceType: ['album']}, MEDIA_TYPE.IMAGES)
+    }
+  } catch (err) {
+    const res = { errMsg: 'chooseImage: fail' }
+    fail?.(res)
+    complete?.(res)
+  }
 }
