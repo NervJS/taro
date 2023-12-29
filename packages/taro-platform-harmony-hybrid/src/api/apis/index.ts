12d713456e79d0384187e3a37731e8c1b6b32d76
guoenxuan <genxuan_gdx@163.com>
2023-12-29 11:10:11 +0800

    feat: 增加导航栏状态更新逻辑

--- packages/taro-platform-harmony-hybrid/src/api/apis/index.ts
+++ packages/taro-platform-harmony-hybrid/src/api/apis/index.ts
@@ -1,5 +1,3 @@
-import Taro from '@tarojs/api'
-
 export * from './base'
 export * from './canvas'
 export * from './device'
@@ -14,12 +12,3 @@ export * from './open-api'
 export * from './route'
 export * from './ui'
 export * from './wxml'
-
-Taro.eventCenter.on('__taroSetNavigationStyle', (style, textStyle, backgroundColor) => {
-  if (typeof window !== 'undefined') {
-    // @ts-ignore
-    window.native?.setNavigationStyle?.(style, textStyle, backgroundColor)
-    // @ts-ignore
-    window.currentNavigationStyle = style
-  }
-})
