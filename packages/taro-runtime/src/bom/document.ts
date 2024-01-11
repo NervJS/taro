bbc694db87a67c4a0b5646d94f092946a3533059
ZakaryCode <zakarycode@gmail.com>
2024-01-11 16:02:48 +0800

    feat(runtime): 更新运行时依赖文件

--- packages/taro-runtime/src/bom/document.ts
+++ packages/taro-runtime/src/bom/document.ts
@@ -1,5 +1,3 @@
-import { isWebPlatform } from '@tarojs/shared'
-
 import {
   APP,
   BODY,
@@ -10,49 +8,39 @@ import {
 import { TaroDocument } from '../dom/document'
 import env from '../env'
 
-let document
+function createDocument (): TaroDocument {
+  /**
+     * <document>
+     *   <html>
+     *     <head></head>
+     *     <body>
+     *       <container>
+     *         <app id="app" />
+     *       </container>
+     *     </body>
+     *   </html>
+     * </document>
+     */
+  const doc = new TaroDocument()
+  const documentCreateElement = doc.createElement.bind(doc)
+  const html = documentCreateElement(HTML)
+  const head = documentCreateElement(HEAD)
+  const body = documentCreateElement(BODY)
+  const app = documentCreateElement(APP)
+  app.id = APP
+  const container = documentCreateElement(CONTAINER) // 多包一层主要为了兼容 vue
 
-if (process.env.TARO_ENV && !isWebPlatform()) {
-  /* eslint-disable no-inner-declarations */
-  function createDocument (): TaroDocument {
-    /**
-       * <document>
-       *   <html>
-       *     <head></head>
-       *     <body>
-       *       <container>
-       *         <app id="app" />
-       *       </container>
-       *     </body>
-       *   </html>
-       * </document>
-       */
-    const doc = new TaroDocument()
-    const documentCreateElement = doc.createElement.bind(doc)
-    const html = documentCreateElement(HTML)
-    const head = documentCreateElement(HEAD)
-    const body = documentCreateElement(BODY)
-    const app = documentCreateElement(APP)
-    app.id = APP
-    const container = documentCreateElement(CONTAINER) // 多包一层主要为了兼容 vue
+  doc.appendChild(html)
+  html.appendChild(head)
+  html.appendChild(body)
+  body.appendChild(container)
+  container.appendChild(app)
 
-    doc.appendChild(html)
-    html.appendChild(head)
-    html.appendChild(body)
-    body.appendChild(container)
-    container.appendChild(app)
+  doc.documentElement = html
+  doc.head = head
+  doc.body = body
 
-    doc.documentElement = html
-    doc.head = head
-    doc.body = body
-
-    return doc
-  }
-  document = env.document = createDocument()
-} else {
-  document = env.document
+  return doc
 }
 
-export {
-  document
-}
+export const document: TaroDocument = process.env.TARO_PLATFORM === 'web' ? env.document : (env.document = createDocument())
