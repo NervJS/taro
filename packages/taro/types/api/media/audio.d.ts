72e4a7211960eae023b0167d93a8c5a71c51201d
Zakary <zakarycode@gmail.com>
2024-01-20 16:14:20 +0800

    Update audio.d.ts

--- packages/taro/types/api/media/audio.d.ts
+++ packages/taro/types/api/media/audio.d.ts
@@ -799,6 +799,7 @@ declare module '../../index' {
      */
     setPosition(...args: any[]): void
   }
+
   namespace createInnerAudioContext {
     interface Option {
       /** 是否使用 WebAudio 作为底层音频驱动，默认关闭。对于短音频、播放频繁的音频建议开启此选项，开启后将获得更优的性能表现。由于开启此选项后也会带来一定的内存增长，因此对于长音频建议关闭此选项。
@@ -807,6 +808,7 @@ declare module '../../index' {
       useWebAudioImplement: boolean
     }
   }
+
   interface TaroStatic {
     /** 结束播放语音。
      * **注意：1.6.0 版本开始，本接口不再维护。建议使用能力更强的 [Taro.createInnerAudioContext](./createInnerAudioContext) 接口**
