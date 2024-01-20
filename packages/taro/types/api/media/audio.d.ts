da1913bc4ccf285815fc8cdb9a202339f5ad3759
Zakary <zakarycode@gmail.com>
2024-01-20 16:13:36 +0800

    Update audio.d.ts

--- packages/taro/types/api/media/audio.d.ts
+++ packages/taro/types/api/media/audio.d.ts
@@ -799,12 +799,11 @@ declare module '../../index' {
      */
     setPosition(...args: any[]): void
   }
-  /**
-   * @supported weapp
-   */
   namespace createInnerAudioContext {
     interface Option {
-      /** 是否使用 WebAudio 作为底层音频驱动，默认关闭。对于短音频、播放频繁的音频建议开启此选项，开启后将获得更优的性能表现。由于开启此选项后也会带来一定的内存增长，因此对于长音频建议关闭此选项。 */
+      /** 是否使用 WebAudio 作为底层音频驱动，默认关闭。对于短音频、播放频繁的音频建议开启此选项，开启后将获得更优的性能表现。由于开启此选项后也会带来一定的内存增长，因此对于长音频建议关闭此选项。
+       * @supported weapp
+       */
       useWebAudioImplement: boolean
     }
   }
