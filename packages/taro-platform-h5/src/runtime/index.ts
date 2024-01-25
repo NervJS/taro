f11e45664fa01224ce57c15ef224a6f7c967d3e0
ZakaryCode <zakarycode@gmail.com>
2024-01-25 10:47:31 +0800

    fix(h5): 修复适配组件 & API 样式引入

--- packages/taro-platform-h5/src/runtime/index.ts
+++ packages/taro-platform-h5/src/runtime/index.ts
@@ -1,6 +1,4 @@
 import '@tarojs/components/dist/taro-components/taro-components.css'
-import '@tarojs/taro-h5/dist/index.css'
-import '@tarojs/components-react/dist/index.css'
 
 import { handlePolyfill } from '@tarojs/runtime'
 
--- packages/taro-platform-harmony-hybrid/src/runtime/index.ts
+++ packages/taro-platform-harmony-hybrid/src/runtime/index.ts
@@ -1,3 +1,1 @@
 import '@tarojs/components/dist/taro-components/taro-components.css'
-import '@tarojs/taro-h5/dist/index.css'
-import '@tarojs/components-react/dist/index.css'
