6bf73615100298a8f91ceb76bb4fa3043db7a113
Zakary <zakarycode@gmail.com>
2024-02-23 17:15:28 +0800

    Update Swiper.d.ts

--- packages/taro-components/types/Swiper.d.ts
+++ packages/taro-components/types/Swiper.d.ts
@@ -27,7 +27,7 @@ interface SwiperProps extends StandardProps {
    */
   current?: number
   /** 当前所在滑块的 item-id ，不能与 current 被同时指定
-   * @supported swan, tt, qq, jd, harmony_hybrid
+   * @supported swan, tt, qq, jd, h5, harmony_hybrid
    * @weapp deprecated
    * @default ""
    */
