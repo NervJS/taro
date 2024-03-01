2489d8c254dc52cdd2691bad27670cd08b9c089c
mayintao3 <mayintao3@jd.com>
2024-03-01 19:39:33 +0800

    feat: 修改movableView组件

--- packages/taro-platform-harmony/src/runtime-ets/dom/element/movableView.ts
+++ packages/taro-platform-harmony/src/runtime-ets/dom/element/movableView.ts
@@ -3,6 +3,7 @@ import { TaroElement } from './element'
 
 import type { MovableViewProps } from '@tarojs/components/types'
 
+@Observed
 export class TaroMovableViewElement extends TaroElement<MovableViewProps & {'animation': undefined}> {
 
   constructor() {
