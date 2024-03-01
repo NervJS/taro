d286522787b758d54ee3e7901163bce1bd83b442
mayintao3 <mayintao3@jd.com>
2024-03-01 19:32:17 +0800

    feat: 修改progress组件

--- packages/taro-platform-harmony/src/runtime-ets/dom/element/progress.ts
+++ packages/taro-platform-harmony/src/runtime-ets/dom/element/progress.ts
@@ -3,6 +3,7 @@ import { TaroElement } from './element'
 
 import type { ProgressProps } from '@tarojs/components/types'
 
+@Observed
 export class TaroProgressElement extends TaroElement<ProgressProps> {
 
   constructor() {
