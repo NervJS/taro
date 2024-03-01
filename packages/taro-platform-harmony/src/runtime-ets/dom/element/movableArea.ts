599e44a455f4cd29d53219c1e319820fc353f784
mayintao3 <mayintao3@jd.com>
2024-03-01 19:40:53 +0800

    feat: 修改movableArea

--- packages/taro-platform-harmony/src/runtime-ets/dom/element/movableArea.ts
+++ packages/taro-platform-harmony/src/runtime-ets/dom/element/movableArea.ts
@@ -3,6 +3,7 @@ import { TaroElement } from './element'
 
 import type { MovableAreaProps } from '@tarojs/components/types'
 
+@Observed
 export class TaroMovableAreaElement extends TaroElement<MovableAreaProps> {
 
   constructor() {
