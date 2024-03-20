da163b710ba98d8bd74ccd9461fdd3391f0837b6
qcjxs <1447515520@qq.com>
2024-03-20 17:23:50 +0800

    fix: 修复mini-program-example下运行run dev:mpharmony时的jsBridgeMode报错问题

--- examples/mini-program-example/src/util/nativeApi.ts
+++ examples/mini-program-example/src/util/nativeApi.ts
@@ -1,10 +1,11 @@
 // @proxyClassSign('')
 class NativeApi {
   // @ts-ignore
-  @window.MethodChannel.jsBridgeMode({ isAsync: false, autoRelease: true })
+  @window.MethodChannel?.jsBridgeMode({ isAsync: false, autoRelease: true })
   harmonyNavigateTo(options: any) {
     return options
   }
 }
 
-export const nativeApi = new NativeApi()
+const nativeApi = new NativeApi()
+export default nativeApi
