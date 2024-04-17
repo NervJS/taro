38cd70a1f3724fb4c94278ac90b36005a2ea5da0
koppt <kopptsoft@gmail.com>
2024-04-17 16:38:46 +0800

    chore(runner): 补充 runner 的 skyline 测试用例

---
+++ packages/taro-webpack5-runner/src/__tests__/fixtures/skyline/src/app.config.js
@@ -0,0 +1,17 @@
+export default {
+  pages: [
+    'pages/index/index'
+  ],
+  lazyCodeLoading: "requiredComponents",
+  rendererOptions: {
+    skyline: {
+      defaultDisplayBlock: true,
+    },
+  },
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+}
