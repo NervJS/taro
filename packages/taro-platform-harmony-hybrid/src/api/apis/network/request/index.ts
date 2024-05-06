48fe07aaf0ce1cc8c51e967eb5866ef67cb7b5b5
刘阳 <liuyang@58.com>
2024-05-06 18:52:13 +0800

    支持addInterceptor和clearInterceptor

--- packages/taro-platform-harmony-hybrid/src/api/apis/network/request/index.ts
+++ packages/taro-platform-harmony-hybrid/src/api/apis/network/request/index.ts
@@ -1,15 +1,43 @@
-import { request as h5Request } from '@tarojs/taro-h5'
+import { request as h5Request, addInterceptor as h5AddInterceptor, cleanInterceptors as h5CleanInterceptors } from '@tarojs/taro-h5'
 
-import { request as nativeReuqest } from './nativeRequest'
-
+import { request as nativeReuqest, addInterceptor as nativeAddInterceptor, cleanInterceptors as nativeCleanInterceptors } from './nativeRequest'
 
 /**
- * 封装请求方法
- * @param options 请求选项
+ * 发起 HTTPS 网络请求
+ *
+ * @canUse request
+ * @__object [url, data, header, timeout, method[OPTIONS, GET, HEAD, POST, PUT, PATCH, DELETE, TRACE, CONNECT], dataType[json, text, base64, arraybuffer], responseType[text, arraybuffer], enableCache]
+ * @__success [data, header, statusCode, cookies]
  * @param useNativeRequest 默认使用true
  */
 export function request (options: any, useNativeRequest: boolean = true) {
   return useNativeRequest ? nativeReuqest(options) : h5Request(options)
+}
+
+
+/**
+ * 网络请求任务对象
+ *
+ * @canUse RequestTask
+ * @__class [abort, onHeadersReceived, offHeadersReceived]
+ */
+
+/**
+ * 使用拦截器
+ *
+ * @canUse addInterceptor
+ */
+export function addInterceptor (interceptor, useNativeRequest: boolean = true){
+  return useNativeRequest ? nativeAddInterceptor(interceptor) : h5AddInterceptor(interceptor)
+}
+
+/**
+ * 清除所有拦截器
+ *
+ * @canUse cleanInterceptors
+ */
+export function cleanInterceptors (interceptor, useNativeRequest: boolean = true) {
+  return useNativeRequest ? nativeCleanInterceptors(interceptor) : h5CleanInterceptors(interceptor)
 }
 
 
