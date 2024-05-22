cb488f00bcacbf2b31949638370e368f90c11ea2
liuzejia <liuzejia@SZMAC-FV0MR4G7.local>
2024-05-22 22:28:23 +0800

    test: taro-api 测试配置修改

--- packages/taro-api/jest.config.js
+++ packages/taro-api/jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
   },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   moduleNameMapper: {
+    '@tarojs/taro': '<rootDir>/src/index.ts',
     '@tarojs/api': '<rootDir>/src/index.ts',
     '@tarojs/shared': path.resolve(__dirname, '..', '..', 'packages/shared/src/index.ts'),
     '@tarojs/runtime': path.resolve(__dirname, '..', '..', 'packages/taro-runtime/dist/runtime.esm.js')
