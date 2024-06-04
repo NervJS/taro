8d306ece673241bdcf8c5ea03bb258b7a806cc53
phy-lei <765373325@qq.com>
2024-06-04 15:51:57 +0800

    fix: 修复合并的问题

--- packages/taro-framework-solid/src/index.ts
+++ packages/taro-framework-solid/src/index.ts
@@ -120,12 +120,5 @@ export default (ctx: IPluginContext) => {
       componentConfig.includes.add(nodeName)
     }
   })
-
-  // 映射、收集使用到的小程序组件
-  ctx.onParseCreateElement(({ nodeName, componentConfig }: IParseCreateElementArgs) => {
-    if (capitalize(toCamelCase(nodeName)) in internalComponents) {
-      componentConfig.includes.add(nodeName)
-    }
-  })
 }
 
