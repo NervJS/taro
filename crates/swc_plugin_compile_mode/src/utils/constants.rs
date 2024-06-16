83b3ef82dfedb1f4f8793181c37e9e55ec780a33
xuanzebin <xuanzebin3@jd.com>
2024-06-16 13:02:11 +0800

    fix: 修复 imagespan verticalAlign 不生效的问题

--- crates/swc_plugin_compile_mode/src/utils/constants.rs
+++ crates/swc_plugin_compile_mode/src/utils/constants.rs
@@ -141,42 +141,23 @@ function createText (node: TaroTextElement) {
   }
 }
 
+function getImageSpanAlignment (align: TaroAny): TaroAny {
+  if (align === Alignment.Top) {
+    return ImageSpanAlignment.TOP
+  } else if (align === Alignment.Bottom) {
+    return ImageSpanAlignment.BOTTOM
+  } else if (align === Alignment.Center) {
+    return ImageSpanAlignment.CENTER
+  }
+}
+
 @Builder
 function createTextChildNode (item: TaroElement) {
   if (item.tagName === 'IMAGE') {
     ImageSpan(item.getAttribute('src'))
-      // .attributeModifier(commonStyleModify.setNode(item))
+      .attributeModifier(commonStyleModify.setNode(item))
       .objectFit(getImageMode(item.getAttribute('mode')))
-      // .verticalAlign(align)
-      .width(item._st.hmStyle.width)
-      .height(item._st.hmStyle.height)
-      .margin({
-        top: item._st.hmStyle.marginTop,
-        left: item._st.hmStyle.marginLeft,
-        right: item._st.hmStyle.marginRight,
-        bottom: item._st.hmStyle.marginBottom,
-      })
-      .padding({
-        top: item._st.hmStyle.paddingTop,
-        left: item._st.hmStyle.paddingLeft,
-        right: item._st.hmStyle.paddingRight,
-        bottom: item._st.hmStyle.paddingBottom,
-      })
-      .textBackgroundStyle({
-        color: item._st.hmStyle.backgroundColor,
-        radius: {
-          topLeft: item._st.hmStyle.borderTopLeftRadius,
-          topRight: item._st.hmStyle.borderTopRightRadius,
-          bottomLeft: item._st.hmStyle.borderBottomLeftRadius,
-          bottomRight: item._st.hmStyle.borderBottomRightRadius,
-        }
-      })
-      .borderRadius({
-        topLeft: item._st.hmStyle.borderTopLeftRadius,
-        topRight: item._st.hmStyle.borderTopRightRadius,
-        bottomLeft: item._st.hmStyle.borderBottomLeftRadius,
-        bottomRight: item._st.hmStyle.borderBottomRightRadius
-      })
+      .verticalAlign(getImageSpanAlignment(item?.hmStyle?.verticalAlign))
       .onClick(shouldBindEvent((e: ClickEvent) => { eventHandler(e, 'click', item) }, item, ['click']))
   } else if (item.nodeType === NodeType.TEXT_NODE) {
     Span(item.textContent)
