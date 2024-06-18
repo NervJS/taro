b55d35bea9a9bab506f8d7165f8632867cf8d573
licunhao1 <1441249604@qq.com>
2024-06-18 11:58:47 +0800

    fix: 部分API不支持，修改为继承h5

--- packages/taro-platform-harmony-hybrid/src/api/apis/device/wifi/getWifiList.ts
+++ packages/taro-platform-harmony-hybrid/src/api/apis/device/wifi/getWifiList.ts
@@ -1,33 +1,6 @@
-import Taro from '@tarojs/taro'
-import { shouldBeObject } from 'src/api/apis/utils'
-import { MethodHandler } from 'src/api/apis/utils/handler'
-
-import nativeWifi from './NativeWifi'
-
 /**
  * 请求获取 Wi-Fi 列表
  *
- * @canUse getWifiList
+ * @canNotUse getWifiList
  */
-export const getWifiList: typeof Taro.getWifiList = (options) => {
-  const name = 'getWifiList'
-  const isValid = shouldBeObject(options).flag || typeof options === 'undefined'
-  if (!isValid) {
-    const res = { errMsg: `${name}:fail invalid params` }
-    console.error(res.errMsg)
-    return Promise.reject(res)
-  }
-  const { success, fail, complete } = options || {}
-  const handle = new MethodHandler({ name, success, fail, complete })
-
-  return new Promise((resolve, reject) => {
-    nativeWifi.getWifiList({
-      success: (res: any) => {
-        handle.success(res, { resolve, reject })
-      },
-      fail: (err: any) => {
-        handle.fail(err, { resolve, reject })
-      },
-    })
-  })
-}
+export { getWifiList } from '@tarojs/taro-h5'
--- packages/taro-platform-harmony-hybrid/src/api/apis/device/wifi/stopWifi.ts
+++ packages/taro-platform-harmony-hybrid/src/api/apis/device/wifi/stopWifi.ts
@@ -1,33 +1,6 @@
-import Taro from '@tarojs/taro'
-import { shouldBeObject } from 'src/api/apis/utils'
-import { MethodHandler } from 'src/api/apis/utils/handler'
-
-import nativeWifi from './NativeWifi'
-
 /**
  * 关闭 Wi-Fi 模块
  *
- * @canUse stopWifi
+ * @canNotUse stopWifi
  */
-export const stopWifi: typeof Taro.stopWifi = (options) => {
-  const name = 'stopWifi'
-  const isValid = shouldBeObject(options).flag || typeof options === 'undefined'
-  if (!isValid) {
-    const res = { errMsg: `${name}:fail invalid params` }
-    console.error(res.errMsg)
-    return Promise.reject(res)
-  }
-  const { success, fail, complete } = options || {}
-  const handle = new MethodHandler({ name, success, fail, complete })
-
-  return new Promise((resolve, reject) => {
-    nativeWifi.stopWifi({
-      success: (res: any) => {
-        handle.success(res, { resolve, reject })
-      },
-      fail: (err: any) => {
-        handle.fail(err, { resolve, reject })
-      },
-    })
-  })
-}
+export { stopWifi } from '@tarojs/taro-h5'
