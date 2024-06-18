d1166c6148e810f2e82e0348e13f91c17a6d0682
licunhao1 <1441249604@qq.com>
2024-06-18 11:49:30 +0800

    fix: 部分API不支持，修改为继承h5

--- packages/taro-platform-harmony-hybrid/src/api/apis/device/wifi/startWifi.ts
+++ packages/taro-platform-harmony-hybrid/src/api/apis/device/wifi/startWifi.ts
@@ -1,33 +1,6 @@
-import Taro from '@tarojs/taro'
-import { shouldBeObject } from 'src/api/apis/utils'
-import { MethodHandler } from 'src/api/apis/utils/handler'
-
-import nativeWifi from './NativeWifi'
-
 /**
  * 初始化 Wi-Fi 模块
  *
- * @canUse startWifi
+ * @canNotUse startWifi
  */
-export const startWifi: typeof Taro.startWifi = (options) => {
-  const name = 'startWifi'
-  const isValid = shouldBeObject(options).flag || typeof options === 'undefined'
-  if (!isValid) {
-    const res = { errMsg: `${name}:fail invalid params` }
-    console.error(res.errMsg)
-    return Promise.reject(res)
-  }
-  const { success, fail, complete } = options || {}
-  const handle = new MethodHandler({ name, success, fail, complete })
-
-  return new Promise((resolve, reject) => {
-    nativeWifi.startWifi({
-      success: (res: any) => {
-        handle.success(res, { resolve, reject })
-      },
-      fail: (err: any) => {
-        handle.fail(err, { resolve, reject })
-      },
-    })
-  })
-}
+export { startWifi } from '@tarojs/taro-h5'
