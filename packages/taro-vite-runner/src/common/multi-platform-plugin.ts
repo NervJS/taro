c62deb4847c099a8edb5ad7db5dac7563b1070d3
liuzejia <liuzejia@SZMAC-FV0MR4G7.local>
2024-06-24 17:31:16 +0800

    fix: 修复vite在h5 dev模式下resolveId 的bug

--- packages/taro-vite-runner/src/common/multi-platform-plugin.ts
+++ packages/taro-vite-runner/src/common/multi-platform-plugin.ts
@@ -7,6 +7,17 @@ import { isVirtualModule } from '../utils'
 import type { ViteH5CompilerContext, ViteHarmonyCompilerContext, ViteMiniCompilerContext } from '@tarojs/taro/types/compile/viteCompilerContext'
 import type { PluginOption } from 'vite'
 
+function isViteDepsPath(filePath: string) {
+  const normalizedPath = path.normalize(filePath);
+  
+  // 判断路径是否包含 node_modules/.vite/deps
+  const isViteDeps = normalizedPath.includes(
+    path.join('node_modules', '.vite', 'deps')
+  );
+  
+  return isViteDeps;
+}
+
 export default function (complier: ViteH5CompilerContext | ViteHarmonyCompilerContext | ViteMiniCompilerContext): PluginOption {
   const { taroConfig } = complier
 
@@ -29,6 +40,13 @@ export default function (complier: ViteH5CompilerContext | ViteHarmonyCompilerCo
       const ext = path.extname(source)
       const dir = path.dirname(source)
       const basename = path.basename(source, ext)
+
+      // Note: H5 端的 dev 模式下，会存在 esbuild 预编译，会把 预编译的 chunk 文件放到 node_modules/.vite/deps 「cacheDir」 目录下，
+      // 当时 vite 的源码里面有个钩子，会对改目录下的 resolveId 进行拦截处理，vitejs/vite/packages/vite/src/node/plugins/optimizedDeps.ts，会直接返回传入的 id，所以不会返回 null
+      // 最全面的做法是，通过 config 钩子拿到 cacheDir，然后判断是否是 cacheDir 下的文件，如果是，则返回 null
+      // 目前先简单处理一下，如果是 node_modules/.vite/deps 目录下 先返回 null
+      const rawResolvedPath = path.resolve(path.dirname(importer), `${path.join(dir, basename)}${ext}`)
+      if (isViteDepsPath(rawResolvedPath)) return null
 
       let resolution = await this.resolve(
         path.resolve(path.dirname(importer), `${path.join(dir, basename)}.${process.env.TARO_ENV}${ext}`),
