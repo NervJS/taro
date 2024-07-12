d3e160f1eb88d14cc21569bf00378b7397cc2893
liuzejia <liuzejia@SZMAC-FV0MR4G7.local>
2024-07-12 14:08:00 +0800

    fix: 修复首次进入页面设置font-size的时，获取不到高度时的报错

--- packages/taro-vite-runner/src/utils/html.ts
+++ packages/taro-vite-runner/src/utils/html.ts
@@ -9,7 +9,7 @@ export function getHtmlScript (entryScript: string, pxtransformOption): string {
   const rootValue = (baseFontSize / options.deviceRatio[designWidth]) * 2
 
   if ((options?.targetUnit ?? 'rem') === 'rem') {
-    htmlScript = `<script>!function(n){function f(){var e=n.document.documentElement,r=e.getBoundingClientRect(),w=Math.min(r.width,r.height),x=${rootValue}*w/${designWidth};e.style.fontSize=x>=${max}?"${max}px":x<=${min}?"${min}px":x+"px"}n.addEventListener("resize",(function(){f()})),f()}(window);</script>\n`
+    htmlScript = `<script>!function(n){function f(){var e=n.document.documentElement,r=e.getBoundingClientRect(),width=r.width,height=r.height,arr=[width,height].filter(function(value){return Boolean(value)}),w=Math.min.apply(Math,arr),x=${rootValue}*w/${designWidth};e.style.fontSize=x>=${max}?"${max}px":x<=${min}?"${min}px":x+"px"}; n.addEventListener("resize",(function(){f()})),f()}(window);</script>\n`
   }
   htmlScript += `  <script type="module" src="${entryScript}"></script>`
   return htmlScript
