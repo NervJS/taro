a672a793c58c7a0c8fb1870b41c408d1a9754ead
ZakaryCode <zakarycode@gmail.com>
2024-07-18 15:22:06 +0800

    fix(harmony): 优化 contextPromise 设置

--- packages/taro-platform-harmony/src/runtime-ets/current.ts
+++ packages/taro-platform-harmony/src/runtime-ets/current.ts
@@ -3,15 +3,38 @@ export const context: any = {
   value: null
 }
 
+export const uiContext: any = {
+  resolver: null,
+  value: null
+}
+
 export const Current: any = {
   app: null,
-  uiContext: null,
+  get uiContext () {
+    return uiContext.value
+  },
+  set uiContext (e) {
+    uiContext.resolver(e)
+  },
   router: {},
   taro: {},
   contextPromise: new Promise(resolve => {
     context.resolver = resolve
 
     return resolve
+  }).then(e => {
+    context.value = e
+
+    return e
+  }),
+  uiContextPromise: new Promise(resolve => {
+    uiContext.resolver = resolve
+
+    return resolve
+  }).then((e) => {
+    uiContext.value = e
+
+    return e
   }),
   createHarmonyElement: null
 }
