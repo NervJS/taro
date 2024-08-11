df9bd5b75c454ff12ed8f1224b3deeca7fda790b
ZakaryCode <zakarycode@gmail.com>
2024-08-11 22:15:16 +0800

    chore(release): publish 4.0.0-beta.134 --tag=beta

--- packages/taro-platform-harmony/src/runtime-ets/dom/node.ts
+++ packages/taro-platform-harmony/src/runtime-ets/dom/node.ts
@@ -190,7 +190,7 @@ export class TaroNode extends TaroDataSourceElement {
     this.childNodes.push(child)
     this.notifyDataAdd(this.childNodes.length - 1)
 
-    if (this.nodeName === "TEXT") {
+    if (this.nodeName === 'TEXT') {
       // 修复beta2版本文字从undefined -> 有值时的 不更新问题
       this.updateComponent()
     }
