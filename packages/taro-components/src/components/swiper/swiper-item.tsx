c195aad7c9e0eed810edff9869befe8ca8098d85
liuzejia <liuzejia@jd.com>
2024-08-13 17:24:39 +0800

    feat: 调整components 一些参数输出

--- packages/taro-components/src/components/swiper/swiper-item.tsx
+++ packages/taro-components/src/components/swiper/swiper-item.tsx
@@ -1,4 +1,4 @@
-import { Component, ComponentInterface, Host, h, Element } from '@stencil/core'
+import { Component, ComponentInterface, Host, h, Element, Prop } from '@stencil/core'
 
 import { handleStencilNodes } from '../../utils'
 import classNames from 'classnames'
@@ -8,7 +8,8 @@ import classNames from 'classnames'
 })
 export class SwiperItem implements ComponentInterface {
   @Element() el: HTMLElement
-  
+  @Prop() itemId: string
+
   //Note: 由于 swiper.js 会通过子元素中的 class 来判断是否为 swiper-slide，所以这里需要在 connectedCallback 中添加 swiper-slide 类名
   connectedCallback() {
     this.el.className = classNames(this.el.className, 'swiper-slide')
@@ -20,7 +21,7 @@ export class SwiperItem implements ComponentInterface {
 
   render () {
     return (
-      <Host>
+      <Host item-id={this.itemId}>
         <slot></slot>
       </Host>
     )
