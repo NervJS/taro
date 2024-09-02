61b074fb4fab9e7ac6746a48a0db1ab5d857b04e
yushijie1 <yushijie1@jd.com>
2024-09-02 11:10:56 +0800

    perf(event): 增加clickview模版,减少冗余事件绑定

--- packages/taro-platform-swan/src/template.ts
+++ packages/taro-platform-swan/src/template.ts
@@ -62,6 +62,7 @@ export class Template extends RecursiveTemplate {
     delete result['pure-view']
     delete result['static-view']
     delete result['catch-view']
+    delete result['click-view']
 
     return result
   }
