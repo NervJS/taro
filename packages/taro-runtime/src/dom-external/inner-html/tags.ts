b1b412722b478a00d7de91ae6a81f45dacace28e
zhengyanan1 <2821084381@qq.com>
2024-11-13 14:27:13 +0800

    fix: 修复innerHTML生成小程序时的相关逻辑. 对于a标签，逻辑保持跟直接a标签基本一致

--- packages/taro-runtime/src/dom-external/inner-html/tags.ts
+++ packages/taro-runtime/src/dom-external/inner-html/tags.ts
@@ -1,4 +1,4 @@
-import { internalComponents } from '@tarojs/shared'
+import { internalComponents, isString } from '@tarojs/shared'
 
 export function makeMap (
   str: string,
@@ -17,6 +17,25 @@ export const specialMiniElements = {
   iframe: 'web-view'
 }
 
+interface SpecialMap {
+  mapName: (props: Record<string, string | boolean>) => string
+}
+
+const specialElements = new Map<string, SpecialMap>([
+  ['a', {
+    mapName (props) {
+      if (props.as && isString(props.as)) return props.as.toLowerCase()
+      return !props.href || isString(props.href) && (/^javascript/.test(props.href)) ? 'view' : 'navigator'
+    }
+  }],
+])
+
+export const getSpecialElementMapping = (tag: string, expectsLowerCase:boolean = true) => {
+  tag = expectsLowerCase ? tag.toLowerCase() : tag
+  return specialElements.get(tag)
+}
+
+
 const internalCompsList = Object.keys(internalComponents)
   .map(i => i.toLowerCase())
   .join(',')
@@ -25,7 +44,10 @@ const internalCompsList = Object.keys(internalComponents)
 export const isMiniElements = makeMap(internalCompsList, true)
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Inline_elements
-export const isInlineElements = makeMap('a,i,abbr,iframe,select,acronym,slot,small,span,bdi,kbd,strong,big,map,sub,sup,br,mark,mark,meter,template,canvas,textarea,cite,object,time,code,output,u,data,picture,tt,datalist,var,dfn,del,q,em,s,embed,samp,b', true)
+export const isInlineElements = makeMap('i,abbr,iframe,select,acronym,slot,small,span,bdi,kbd,strong,big,map,sub,sup,br,mark,mark,meter,template,canvas,textarea,cite,object,time,code,output,u,data,picture,tt,datalist,var,dfn,del,q,em,s,embed,samp,b', true)
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Block-level_elements
 export const isBlockElements = makeMap('address,fieldset,li,article,figcaption,main,aside,figure,nav,blockquote,footer,ol,details,form,p,dialog,h1,h2,h3,h4,h5,h6,pre,dd,header,section,div,hgroup,table,dl,hr,ul,dt', true)
+
+// specialElements
+export const isSpecialElements = makeMap('a', true)
