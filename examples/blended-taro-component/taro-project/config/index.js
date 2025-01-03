aae8e2e12b5054ef76cfaae8c29ae394821f773d
hardenzheng <18740459807@163.com>
2025-01-03 11:07:32 +0800

    fix: 调整设计宽度

--- examples/blended-taro-component/taro-project/config/index.js
+++ examples/blended-taro-component/taro-project/config/index.js
@@ -14,11 +14,12 @@ tryGetComponentPath()
 const config = {
   projectName: 'test',
   date: '2021-1-18',
-  designWidth: 750,
+  designWidth: 375,
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
-    828: 1.81 / 2
+    828: 1.81 / 2,
+    375: 2 / 1
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
