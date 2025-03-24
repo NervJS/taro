b1492096d4e43b6c10562eedd5065874cbdcb5aa
Single-Dancer <chenchiajun@gmail.com>
2025-03-24 10:59:13 +0800

    feat: 添加 babel 插件转换 List, ListItem 组件, 以支持非半编译情况下使用

--- packages/shared/src/template.ts
+++ packages/shared/src/template.ts
@@ -235,6 +235,11 @@ export class BaseTemplate {
           result[compName] = {
             name: newComp?.name,
           }
+        } else if (compName === 'list-builder') {
+          result[compName] = {
+            ...newComp,
+            list: 'i.cn',
+          }
         } else {
           result[compName] = newComp
         }
@@ -360,11 +365,16 @@ export class BaseTemplate {
     if (isFunction(this.modifyLoopBody)) {
       child = this.modifyLoopBody(child, comp.nodeName)
     }
-
+    const isListBuilder = comp.nodeName === 'list-builder'
+    const expr = isListBuilder
+      ? 'slot:item slot:index'
+      : `${Adapter.for}="{{i.${Shortcuts.Childnodes}}}" ${Adapter.key}="sid"`
     let children = this.voidElements.has(comp.nodeName)
       ? ''
       : `
-    ${indent(child, 6)}
+    <block ${expr}>
+      ${indent(child, 6)}
+    </block>
   `
 
     if (isFunction(this.modifyLoopContainer)) {
