193292defa32ed009fe470ce52920f185ad00ef0
zhengyanan18 <zhengyanan18@jd.com>
2025-03-28 11:16:26 +0800

    feat: cli安装后，如果内网自动安装效能插件

---
+++ packages/taro-cli/postinstall.js
@@ -0,0 +1,19 @@
+const { exec } = require('child_process')
+
+fetch('https://taro.jd.com/')
+  .then(() => {
+    // JD内网
+    console.log('*******JD内网:*******', process.cwd())
+    exec('taro global-config add-plugin  @jdtaro/plugin-report-performance-data@latest --registry http://registry.m.jd.com', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`执行错误: ${error}`)
+        return
+      }
+      console.log(`stdout:{\n ${stdout} \n}`)
+      console.log(`stderr:{\n ${stderr} \n}`)
+    })
+  })
+  .catch(() => {
+    // 公网
+    console.log('*******公网:*******')
+  })
