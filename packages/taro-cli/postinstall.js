d333215f89e4ff62880aad6c76455145814d1520
zhengyanan18 <zhengyanan18@jd.com>
2025-04-01 17:05:15 +0800

    fix: 更新效能插件包名

--- packages/taro-cli/postinstall.js
+++ packages/taro-cli/postinstall.js
@@ -5,7 +5,7 @@ axios.get('https://taro.jd.com/', { timeout: 5000 })
   .then(() => {
     // JD内网
     console.log('*******JD内网:*******', process.cwd())
-    exec('taro global-config add-plugin  @jdtaro/plugin-report-performance-data@latest --registry http://registry.m.jd.com', (error, stdout, stderr) => {
+    exec('taro global-config add-plugin  @jdtaro/plugin-build-report-performance@latest --registry http://registry.m.jd.com', (error, stdout, stderr) => {
       if (error) {
         console.error(`执行错误: ${error}`)
         return
