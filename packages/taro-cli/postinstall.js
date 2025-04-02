0f79b4309f18bcac00df34dc54b3bb560b456e3b
zhengyanan18 <zhengyanan18@jd.com>
2025-04-02 15:21:50 +0800

    fix: 全局命令默认加载所有全局插件

--- packages/taro-cli/postinstall.js
+++ packages/taro-cli/postinstall.js
@@ -4,17 +4,17 @@ const axios = require('axios')
 axios.get('https://taro.jd.com/', { timeout: 5000 })
   .then(() => {
     // JD内网
-    console.log('*******JD内网:*******', process.cwd())
+    // console.log('*******JD内网:*******')
     exec('taro global-config add-plugin  @jdtaro/plugin-build-report-performance@latest --registry http://registry.m.jd.com', (error, stdout, stderr) => {
       if (error) {
-        console.error(`执行错误: ${error}`)
+        console.error(`install performance plugin error: ${error}`)
         return
       }
-      console.log(`stdout:{\n ${stdout} \n}`)
-      console.log(`stderr:{\n ${stderr} \n}`)
+      console.log(`install performance plugin stdout: ${stdout}`)
+      console.log(`install performance plugin stderr: ${stderr}`)
     })
   })
   .catch(() => {
     // 公网
-    console.log('*******公网:*******')
+    // console.log('*******公网:*******')
   })
