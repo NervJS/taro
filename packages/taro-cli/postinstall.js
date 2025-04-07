5b4493bb595149e3aaf791d44014006e05652d20
--global <zhengyanan18@jd.com>
2025-04-07 21:17:37 +0800

    fix: 修正项目内taro的引用路径

--- packages/taro-cli/postinstall.js
+++ packages/taro-cli/postinstall.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 
 axios.get('https://taro.jd.com/', { timeout: 5000 })
   .then(() => {
-    exec('./node_modules/.bin/taro global-config add-plugin  @jdtaro/plugin-build-report-performance@latest --registry http://registry.m.jd.com', (error, _stdout, _stderr) => {
+    exec('./bin/taro global-config add-plugin  @jdtaro/plugin-build-report-performance@latest --registry http://registry.m.jd.com', (error, _stdout, _stderr) => {
       if (error) {
         console.error(`install performance plugin error: ${error}`)
       }
