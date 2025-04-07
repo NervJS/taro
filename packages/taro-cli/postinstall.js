c3066b8aaa7646e9540c620cf27fa20b19c6783d
--global <zhengyanan18@jd.com>
2025-04-07 21:17:37 +0800

    test: 添加测试代码

--- packages/taro-cli/postinstall.js
+++ packages/taro-cli/postinstall.js
@@ -3,7 +3,13 @@ const axios = require('axios')
 
 axios.get('https://taro.jd.com/', { timeout: 5000 })
   .then(() => {
-    exec('./node_modules/.bin/taro global-config add-plugin  @jdtaro/plugin-build-report-performance@latest --registry http://registry.m.jd.com', (error, _stdout, _stderr) => {
+    exec('pwd', (error, _stdout, _stderr) => {
+      console.log('pwd-error', error, '_stdout', _stdout, '_stderr', _stderr)
+    })
+    exec('ls', (error, _stdout, _stderr) => {
+      console.log('ls-error', error, '_stdout', _stdout, '_stderr', _stderr)
+    })
+    exec('./bin/taro global-config add-plugin  @jdtaro/plugin-build-report-performance@latest --registry http://registry.m.jd.com', (error, _stdout, _stderr) => {
       if (error) {
         console.error(`install performance plugin error: ${error}`)
       }
