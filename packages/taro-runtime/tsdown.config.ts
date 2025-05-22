cce59175e8baaaf639135761b3670db539fb2976
ian <yihengz@outlook.com>
2025-05-22 23:01:01 +0800

    chore: taro-runtime打包@tarojs/shared

--- packages/taro-runtime/tsdown.config.ts
+++ packages/taro-runtime/tsdown.config.ts
@@ -3,16 +3,16 @@ import { defineConfig } from 'tsdown'
 export default [
   defineConfig({
     entry: 'src/**/*.ts',
+    noExternal: ['@tarojs/shared'],
     treeshake: false,
     sourcemap: true,
-    platform: 'neutral'
   }),
   defineConfig({
     entry: {
       'runtime.esm': 'src/index.ts',
     },
+    noExternal: ['@tarojs/shared'],
     treeshake: false,
     sourcemap: true,
-    platform: 'neutral'
   })
 ]
