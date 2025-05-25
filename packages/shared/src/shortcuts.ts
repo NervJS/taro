bcf18553e04c2ea26d8737725e68de5a807b7db7
ian <yihengz@outlook.com>
2025-05-26 03:46:10 +0800

    chore: enum Shortcuts

--- packages/shared/src/shortcuts.ts
+++ packages/shared/src/shortcuts.ts
@@ -1,17 +1,32 @@
 // 字符串简写
 // 不要使用 const enum https://www.typescriptlang.org/docs/handbook/enums.html#const-enum-pitfalls
-export const Shortcuts = {
-  Container: 'container',
-  Childnodes: 'cn',
-  Text: 'v',
-  NodeType: 'nt',
-  NodeName: 'nn',
+export enum Shortcuts {
+  Container = 'container',
+  Childnodes = 'cn',
+  Text = 'v',
+  NodeType = 'nt',
+  NodeName = 'nn',
 
-  // Attributes
-  Sid: 'sid',
-  Style: 'st',
-  Class: 'cl',
-  Src: 'src'
-} as const
+  // Attrtibutes
+  Sid = 'sid',
+  Style = 'st',
+  Class = 'cl',
+  Src = 'src'
+}
 
-export type Shortcuts = typeof Shortcuts[keyof typeof Shortcuts]
+// 会导致 @taro/tests 运行失败，暂时不用
+// export const Shortcuts = {
+//   Container: 'container',
+//   Childnodes: 'cn',
+//   Text: 'v',
+//   NodeType: 'nt',
+//   NodeName: 'nn',
+
+//   // Attributes
+//   Sid: 'sid',
+//   Style: 'st',
+//   Class: 'cl',
+//   Src: 'src'
+// } as const
+
+// export type Shortcuts = typeof Shortcuts[keyof typeof Shortcuts]
