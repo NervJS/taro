24d2bc64932b2c9843c3c08b0f5cf5b4675e5909
ian <yihengz@outlook.com>
2025-06-09 06:55:08 +0800

    chore: keep shortcuts

--- packages/shared/src/shortcuts.ts
+++ packages/shared/src/shortcuts.ts
@@ -1,18 +1,14 @@
-import type { ValueOf } from './types'
-
 // 字符串简写
-export const Shortcuts = {
-  Container: 'container',
-  Childnodes: 'cn',
-  Text: 'v',
-  NodeType: 'nt',
-  NodeName: 'nn',
+export const enum Shortcuts {
+  Container = 'container',
+  Childnodes = 'cn',
+  Text = 'v',
+  NodeType = 'nt',
+  NodeName = 'nn',
 
-  // Attrtibutes
-  Sid: 'sid',
-  Style: 'st',
-  Class: 'cl',
-  Src: 'src'
-} as const
-
-export type Shortcuts = ValueOf<typeof Shortcuts>;
+  // Attributes
+  Sid = 'sid',
+  Style = 'st',
+  Class = 'cl',
+  Src = 'src'
+}
