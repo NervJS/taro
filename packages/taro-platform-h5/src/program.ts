abe5899d5ec70492e98a4fa7e3bd0f6d110fb193
ian <yihengz@outlook.com>
2025-06-20 03:36:32 +0800

    fix: resolve issue

--- packages/taro-platform-h5/src/program.ts
+++ packages/taro-platform-h5/src/program.ts
@@ -56,9 +56,9 @@ export default class H5 extends TaroPlatformWeb {
       }
       return require.resolve('./runtime/components')
     } else if (this.useDeprecatedAdapterComponent) {
-      return require.resolve(`@tarojs/components/lib/${this.aliasFramework}/component-lib/index.js`)
+      return require.resolve(`@tarojs/components/lib/${this.aliasFramework}/component-lib`)
     } else {
-      return require.resolve(`@tarojs/components/lib/${this.aliasFramework}/index.js`)
+      return require.resolve(`@tarojs/components/lib/${this.aliasFramework}`)
     }
   }
 
