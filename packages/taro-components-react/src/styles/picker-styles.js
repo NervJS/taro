2025dc92863061ecbeb76ca53c2220d1946872f3
zhuqingan.3 <zhuqingan.3@jd.com>
2025-07-14 16:20:58 +0800

    fix: 屏蔽picker-view

--- packages/taro-components-react/src/styles/picker-styles.js
+++ packages/taro-components-react/src/styles/picker-styles.js
@@ -1,5 +1,5 @@
 // 导入Picker和PickerView组件的样式
 import '../components/picker/style/index.scss'
-import '../components/picker-view/style/index.scss'
+// import '../components/picker-view/style/index.scss'
 
 export default {}
