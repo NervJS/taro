b7e5b783248b2f9762fc3664c28cfa14becc2948
chenzhiyong <chenzhiyong@didachuxing.com>
2025-10-21 18:59:32 +0800

    fix(loader): 缓存Page 和 App 源码的同时也对 sourcemap 进行缓存,并将其传递下去,避免 sourcemap 链断裂

--- packages/taro-loader/src/entry-cache.ts
+++ packages/taro-loader/src/entry-cache.ts
@@ -1,10 +1,12 @@
-export const entryCache = new Map<string, string>()
+export const entryCache = new Map<string, any>()
 
 export default function () {
+  const callback = this.async()
   const { name } = this.getOptions()
   if (name && entryCache.has(name)) {
     const content = entryCache.get(name)
-    entryCache.delete(name)
-    return content
+    // just in case, delete cache in next tick
+    setImmediate(() => entryCache.delete(name))
+    callback(null, content!.source, content!.map)
   }
 }
