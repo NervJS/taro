51ab1dce617b93c6c8cc78f670a93c242a979a1d
ian <yihengz@outlook.com>
2025-11-11 19:34:51 +0800

    test(babel-plugin-transform-solid-jsx): vitest

---
+++ packages/babel-plugin-transform-solid-jsx/vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+      include: ['index.js']
+    }
+  }
+})
