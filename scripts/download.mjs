b49f24243f363d8eeacf0189cde394343565c733
ian <yihengz@outlook.com>
2025-12-03 01:00:47 +0800

    chore: debug ci

--- scripts/download.mjs
+++ scripts/download.mjs
@@ -1,3 +1,9 @@
 import { downloadBrowser } from 'puppeteer/internal/node/install.js';
 
-downloadBrowser();
+Promise.race([
+  downloadBrowser(),
+  new Promise((resolve, reject) => {
+    setTimeout(() => { console.warn('下载超时'); resolve(0); }, 360000);
+  }),
+])
+  .finally(() => { process.exit(0); })
