9317b3d1f6adcd49b323d14f26b355a7ee775f0a
ian <yihengz@outlook.com>
2025-12-04 09:45:54 +0800

    test(taro-h5): vitest

--- packages/taro-h5/__mocks__/setEnv.ts
+++ packages/taro-h5/__mocks__/setEnv.ts
@@ -1,3 +1,5 @@
+import '@testing-library/jest-dom'
+
 process.env.NODE_ENV = 'production'
 process.env.TARO_ENV = 'h5'
 process.env.TARO_PLATFORM = 'web'
--- packages/taro-h5/__tests__/ui/interaction/modal.test.ts
+++ packages/taro-h5/__tests__/ui/interaction/modal.test.ts
@@ -1,5 +1,3 @@
-import '@testing-library/jest-dom/extend-expect'
-
 import * as Taro from '@tarojs/taro-h5'
 import { vi } from 'vitest'
 
