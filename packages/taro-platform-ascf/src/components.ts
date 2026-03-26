63ab20ab7147b43fdca7725db0534a3b39fd5081
2extliuweijian32 <ext.liuweijian3@jd.com>
2026-03-26 15:09:17 +0800

    fix(ascf): 修复open-type类型声明&增加事件回调 (#19027)
    * fix(ascf): 修复open-type类型声明&增加事件回调
    
    * fix(ascf): 增加getUserRiskLevel

--- packages/taro-platform-ascf/src/components.ts
+++ packages/taro-platform-ascf/src/components.ts
@@ -70,7 +70,8 @@ export const components = {
     bindError: _empty,
     bindOpenSetting: _empty,
     bindLaunchApp: _empty,
-    bindAgreePrivacyAuthorization: _empty
+    bindAgreePrivacyAuthorization: _empty,
+    bindGetPhoneNumberAndRiskLevel: _empty,
   },
   Form: {
     'report-submit-timeout': _zero
