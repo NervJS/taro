6f6b08dabeae5af52d9d1a1b378a3d9d56b3d5c4
Jarvis <mengda636431@gmail.com>
2026-05-25 21:03:11 +0800

    fix(types): adjust setStorage callback result

--- packages/taro/types/api/storage/index.d.ts
+++ packages/taro/types/api/storage/index.d.ts
@@ -20,14 +20,16 @@ declare module '../../index' {
       /** 本地缓存中指定的 key */
       key: string
       /** 接口调用结束的回调函数（调用成功、失败都会执行） */
-      complete?: (res: TaroGeneral.CallbackResult) => void
+      complete?: (res: SuccessCallbackResult) => void
       /** 接口调用失败的回调函数 */
       fail?: (res: TaroGeneral.CallbackResult) => void
       /** 接口调用成功的回调函数 */
       success?: (res: SuccessCallbackResult) => void
     }
 
-    interface SuccessCallbackResult extends TaroGeneral.CallbackResult {
+    interface SuccessCallbackResult extends Omit<TaroGeneral.CallbackResult, 'errMsg'> {
+      /** 调用结果 */
+      errMsg?: string
       /** 是否执行成功
        * @supported alipay
        */
