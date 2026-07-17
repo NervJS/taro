09a44eab993e8d34482fda134313b5db9fcb636f
Il Harper <hi@ilharper.com>
2026-07-17 19:04:33 +0800

    fix(runner): recalc `fontSize` 0.5s after window resize, fix #18653 (#19218)
    部分移动端浏览器（如微信 WebView）加载或刷新时有概率将 window resize 为一个 height 很小的大小，之后又恢复。经过测试在 window resize event 0.5s 之后再次尝试计算 fontSize 可解决。
    
    影响范围：v3.7.0-alpha.27 以来的所有版本
    
    详情看 #18653 中评论

--- packages/taro-vite-runner/src/utils/html.ts
+++ packages/taro-vite-runner/src/utils/html.ts
@@ -11,7 +11,7 @@ export function getHtmlScript (entryScript: string, pxtransformOption): string {
   const rootValue = (baseFontSize / options.deviceRatio[designWidth]) * 2
 
   if ((options?.targetUnit ?? 'rem') === 'rem') {
-    htmlScript = `<script>!function(n){function f(){var e=n.document.documentElement,r=e.getBoundingClientRect(),width=r.width,height=r.height,arr=[width,height].filter(function(value){return Boolean(value)}),w=Math.min.apply(Math,arr),x=${rootValue}*w/${designWidth};e.style.fontSize=x>=${max}?"${max}px":x<=${min}?"${min}px":x+"px"}; n.addEventListener("resize",(function(){f()})),f()}(window);</script>\n`
+    htmlScript = `<script>!function(n){function f(){var e=n.document.documentElement,r=e.getBoundingClientRect(),width=r.width,height=r.height,arr=[width,height].filter(function(value){return Boolean(value)}),w=Math.min.apply(Math,arr),x=${rootValue}*w/${designWidth};e.style.fontSize=x>=${max}?"${max}px":x<=${min}?"${min}px":x+"px"}; n.addEventListener("resize",(function(){f();setTimeout(f,500)})),f()}(window);</script>\n`
   }
   htmlScript += `  <script type="module" src="${normalizePath(entryScript)}"></script>`
   return htmlScript
