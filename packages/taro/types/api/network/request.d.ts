f90e76f9f6368c890b4eb2f80be16f8a743dc288
dvd233 <111864431+dvd233@users.noreply.github.com>
2026-09-21 18:27:56 +0800

    fix(types): resolve request declaration errors (#19495)

--- packages/taro/types/api/network/request.d.ts
+++ packages/taro/types/api/network/request.d.ts
@@ -2,7 +2,7 @@ import Taro from '../../index'
 
 declare module '../../index' {
   namespace request {
-    interface Option<T = any, U extends string | TaroGeneral.IAnyObject | ArrayBuffer = any | any> {
+    interface Option<T = any, U = any> {
       /** 开发者服务器接口地址 */
       url: string
       /** 请求的参数 */
@@ -150,8 +150,7 @@ declare module '../../index' {
       storeCheck?(): boolean
     }
 
-    interface SuccessCallbackResult<T extends string | TaroGeneral.IAnyObject | ArrayBuffer = any | any>
-      extends TaroGeneral.CallbackResult {
+    interface SuccessCallbackResult<T = any> extends TaroGeneral.CallbackResult {
       /** 开发者服务器返回的数据 */
       data: T
       /** 开发者服务器返回的 HTTP Response Header */
@@ -377,6 +376,11 @@ declare module '../../index' {
         data: ArrayBuffer
       }
     }
+  }
+
+  /** @ignore */
+  interface RequestParams<T = any> extends request.Option<T, any> {
+    [propName: string]: any
   }
 
   /** @ignore */
